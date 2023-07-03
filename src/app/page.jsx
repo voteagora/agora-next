0a9695815dd5ec3a97ac415c8ee9ac4bea065baa
Kent Fenwick <kent.fenwick@gmail.com>
2023-07-02 21:28:09 -0400

    merged in global css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import {
   EnvelopeOpenIcon,
   UsersIcon,
 } from "@heroicons/react/24/outline";
+import { Button } from "@/components/ui/button"
 
 const stats = [
   {
@@ -38,31 +39,36 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div>
-      <h1 className="pageTitle">Stats</h1>
+    <>
+      <div>
+        <h1 className="pageTitle">Stats</h1>
 
-      <dl className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
-        {stats.map((item) => (
-          <div
-            key={item.id}
-            className="relative overflow-hidden rounded-lg bg-white p-4 shadow"
-          >
-            <dt>
-              <div className="absolute rounded-md bg-gray-100 p-3">
-                <item.icon className="h-6 w-6" aria-hidden="true" />
-              </div>
-              <p className="ml-16 truncate text-sm font-medium text-gray-500">
-                {item.name}
-              </p>
-            </dt>
-            <dd className="ml-16 flex items-baseline">
-              <p className="text-md font-semibold text-gray-900">
-                {item.subHeading}
-              </p>
-            </dd>
-          </div>
-        ))}
-      </dl>
-    </div>
+        <dl className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+          {stats.map((item) => (
+            <div
+              key={item.id}
+              className="relative overflow-hidden rounded-lg bg-white p-4 shadow"
+            >
+              <dt>
+                <div className="absolute rounded-md bg-gray-100 p-3">
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                </div>
+                <p className="ml-16 truncate text-sm font-medium text-gray-500">
+                  {item.name}
+                </p>
+              </dt>
+              <dd className="ml-16 flex items-baseline">
+                <p className="text-md font-semibold text-gray-900">
+                  {item.subHeading}
+                </p>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+      <div>
+        <Button>Button</Button>
+      </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./components/**/*.{ts,tsx,js,jsx}",
     "./app/**/*.{ts,tsx,js,jsx}",
     "./src/**/*.{ts,tsx,js,jsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     container: {
