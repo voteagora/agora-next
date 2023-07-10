49b0ba00caadd2d1d04b9ecea1ec9adb12f89f3c
Kent Fenwick <kent.fenwick@gmail.com>
2023-07-10 13:48:26 -0400

    experimenting with /app/api routes

--- src/app/api/proposals/route.js
+++ src/app/api/proposals/route.js
@@ -1,16 +1,38 @@
-// app/api/proposals/route.js
-
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
-import postgres from "postgres";
-
-const conn = postgres();
-
-async function getAllProposals() {
-  return conn.query("SELECT * FROM goldsky_daos.proposals");
-}
 export async function GET(request) {
-  // Do whatever you want
-  const proposals = await getAllProposals();
-  return NextResponse.json({ proposals });
+  const prisma = new PrismaClient();
+
+  let page = parseInt(request.nextUrl.searchParams.get("page"), 10);
+  if (isNaN(page) || page < 1) {
+    page = 1;
+  }
+
+  const pageSize = 25;
+  const total_count = await prisma.proposals.count();
+  const total_pages = Math.ceil(total_count / pageSize);
+
+  const proposals = await prisma.proposals.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+
+  await prisma.$disconnect();
+
+  // Construct the response
+  const response = {
+    meta: {
+      current_page: page,
+      total_pages: total_pages,
+      page_size: pageSize,
+      total_count: total_count,
+    },
+    proposals: proposals.map((proposal) => ({
+      id: proposal.id,
+      uuid: proposal.uuid,
+    })),
+  };
+
+  return NextResponse.json(response);
 }
