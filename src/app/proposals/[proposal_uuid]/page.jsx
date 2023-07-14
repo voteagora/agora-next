d69c5b1b080da229b7afa1d0a74d48f83bc4fd1e
Kent Fenwick <kent.fenwick@gmail.com>
2023-07-13 23:00:47 -0400

    added markdown

--- src/app/proposals/[proposal_uuid]/page.jsx
+++ src/app/proposals/[proposal_uuid]/page.jsx
@@ -26,7 +26,7 @@ export default async function Page({ params: { proposal_uuid } }) {
     <section className={styles.proposal_show}>
       <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          <div className="-mx-4 px-4 py-8 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-16">
+          <div className="-mx-4 px-2 py-8 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-4">
             <div className="agora_markdown">
               {formattedDescription.split("\n").map((line, index) => {
                 if (index === 0) {
