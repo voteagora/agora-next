ece54302e7095d2a6c624db5affc8f7a25d21636
Kent Fenwick <kent.fenwick@gmail.com>
2023-07-20 14:05:52 -0400

    updated prisma

--- src/app/lib/prisma.js
+++ src/app/lib/prisma.js
@@ -1,3 +1,6 @@
+// Define and export a new Prisma client instance
+// every time the function is called in development
+// or use the global instance in production
 import { PrismaClient } from "@prisma/client";
 
 if (process.env.NODE_ENV === "production") {
