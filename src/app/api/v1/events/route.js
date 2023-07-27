24d2c7b53196cfe3e246199fb54e51db45223f13
Kent Fenwick <kent.fenwick@gmail.com>
2023-07-26 17:00:43 -0700

    fixed the prisma export import

--- src/app/api/v1/events/route.js
+++ src/app/api/v1/events/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/app/lib/prisma";
 import { authenticateAgoraApiUser } from "src/app/lib/middlewear/authenticateAgoraApiUser";
 
 export async function GET(request) {
@@ -8,8 +8,6 @@ export async function GET(request) {
   if (authResponse) {
     return authResponse;
   }
-
-  const prisma = new PrismaClient();
 
   let page = parseInt(request.nextUrl.searchParams.get("page"), 10);
   if (isNaN(page) || page < 1) {
@@ -25,7 +23,6 @@ export async function GET(request) {
     skip: (page - 1) * pageSize,
   });
 
-  await prisma.$disconnect();
 
   // Build out proposal response
   const response = {
