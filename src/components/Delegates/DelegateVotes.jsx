8ade3d50b475e0b083c46812682fdd6f14d25da1
Kent Fenwick <kent.fenwick@gmail.com>
2023-08-01 19:19:15 -0400

    updated delegate votes

--- src/components/Delegates/DelegateVotes.jsx
+++ src/components/Delegates/DelegateVotes.jsx
@@ -7,6 +7,10 @@ import HumanVote from "../shared/HumanVote";
 import HumanAddress from "../shared/HumanAddress";
 import Image from "next/image";
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function DelegateVotes({ initialVotes, fetchDelegateVotes }) {
   const router = useRouter();
   const fetching = React.useRef(false);
@@ -59,46 +63,54 @@ export default function DelegateVotes({ initialVotes, fetchDelegateVotes }) {
               }
               element="main"
             >
-              {delegateVotes.map((vote) => (
-                <li key={vote.address} className="relative flex gap-x-4 w-full">
-                  <div
-                    className={classNames(
-                      voteIdx === votes.length - 1 ? "h-6" : "-bottom-6",
-                      "absolute left-0 top-0 flex w-6 justify-center"
-                    )}
-                  >
-                    <div className="w-px bg-gray-200" />
-                  </div>
-
-                  <div className="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
-                    <div className="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-                  </div>
-                  <div
-                    className={classNames(
-                      "flex-auto rounded-md p-3 ring-1 ring-inset",
-                      vote.support === 1 ? "ring-green-200" : "ring-red-200"
-                    )}
-                  >
-                    <div className="flex justify-between gap-x-4">
-                      <div className="py-0.5 text-xs leading-5 text-gray-500">
-                        <span className="font-medium text-gray-900">
-                          <HumanAddress address={vote.address} />
-                        </span>{" "}
-                        voted <HumanVote support={vote.support} />
-                      </div>
-                      <time
-                        dateTime="2023-01-24T09:20"
-                        className="flex-none py-0.5 text-xs leading-5 text-gray-500"
+              {delegateVotes.map(
+                (vote, voteIdx) =>
+                  vote && (
+                    <li
+                      key={vote.address}
+                      className="relative flex gap-x-4 w-full"
+                    >
+                      <div
+                        className={classNames(
+                          voteIdx === delegateVotes.length - 1
+                            ? "h-6"
+                            : "-bottom-6",
+                          "absolute left-0 top-0 flex w-6 justify-center"
+                        )}
                       >
-                        1d ago
-                      </time>
-                    </div>
-                    <p className="text-sm leading-6 text-gray-500">
-                      {vote.reason}
-                    </p>
-                  </div>
-                </li>
-              ))}
+                        <div className="w-px bg-gray-200" />
+                      </div>
+
+                      <div className="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
+                        <div className="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
+                      </div>
+                      <div
+                        className={classNames(
+                          "flex-auto rounded-md p-3 ring-1 ring-inset",
+                          vote.support === 1 ? "ring-green-200" : "ring-red-200"
+                        )}
+                      >
+                        <div className="flex justify-between gap-x-4">
+                          <div className="py-0.5 text-xs leading-5 text-gray-500">
+                            <span className="font-medium text-gray-900">
+                              <HumanAddress address={vote.address} />
+                            </span>{" "}
+                            voted <HumanVote support={vote.support} />
+                          </div>
+                          <time
+                            dateTime="2023-01-24T09:20"
+                            className="flex-none py-0.5 text-xs leading-5 text-gray-500"
+                          >
+                            1d ago
+                          </time>
+                        </div>
+                        <p className="text-sm leading-6 text-gray-500">
+                          {vote.reason}
+                        </p>
+                      </div>
+                    </li>
+                  )
+              )}
             </InfiniteScroll>
           </ul>
         </div>
