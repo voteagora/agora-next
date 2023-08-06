4617e45a231d677c4f48d8f16b48e1eb7388f626
Kent Fenwick <kent.fenwick@gmail.com>
2023-08-06 14:22:53 -0400

    fixed the delegate statements return

--- src/app/api/v1/statements/route.js
+++ src/app/api/v1/statements/route.js
@@ -9,25 +9,49 @@ export async function GET(request) {
     return authResponse;
   }
 
-  const statement = await prisma.delegate_statements.findOne({
-    where: {
-      id: 1,
-    }
+  const statements = await prisma.delegate_statements.findMany({
+    where: { token: process.env.NEXT_PUBLIC_AGORA_INSTANCE_TOKEN },
+    include: {
+      delegate_bios: true,
+    },
   });
 
   // Build out proposal response
   const response = {
-    statement: events.map((event) => ({
-      // Just testing out, not meant for production
-      id: event.id,
-      kind: event.kind,
-      event_data: event.event_data,
+    statement: statements.map((statement) => ({
+      id: statement.id,
+      address: statement.address,
+      statement: statement.statement,
+      token: statement.token,
+      created_at: statement.created_at,
+      updated_at: statement.updated_at,
+      delegate_bio: {
+        id: statement.delegate_bios.id,
+        delegate_statement_id: statement.delegate_bios.delegate_statement_id,
+        address: statement.delegate_bios.address,
+        token: statement.delegate_bios.token,
+        twitter_handle: statement.delegate_bios.twitter_handle,
+        discord_handle: statement.delegate_bios.discord_handle,
+        farcaster_handle: statement.delegate_bios.farcaster_handle,
+        telegram_handle: statement.delegate_bios.telegram_handle,
+        email: statement.delegate_bios.email,
+        website: statement.delegate_bios.website,
+        github_handle: statement.delegate_bios.github_handle,
+        email_verified: statement.delegate_bios.email_verified,
+        open_to_delegation: statement.delegate_bios.open_to_delegation,
+        open_to_proposals: statement.delegate_bios.open_to_proposals,
+        open_to_questions: statement.delegate_bios.open_to_questions,
+        agreed_to_code_of_conduct:
+          statement.delegate_bios.agreed_to_code_of_conduct,
+        created_at: statement.delegate_bios.created_at,
+        updated_at: statement.delegate_bios.updated_at,
+      },
     })),
   };
 
   return NextResponse.json(response);
 }
-    
+
 export async function POST(request) {
   // Check if the session is authenticated first
   const authResponse = authenticateAgoraApiUser(request);
@@ -36,10 +60,24 @@ export async function POST(request) {
   }
   const prisma = new PrismaClient();
 
-
   const data = await request.json();
 
-  const { title, content, twitter_handle, discord_handle, farcaster_handle, telegram_handle, email, website, github_handle, email_verified, open_to_delegation, open_to_proposals, open_to_questions, agreed_to_code_of_conduct } = data
+  const {
+    title,
+    content,
+    twitter_handle,
+    discord_handle,
+    farcaster_handle,
+    telegram_handle,
+    email,
+    website,
+    github_handle,
+    email_verified,
+    open_to_delegation,
+    open_to_proposals,
+    open_to_questions,
+    agreed_to_code_of_conduct,
+  } = data;
 
   // Attempt to create the new statement in the database
   try {
@@ -59,7 +97,7 @@ export async function POST(request) {
         open_to_proposals: open_to_proposals,
         open_to_questions: open_to_questions,
         agreed_to_code_of_conduct: agreed_to_code_of_conduct,
-        token: "NOUN",
+        token: process.env.NEXT_PUBLIC_AGORA_INSTANCE_TOKEN,
         created_at: new Date(),
         updated_at: new Date(),
       },
