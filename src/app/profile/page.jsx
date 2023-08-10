7af941a1c4ccde608e56eea05791923026032018
Kent Fenwick <kent.fenwick@gmail.com>
2023-08-10 00:23:25 -0400

    updating issue on the wagmi side

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,31 +1,35 @@
-"use client";
-
-import DelegateStatementForm from "@/components/DelegateStatements/DelegateStatementForm";
+"use client"
+import AgoraAPI from "../lib/agoraAPI";
 import { useAccount } from "wagmi";
+import Link from "next/link";
 
-async function fetchDelegateStaement(address) {
-  "use server";
-
+async function fetchDelegateStatement(address) {
   const api = new AgoraAPI();
-  const data = await api.get(`/statement?address=${address}`);
-  return { proposals: data.proposals, meta: data.meta };
+  const data = await api.get(`/statement/${address}`);
+  console.log(data);
+  return { data };
 }
 
 export default async function Page() {
   const { address } = useAccount();
   const isAuthenticated = Boolean(address);
+  console.log(address);
+  // const delegateStatement = await fetchDelegateStatement(address);
 
   return (
     <>
       <section>
         <h1>Profile</h1>
       </section>
-      {isAuthenticated ? (
-        <DelegateStatementForm address={address} />
+      {isAuthenticated && delegateStatement ? (
+        <>
+          <p>Delegate statement goes here</p>
+          <Link href="/profile/edit">Edit your delegate statement</Link>
+        </>
       ) : (
         <div>
-          <h1>Your profile goes here!</h1>
-          
+          <h1>Create a delegate statement</h1>
+          <Link href="/profile/create">Create a delegate statement</Link>
         </div>
       )}
     </>
