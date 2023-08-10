b0a4b587f1c1c4377412d649064455149b742434
Kent Fenwick <kent.fenwick@gmail.com>
2023-08-10 00:35:00 -0400

    updated the profile react with use hook

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,34 +1,37 @@
-"use client"
+"use client";
+import { useState, useEffect } from "react";
 import AgoraAPI from "../lib/agoraAPI";
 import { useAccount } from "wagmi";
 import Link from "next/link";
 
-async function fetchDelegateStatement(address) {
-  const api = new AgoraAPI();
-  const data = await api.get(`/statement/${address}`);
-  console.log(data);
-  return { data };
-}
-
 export default async function Page() {
   const { address } = useAccount();
+  const [delegateStatement, setDelegateStatement] = useState(null);
   const isAuthenticated = Boolean(address);
-  console.log(address);
-  // const delegateStatement = await fetchDelegateStatement(address);
+
+  useEffect(() => {
+    async function fetchDelegateStatement() {
+      if (address) {
+        const api = new AgoraAPI();
+        const data = await api.get(`/statement/${address}`);
+        setDelegateStatement(data);
+      }
+    }
+    fetchDelegateStatement();
+  }, [address]);
 
   return (
     <>
       <section>
         <h1>Profile</h1>
       </section>
-      {isAuthenticated && delegateStatement ? (
+      {isAuthenticated && delegateStatement != {} ? (
         <>
           <p>Delegate statement goes here</p>
           <Link href="/profile/edit">Edit your delegate statement</Link>
         </>
       ) : (
         <div>
-          <h1>Create a delegate statement</h1>
           <Link href="/profile/create">Create a delegate statement</Link>
         </div>
       )}
