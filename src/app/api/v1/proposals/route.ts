a16bf70aec468d6d592c3165b230b6f393cb368c
stepandel <stepandel@gmail.com>
2023-11-14 14:25:15 -0500

    fix imports

--- src/app/api/v1/proposals/route.ts
+++ src/app/api/v1/proposals/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { getHumanBlockTime } from "@/lib/blockTimes";
-import { getQuorumForProposal, parseProposalType } from "@/lib/proposalUtils";
+import { parseProposalType } from "@/lib/proposalUtils";
 import provider from "@/app/lib/provider";
 
 BigInt.prototype.toJSON = function () {
