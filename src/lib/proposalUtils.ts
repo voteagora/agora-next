ca08562530a7e8ee33c024776a7579773e9f4db9
stepandel <stepandel@gmail.com>
2023-11-14 14:24:48 -0500

    move getQuorumForProposal to gov utils

--- src/lib/proposalUtils.ts
+++ src/lib/proposalUtils.ts
@@ -1,31 +1,3 @@
-import { Prisma } from "@prisma/client";
-import { NounsContracts, OptimismContracts } from "./contracts/contracts";
-
-/**
- *
- *
- *
- **/
-export async function getQuorumForProposal(
-  proposal: Prisma.ProposalsListGetPayload<true> | null,
-  dao: "OPTIMISM" | "NOUNS"
-) {
-  switch (dao) {
-    case "NOUNS": {
-      if (!proposal) {
-        return null;
-      }
-      return NounsContracts.governor.quorumVotes(proposal.proposal_id);
-    }
-    case "OPTIMISM": {
-      if (!proposal?.start_block) {
-        return null;
-      }
-      return OptimismContracts.governor.quorum(proposal.start_block);
-    }
-  }
-}
-
 export function parseProposalType(
   proposalData: string
 ): "STANDARD" | "APPROVAL" {
