dfabcbd965963a0a0a10a5cdc8c953833ada143d
stepandel <stepandel@gmail.com>
2023-11-15 15:05:49 -0500

    use wagmi for ens name

--- src/components/shared/ENSName.jsx
+++ src/components/shared/ENSName.jsx
@@ -1,5 +1,5 @@
-import { ethers } from "ethers";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useEnsName } from "wagmi";
 
 function truncateAddress(address) {
   return `${address.substring(0, 4)}...${address.substring(
@@ -9,35 +9,13 @@ function truncateAddress(address) {
 
 // This component will display the ENS name for a given address
 const ENSName = ({ address }) => {
-  const addy = address.split("-")[0];
-  const truncatedAddress = truncateAddress(addy);
-  const [ensName, setEnsName] = useState(truncatedAddress); // Initialize with the address as the ENS name
-  const [attemptedLookup, setAttemptedLookup] = useState(false);
+  const [ensName, setEnsName] = useState(truncateAddress(address));
+
+  const { data } = useEnsName({ chainId: 1, address });
 
   useEffect(() => {
-    const getEnsName = async () => {
-      // Only attempt lookup if it hasn't been attempted before
-      if (!attemptedLookup) {
-        const provider = new ethers.AlchemyProvider(
-          "mainnet",
-          process.env.NEXT_PUBLIC_ALCHEMY_ID
-        );
-
-        const ensName = await provider.lookupAddress(addy);
-        if (ensName) {
-          setEnsName(ensName);
-          setAttemptedLookup(true);
-          return;
-        } else {
-          setEnsName(truncatedAddress);
-          setAttemptedLookup(true);
-          return;
-        }
-      }
-    };
-
-    getEnsName();
-  }, [addy, truncatedAddress, attemptedLookup]);
+    setEnsName(data);
+  }, [data]);
 
   return <span>{ensName}</span>;
 };
