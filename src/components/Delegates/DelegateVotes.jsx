8105dcb742063b01426a6918f2946922af27cdd5
stepandel <stepandel@gmail.com>
2023-11-15 18:59:09 -0500

    add proposal details to the delgate votes

--- src/components/Delegates/DelegateVotes.jsx
+++ src/components/Delegates/DelegateVotes.jsx
@@ -1,14 +1,33 @@
 "use client";
 
 import * as React from "react";
-import { VStack } from "../Layout/Stack";
+import { HStack, VStack } from "../Layout/Stack";
 import { css } from "@emotion/css";
 import * as theme from "@/lib/theme";
-import { colorForSupportType } from "@/lib/proposalUtils";
+import {
+  colorForSupportType,
+  getTitleFromProposalDescription,
+} from "@/lib/proposalUtils";
 import { pluralizeVote } from "@/lib/tokenUtils";
+import { shortAddress } from "@/lib/utils";
+import { formatDistanceToNow } from "date-fns";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
+}
+
+function shortPropTitle(title, proosalId) {
+  // This is a hack to hide a proposal formatting mistake from the OP Foundation
+  const proposalsWithBadFormatting = [
+    "114732572201709734114347859370226754519763657304898989580338326275038680037913",
+    "27878184270712708211495755831534918916136653803154031118511283847257927730426",
+    "90839767999322802375479087567202389126141447078032129455920633707568400402209",
+  ];
+
+  // This is a hack to hide a proposal formatting mistake from the OP Foundation
+  return proposalsWithBadFormatting.includes(proosalId)
+    ? title.split("-")[0].split("(")[0]
+    : title;
 }
 
 export default function DelegateVotes({ initialVotes, fetchDelegateVotes }) {
@@ -57,12 +76,59 @@ export default function DelegateVotes({ initialVotes, fetchDelegateVotes }) {
                   }
                 `}
               >
-                {vote.proposalType === "APPROVAL" && (
-                  <ApprovalVoteContainer {...vote} />
-                )}
-                {vote.proposalType === "STANDARD" && (
-                  <StandardVoteContainer {...vote} />
-                )}
+                <VStack
+                  className={css`
+                    padding: ${theme.spacing["4"]} ${theme.spacing["6"]};
+                  `}
+                >
+                  <HStack
+                    gap="1"
+                    className={css`
+                      font-size: ${theme.fontSize.xs};
+                      font-weight: ${theme.fontWeight.medium};
+                      color: #66676b;
+                    `}
+                  >
+                    <a
+                      href={`/proposals/vote.proposal_id`}
+                      title={`Prop ${vote.proposal_id}`}
+                    >
+                      Prop {shortAddress(vote.proposal_id)}
+                    </a>
+                    {/* <>
+                      {!amount.isZero() ? (
+                        <> asking {utils.formatEther(amount)} ETH</>
+                      ) : null}{" "}
+                    </> */}
+                    {vote.timestamp && (
+                      <div>
+                        - {formatDistanceToNow(new Date(vote.timestamp))} ago
+                      </div>
+                    )}
+                  </HStack>
+                  <h2
+                    className={css`
+                      font-size: ${theme.fontSize.base};
+                      padding: ${theme.spacing[1]} 0;
+                      overflow: hidden;
+                      text-overflow: ellipsis;
+                    `}
+                  >
+                    <a href={`/proposals/vote.proposal_id`}>
+                      {shortPropTitle(
+                        getTitleFromProposalDescription(
+                          vote.proposalDescription
+                        )
+                      )}
+                    </a>
+                  </h2>
+                  {vote.proposalType === "APPROVAL" && (
+                    <ApprovalVoteContainer {...vote} />
+                  )}
+                  {vote.proposalType === "STANDARD" && (
+                    <StandardVoteContainer {...vote} />
+                  )}
+                </VStack>
                 {vote.reason && <VoteReason reason={vote.reason} />}
               </div>
             </VoteDetailsContainer>
