f56a0909ec021a68b43ed86a34fab39f73b53a28
stepandel <stepandel@gmail.com>
2023-11-16 10:04:01 -0500

    remove duplicate address from delegate page

--- src/app/delegates/[addressOrENSName]/page.jsx
+++ src/app/delegates/[addressOrENSName]/page.jsx
@@ -33,7 +33,6 @@ export default async function Page({ params: { addressOrENSName } }) {
       <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <div className="-mx-4 px-4 py-8 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-16">
-            <h1>{addressOrENSName}</h1>
             <DelegateCard delegate={delegate} />
             <DelegateVotes
               initialVotes={delegateVotes}
