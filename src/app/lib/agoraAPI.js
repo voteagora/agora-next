1c83de5fbf7d42a275e40aae7430e4ff12d7d7de
stepandel <stepandel@gmail.com>
2023-11-17 11:38:59 -0500

    remove hardcoded baseUrl

--- src/app/lib/agoraAPI.js
+++ src/app/lib/agoraAPI.js
@@ -10,8 +10,7 @@ class AgoraAPI {
   constructor() {
     this.apiKey = process.env.NEXT_PUBLIC_AGORA_API_KEY;
     this.instanceToken = process.env.NEXT_PUBLIC_AGORA_INSTANCE_TOKEN;
-    this.baseURL =
-      process.env.NEXT_PUBLIC_AGORA_BASE_URL || "http://localhost:3000/api/v1";
+    this.baseURL = process.env.NEXT_PUBLIC_AGORA_BASE_URL;
   }
 
   /**
