5df3b2cd563e903d5ef3628adc7900ad850c0261
stepandel <stepandel@gmail.com>
2023-11-17 08:50:57 -0500

    restructure contracts config

--- src/lib/contracts/contracts.ts
+++ src/lib/contracts/contracts.ts
@@ -6,20 +6,35 @@ import {
 import provider from "@/app/lib/provider";
 
 export const OptimismContracts = {
-  governor: OptimismGovernor__factory.connect(
-    "0xcDF27F107725988f2261Ce2256bDfCdE8B382B10",
-    provider
-  ),
+  governor: {
+    contract: OptimismGovernor__factory.connect(
+      "0xcDF27F107725988f2261Ce2256bDfCdE8B382B10",
+      provider
+    ),
+    address: "0xcDF27F107725988f2261Ce2256bDfCdE8B382B10",
+    chainId: 10,
+    abi: OptimismGovernor__factory.abi,
+  },
 
-  token: OptimismToken__factory.connect(
-    "0x4200000000000000000000000000000000000042",
-    provider
-  ),
+  token: {
+    contract: OptimismToken__factory.connect(
+      "0x4200000000000000000000000000000000000042",
+      provider
+    ),
+    address: "0x4200000000000000000000000000000000000042",
+    chainId: 10,
+    abi: OptimismToken__factory.abi,
+  },
 };
 
 export const NounsContracts = {
-  governor: NounsGovernor__factory.connect(
-    "0x6f3e6272a167e8accb32072d08e0957f9c79223d",
-    provider
-  ),
+  governor: {
+    contract: NounsGovernor__factory.connect(
+      "0x6f3e6272a167e8accb32072d08e0957f9c79223d",
+      provider
+    ),
+    address: "0x6f3e6272a167e8accb32072d08e0957f9c79223d",
+    chainId: 1,
+    abi: NounsGovernor__factory.abi,
+  },
 };
