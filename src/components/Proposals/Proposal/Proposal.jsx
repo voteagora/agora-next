0ecfdb661e26475fdc8910c0b339c834689566b6
Kent Fenwick <kent.fenwick@gmail.com>
2023-11-19 17:41:47 -0500

    updated proposal view to remove hydration errors

--- src/components/Proposals/Proposal/Proposal.jsx
+++ src/components/Proposals/Proposal/Proposal.jsx
@@ -1,43 +1,37 @@
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import styles from "./proposal.module.scss";
-import { VStack } from "@/components/Layout/Stack";
+import { HStack, VStack } from "@/components/Layout/Stack";
 import ProposalStatus from "../ProposalStatus/ProposalStatus";
 
 export default function Proposal({ proposal }) {
   return (
-    <div className={styles.proposal_row_on_chain}>
-      <div className={styles.proposal_row_cell_primary}>
-        <Link href={`/proposals/${proposal.id}`}>
-          <VStack className={styles.cell_content} alignItems="items-start">
-            <div className={styles.cell_content_title}>
-              <>
-                Proposal by {proposal.proposer}
-                <span className={styles.proposal_status}></span>
-              </>
-            </div>
-            <div className={styles.cell_content_body}>
-              {proposal.markdowntitle.length > 100
-                ? `${proposal.markdowntitle.slice(0, 98)}...`
-                : proposal.markdowntitle}
-            </div>
-          </VStack>
-        </Link>
-      </div>
-      <div className={styles.proposal_row_cell}>
+    <Link href={`/proposals/${proposal.id}`}>
+      <HStack justifyContent="justify-between" alignItems="items-start" className="p-4 border-b-2">
         <VStack className={styles.cell_content} alignItems="items-start">
+          <div className={styles.cell_content_title}>
+            <>
+              Proposal by {proposal.proposer}
+              <span className={styles.proposal_status}></span>
+            </>
+          </div>
+          <div className={styles.cell_content_body}>
+            {proposal.markdowntitle.length > 100
+              ? `${proposal.markdowntitle.slice(0, 98)}...`
+              : proposal.markdowntitle}
+          </div>
+        </VStack>
+        <VStack className={styles.cell_content} alignItems="items-center">
           <div className={styles.cell_content_title}>Title</div>
           <div className={styles.cell_content_body}>
             <ProposalStatus proposal={proposal} />
           </div>
         </VStack>
-      </div>
-      <div className={styles.proposal_row_cell}>
-        <VStack className={styles.cell_content} alignItems="items-start">
+        <VStack className={styles.cell_content} alignItems="items-end">
           <div className={styles.cell_content_title}>Vote ended 2 days ago</div>
           <div className={styles.cell_content_body}>8 Options</div>
         </VStack>
-      </div>
-    </div>
+      </HStack>
+    </Link>
   );
 }
