e6b875729d8ac6350ef98ecd8dfbbe8e54a0360b
stepandel <stepandel@gmail.com>
2023-11-19 15:14:56 -0500

    fix getTokenSupply function name

--- src/app/api/v1/metrics/route.ts
+++ src/app/api/v1/metrics/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { getCurrentQuorum } from "@/lib/governorUtils";
-import { getVotableSupply } from "@/lib/tokenUtils";
+import { getTokenSupply } from "@/lib/tokenUtils";
 
 export async function GET(request: NextRequest) {
   const votableSupply = await prisma.votableSupply.findFirst({});
-  const totalSupply = await getVotableSupply("OPTIMISM");
+  const totalSupply = await getTokenSupply("OPTIMISM");
   const quorum = await getCurrentQuorum("OPTIMISM");
 
   // Build out delegate JSON response
