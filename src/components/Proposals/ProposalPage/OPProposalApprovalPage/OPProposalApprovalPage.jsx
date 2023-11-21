b93aaa1bb28388e0630d1aec03ca9f27f24c2b19
Kent Fenwick <kent.fenwick@gmail.com>
2023-11-21 00:02:17 -0500

    added markdown for proposal view

--- src/components/Proposals/ProposalPage/OPProposalApprovalPage/OPProposalApprovalPage.jsx
+++ src/components/Proposals/ProposalPage/OPProposalApprovalPage/OPProposalApprovalPage.jsx
@@ -1,16 +1,24 @@
 import { HStack, VStack } from "@/components/Layout/Stack";
 import ProposalResults from "../ProposalResults";
-import ProposalDescription from "../ProposalDescription";
+import ProposalDescription from "../ProposalDescription/ProposalDescription";
 import { ProposalVotes } from "../ProposalVotes";
+import styles from "./OPProposalApprovalPage.module.scss";
 
 export default function OPProposalApprovalPage({ proposal }) {
   return (
-    <HStack justifyContent="justify-between">
-      <div>
-        APPROVAL
-        <ProposalDescription proposal={proposal} />
-      </div>
-      <VStack gap={6}>APPROVAL VOTES</VStack>
+    <HStack
+      gap={16}
+      justifyContent="justify-between"
+      alignItems="items-start"
+      className={styles.proposal_description_container}
+    >
+      <ProposalDescription proposal={proposal} />
+      <VStack
+        justifyContent="space-between"
+        className={styles.proposal_votes_container}
+      >
+        APPROVAL VOTES
+      </VStack>
     </HStack>
   );
 }
---
+++ src/components/Proposals/ProposalPage/ProposalTitle/ProposalTitle.jsx
@@ -0,0 +1,16 @@
+import HumanAddress from "@/components/shared/HumanAddress";
+import styles from "./ProposalTitle.module.scss";
+import { VStack } from "@/components/Layout/Stack";
+
+export default function ProposalTitle({ title, proposerAddress }) {
+  console.log("Proposer", proposerAddress);
+  return (
+    <VStack className={styles.proposal_title_container}>
+      <h2>{title}</h2>
+      <div className={styles.proposal_proposer}>
+        by &nbsp;
+        <HumanAddress address={proposerAddress} />
+      </div>
+    </VStack>
+  );
+}
