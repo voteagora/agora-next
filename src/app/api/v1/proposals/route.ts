db203f0611bcfff2ae3208a3452b6537ea63385d
stepandel <stepandel@gmail.com>
2023-11-21 16:44:02 -0500

    update proposals route, parse status & results

--- src/app/api/v1/proposals/route.ts
+++ src/app/api/v1/proposals/route.ts
@@ -1,7 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { getHumanBlockTime } from "@/lib/blockTimes";
-import { parseProposalType } from "@/lib/proposalUtils";
+import {
+  getProposalStatus,
+  parseProposalData,
+  parseProposalResults,
+  getTitleFromProposalDescription,
+} from "@/lib/proposalUtils";
 import provider from "@/app/lib/provider";
 
 BigInt.prototype.toJSON = function () {
@@ -14,52 +19,79 @@ export async function GET(request: NextRequest) {
     page = 1;
   }
 
+  const pageSize = 4;
+
+  const proposals = await prisma.proposals.findMany({
+    take: pageSize + 1,
+    skip: (page - 1) * pageSize,
+    orderBy: {
+      ordinal: "desc",
+    },
+  });
+
   const latestBlock = await provider.getBlock("latest");
 
-  const pageSize = 4;
-  const total_count = await prisma.proposalsList.count();
-  const total_pages = Math.ceil(total_count / pageSize);
+  const resolvedProposals = Promise.all(
+    proposals.slice(0, -1).map(async (proposal) => {
+      const proposalData = parseProposalData(
+        JSON.stringify(proposal.proposal_data || {}),
+        proposal.proposal_type as "STANDARD" | "APPROVAL"
+      );
+      const proposalResutsls = parseProposalResults(
+        JSON.stringify(proposal.proposal_results || {}),
+        proposalData
+      );
+      return {
+        id: proposal.proposal_id,
+        proposer: proposal.proposer,
+        created_time: latestBlock
+          ? getHumanBlockTime(
+              proposal.created_block,
+              latestBlock.number,
+              latestBlock.timestamp
+            )
+          : null,
+        start_time: latestBlock
+          ? getHumanBlockTime(
+              proposal.start_block,
+              latestBlock.number,
+              latestBlock.timestamp
+            )
+          : null,
+        end_time: latestBlock
+          ? getHumanBlockTime(
+              proposal.end_block,
+              latestBlock.number,
+              latestBlock.timestamp
+            )
+          : null,
+        markdowntitle: getTitleFromProposalDescription(
+          proposal.description || ""
+        ),
+        proposaData: proposalData.kind,
+        proposalResults: proposalResutsls.kind,
+        proposalType: proposal.proposal_type,
+        status: latestBlock
+          ? await getProposalStatus(
+              proposal,
+              proposalResutsls,
+              latestBlock.number
+            )
+          : null,
+      };
+    })
+  );
 
-  const proposals = await prisma.proposalsList.findMany({
-    take: pageSize,
-    skip: (page - 1) * pageSize,
-  });
-
-  const resolvedProposals = proposals.map((proposal) => {
-    return {
-      id: proposal.proposal_id,
-      proposer: proposal.proposer,
-      start_block: proposal.start_block,
-      end_block: proposal.end_block,
-      start_time: latestBlock
-        ? getHumanBlockTime(
-            proposal.start_block,
-            latestBlock.number,
-            latestBlock.timestamp
-          )
-        : null,
-      end_time: latestBlock
-        ? getHumanBlockTime(
-            proposal.end_block,
-            latestBlock.number,
-            latestBlock.timestamp
-          )
-        : null,
-      markdowntitle: proposal.description?.replace(/\\n/g, "\n").split("\n")[0],
-      proposaData: proposal.proposal_data,
-      proposalType: parseProposalType(proposal.proposal_data ?? "{}"),
-    };
-  });
+  const hasNextPage = proposals.length > pageSize;
 
   // Build out proposal response
   const response = {
     meta: {
-      current_page: page,
-      total_pages: total_pages,
-      page_size: pageSize,
-      total_count: total_count,
+      currentPage: page,
+      pageSize,
+      hasNextPage,
     },
-    proposals: resolvedProposals,
+    proposals: await resolvedProposals,
   };
 
   return NextResponse.json(response);
