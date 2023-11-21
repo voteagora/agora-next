01ce8788664148cd155a042216626fc20a37f795
stepandel <stepandel@gmail.com>
2023-11-20 19:39:48 -0500

    add pagination to delegates wip

--- src/app/api/v1/delegates/route.ts
+++ src/app/api/v1/delegates/route.ts
@@ -2,15 +2,26 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
 export async function GET(request: NextRequest) {
+  const page = request.nextUrl.searchParams.get("page");
+  const pageSize = 100;
+
   const delegates = await prisma.delegates.findMany({
-    take: 100,
+    skip: Number(page ?? 1) * pageSize,
+    take: 101,
     orderBy: {
       voting_power: "desc",
     },
   });
 
+  const hasNextPage = delegates.length > pageSize;
+
   // Build out proposal response
   const response = {
+    meta: {
+      currentPage: page,
+      pageSize,
+      hasNextPage,
+    },
     delegates: delegates.map((delegate) => ({
       address: delegate.delegate,
       votingPower: delegate.voting_power?.toFixed(),
