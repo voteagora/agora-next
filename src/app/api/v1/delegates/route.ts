8188af4d9a88410ef83063eb37aeb0cbd7e03bc6
stepandel <stepandel@gmail.com>
2023-11-20 19:58:59 -0500

    implement sorts on delgates

--- src/app/api/v1/delegates/route.ts
+++ src/app/api/v1/delegates/route.ts
@@ -1,17 +1,44 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
+import { Prisma } from "@prisma/client";
 
 export async function GET(request: NextRequest) {
   const page = request.nextUrl.searchParams.get("page");
   const pageSize = 100;
-
-  const delegates = await prisma.delegates.findMany({
-    skip: Number(page ?? 1) * pageSize,
-    take: 101,
-    orderBy: {
-      voting_power: "desc",
-    },
-  });
+  const seed = Number(
+    request.nextUrl.searchParams.get("seed") ?? Math.random()
+  );
+  console.log(seed);
+  const delegates = await (() => {
+    switch (request.nextUrl.searchParams.get("sort")) {
+      case "most_delegators":
+        return prisma.delegates.findMany({
+          skip: (Number(page ?? 1) - 1) * pageSize,
+          take: 101,
+          orderBy: {
+            num_for_delegators: "desc",
+          },
+        });
+      case "weigted_random":
+        return prisma.$queryRaw<Prisma.DelegatesGetPayload<true>[]>(
+          Prisma.sql`
+          SELECT *, setseed(${seed})::Text
+          FROM center.delegates
+          WHERE voting_power > 0
+          ORDER BY -log(random()) / voting_power
+          LIMIT ${pageSize};
+          `
+        );
+      default:
+        return prisma.delegates.findMany({
+          skip: Number(page ?? 1) * pageSize,
+          take: 101,
+          orderBy: {
+            voting_power: "desc",
+          },
+        });
+    }
+  })();
 
   const hasNextPage = delegates.length > pageSize;
 
