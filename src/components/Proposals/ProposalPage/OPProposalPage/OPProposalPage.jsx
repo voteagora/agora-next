0ae8b61beb76f34642534c1c453b5c24b080d7b7
Kent Fenwick <kent.fenwick@gmail.com>
2023-11-21 00:06:40 -0500

    stubbed out proposal standard page:

--- src/components/Proposals/ProposalPage/OPProposalPage/OPProposalPage.jsx
+++ src/components/Proposals/ProposalPage/OPProposalPage/OPProposalPage.jsx
@@ -2,15 +2,23 @@ import { HStack, VStack } from "@/components/Layout/Stack";
 import ProposalResults from "../ProposalResults";
 import ProposalDescription from "../ProposalDescription/ProposalDescription";
 import { ProposalVotes } from "../ProposalVotes";
+import styles from "./OPProposalPage.module.scss";
 
-export default function OPProposalApprovalPage({ proposal }) {
+export default function OPProposalPage({ proposal }) {
   return (
-    <HStack justifyContent="justify-between">
-      <div>
-        STANDARD
-        <ProposalDescription proposal={proposal} />
-      </div>
-      <VStack gap={6}>STANDARD VOTES</VStack>
+    <HStack
+      gap={16}
+      justifyContent="justify-between"
+      alignItems="items-start"
+      className={styles.proposal_description_container}
+    >
+      <ProposalDescription proposal={proposal} />
+      <VStack
+        justifyContent="space-between"
+        className={styles.proposal_votes_container}
+      >
+        Standard VOTES
+      </VStack>
     </HStack>
   );
 }
