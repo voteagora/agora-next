95b3a4109bf5e31778a3701ef6194ba1b7b07c30
stepandel <stepandel@gmail.com>
2023-11-21 18:18:08 -0500

    make pagination wrapper

--- src/app/api/v1/delegates/route.ts
+++ src/app/api/v1/delegates/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { Prisma } from "@prisma/client";
+import { paginatePrismaResult } from "@/app/lib/pagination";
 
 export async function GET(request: NextRequest) {
   let page = parseInt(request.nextUrl.searchParams.get("page") ?? "0", 10);
@@ -13,49 +14,46 @@ export async function GET(request: NextRequest) {
     request.nextUrl.searchParams.get("seed") ?? Math.random()
   );
 
-  const delegates = await (() => {
-    switch (request.nextUrl.searchParams.get("sort")) {
-      case "most_delegators":
-        return prisma.delegates.findMany({
-          skip: (page - 1) * pageSize,
-          take: pageSize + 1,
-          orderBy: {
-            num_for_delegators: "desc",
-          },
-        });
-      case "weigted_random":
-        return prisma.$queryRaw<Prisma.DelegatesGetPayload<true>[]>(
-          Prisma.sql`
-          SELECT *, setseed(${seed})::Text
-          FROM center.delegates
-          WHERE voting_power > 0
-          ORDER BY -log(random()) / voting_power
-          OFFSET ${pageSize * (page - 1)}
-          LIMIT ${pageSize + 1}; // Fetch one extra record
-          `
-        );
-      default:
-        return prisma.delegates.findMany({
-          skip: (page - 1) * pageSize,
-          take: pageSize + 1,
-          orderBy: {
-            voting_power: "desc",
-          },
-        });
-    }
-  })();
-
-  const hasNextPage = delegates.length > pageSize;
-
-  const theDelegates = delegates.slice(0, pageSize);
+  const { meta, data: delegates } = await paginatePrismaResult(
+    (skip: number, take: number, seed: number) => {
+      switch (request.nextUrl.searchParams.get("sort")) {
+        case "most_delegators":
+          return prisma.delegates.findMany({
+            skip,
+            take,
+            orderBy: {
+              num_for_delegators: "desc",
+            },
+          });
+        case "weigted_random":
+          return prisma.$queryRaw<Prisma.DelegatesGetPayload<true>[]>(
+            Prisma.sql`
+            SELECT *, setseed(${seed})::Text
+            FROM center.delegates
+            WHERE voting_power > 0
+            ORDER BY -log(random()) / voting_power
+            OFFSET ${skip}
+            LIMIT ${take}; // Fetch one extra record
+            `
+          );
+        default:
+          return prisma.delegates.findMany({
+            skip,
+            take,
+            orderBy: {
+              voting_power: "desc",
+            },
+          });
+      }
+    },
+    page,
+    pageSize,
+    { seed }
+  );
 
   const response = {
-    meta: {
-      currentPage: page,
-      pageSize,
-      hasNextPage,
-    },
-    delegates: theDelegates.map((delegate) => ({
+    meta,
+    delegates: delegates.map((delegate) => ({
       address: delegate.delegate,
       votingPower: delegate.voting_power?.toFixed(),
     })),
