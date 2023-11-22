b7dd0a83306716a03539eb72e8b20a121061e468
Kent Fenwick <kent.fenwick@gmail.com>
2023-11-21 22:35:18 -0500

    pulled in delegate statemets

--- src/app/delegates/[addressOrENSName]/page.jsx
+++ src/app/delegates/[addressOrENSName]/page.jsx
@@ -8,6 +8,7 @@ import DelegateCard from "@/components/Delegates/DelegateCard/DelegateCard";
 import DelegateVotes from "@/components/Delegates/DelegateVotes";
 import { HStack, VStack } from "@/components/Layout/Stack";
 import styles from "./styles.module.scss";
+import DelegateStatement from "@/components/Delegates/DelegateStatement/DelegateStatement";
 
 async function getDelegate(addressOrENSName) {
   "use server";
@@ -26,9 +27,18 @@ async function getDelegateVotes(addressOrENSName, page = 1) {
   return { delegateVotes: data.votes, meta: data.meta };
 }
 
+async function getDelegateStatement(addressOrENSName) {
+  "use server";
+
+  const api = new AgoraAPI();
+  const data = await api.get(`/delegates/${addressOrENSName}/statement`);
+  return data.statement;
+}
+
 export default async function Page({ params: { addressOrENSName } }) {
   const delegate = await getDelegate(addressOrENSName);
   const delegateVotes = await getDelegateVotes(addressOrENSName);
+  const statement = await getDelegateStatement(addressOrENSName);
 
   return (
     <HStack
@@ -38,18 +48,22 @@ export default async function Page({ params: { addressOrENSName } }) {
     >
       <VStack className={styles.left_container}>
         <DelegateCard delegate={delegate} />
+      </VStack>
 
-        {!delegate.statement && (
+      <VStack className={styles.right_container}>
+        {!statement.delegateStatement && (
           <p>
             This voter has not submitted a statement. Is this you? Connect your
             wallet to verify your address, and tell your community what you’d
             like to see.
           </p>
         )}
-      </VStack>
 
-      <VStack className={styles.right_container}>
-        {/** Statement section goes here */}
+        {statement.delegateStatement && (
+          <DelegateStatement
+            statement={statement.delegateStatement}
+          />
+        )}
 
         <DelegateVotes
           initialVotes={delegateVotes}
