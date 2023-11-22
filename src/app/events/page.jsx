aee57b988100a2665b349d199956c6b904e3dc07
Kent Fenwick <kent.fenwick@gmail.com>
2023-11-22 01:50:55 -0500

    fixed TS errors for Next and Vercel

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,23 +1,8 @@
-import EventFeed from "../../components/Events/EventFeed";
-import AgoraAPI from "../lib/agoraAPI";
-
-// This is the server-side function that fetches events from the Agora API
-async function fetchEvents(page = 1) {
-  "use server";
-
-  const api = new AgoraAPI();
-  const data = await api.get(`/events?page=${page}`);
-  return { events: data.events, meta: data.meta };
-}
-
 export default async function Page() {
-  const events = await fetchEvents();
-
   return (
     <>
       <section>
         <h1>Activity Feed</h1>
-        <EventFeed initialEvents={events} fetchEvents={fetchEvents} />
       </section>
     </>
   );
