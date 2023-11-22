ffc4029db881b9960726311549dd0ebb56df8226
stepandel <stepandel@gmail.com>
2023-11-22 16:03:54 -0500

    fix pagination for delegate votes

--- src/components/Delegates/DelegateVotes.jsx
+++ src/components/Delegates/DelegateVotes.jsx
@@ -8,6 +8,8 @@ import { colorForSupportType } from "@/lib/voteUtils";
 import { formatNumber, pluralizeVote } from "@/lib/tokenUtils";
 import { shortAddress } from "@/lib/utils";
 import { formatDistanceToNow } from "date-fns";
+import InfiniteScroll from "react-infinite-scroller";
+import Image from "next/image";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -28,105 +30,117 @@ function shortPropTitle(title, proosalId) {
 }
 
 export default function DelegateVotes({ initialVotes, fetchDelegateVotes }) {
-  // const fetching = React.useRef(false);
+  const fetching = React.useRef(false);
   const [pages, setPages] = React.useState([initialVotes]);
-  // const [meta, setMeta] = React.useState(initialVotes.meta);
+  const [meta, setMeta] = React.useState(initialVotes.meta);
 
   const loadMore = async (page) => {
-    // if (!fetching.current && page <= meta.total_pages) {
-    //   fetching.current = true;
-    //   const data = await fetchDelegateVotes(page);
-    //   const existingIds = new Set(delegateVotes.map((v) => v.id));
-    //   const uniqueDelegateVotes = data.delegateVotes.filter(
-    //     (p) => !existingIds.has(p.id)
-    //   );
-    //   setPages((prev) => [
-    //     ...prev,
-    //     { ...data, delegateVotes: uniqueDelegateVotes },
-    //   ]);
-    //   setMeta(data.meta);
-    //   fetching.current = false;
-    // }
+    if (!fetching.current && meta.hasNextPage) {
+      fetching.current = true;
+      const data = await fetchDelegateVotes(page);
+      const existingIds = new Set(delegateVotes.map((d) => d.transactionHash));
+      const uniqueVotes = data.votes.filter(
+        (d) => !existingIds.has(d.transactionHash)
+      );
+      setPages((prev) => [...prev, { ...data, votes: uniqueVotes }]);
+      setMeta(data.meta);
+      fetching.current = false;
+    }
   };
 
-  const delegateVotes = pages.reduce(
-    (all, page) => all.concat(page.delegateVotes),
-    []
-  );
+  const delegateVotes = pages.reduce((all, page) => all.concat(page.votes), []);
 
   return (
     <VStack gap="4">
-      {delegateVotes.map(
-        (vote, voteIdx) =>
-          vote && (
-            <VoteDetailsContainer key={voteIdx}>
-              <div
-                className={css`
-                  display: grid;
-                  overflow-y: hidden;
-                  grid-template-columns: 1fr 1px 1fr;
-
-                  @media (max-width: ${theme.maxWidth["2xl"]}) {
-                    grid-template-rows: 1fr;
-                    grid-template-columns: none;
-                    overflow-y: scroll;
-                  }
-                `}
-              >
-                <VStack
+      <InfiniteScroll
+        hasMore={meta.hasNextPage}
+        pageStart={0}
+        loadMore={loadMore}
+        loader={
+          <div key="loader">
+            Loading... <br />
+            <Image
+              src="/images/blink.gif"
+              alt="Blinking Agora Logo"
+              width={50}
+              height={20}
+            />
+          </div>
+        }
+        element="main"
+      >
+        {delegateVotes.map(
+          (vote) =>
+            vote && (
+              <VoteDetailsContainer key={vote.transactionHash}>
+                <div
                   className={css`
-                    padding: ${theme.spacing["4"]} ${theme.spacing["6"]};
+                    display: grid;
+                    overflow-y: hidden;
+                    grid-template-columns: 1fr 1px 1fr;
+
+                    @media (max-width: ${theme.maxWidth["2xl"]}) {
+                      grid-template-rows: 1fr;
+                      grid-template-columns: none;
+                      overflow-y: scroll;
+                    }
                   `}
                 >
-                  <HStack
-                    gap="1"
+                  <VStack
                     className={css`
-                      font-size: ${theme.fontSize.xs};
-                      font-weight: ${theme.fontWeight.medium};
-                      color: #66676b;
+                      padding: ${theme.spacing["4"]} ${theme.spacing["6"]};
                     `}
                   >
-                    <a
-                      href={`/proposals/${vote.proposal_id}`}
-                      title={`Prop ${vote.proposal_id}`}
+                    <HStack
+                      gap="1"
+                      className={css`
+                        font-size: ${theme.fontSize.xs};
+                        font-weight: ${theme.fontWeight.medium};
+                        color: #66676b;
+                      `}
                     >
-                      Prop {shortAddress(vote.proposal_id)}
-                    </a>
-                    <>
-                      {vote.proposalValue != 0n ? (
-                        <> asking {formatNumber(vote.proposalValue)} ETH</>
-                      ) : null}{" "}
-                    </>
-                    {vote.timestamp && (
-                      <div>
-                        - {formatDistanceToNow(new Date(vote.timestamp))} ago
-                      </div>
+                      <a
+                        href={`/proposals/${vote.proposal_id}`}
+                        title={`Prop ${vote.proposal_id}`}
+                      >
+                        Prop {shortAddress(vote.proposal_id)}
+                      </a>
+                      <>
+                        {vote.proposalValue != 0n ? (
+                          <> asking {formatNumber(vote.proposalValue)} ETH</>
+                        ) : null}{" "}
+                      </>
+                      {vote.timestamp && (
+                        <div>
+                          - {formatDistanceToNow(new Date(vote.timestamp))} ago
+                        </div>
+                      )}
+                    </HStack>
+                    <h2
+                      className={css`
+                        font-size: ${theme.fontSize.base};
+                        padding: ${theme.spacing[1]} 0;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                      `}
+                    >
+                      <a href={`/proposals/${vote.proposal_id}`}>
+                        {shortPropTitle(vote.proposalTitle, vote.proposal_id)}
+                      </a>
+                    </h2>
+                    {vote.proposalType === "APPROVAL" && (
+                      <ApprovalVoteContainer {...vote} />
                     )}
-                  </HStack>
-                  <h2
-                    className={css`
-                      font-size: ${theme.fontSize.base};
-                      padding: ${theme.spacing[1]} 0;
-                      overflow: hidden;
-                      text-overflow: ellipsis;
-                    `}
-                  >
-                    <a href={`/proposals/${vote.proposal_id}`}>
-                      {shortPropTitle(vote.proposalTitle)}
-                    </a>
-                  </h2>
-                  {vote.proposalType === "APPROVAL" && (
-                    <ApprovalVoteContainer {...vote} />
-                  )}
-                  {vote.proposalType === "STANDARD" && (
-                    <StandardVoteContainer {...vote} />
-                  )}
-                </VStack>
-                {vote.reason && <VoteReason reason={vote.reason} />}
-              </div>
-            </VoteDetailsContainer>
-          )
-      )}
+                    {vote.proposalType === "STANDARD" && (
+                      <StandardVoteContainer {...vote} />
+                    )}
+                  </VStack>
+                  {vote.reason && <VoteReason reason={vote.reason} />}
+                </div>
+              </VoteDetailsContainer>
+            )
+        )}
+      </InfiniteScroll>
     </VStack>
   );
 }
