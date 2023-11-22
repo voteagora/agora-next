b511aaee6a99abc806a1103b9bb84bcbc91ec990
stepandel <stepandel@gmail.com>
2023-11-22 11:45:05 -0500

    parse votes

--- src/lib/voteUtils.ts
+++ src/lib/voteUtils.ts
@@ -3,36 +3,32 @@
  */
 
 import { ProposalType } from "@prisma/client";
-import { parseProposalData } from "./proposalUtils";
+import { ParsedProposalData, parseProposalData } from "./proposalUtils";
 
 type ParsedParams = {
   APPROVAL: {
     key: "APPROVAL";
     kind: string[];
   };
+  STANDARD: {
+    key: "STANDARD";
+    kind: null;
+  };
 };
 
 export function parseParams(
   params: string | null,
-  proposaData: string,
-  proposalType: ProposalType
-): string[] | null {
-  if (params === null || proposalType !== "APPROVAL") {
+  proposaData: ParsedProposalData[ProposalType]
+): ParsedParams[ProposalType]["kind"] {
+  if (params === null || proposaData.key !== "APPROVAL") {
     return null;
   }
 
   try {
     const parsedParams = JSON.parse(params);
-    const parsedProposalData = parseProposalData(proposaData, proposalType);
-    // BS way to make to force parse options
-    const proposalOptions =
-      parsedProposalData.key === proposalType
-        ? parsedProposalData.kind.options
-        : [];
-
     return parsedParams[0].map((param: string) => {
       const idx = Number(param);
-      return proposalOptions[idx].description;
+      return proposaData.kind.options[idx].description;
     });
   } catch (e) {
     return null;
