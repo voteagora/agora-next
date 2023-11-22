4857da627401ed6b75c874b1c0b9d54931400dde
stepandel <stepandel@gmail.com>
2023-11-22 11:58:43 -0500

    return same votes data for both proposal and delegate

--- src/app/api/v1/delegates/[addressOrENSName]/votes/route.ts
+++ src/app/api/v1/delegates/[addressOrENSName]/votes/route.ts
@@ -2,14 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 import { isAddress } from "viem";
 import { resolveENSName } from "@/app/lib/utils";
-import {
-  getProposalTotalValue,
-  parseProposalData,
-  parseSupport,
-} from "@/lib/proposalUtils";
-import { getHumanBlockTime } from "@/lib/blockTimes";
 import provider from "@/app/lib/provider";
-import { parseParams } from "@/lib/voteUtils";
+import { parseVotes } from "@/lib/voteUtils";
 
 export async function GET(
   request: NextRequest,
@@ -27,31 +21,7 @@ export async function GET(
 
   // Build out proposal response
   const response = {
-    votes: delegateVotes.map((vote) => {
-      const proposalData = parseProposalData(
-        JSON.stringify(vote.proposal_data || {}),
-        vote.proposal_type
-      );
-      return {
-        timestamp: latestBlock
-          ? getHumanBlockTime(
-              vote.block_number,
-              latestBlock.number,
-              latestBlock.timestamp
-            )
-          : null,
-        address: vote.voter,
-        proposal_id: vote.proposal_id,
-        support: parseSupport(vote.support, vote.proposal_type),
-        weight: vote.weight,
-        reason: vote.reason,
-        params: parseParams(vote.params, proposalData),
-        proposalType: vote.proposal_type,
-        proposalData: vote.proposal_data,
-        proposalDescription: vote.description,
-        proposalValue: getProposalTotalValue(proposalData),
-      };
-    }),
+    votes: parseVotes(delegateVotes, latestBlock),
   };
 
   return NextResponse.json(response);
