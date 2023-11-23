9e8d307df625a11ee1d1aee1ffff75d5951d8c1c
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-11-23 23:12:09 +0100

    init admin page

---
+++ src/app/admin/components/AdminForm.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters."
+  })
+})
+
+export default function AdminForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ""
+    }
+  })
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 my-4">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { useAccount } from "wagmi"
+import AdminForm from "./components/AdminForm"
+import { useAgoraContext } from "../AgoraContext"
+
+const whitelistedAddresses = ["0x6EF3E0179e669C77C82664D0feDad3a637121Efe"]
+
+export default function Page() {
+  const { address, isConnecting, isReconnecting } = useAccount()
+  const { isConnected } = useAgoraContext()
+
+  const isAllowed = isConnected && whitelistedAddresses.includes(address!)
+
+  return (
+    <>
+      <section>
+        <h1>Governor settings</h1>
+        <p>Set how all proposals work</p>
+      </section>
+      {isAllowed ? (
+        <AdminForm />
+      ) : isConnecting || isReconnecting ? (
+        <p>Loading</p>
+      ) : null}
+    </>
+  )
+}
