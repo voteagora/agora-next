b9767565eb8cf681e6a7b596a72e38630b33a232
Kent Fenwick <kent.fenwick@gmail.com>
2023-11-28 00:30:54 -0500

    updated vote cast proposals

--- src/app/api/proposals/proposal.d.ts
+++ src/app/api/proposals/proposal.d.ts
@@ -1,5 +1,6 @@
 import { ParsedProposalData, ProposalStatus } from "@/lib/proposalUtils";
 import { ProposalType } from "@prisma/client";
+import { BigNumberish } from "ethers";
 
 export type Proposal = {
   id: string;
@@ -8,6 +9,8 @@ export type Proposal = {
   start_time: Date | null;
   end_time: Date | null;
   markdowntitle: string;
+  description: string | null;
+  quorum: BigNumberish | null;
   proposalData: ParsedProposalData[ProposalType]["kind"];
   proposalResults: ParsedProposalResults[ProposalType]["kind"];
   proposalType: ProposalType | null;
