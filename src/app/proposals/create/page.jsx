0fa2f2f7ffce4de3b71b83258aa6941fecaaed38
Dom-Mac <domenicochad@gmail.com>
2023-11-28 20:19:26 +0100

    create proposal form refactor

--- src/app/proposals/create/page.jsx
+++ src/app/proposals/create/page.jsx
@@ -1,6 +1,7 @@
 import { HStack } from "@/components/Layout/Stack";
 import InfoPanel from "@/components/Proposals/ProposalCreation/InfoPanel";
 import styles from "./styles.module.scss";
+import { CreateProposalForm } from "@/components/Proposals/ProposalCreation/CreateProposalForm";
 
 export default function CreateProposalPage() {
   return (
@@ -9,8 +10,7 @@ export default function CreateProposalPage() {
       gap={16}
       className={styles.create_prop_container}
     >
-      {/* <CreateProposalForm /> */}
-      <p>form</p>
+      <CreateProposalForm />
       <div className={styles.create_prop_container_right}>
         <InfoPanel />
       </div>
