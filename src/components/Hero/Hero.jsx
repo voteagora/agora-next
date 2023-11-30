7eea8618e2fc0061e46a252733ae10316f65ba90
Yitong <zha.yitong@gmail.com>
2023-11-29 23:23:58 -0800

    Styling the proposalslist page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { HStack, VStack } from "../Layout/Stack";
-import heroBackGroundImage from "@/assets/optimism/optimism_hero_background.png";
+import sunnyFace from "@/assets/optimism/sunny-face.svg";
+import sunnyBg from "@/assets/optimism/sunny-bg.svg";
 import styles from "./hero.module.scss";
 
 export default function Hero() {
@@ -9,7 +10,7 @@ export default function Hero() {
     <HStack justifyContent="justify-between" className={styles.hero_container}>
       <VStack className={styles.content_container}>
         <h1>
-          Agora is the home of <span>Optimism</span> Voters
+          Agora is the home of <span>Optimism</span> voters
         </h1>
         <p>
           {" "}
@@ -20,11 +21,14 @@ export default function Hero() {
             based on the reputation as the Optimism Collective members.`}
         </p>
       </VStack>
-      <Image
-        src={heroBackGroundImage}
-        alt="optimism background"
-        className="gl_hero_bg_image"
-      />
+      <div className={styles.partner_image_container}>
+        <Image src={sunnyBg} alt="optimism background" className={styles.sunny_bg}/>
+        <Image
+          src={sunnyFace}
+          alt="optimism background"
+          className={styles.sunny_face}
+        />
+      </div>
     </HStack>
   );
 }
