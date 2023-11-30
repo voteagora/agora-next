92fd058cf16e1df2e7361760d6dc84f0c274f34f
Yitong <zha.yitong@gmail.com>
2023-11-29 23:23:58 -0800

    Styling the proposalslist page

--- src/components/Layout/Loader.jsx
+++ src/components/Layout/Loader.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
+import { HStack } from "./Stack";
 
 export default function Loader() {
   return (
-    <div key="loader" className="gl_loader">
-      Loading... <br />
+    <HStack key="loader" className="gl_loader justify-center py-5">
       <Image
         src="/images/blink.gif"
         alt="Blinking Agora Logo"
-        width={50}
-        height={20}
+        width={32}
+        height={8}
       />
-    </div>
+    </HStack>
   );
 }
