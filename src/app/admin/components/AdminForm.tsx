89c75f9c3d017ba1e1d35ac7d2374f85dad2d214
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-11-29 22:55:15 -0500

    governor settings wip

--- src/app/admin/components/AdminForm.tsx
+++ src/app/admin/components/AdminForm.tsx
@@ -1,60 +1,14 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import ProposalTypeSettings from "./ProposalTypeSettings"
+import GovernorSettings from "./GovernorSettings"
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters."
-  })
-})
-
+// TODO: Take init values from the chain
 export default function AdminForm() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: ""
-    }
-  })
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
-
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 my-4">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="space-y-8">
+      <GovernorSettings />
+      <ProposalTypeSettings />
+    </div>
   )
 }
