16268fc406ff42bbf4a6c466fb73313b0fcd2a6d
ZeroHex <domenicochad@gmail.com>
2023-12-01 22:58:13 +0100

    OP proposal creation + Admin page (#20)
    * feat: add shadcn ui form components
    
    * feat: add AgoraContext
    
    * init admin page
    
    * prettier local file ignored
    
    * wip - style working
    
    * use form hook
    
    * create proposal form refactor
    
    * import proposal type switch
    
    * title description row imported
    
    * approval proposal form
    
    * fix style
    
    * simulate transaction
    
    * standard form
    
    * submit logic imported and corrected
    
    * add abis
    
    * fix shadcn styling
    
    * governor settings wip
    
    * testnet contracts
    
    * add proposal settings
    
    * feat: proposalType ui/ux (proposalSettings)
    
    * wip
    
    * wip
    
    * wip
    
    * feat: proposal creation with types
    
    * fix: gitignore and prettier
    
    * fix: response type
    
    ---------
    
    Co-authored-by: jj_ranalli <jacopo.ranalli@mail.polimi.it>

---
+++ src/app/admin/components/GovernorSettings.tsx
@@ -0,0 +1,170 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { LoaderIcon, Lock } from "lucide-react"
+import {
+  useAccount,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction
+} from "wagmi"
+
+import GovernorAbi from "@/lib/contracts/abis/OptimismGovernor.json"
+
+const secondsPerBlock = 2
+
+// TODO: Take init state values from the chain
+export default function GovernorSettings() {
+  const [votingPeriod, setVotingPeriod] = useState(7)
+  const { config: setVotingPeriodConfig } = usePrepareContractWrite({
+    address: "0x6E17cdef2F7c1598AD9DfA9A8acCF84B1303f43f",
+    abi: GovernorAbi,
+    functionName: "setVotingPeriod",
+    args: [
+      (BigInt(votingPeriod || 0) * 60n * 60n * 24n) / BigInt(secondsPerBlock)
+    ]
+  })
+  const {
+    data: resultSetVotingPeriod,
+    write: writeSetVotingPeriod,
+    isLoading: isLoadingSetVotingPeriod
+  } = useContractWrite(setVotingPeriodConfig)
+  const { isLoading: isLoadingSetVotingPeriodTransaction } =
+    useWaitForTransaction({
+      hash: resultSetVotingPeriod?.hash
+    })
+  const isDisabledSetVotingPeriod =
+    isLoadingSetVotingPeriod || isLoadingSetVotingPeriodTransaction
+
+  const [votingDelay, setVotingDelay] = useState(168)
+  const { config: setVotingDelayConfig } = usePrepareContractWrite({
+    address: "0x6E17cdef2F7c1598AD9DfA9A8acCF84B1303f43f",
+    abi: GovernorAbi,
+    functionName: "setVotingDelay",
+    args: [
+      (BigInt(votingDelay || 0) * 60n * 60n * 24n) / BigInt(secondsPerBlock)
+    ]
+  })
+  const {
+    data: resultSetVotingDelay,
+    write: writeSetVotingDelay,
+    isLoading: isLoadingSetVotingDelay
+  } = useContractWrite(setVotingDelayConfig)
+  const { isLoading: isLoadingSetVotingDelayTransaction } =
+    useWaitForTransaction({
+      hash: resultSetVotingDelay?.hash
+    })
+  const isDisabledSetVotingDelay =
+    isLoadingSetVotingDelay || isLoadingSetVotingDelayTransaction
+
+  // const [managerAddress, setManagerAddress] = useState(currentManager as string)
+  // const { config: setManagerAddressConfig } = usePrepareContractWrite({
+  //   address: "0x6E17cdef2F7c1598AD9DfA9A8acCF84B1303f43f",
+  //   abi: GovernorAbi,
+  //   functionName: "setManagerAddress",
+  //   args: [
+  //     (BigInt(managerAddress || 0) * 60n * 60n * 24n) / BigInt(secondsPerBlock)
+  //   ]
+  // })
+  // const {
+  //   data: resultSetManagerAddress,
+  //   write: writeSetManagerAddress,
+  //   isLoading: isLoadingSetManagerAddress,
+  //   isError: isErrorSetManagerAddress
+  // } = useContractWrite(setManagerAddressConfig)
+  // const { isLoading: isLoadingSetManagerAddressTransaction } =
+  //   useWaitForTransaction({
+  //     hash: resultSetManagerAddress?.hash
+  //   })
+  // const isDisabledSetManagerAddress =
+  //   isLoadingSetVotingDelay || isLoadingSetVotingDelayTransaction
+
+  return (
+    <div className="gl_box">
+      <section>
+        <h1>Governor settings</h1>
+        <p>Set how all proposals work</p>
+      </section>
+      <div className="space-y-8 my-4">
+        <div className="flex justify-between gap-4">
+          <div className="flex-1">
+            <Label>Voting period</Label>
+            <div className="relative flex items-center">
+              <Input
+                value={votingPeriod}
+                onChange={(e) => setVotingPeriod(parseInt(e.target.value))}
+                disabled={isDisabledSetVotingPeriod}
+                type="number"
+              />
+              <p className="absolute text-sm text-muted-foreground right-[96px]">
+                Hours
+              </p>
+              <Button
+                variant="outline"
+                size="sm"
+                className="absolute right-[6px] rounded-sm bg-white"
+                loading={isDisabledSetVotingPeriod}
+                disabled={isDisabledSetVotingPeriod}
+                onClick={() => {
+                  writeSetVotingPeriod?.()
+                }}
+              >
+                Update
+              </Button>
+            </div>
+          </div>
+          <div className="flex-1">
+            <Label>Voting delay</Label>
+            <div className="relative flex items-center">
+              <Input
+                value={votingDelay}
+                onChange={(e) => setVotingDelay(parseInt(e.target.value))}
+                disabled={isDisabledSetVotingDelay}
+                type="number"
+              />
+              <p className="absolute text-sm text-muted-foreground right-[96px]">
+                Hours
+              </p>
+              <Button
+                className="absolute right-[6px] rounded-sm"
+                variant="outline"
+                size="sm"
+                loading={isDisabledSetVotingDelay}
+                disabled={isDisabledSetVotingDelay}
+                onClick={() => {
+                  writeSetVotingDelay?.()
+                }}
+              >
+                Update
+              </Button>
+            </div>
+          </div>
+        </div>
+        {/* <div className={!isManager ? "opacity-70" : ""}>
+          <Label>ManagerAddress</Label>
+          <div className="relative flex items-center">
+            <Input
+              value={managerAddress}
+              onChange={(e) => setManagerAddress(e.target.value)}
+              disabled={!isManager}
+            />
+            {!isManager && (
+              <Lock className="absolute text-sm text-muted-foreground right-[96px] w-3.5 h-3.5 cursor-not-allowed" />
+            )}
+            <Button
+              className="absolute top-0 right-0"
+              variant="outline"
+              size='sm'
+              disabled={!isManager}
+            >
+              Update
+            </Button>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Proposals/ProposalCreation/SubmitButton.tsx
@@ -0,0 +1,232 @@
+"use client";
+
+import { css, cx } from "@emotion/css";
+import { Form } from "./CreateProposalForm";
+import { ethers, AbiCoder } from "ethers";
+import * as theme from "@/styles/theme";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  OptimismContracts,
+  approvalModuleAddress,
+} from "@/lib/contracts/contracts";
+import { useOpenDialog } from "@/components/Dialogs/DialogProvider/DialogProvider";
+import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+
+const abiCoder = new AbiCoder();
+const governorTokenContract = OptimismContracts.governor;
+const governanceTokenContract = OptimismContracts.token;
+
+export default function SubmitButton({
+  formTarget,
+  form,
+}: {
+  formTarget: React.RefObject<HTMLFormElement>;
+  form: Form;
+}) {
+  const { governorFunction, inputData } = getInputData(form);
+  const { isConnected } = useAccount();
+  const { open } = useWeb3Modal();
+  const [isClient, setIsClient] = useState(false);
+
+  const { config, isError: onPrepareError } = usePrepareContractWrite({
+    address: governorTokenContract.address as any,
+    abi: governorTokenContract.abi,
+    functionName: governorFunction,
+    args: inputData as any,
+  });
+
+  const { data, isLoading, isSuccess, isError, write } =
+    useContractWrite(config);
+
+  const openDialog = useOpenDialog();
+
+  function submitProposal() {
+    write?.();
+    openDialog({
+      type: "CAST_PROPOSAL",
+      params: {
+        isLoading,
+        isError,
+        isSuccess,
+        txHash: data?.hash,
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (isSuccess || isError || isLoading || data?.hash) {
+      openDialog({
+        type: "CAST_PROPOSAL",
+        params: {
+          isLoading,
+          isError,
+          isSuccess,
+          txHash: data?.hash,
+        },
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading, isError, isSuccess, data?.hash]);
+
+  /* hack to suppress Suspense boundary error */
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <Button
+      type="submit"
+      variant={"outline"}
+      disabled={isLoading || onPrepareError}
+      className={cx([
+        css`
+          width: 40%;
+        `,
+        onPrepareError &&
+          css`
+            background: ${theme.colors.gray.eb} !important;
+            cursor: not-allowed;
+          `,
+      ])}
+      onClick={(e) => {
+        e.preventDefault();
+        if (!isConnected) {
+          open();
+          return;
+        }
+        if (formTarget.current?.checkValidity() && !onPrepareError) {
+          formTarget.current?.reportValidity();
+          submitProposal();
+        }
+      }}
+    >
+      {/* hack to suppress Suspense boundary error */}
+      {isClient && isConnected ? "Submit proposal" : "Connect wallet"}
+    </Button>
+  );
+}
+
+type BasicInputData = [string[], bigint[], string[], string, Number];
+type ApprovalInputData = [string, string, string, Number];
+type InputData = BasicInputData | ApprovalInputData;
+
+function getInputData(form: Form): {
+  governorFunction: "propose" | "proposeWithModule";
+  inputData: InputData;
+} {
+  const description = "# " + form.state.title + "\n" + form.state.description;
+  let governorFunction: "propose" | "proposeWithModule" = "propose";
+
+  // provide default values for basic proposal
+  let targets: string[] = [];
+  let values: bigint[] = [];
+  let calldatas: string[] = [];
+  // TODO
+  let proposalSettings = Number(form.state.proposalSettings); // index as uint8 as last argument on propose and proposeWithModule
+  let inputData: InputData = [
+    targets,
+    values,
+    calldatas,
+    description,
+    proposalSettings,
+  ];
+
+  try {
+    // if basic proposal, format data for basic proposal
+    if (form.state.proposalType === "Basic") {
+      governorFunction = "propose";
+
+      if (form.state.options[0].transactions.length === 0) {
+        targets.push(ethers.ZeroAddress);
+        values.push(BigInt(0));
+        calldatas.push("0x");
+      } else {
+        form.state.options[0].transactions.forEach((t) => {
+          if (t.type === "Transfer") {
+            targets.push(governanceTokenContract.address);
+            values.push(BigInt(0));
+            calldatas.push(encodeTransfer(t.transferTo, t.transferAmount));
+          } else {
+            targets.push(ethers.getAddress(t.target));
+            values.push(ethers.parseEther(t.value.toString() || "0"));
+            calldatas.push(t.calldata);
+          }
+        });
+      }
+    } else if (form.state.proposalType === "Approval") {
+      // if APPROVAL proposal, format data for approval proposal
+      governorFunction = "proposeWithModule";
+      // first bigint is the sum of all op transfer per option
+      let options: [bigint, string[], bigint[], string[], string][] = [];
+
+      form.state.options.forEach((option) => {
+        const formattedOption: [bigint, string[], bigint[], string[], string] =
+          [BigInt(0), [], [], [], option.title];
+
+        option.transactions.forEach((t) => {
+          if (t.type === "Transfer") {
+            formattedOption[0] += BigInt(t.transferAmount);
+            formattedOption[1].push(governanceTokenContract.address);
+            formattedOption[2].push(BigInt(0));
+            formattedOption[3].push(
+              encodeTransfer(t.transferTo, t.transferAmount)
+            );
+          } else {
+            formattedOption[1].push(ethers.getAddress(t.target));
+            formattedOption[2].push(
+              ethers.parseEther(t.value.toString() || "0")
+            );
+            formattedOption[3].push(t.calldata);
+          }
+        });
+
+        console.log(formattedOption);
+
+        options.push(formattedOption);
+      });
+
+      const settings = [
+        form.state.maxOptions,
+        form.state.criteriaType === "Threshold" ? 0 : 1,
+        form.state.budget > 0
+          ? governanceTokenContract.address
+          : ethers.ZeroAddress,
+        form.state.criteriaType === "Threshold"
+          ? ethers.parseEther(form.state.threshold.toString())
+          : form.state.topChoices,
+        ethers.parseEther(form.state.budget.toString()),
+      ];
+
+      inputData = [
+        approvalModuleAddress,
+        abiCoder.encode(
+          [
+            "tuple(uint256,address[],uint256[],bytes[],string)[]",
+            "tuple(uint8,uint8,address,uint128,uint128)",
+          ],
+          [options, settings]
+        ),
+        description,
+        proposalSettings,
+      ];
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  return { governorFunction, inputData };
+}
+
+function encodeTransfer(to: string, amount: number): string {
+  return (
+    "0xa9059cbb" +
+    abiCoder
+      .encode(
+        ["address", "uint256"],
+        [ethers.getAddress(to), ethers.parseEther(amount.toString() || "0")]
+      )
+      .slice(2)
+  );
+}
