41e3d236b5fa6ab8540ff375d599a63b959c14c5
ferrodri <f.rodriguez.hervias@gmail.com>
2023-12-05 19:11:00 +0100

    migrate to listbox from headless ui

--- src/components/Delegates/DelegatesFilter/DelegatesFilter.jsx
+++ src/components/Delegates/DelegatesFilter/DelegatesFilter.jsx
@@ -1,49 +1,57 @@
 "use client";
 
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import { delegatesFilterOptions } from "@/lib/constants";
+import { Listbox } from "@headlessui/react";
+import { Fragment, useState, useEffect } from "react";
+import { ChevronDown } from "lucide-react";
 
 export default function DelegatesFilter() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const orderByParam = searchParams.get("orderBy");
-  const defaultValue = delegatesFilterOptions[orderByParam]?.value
-    ? orderByParam
-    : "weightedRandom";
+  const [selected, setSelected] = useState(orderByParam || "weightedRandom");
 
-  const handleChanges = (value) => {
-    value === "weightedRandom"
-      ? router.push("/delegates")
-      : router.push(`/delegates/?orderBy=${value}`);
-  };
+  useEffect(() => {
+    const handleChanges = (value) => {
+      value === "weightedRandom"
+        ? router.push("/delegates")
+        : router.push(`/delegates/?orderBy=${value}`);
+    };
+
+    handleChanges(selected);
+  }, [router, selected]);
 
   return (
-    <Select
-      onValueChange={(value) => handleChanges(value)}
-      defaultValue={defaultValue}
-    >
-      <SelectTrigger className="w-fit focus:ring-0 bg-[#F7F7F7] text-base font-medium border-none rounded-full py-2 px-4 flex items-center">
-        <SelectValue placeholder={defaultValue} />
-      </SelectTrigger>
-      <SelectContent className="bg-[#F7F7F7] p-2 rounded-[1rem]">
-        {Object.entries(delegatesFilterOptions).map(([key, value]) => (
-          <SelectItem
-            key={key}
-            value={key}
-            className="bg-white text-black py-2 px-3 border rounded-xl border-[#ebebeb] text-base box-border"
-          >
-            {value.value}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <Listbox as="div" value={selected} onChange={setSelected}>
+      {() => (
+        <>
+          <Listbox.Button className="w-fit bg-[#F7F7F7] text-base font-medium border-none rounded-full py-2 px-4 flex items-center">
+            {delegatesFilterOptions[selected]?.value || "Weighted Random"}
+            <ChevronDown className="h-4 w-4 ml-[2px] opacity-30 hover:opacity-100" />
+          </Listbox.Button>
+          <Listbox.Options className="mt-3 absolute bg-[#F7F7F7] border border-[#ebebeb] p-2 rounded-[1rem] flex flex-col gap-1">
+            {Object.entries(delegatesFilterOptions).map(([key, option]) => (
+              <Listbox.Option key={key} value={key} as={Fragment}>
+                {({ selected }) => {
+                  return (
+                    <li
+                      className={`cursor-pointer text-base py-2 px-3 border rounded-xl font-medium ${
+                        selected
+                          ? "text-black bg-white border-[#ebebeb]"
+                          : "text-[#66676b] border-transparent"
+                      }`}
+                    >
+                      {option.value}
+                    </li>
+                  );
+                }}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </>
+      )}
+    </Listbox>
   );
 }
