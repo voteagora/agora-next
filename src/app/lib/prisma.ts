83da73c37d1b6b9315861b6402f023815c2606ee
stepandel <stepandel@gmail.com>
2023-12-06 15:51:14 -0500

    Revert "updated prisma to use the new extensions (#25)"
    This reverts commit dfa221b021bff86e3376316bcfdcedbfeebe244a.

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,32 +1,28 @@
 import { PrismaClient } from "@prisma/client";
-import { performance } from "perf_hooks";
-import * as util from "util";
 
-let prisma: any;
+declare global {
+  var prisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  prisma = new PrismaClient().$extends({
-    query: {
-      $allModels: {
-        async $allOperations({ operation, model, args, query }) {
-          const start = performance.now();
-          const result = await query(args);
-          const end = performance.now();
-          const time = end - start;
-          console.log(
-            util.inspect(
-              { model, operation, args, time },
-              { showHidden: false, depth: null, colors: true }
-            )
-          );
-          return result;
-        },
-      },
-    },
-  });
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
+
+// Logging middleware
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+  const result = await next(params);
+  const after = Date.now();
+
+  return result;
+});
 
 export default prisma;
 
