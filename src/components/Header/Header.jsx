1fae9e0fb0cfce8cffea8a8549c86f0dc7c18d93
ferrodri <f.rodriguez.hervias@gmail.com>
2023-12-06 01:08:38 +0100

    mobile connectbutton done, refactor into one component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,25 +4,16 @@
 import Navbar from "./Navbar";
 import { HStack } from "../Layout/Stack";
 import LogoLink from "./LogoLink";
-import { useAccount } from "wagmi";
-import { MobileConnectButton } from "../ConnectButton/MobileConnectButton";
-import { DesktopConnectButton } from "../ConnectButton/DesktopConnectButton";
+import { ConnectButton } from "../ConnectButton/ConnectButton";
 
 export default function Header() {
-  const { address } = useAccount();
-
   return (
     <HStack className="main_header" justifyContent="justify-between">
       <LogoLink instance_name="Optimism" />
       <Navbar />
 
-      <HStack alignItems="center" gap="3" className="h-6">
-        <HStack justifyContent="center" className="hidden md:block">
-          <DesktopConnectButton />
-        </HStack>
-        <HStack justifyContent="center" className="block md:hidden">
-          <MobileConnectButton />
-        </HStack>
+      <HStack gap="3" className="h-6 items-center">
+        <ConnectButton />
       </HStack>
     </HStack>
   );
--- src/components/ProfileDropDown/ProfileDropDown.jsx
+++ src/components/ProfileDropDown/ProfileDropDown.jsx
@@ -1,36 +1,10 @@
-// TODO: replace mock data with real data from backend
-const delegate = {
-  address: {
-    resolvedName: {
-      address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-      name: "agora.eth",
-    },
-  },
-  delegateMetrics: {
-    tokenHoldersRepresentedCount: 50,
-  },
-  tokensRepresented: {
-    amount: "1000",
-  },
-  delegatingTo: {
-    address: {
-      resolvedName: {
-        address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-      },
-    },
-  },
-  amountOwned: {
-    amount: "50",
-  },
-};
-
 import { HStack, VStack } from "../Layout/Stack";
 import { useEffect, useState } from "react";
 import { TokenAmountDisplay } from "@/lib/utils";
 import { icons } from "@/icons/icons";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { pluralizeAddresses, shortAddress } from "@/lib/utils";
-import { Popover, Transition } from "@headlessui/react";
+import { Popover } from "@headlessui/react";
 import ENSAvatar from "../shared/ENSAvatar";
 import { PanelRow } from "../shared/PanelRow/PanelRow";
 import { useDisconnect } from "wagmi";
@@ -40,15 +14,16 @@ import { isAddressEqual } from "viem";
 import { useSIWE } from "connectkit";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { ProfileDropDownTransition } from "./ProfileDropDownTransition";
 
 const ValueWrapper = ({ children }) => (
   <div className="text-base">{children}</div>
 );
 
-export const ProfileDropDownButton = ({
+export const ProfileDropDown = ({
   address,
   ensName,
-  hasStatement,
+  delegate,
   handleSignOut,
 }) => {
   const router = useRouter();
@@ -78,10 +53,19 @@ export const ProfileDropDownButton = ({
 
   return (
     <Popover className="relative">
-      {({ close, open }) => (
+      {({ open }) => (
         <>
-          <Popover.Button className="outline-none py-2 px-5">
-            <HStack gap="1" className="items-center">
+          <Popover.Button className="outline-none mt-1 md:mt-auto md:py-2 md:px-5">
+            <HStack className="items-center flex md:hidden" gap="1">
+              <Image
+                height="24"
+                width="24"
+                className="opacity-[1]"
+                src={icons.walletConnected}
+                alt="connect wallet button"
+              />
+            </HStack>
+            <HStack gap="1" className="items-center hidden md:flex">
               {isSignedIn ? (
                 <Image
                   height="24"
@@ -113,24 +97,16 @@ export const ProfileDropDownButton = ({
             </AnimatePresence>
           )}
 
-          <Transition
-            className="absolute z-10 right-0"
-            enter="transition duration-00 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
-          >
+          <ProfileDropDownTransition>
             <Popover.Panel>
               {({ close }) => (
-                <div className="bg-white py-7 px-6 mt-2 rounded-[1rem] w-[350px]">
+                <div className="fixed md:relative bottom-0 left-0 rounded-tl-[1rem] rounded-tr-[1rem] md:rounded-[1rem] bg-white py-7 px-6 mt-2 w-[100vw] md:w-[350px]">
                   <VStack className="gap-3">
                     <HStack className="items-center gap-2 mb-1">
                       <div className="relative aspect-w-1 aspect-h-1">
                         {isSignedIn && (
                           <Image
-                            className="absolute z-[1] -bottom-[5px] -right-[7px]"
+                            className="absolute z-[1] -bottom-[2px] -right-[2px] md:-bottom-[5px] md:-right-[7px]"
                             width="24"
                             heoght="24"
                             src={icons.badge}
@@ -138,7 +114,7 @@ export const ProfileDropDownButton = ({
                           />
                         )}
                         <ENSAvatar
-                          className="rounded-full w-[44px] h-[44px]"
+                          className="rounded-full w-[51px] h-[51px] md:w-[44px] md:h-[44px]"
                           fragment={delegate.address.resolvedName}
                         />
                       </div>
@@ -146,7 +122,7 @@ export const ProfileDropDownButton = ({
                         {delegate.address.resolvedName.name ? (
                           <>
                             <span className="text-base">{ensName}</span>
-                            <span className="text-xs text-[#4f4f4f]">
+                            <span className="text-base md:text-xs text-black md:text-[#4f4f4f]">
                               {shortAddress(
                                 delegate.address.resolvedName.address
                               )}
@@ -166,12 +142,13 @@ export const ProfileDropDownButton = ({
                       <Image
                         src={icons.power}
                         onClick={(e) => {
-                          if (handleSignOut) handleSignOut();
+                          handleSignOut && handleSignOut();
                           disconnect();
                         }}
                         alt="Disconnect Wallet"
                         width="32"
                         height="32"
+                        className="mr-[5px] md:mr-auto"
                       />
                     </HStack>
 
@@ -250,7 +227,7 @@ export const ProfileDropDownButton = ({
                       }
                       className="rounded-lg border py-3 px-2 text-gray-200 bg-black flex justify-center mt-1 hover:bg-gray-800"
                     >
-                      {hasStatement
+                      {delegate.statement
                         ? "Edit delegate statement"
                         : "Create delegate statement"}
                     </button>
@@ -261,13 +238,13 @@ export const ProfileDropDownButton = ({
                         onClick={() =>
                           navigateAndClosePopover("/retroPGF/3/ballot", close)
                         }
-                        className="rounded-lg border py-3 px-2 text-black bg-white mt-1 flex justify-center hover:bg-gray-800 bg:text-white"
+                        className="rounded-lg border py-3 px-2 text-black bg-white mt-1 hidden md:flex justify-center hover:bg-gray-800 hover:text-white"
                       >
                         View my ballot
                       </button>
                     )}
 
-                    {hasStatement && (
+                    {delegate.statement && (
                       <button
                         onClick={() =>
                           navigateAndClosePopover(
@@ -278,7 +255,7 @@ export const ProfileDropDownButton = ({
                             close
                           )
                         }
-                        className="rounded-lg border py-3 px-2 text-black bg-white mt-1 flex justify-center hover:bg-gray-800 bg:text-white"
+                        className="rounded-lg border py-3 px-2 text-black bg-white mt-1 flex justify-center hover:bg-gray-800 hover:text-white"
                       >
                         View my profile
                       </button>
@@ -287,7 +264,7 @@ export const ProfileDropDownButton = ({
                 </div>
               )}
             </Popover.Panel>
-          </Transition>
+          </ProfileDropDownTransition>
         </>
       )}
     </Popover>
