ec27abc3451c7b06b13feda8bf904dc57cd1a0d3
Dom-Mac <domenicochad@gmail.com>
2023-12-11 19:42:47 +0100

    fix: broken style in proposal creation page

--- src/components/shared/Switch.tsx
+++ src/components/shared/Switch.tsx
@@ -9,29 +9,29 @@ type Props = {
   options: string[];
 };
 
+const optionStyle = styles.switch__option;
+
+const selectedStyle = styles.switch__selected;
+
 export function Switch({ onSelectionChanged, selection, options }: Props) {
   return (
     <HStack className={styles.switch} gap={2}>
       <div
         onClick={() => onSelectionChanged(options[0])}
-        className={
-          optionStyle + " " + selection === options[0] ? selectedStyle : ""
-        }
+        className={`${optionStyle}  ${
+          selection === options[0] ? selectedStyle : ""
+        }`}
       >
         {options[0]}
       </div>
       <div
         onClick={() => onSelectionChanged(options[1])}
-        className={
-          optionStyle + " " + selection === options[1] ? selectedStyle : ""
-        }
+        className={`${optionStyle}  ${
+          selection === options[1] ? selectedStyle : ""
+        }`}
       >
         {options[1]}
       </div>
     </HStack>
   );
 }
-
-const optionStyle = styles.switch__option;
-
-const selectedStyle = styles.switch_selected;
