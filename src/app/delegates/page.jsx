56f1e040bce8f1ed5974ff4c75984ea482bcf8b1
Ferrodri <f.rodriguez.hervias@gmail.com>
2023-12-11 07:23:00 +0100

    search delegates done (#31)

--- src/app/delegates/page.jsx
+++ src/app/delegates/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { getDelegates } from "../api/delegates/getDelegates";
-import DelegateCardList from "../../components/Delegates/DelegateCardList/DelegateCardList";
-import DelegatesFilter from "../../components/Delegates/DelegatesFilter/DelegatesFilter";
+import DelegateCardList from "@/components/Delegates/DelegateCardList/DelegateCardList";
+import DelegatesFilter from "@/components/Delegates/DelegatesFilter/DelegatesFilter";
+import DelegatesSearch from "@/components/Delegates/DelegatesSearch/DelegatesSearch";
 import PageHeader from "@/components/Layout/PageHeader/PageHeader";
-import { HStack } from "@/components/Layout/Stack";
 import { delegatesFilterOptions } from "@/lib/constants";
 
 async function fetchDelegates(sort, page = 1, seed) {
@@ -20,10 +20,13 @@ export default async function Page({ searchParams }) {
 
   return (
     <section>
-      <HStack className="justify-between items-baseline">
+      <div className="flex flex-col md:flex-row justify-between items-baseline gap-2">
         <PageHeader headerText="All Delegates" />
-        <DelegatesFilter />
-      </HStack>
+        <div className="flex flex-col md:flex-row justify-between gap-4 w-full md:w-fit">
+          <DelegatesSearch />
+          <DelegatesFilter />
+        </div>
+      </div>
       <DelegateCardList
         initialDelegates={delegates}
         fetchDelegates={async (page) => {
---
+++ src/components/Delegates/DelegatesSearch/DelegatesSearch.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { TextInputWithTooltip } from "@/components/shared/Form/TextInputWithTooltip";
+import { VStack } from "@/components/Layout/Stack";
+import { useState } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { useRouter } from "next/navigation";
+
+export default function DelegatesSearch() {
+  const router = useRouter();
+  const [enteredName, setEnteredName] = useState("");
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        router.push(`/delegates/${enteredName}`);
+      }}
+    >
+      <VStack className="relative">
+        <VStack className="justify-center absolute left-0 top-0 bottom-0 p-2">
+          <MagnifyingGlassIcon className="text-gray-4f w-4 h-4" />
+        </VStack>
+        <TextInputWithTooltip
+          onChange={(value) => setEnteredName(value)}
+          placeholder="Exact ENS or address"
+          tooltipMessage="Please input exact ENS or address. Partial and fuzzy search is not supported yet."
+          className="py-2 px-4 pl-8 rounded-full bg-gray-fa border border-[#ebebeb] w-full 2xl:w-auto placeholder-gray-af"
+        />
+      </VStack>
+    </form>
+  );
+}
