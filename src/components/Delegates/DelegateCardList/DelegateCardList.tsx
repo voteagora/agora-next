da59fcc175fef2b9cbd5ffcb69a954b4b49d75a7
Dom-Mac <domenicochad@gmail.com>
2023-12-13 18:14:19 +0100

    fix: delegateCartList + feat: isAdvancedUser

--- src/components/Delegates/DelegateCardList/DelegateCardList.tsx
+++ src/components/Delegates/DelegateCardList/DelegateCardList.tsx
@@ -10,6 +10,35 @@ import { DelegateProfileImage } from "../DelegateCard/DelegateProfileImage";
 import styles from "./DelegateCardList.module.scss";
 import { useRouter } from "next/navigation";
 import { DialogProvider } from "@/components/Dialogs/DialogProvider/DialogProvider";
+import { Delegate } from "@/app/api/delegates/delegate";
+import { useAccount, useContractRead } from "wagmi";
+import { useAgoraContext } from "@/contexts/AgoraContext";
+import { OptimismContracts } from "@/lib/contracts/contracts";
+import { parseUnits } from "viem";
+
+export type DelegateChunk = Pick<
+  Delegate,
+  "address" | "votingPower" | "statement"
+>;
+
+interface DelegatePaginated {
+  meta: any;
+  delegates: DelegateChunk[];
+}
+
+interface Props {
+  initialDelegates: DelegatePaginated;
+  fetchDelegates: (page: number) => Promise<DelegatePaginated>;
+  fetchBalanceForDirectDelegation: (
+    addressOrENSName: string
+  ) => Promise<string>;
+  fetchVotingPowerForSubdelegation: (
+    addressOrENSName: string
+  ) => Promise<string>;
+  checkIfDelegatingToProxy: (addressOrENSName: string) => Promise<boolean>;
+  fetchCurrentDelegatees: (addressOrENSName: string) => Promise<any>;
+  getProxyAddress: (addressOrENSName: string) => Promise<string>;
+}
 
 export default function DelegateCardList({
   initialDelegates,
@@ -19,27 +48,51 @@ export default function DelegateCardList({
   checkIfDelegatingToProxy,
   fetchCurrentDelegatees,
   getProxyAddress,
-}) {
+}: Props) {
   const router = useRouter();
   const fetching = React.useRef(false);
   const [pages, setPages] = React.useState([initialDelegates] || []);
   const [meta, setMeta] = React.useState(initialDelegates.meta);
+  const [isAdvancedUser, setIsAdvancedUser] = React.useState(false);
+  const allowList = [] as `0x${string}`[];
+  const { isConnected } = useAgoraContext();
+  const { address } = useAccount();
+
+  useContractRead({
+    address: OptimismContracts.token.address as `0x${string}`,
+    abi: OptimismContracts.token.abi,
+    functionName: "balanceOf",
+    enabled: isConnected && !!address,
+    args: [address!],
+    onSuccess: (balance) => {
+      // TODO: change allowed balance to 10K OP
+      const allowedBalance = parseUnits("1", 18);
+      setIsAdvancedUser(
+        balance >= allowedBalance || allowList.includes(address!)
+      );
+    },
+  });
 
   useEffect(() => {
     setPages([initialDelegates]);
     setMeta(initialDelegates.meta);
   }, [initialDelegates]);
 
-  const handleClick = (e, href) => {
+  const handleClick = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    href: string
+  ) => {
     e.preventDefault();
     router.push(href);
   };
 
-  const loadMore = async (page) => {
+  const loadMore = async (page: any) => {
     if (!fetching.current && meta.hasNextPage) {
       fetching.current = true;
       const data = await fetchDelegates(page);
-      const existingIds = new Set(delegates.map((d) => d.address));
+      const existingIds = new Set(
+        pages.flatMap((page) => page.delegates.map((d) => d.address))
+      );
       const uniqueDelegates = data.delegates.filter(
         (d) => !existingIds.has(d.address)
       );
@@ -49,7 +102,10 @@ export default function DelegateCardList({
     }
   };
 
-  const delegates = pages.reduce((all, page) => all.concat(page.delegates), []);
+  const delegates = pages.reduce(
+    (all: DelegateChunk[], page) => all.concat(page.delegates),
+    []
+  );
 
   return (
     <DialogProvider>
@@ -82,26 +138,26 @@ export default function DelegateCardList({
           }
 
           return (
-            <div
-              key={delegate.address}
-              onClick={(e) => handleClick(e, `/delegates/${delegate.address}`)}
-              className={styles.link}
-            >
+            <div key={delegate.address} className={styles.link}>
               <VStack className={styles.link_container}>
-                <VStack gap="4" className="h-full">
-                  <VStack justifyContent="center">
-                    <DelegateProfileImage
-                      address={delegate.address}
-                      votingPower={delegate.votingPower}
-                    />
-                  </VStack>
-                  <p className={styles.summary}>{truncatedStatement}</p>
+                <VStack gap={4} className="h-full">
+                  <div
+                    onClick={(e) =>
+                      handleClick(e, `/delegates/${delegate.address}`)
+                    }
+                  >
+                    <VStack gap={4} justifyContent="justify-center">
+                      <DelegateProfileImage
+                        address={delegate.address}
+                        votingPower={delegate.votingPower}
+                      />
+                      <p className={styles.summary}>{truncatedStatement}</p>
+                    </VStack>
+                  </div>
                   <div className="flex-grow" />
                   <DelegateActions
-                    address={delegate.address}
-                    votingPower={delegate.votingPower}
-                    discord={delegate?.statement?.discord}
-                    twitter={delegate?.statement?.twitter}
+                    delegate={delegate}
+                    isAdvancedUser={isAdvancedUser}
                     fetchBalanceForDirectDelegation={
                       fetchBalanceForDirectDelegation
                     }
