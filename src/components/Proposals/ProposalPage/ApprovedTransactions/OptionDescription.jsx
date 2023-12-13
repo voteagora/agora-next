a76dab899cb3d76af624b81fe0f807fdeaab7729
Dom-Mac <domenicochad@gmail.com>
2023-12-13 12:55:40 +0100

    fix: display token amount

--- src/components/Proposals/ProposalPage/ApprovedTransactions/OptionDescription.jsx
+++ src/components/Proposals/ProposalPage/ApprovedTransactions/OptionDescription.jsx
@@ -1,11 +1,14 @@
+import TokenAmountDisplay from "@/components/shared/TokenAmountDisplay";
 import { shortAddress } from "@/lib/utils";
 
 export default function OptionDescription({ description, value, target }) {
-  
+  console.log(value);
   return (
     <span>
       {/* TODO: Warning – this dangerously assumes all tokens are OP. For now, ok, but won't always be the case */}
-      {"//"} {description} requesting {value} OP transfer to {shortAddress(target)}
+      {"//"} {description} requesting{" "}
+      <TokenAmountDisplay amount={value} decimals={18} currency="OP" /> transfer
+      to {shortAddress(target)}
     </span>
   );
 }
--- src/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesSummary/ProposalVotesSummary.tsx
+++ src/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesSummary/ProposalVotesSummary.tsx
@@ -1,24 +1,48 @@
 import { VStack, HStack } from "@/components/Layout/Stack";
 import styles from "./proposalVotesSummary.module.scss";
-import { TokenAmountDisplay } from "@/lib/utils";
 import ProposalTimeStatus from "@/components/Proposals/Proposal/ProposalTimeStatus";
 import ProposalVotesBar from "../ProposalVotesBar/ProposalVotesBar";
+import { Proposal } from "@/app/api/proposals/proposal";
+import TokenAmountDisplay from "@/components/shared/TokenAmountDisplay";
+import { ParsedProposalResults } from "@/lib/proposalUtils";
 
-export default function ProposalVotesSummary({ proposal }) {
+export default function ProposalVotesSummary({
+  proposal,
+}: {
+  proposal: Proposal;
+}) {
+  const results =
+    proposal.proposalResults as ParsedProposalResults["STANDARD"]["kind"];
   return (
     <VStack gap={2} className={styles.proposal_votes_summary_container}>
       <HStack justifyContent="justify-between" className="mt-2">
         <div className="gl_votes_for">
-          FOR {TokenAmountDisplay(proposal.proposalResults.for, 18, "OP")}
+          FOR{" "}
+          <TokenAmountDisplay
+            amount={results.for}
+            decimals={18}
+            currency={"OP"}
+          />
         </div>
         <div className="gl_votes_against">
           AGAINST{" "}
-          {TokenAmountDisplay(proposal.proposalResults.against, 18, "OP")}
+          <TokenAmountDisplay
+            amount={results.against}
+            decimals={18}
+            currency={"OP"}
+          />
         </div>
       </HStack>
       <ProposalVotesBar proposal={proposal} />
       <HStack justifyContent="justify-between" className="text-gray-4f">
-        <div>QUORUM {TokenAmountDisplay(proposal.quorum, 18, "OP")}</div>
+        <div>
+          QUORUM{" "}
+          <TokenAmountDisplay
+            amount={proposal.quorum}
+            decimals={18}
+            currency={"OP"}
+          />
+        </div>
         <ProposalTimeStatus
           proposalStatus={proposal.status}
           proposalEndTime={proposal.end_time}
