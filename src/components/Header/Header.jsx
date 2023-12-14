4a5301afb685d14633c435ce2f05b3e983c85d19
Kent Fenwick <kent.fenwick@gmail.com>
2023-12-14 14:03:38 -0500

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 // Header component
 import Navbar from "./Navbar";
-import { HStack } from "../Layout/Stack";
+import { HStack, VStack } from "../Layout/Stack";
 import LogoLink from "./LogoLink";
 import styles from "./header.module.scss";
 import { ConnectKitButton } from "connectkit";
 
 export default function Header() {
   return (
-    <HStack className="main_header" justifyContent="justify-between">
-      <LogoLink instance_name="Optimism" />
-      <Navbar />
-      <ConnectButton />
-    </HStack>
+    <VStack>
+      <HStack className="main_header" justifyContent="justify-between">
+        <LogoLink instance_name="Optimism" />
+        <Navbar />
+        <ConnectButton />
+      </HStack>
+    </VStack>
   );
 }
 
