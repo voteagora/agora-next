4f40900f300a3a308f0692315aa051c4d553cb75
Dom-Mac <domenicochad@gmail.com>
2023-12-14 11:28:22 +0100

    fix: default prop to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
 
-export function Button({ href, ...props }) {
+export function Button({ href = "", ...props }) {
   return (
     <div className={`${styles.button} ${props.className || ""}`}>
       {href ? <Link href={href} {...props} /> : <button {...props} />}
