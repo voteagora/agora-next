68864049f0fec5bc4e29ca29b6ead60007192775
Dom-Mac <domenicochad@gmail.com>
2023-12-14 20:32:24 +0100

    feat: sub delegation form

--- src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
+++ src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
@@ -1,13 +1,26 @@
 import { Delegation } from "@/app/api/delegations/delegation";
 import { HStack } from "@/components/Layout/Stack";
 import HumanAddress from "@/components/shared/HumanAddress";
+import { Input } from "@/components/ui/input";
 import { TokenAmountDisplay } from "@/lib/utils";
 
-function SubdelegationToRow({ to, amount }: { to: string; amount: string }) {
+function SubdelegationToRow({
+  to,
+  allowance,
+  setAllowance,
+}: {
+  to: string;
+  allowance: number;
+  setAllowance: (value: number) => void;
+}) {
   return (
     <HStack gap={2}>
       <HumanAddress address={to} />
-      <div>{TokenAmountDisplay(amount, 18, "OP", 6)}</div>
+      <Input
+        value={allowance}
+        onChange={(e) => setAllowance(parseInt(e.target.value))}
+        type="number"
+      />
     </HStack>
   );
 }
