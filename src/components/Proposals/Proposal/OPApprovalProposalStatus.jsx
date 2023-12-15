c797c67c07fed9380cb5ddaa9c6530f13ef17519
Yitong <zha.yitong@gmail.com>
2023-12-15 08:06:28 -0800

    added progress bars drafts to proposals list

--- src/components/Proposals/Proposal/OPApprovalProposalStatus.jsx
+++ src/components/Proposals/Proposal/OPApprovalProposalStatus.jsx
@@ -3,6 +3,8 @@ import styles from "./proposal.module.scss";
 import { pluralize } from "@/lib/utils";
 
 export default function OPApprovalProposalStatus({ proposal }) {
+  console.log(proposal.description);
+  console.log(proposal.proposalData);
   return (
     <HStack className={styles.proposal_status} gap={1}>
       {pluralize("Choice", proposal.proposalData.options.length)}
