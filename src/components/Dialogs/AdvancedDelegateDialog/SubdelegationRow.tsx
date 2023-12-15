73d1b1ba31a6972afa05c7e542f15ac176a889a1
Dom-Mac <domenicochad@gmail.com>
2023-12-15 23:16:09 +0100

    feat: sub delegation row

--- src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
+++ src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
@@ -1,25 +1,79 @@
-import { HStack } from "@/components/Layout/Stack";
+import { HStack, VStack } from "@/components/Layout/Stack";
+import ENSAvatar from "@/components/shared/ENSAvatar";
 import HumanAddress from "@/components/shared/HumanAddress";
 import { Input } from "@/components/ui/input";
+import styles from "./advancedDelegateDialog.module.scss";
+import { useEnsName } from "wagmi";
+import { formatUnits } from "viem";
+import { Dispatch, SetStateAction } from "react";
 
 function SubdelegationToRow({
   to,
-  allowance,
   setAllowance,
+  availableBalance,
+  allowances,
+  index,
 }: {
   to: string;
-  allowance: number;
-  setAllowance: (value: number) => void;
+  setAllowance: Dispatch<SetStateAction<number[]>>;
+  availableBalance: string;
+  allowances: number[];
+  index: number;
 }) {
+  const allowance = allowances[index];
+  const { data } = useEnsName({
+    chainId: 1,
+    address: to as `0x${string}`,
+  });
+  const availableBalanceNumber = parseInt(
+    formatUnits(BigInt(availableBalance), 18)
+  );
+
+  const sumOtherAllowances = allowances.reduce((sum, current, idx) => {
+    return idx === index ? sum : sum + current;
+  }, 0);
+
+  const amountToAllocate = availableBalanceNumber - sumOtherAllowances;
+
+  const percent =
+    Number.isNaN(allowance) || allowance === 0
+      ? 0
+      : Math.round(((allowance / availableBalanceNumber) * 100_000) / 1000);
+
   return (
-    <HStack gap={2}>
-      <HumanAddress address={to} />
-      <Input
-        value={allowance}
-        onChange={(e) => setAllowance(parseInt(e.target.value))}
-        type="number"
-      />
-    </HStack>
+    <div className={styles.sub_row}>
+      <HStack>
+        <div className={styles.profile_image}>
+          <ENSAvatar ensName={data} />
+        </div>
+        <div>
+          <p>Delegated to</p>
+          <HumanAddress address={to} />
+        </div>
+      </HStack>
+      <div className="relative">
+        <Input
+          value={allowance}
+          className={styles.sub_row_input}
+          onChange={(e) => {
+            let newAllowanceValue = parseInt(e.target.value, 10);
+            if (newAllowanceValue > amountToAllocate) {
+              newAllowanceValue = amountToAllocate;
+            }
+            const newAllowances = [...allowances];
+            newAllowances[index] = newAllowanceValue;
+            setAllowance(newAllowances);
+          }}
+          type="number"
+          min={0}
+          max={amountToAllocate}
+        />
+
+        <p className={styles.sub_row_percent}>
+          OP | <span>{percent}%</span>
+        </p>
+      </div>
+    </div>
   );
 }
 
