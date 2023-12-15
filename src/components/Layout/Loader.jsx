aa1d582718204ecfebacc88841e7b4395e92a777
Yitong <zha.yitong@gmail.com>
2023-12-15 08:06:28 -0800

    added progress bars drafts to proposals list

--- src/components/Layout/Loader.jsx
+++ src/components/Layout/Loader.jsx
@@ -3,13 +3,17 @@ import { HStack } from "./Stack";
 
 export default function Loader() {
   return (
-    <HStack key="loader" className="gl_loader justify-center py-5">
-      <Image
+    <HStack
+      key="loader"
+      className="gl_loader justify-center py-4 text-sm text-stone-500"
+    >
+      {/* <Image
         src="/images/blink.gif"
         alt="Blinking Agora Logo"
         width={32}
         height={8}
-      />
+      /> */}
+      Loading more items
     </HStack>
   );
 }
