eda20c83fedfacd806b0e59e4b8a314201f174f9
Yitong <zha.yitong@gmail.com>
2023-12-17 00:02:48 -0800

    mobile web ok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className=" text-stone-500 pt-4 pb-8 gap-1 flex w-full justify-center text-sm">
+    <footer className=" text-stone-500 pt-4 pb-8 gap-1 flex w-full justify-center text-sm px-4">
       <div className="text-stone-300">&copy;</div>
-      <p>{new Date().getFullYear()} Agora, the onchain governance company</p>
+      <p>
+        {new Date().getFullYear()} Agora
+        <span className="hidden lg:inline">
+          , the onchain governance company
+        </span>
+      </p>
       <div className="text-stone-300">/</div>
       <a
         href="https://twitter.com/home"
