fcd876afa5b9fa0ef24baee9b6ca9229fce67afd
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2023-12-19 00:56:38 +0100

    fix: pagination in homepage

--- src/lib/proposalUtils.ts
+++ src/lib/proposalUtils.ts
@@ -189,7 +189,7 @@ export async function parseProposal(
   const getQuorum = async () => {
     let quorum = await getQuorumForProposal(proposal);
 
-    if (process.env.NEXT_PUBLIC_AGORA_ENV !== "prod" && !quorum) {
+    if (!quorum) {
       const votableSupply = await prisma.votableSupply.findFirst({});
 
       quorum = (BigInt(Number(votableSupply?.votable_supply)) * 30n) / 100n;
