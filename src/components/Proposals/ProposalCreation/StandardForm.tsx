3cd31d7e93390e387a76decc62784660548cde61
Yitong <zha.yitong@gmail.com>
2023-12-18 18:50:52 -0800

    first pass at styling proposal creation form

--- src/components/Proposals/ProposalCreation/StandardForm.tsx
+++ src/components/Proposals/ProposalCreation/StandardForm.tsx
@@ -8,7 +8,7 @@ import styles from "./styles.module.scss";
 function StandardForm({ form }: { form: Form }) {
   return (
     <VStack>
-      <h4 className={styles.create_prop_form__heading}>Proposed transaction</h4>
+      <h4 className={styles.create_prop_form__title}>Proposed transaction</h4>
       <p className={styles.standard__text}>
         Proposed transactions will execute if your proposal passes. If you skip
         this step, a transfer of 0 ETH to the 0 address will be added.
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
-import { LoaderIcon } from "lucide-react"
+import { cn } from "@/lib/utils";
+import { LoaderIcon } from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -18,32 +18,32 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline"
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-7 rounded-md px-4",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10"
-      }
+        icon: "h-10 w-10",
+      },
     },
     defaultVariants: {
       variant: "default",
-      size: "default"
-    }
+      size: "default",
+    },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  loading?: boolean
-  asChild?: boolean
+  loading?: boolean;
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, loading, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
@@ -56,9 +56,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           props.children
         )}
       </Comp>
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
