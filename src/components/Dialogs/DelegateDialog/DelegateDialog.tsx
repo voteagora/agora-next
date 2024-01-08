0c3b2e50a7812e30e5f9c822c11ba9df1add981d
Dom-Mac <domenicochad@gmail.com>
2024-01-08 23:53:26 +0100

    feat: new design for direct delegation

--- src/components/Dialogs/DelegateDialog/DelegateDialog.tsx
+++ src/components/Dialogs/DelegateDialog/DelegateDialog.tsx
@@ -1,8 +1,7 @@
-import { useAccount, useBalance, useContractWrite } from "wagmi";
+import { useAccount, useBalance, useContractWrite, useEnsName } from "wagmi";
 import { ArrowDownIcon } from "@heroicons/react/20/solid";
-import { VStack } from "@/components/Layout/Stack";
+import { HStack, VStack } from "@/components/Layout/Stack";
 import { OptimismContracts } from "@/lib/contracts/contracts";
-import ENSName from "@/components/shared/ENSName";
 import { DelegationDisplayAmount } from "./DelegationDisplayAmount";
 import { Button } from "@/components/Button";
 import styles from "./delegateDialog.module.scss";
@@ -12,7 +11,10 @@ import { DelegateChunk } from "@/components/Delegates/DelegateCardList/DelegateC
 import { useCallback, useEffect, useState } from "react";
 import { Delegatees } from "@prisma/client";
 import { AgoraLoaderSmall } from "@/components/shared/AgoraLoader/AgoraLoader";
-import HumanAddress from "@/components/shared/HumanAddress";
+import ENSAvatar from "@/components/shared/ENSAvatar";
+import ENSName from "@/components/shared/ENSName";
+import { InfoIcon } from "lucide-react";
+import { AdvancedDelegationDisplayAmount } from "../AdvancedDelegateDialog/AdvancedDelegationDisplayAmount";
 
 export function DelegateDialog({
   delegate,
@@ -33,6 +35,16 @@ export function DelegateDialog({
   const [delegatee, setDelegatee] = useState<Delegatees | null>(null);
   const [isReady, setIsReady] = useState(false);
 
+  const { data: delegateEnsName } = useEnsName({
+    chainId: 1,
+    address: delegate.address as `0x${string}`,
+  });
+
+  const { data: delegateeEnsName } = useEnsName({
+    chainId: 1,
+    address: delegatee?.delegatee as `0x${string}`,
+  });
+
   const { isLoading, isSuccess, isError, write } = useContractWrite({
     address: OptimismContracts.token.address as any,
     abi: OptimismContracts.token.abi,
@@ -49,6 +61,7 @@ export function DelegateDialog({
 
     const direct = await fetchDirectDelegatee(accountAddress);
     setDelegatee(direct);
+
     setIsReady(true);
   }, [fetchBalanceForDirectDelegation, accountAddress, fetchDirectDelegatee]);
 
@@ -77,51 +90,51 @@ export function DelegateDialog({
             alignItems="items-center"
             className={styles.details_container}
           >
-            <VStack
-              className={styles.amount_container}
-              alignItems="items-center"
-              gap={3}
-            >
-              {(() => {
-                if (!votingPower) {
-                  return <div>{`You don't have any tokens to delegate`}</div>;
-                }
-                return (
-                  <>
-                    <div>Delegating your</div>
-
-                    <DelegationDisplayAmount amount={votingPower} />
-                  </>
-                );
-              })()}
-            </VStack>
-
-            <VStack
-              className={styles.transfer_symbol_container}
-              alignItems="items-center"
-            >
-              <VStack
-                justifyContent="justify-center"
-                className={styles.circle_container}
-              >
-                <div className={styles.circle} />
-              </VStack>
-
-              <VStack className={styles.arrow_container}>
-                <ArrowDownIcon className="text-black" />
-              </VStack>
-            </VStack>
-
             <VStack className={styles.amount_container}>
-              <div className="text-center">
-                To <ENSName address={delegate.address} /> who represents
+              <HStack alignItems="items-center" gap={1}>
+                Your total delegatable votes{" "}
+                <InfoIcon size={12} className="opacity-50" />
+              </HStack>
+              <AdvancedDelegationDisplayAmount amount={votingPower} />
+            </VStack>
+            <VStack className="relative w-full">
+              <HStack
+                gap={3}
+                alignItems="items-center"
+                className="p-2 pb-4 pl-0 border-b border-gray-300"
+              >
+                <div className={styles.avatar}>
+                  <ENSAvatar ensName={delegateeEnsName} />
+                </div>
+                <VStack>
+                  <p className={styles.subtitle}>Currently delegated to</p>
+                  <div className={styles.address_to}>
+                    <ENSName address={delegatee.delegatee} />
+                  </div>
+                </VStack>
+              </HStack>
+              <div className="absolute flex items-center justify-center w-10 h-10 translate-x-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full right-1/2 top-1/2">
+                <ArrowDownIcon className="w-4 h-4 text-black" />
               </div>
-
-              {/* <DelegationDisplayAmount amount={votingPower} /> */}
+              <HStack
+                gap={3}
+                alignItems="items-center"
+                className="p-2 pt-4 pl-0"
+              >
+                <div className={styles.avatar}>
+                  <ENSAvatar ensName={delegateEnsName} />
+                </div>
+                <VStack>
+                  <p className={styles.subtitle}>Delegating to</p>
+                  <div className={styles.address_to}>
+                    <ENSName address={delegate.address} />
+                  </div>
+                </VStack>
+              </HStack>
             </VStack>
           </VStack>
         ) : (
-          <div>
+          <VStack gap={4}>
             <p className="text-xl font-bold text-left">
               Set <ENSName address={delegate.address} /> as your delegate
             </p>
@@ -129,7 +142,38 @@ export function DelegateDialog({
               <ENSName address={delegate.address} /> will be able to vote with
               any token owned by your address
             </div>
-          </div>
+            <VStack className="relative border border-gray-300 rounded-lg">
+              <HStack
+                gap={3}
+                alignItems="items-center"
+                className="p-2 border-b border-gray-300"
+              >
+                <div className={styles.avatar}>
+                  <ENSAvatar ensName={""} />
+                </div>
+                <VStack>
+                  <p className={styles.subtitle}>Currently delegated to</p>
+                  <div className={styles.address_to}>
+                    <p>N/A</p>
+                  </div>
+                </VStack>
+              </HStack>
+              <div className="w-10 h-10 flex items-center justify-center bg-white border border-gray-300 rounded-full absolute right-4 top-[50%] translate-y-[-50%]">
+                <ArrowDownIcon className="w-4 h-4 text-black" />
+              </div>
+              <HStack gap={3} alignItems="items-center" className="p-2">
+                <div className={styles.avatar}>
+                  <ENSAvatar ensName={delegateEnsName} />
+                </div>
+                <VStack>
+                  <p className={styles.subtitle}>Delegating to</p>
+                  <div className={styles.address_to}>
+                    <ENSName address={delegate.address} />
+                  </div>
+                </VStack>
+              </HStack>
+            </VStack>
+          </VStack>
         )}
         {!accountAddress && (
           <ShadcnButton variant="outline" onClick={() => setOpen(true)}>
@@ -146,9 +190,7 @@ export function DelegateDialog({
           </Button>
         )}
         {!isError && !isSuccess && !isLoading && accountAddress && (
-          <Button disabled={false} onClick={() => write()}>
-            Delegate your votes
-          </Button>
+          <ShadcnButton onClick={() => write()}>Delegate</ShadcnButton>
         )}
       </VStack>
     </VStack>
