eb74642bfbea7fa3a0f7e0543c67cae2e3e7cbf7
ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-09 00:29:45 +0100

    fix no default instance token

--- src/lib/alligatorUtils.ts
+++ src/lib/alligatorUtils.ts
@@ -1,14 +1,13 @@
 import { DEPLOYMENT_NAME } from "./config";
 import { OptimismContracts } from "./contracts/contracts";
 
-// TODO: improve default logic
 export async function getProxyAddress(address: string) {
   switch (DEPLOYMENT_NAME) {
     case "optimism": {
       return OptimismContracts.alligator.contract.proxyAddress(address);
     }
     default: {
-      return OptimismContracts.alligator.contract.proxyAddress(address);
+      throw new Error("Can't find Agora Instance token");
     }
   }
 }
