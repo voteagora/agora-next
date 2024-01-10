3a2c3a9dccf657d3e834eb0cca1eb79918b31ce1
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2024-01-10 16:25:21 +0100

    remove mock proposal types

--- src/app/admin/components/AdminForm.tsx
+++ src/app/admin/components/AdminForm.tsx
@@ -1,14 +1,16 @@
 import ProposalTypeSettings from "./ProposalTypeSettings";
 import GovernorSettings from "./GovernorSettings";
 import FAQs from "./FAQs";
+import { getProposalTypes } from "@/app/api/proposals/getProposals";
 
 // TODO: Take init values from the chain
-export default function AdminForm() {
+export default async function AdminForm() {
+  const proposalTypes = await getProposalTypes();
   return (
     <div className="space-y-8 lg:space-y-0 lg:flex lg:gap-12 mt-12">
       <div className="space-y-8 lg:flex-grow">
         <GovernorSettings />
-        <ProposalTypeSettings />
+        <ProposalTypeSettings initProposalTypes={proposalTypes} />
       </div>
       <FAQs />
     </div>
