35016bebc35140f665033a1672ac6e1f61b73064
wojtekwtf <wojciechkulikowski@icloud.com>
2024-01-10 22:04:50 +0100

    Use Link next compontent

--- src/components/Votes/VoterCard.tsx
+++ src/components/Votes/VoterCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { VStack, HStack } from "@/components/Layout/Stack";
 import { DelegateActions } from "../Delegates/DelegateCard/DelegateActions";
-import { DelegateProfileImage } from '../Delegates/DelegateCard/DelegateProfileImage';
+import { DelegateProfileImage } from "../Delegates/DelegateCard/DelegateProfileImage";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface Props {
   address: string;
@@ -31,7 +32,6 @@ export default function VoterCard({
   getProxyAddress,
   isAdvancedUser,
 }: Props) {
-
   const router = useRouter();
   const [delegate, setDelegate] = useState<any>();
   const [loading, setLoading] = useState<boolean>(true);
@@ -48,50 +48,44 @@ export default function VoterCard({
     const delegate = await fetchDelegate(addressOrENSName);
     setLoading(false);
     setDelegate(delegate);
-  }
+  };
 
   useEffect(() => {
     fetchDelegateAndSet(address);
   }, []);
 
-return (
-  <>
-  {loading ? (
-    <p>Loading...</p>
-  ) : (
-    <VStack gap={4} className="h-full w-[300px]">
-      <div
-        onClick={(e) =>
-          handleClick(e, `/delegates/${delegate.address}`)
-        }
-      >
-        <VStack gap={4} justifyContent="justify-center">
-          <DelegateProfileImage
-            address={delegate.address}
-            votingPower={delegate.votingPower}
+  return (
+    <>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <VStack gap={4} className="h-full w-[300px]">
+          <Link href={`/delegates/${delegate.address}`}>
+            <VStack gap={4} justifyContent="justify-center">
+              <DelegateProfileImage
+                address={delegate.address}
+                votingPower={delegate.votingPower}
+              />
+              <p
+                className={`break-words text-gray-600 overflow-hidden line-clamp-2 text-ellipsis`}
+              >
+                {delegate.statement?.delegateStatement &&
+                  `${delegate.statement?.delegateStatement.slice(0, 120)}`}
+              </p>
+            </VStack>
+          </Link>
+          <div className="flex-grow" />
+          <DelegateActions
+            delegate={delegate}
+            fetchBalanceForDirectDelegation={fetchBalanceForDirectDelegation}
+            fetchVotingPowerForSubdelegation={fetchVotingPowerForSubdelegation}
+            checkIfDelegatingToProxy={checkIfDelegatingToProxy}
+            fetchCurrentDelegatees={fetchCurrentDelegatees}
+            getProxyAddress={getProxyAddress}
+            isAdvancedUser={isAdvancedUser}
           />
-          <p
-            className={`break-words text-gray-600 overflow-hidden line-clamp-2 text-ellipsis`}>
-            {delegate.statement?.delegateStatement && `${delegate.statement?.delegateStatement.slice(0, 120)}`}
-          </p>
         </VStack>
-      </div>
-      <div className="flex-grow" />
-      <DelegateActions
-        delegate={delegate}
-        fetchBalanceForDirectDelegation={
-          fetchBalanceForDirectDelegation
-        }
-        fetchVotingPowerForSubdelegation={
-          fetchVotingPowerForSubdelegation
-        }
-        checkIfDelegatingToProxy={checkIfDelegatingToProxy}
-        fetchCurrentDelegatees={fetchCurrentDelegatees}
-        getProxyAddress={getProxyAddress}
-        isAdvancedUser={isAdvancedUser}
-      />
-    </VStack>
-  )}
-</>
-  )
+      )}
+    </>
+  );
 }
