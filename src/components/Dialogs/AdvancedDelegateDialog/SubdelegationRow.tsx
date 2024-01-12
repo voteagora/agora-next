0a3607dbed93f13ce30acc5ce09de6a10e35f051
wojtekwtf <wojciechkulikowski@icloud.com>
2024-01-12 18:26:23 +0100

    Fix calculating percentage with 3 decimals precision

--- src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
+++ src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
@@ -42,9 +42,16 @@ function SubdelegationToRow({
       ? 0
       : Math.round((allowance / availableBalanceNumber) * 100_00) / 100;
 
+  // alert(
+  //   "allowance is " +
+  //     allowance +
+  //     " and available balance is " +
+  //     availableBalanceNumber
+  // );
+
   const handleAllowanceInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     function formatNumber(value: number) {
-      return Math.floor(Math.round(value * 1000) / 10) / 100;
+      return Math.floor(Math.round(value * 10000) / 10) / 1000;
     }
 
     function getCleanInput(value: string) {
