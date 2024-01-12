f7f11cc9e6db66c05c855b9aa9b7c12e7ce570f6
ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-12 21:11:23 +0100

    fix styles of advanced delegation dialog

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,39 @@
 import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-11 w-full rounded-md border border-input bg-gray-fa px-3 py-2 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
+const inputVariants = cva(
+  "flex h-11 w-full rounded-md bg-gray-fa border px-3 py-2 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "border-input",
+        none: "border-none",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
   }
 );
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  InputProps & VariantProps<typeof inputVariants>
+>(({ className, variant, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(inputVariants({ className, variant }))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
 Input.displayName = "Input";
 
 const InputDescription = React.forwardRef<
