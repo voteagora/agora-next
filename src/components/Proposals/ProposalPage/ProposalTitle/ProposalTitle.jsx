b1757d7d81db00414f86f2244c36f5a14afc4019
Kent Fenwick <kent.fenwick@gmail.com>
2024-01-15 11:11:42 -0500

    updated the text to reflect the proposal type (#58)

--- src/components/Proposals/ProposalPage/ProposalTitle/ProposalTitle.jsx
+++ src/components/Proposals/ProposalPage/ProposalTitle/ProposalTitle.jsx
@@ -1,14 +1,20 @@
 import HumanAddress from "@/components/shared/HumanAddress";
 import styles from "./proposalTitle.module.scss";
 import { VStack } from "@/components/Layout/Stack";
+import { getProposalTypeText } from "@/lib/utils";
 
-export default function ProposalTitle({ title, proposerAddress }) {
+export default function ProposalTitle({
+  title,
+  proposalType,
+  proposerAddress,
+}) {
+  const proposalText = getProposalTypeText(proposalType);
   return (
     <VStack className={styles.proposal_title_container}>
       <h2>{title}</h2>
       <div className={styles.proposal_proposer}>
         {/* Warning: this assumes OP FND is the only proposer. Will need to maintain an array of OP Foundation proposals eventually */}
-        by Optimism Foundation
+        {proposalText} by The Optimism Foundation
         {/* <HumanAddress address={proposerAddress} /> */}
       </div>
     </VStack>
