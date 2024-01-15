c9931475dc06538010be624f0915e4c9b7aa9ffb
ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-15 18:04:33 +0100

    WIP

--- src/app/retropgf/3/page.tsx
+++ src/app/retropgf/3/page.tsx
@@ -1,11 +1,27 @@
+// TODO: merge with main once summary retropgf is merged and update links
 import RetroPGFResults from "@/components/RetroPGF/RetroPGFResults";
 import { VStack } from "@/components/Layout/Stack";
 
+/**
+ * TODO:
+ * - Get exact fields from query, where is image coming from
+ * - Text-ellipsis if project text is too long?
+ * - Get filters on query
+ * - Get mobile design
+ * - Check if url is prod or not
+ * - Infinite scroll?
+ * - Where is this conversion of these impactCategories [ 'DEVELOPER_ECOSYSTEM', 'END_USER_EXPERIENCE_AND_ADOPTION' ] into "Tooling and utilities", "End UX & Adoption"
+ */
+// TODO: frh -> investigate apollo for infinite scroll
+// TODO: frh -> filters
 export async function getRetroPGFResults() {
   const query = `
     query {
       retroPGF {
         projects(first: 5) {
+          pageInfo {
+            hasNextPage
+          }
           edges {
             node {
               applicant {
@@ -49,15 +65,19 @@ export async function getRetroPGFResults() {
   const response = await fetch(url, options);
   const data = await response.json();
 
-  return data.data.retroPGF.projects.edges;
+  return data.data.retroPGF.projects;
 }
 
 export default async function Page() {
-  const results = await getRetroPGFResults();
+  const projects = await getRetroPGFResults();
+  console.log("results page: ", projects);
 
   return (
     <VStack className="my-8 max-w-6xl rounded-xl border border-gray-300 shadow-newDefault overflow-hidden">
-      <RetroPGFResults results={results} />
+      <RetroPGFResults
+        results={projects.edges}
+        hasNextPage={projects.pageInfo.hasNextPage}
+      />
     </VStack>
   );
 }
--- src/components/RetroPGF/RetroPGFResults.tsx
+++ src/components/RetroPGF/RetroPGFResults.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Table,
   TableBody,
@@ -8,6 +10,14 @@ import {
 } from "@/components/ui/table";
 import profileIcon from "@/icons/profile.svg";
 import Image from "next/image";
+import InfiniteScroll from "react-infinite-scroller";
+import { useRef } from "react";
+
+// TODO: frh -> do infinite scroll and see if there are more categories
+const categories = {
+  END_USER_EXPERIENCE_AND_ADOPTION: "End UX & Adoption",
+  DEVELOPER_ECOSYSTEM: "Tooling and utilities",
+};
 
 export type Results = {
   node: {
@@ -23,7 +33,7 @@ export type Results = {
     awarded: string;
     displayName: string;
     id: string;
-    impactCategory: string[];
+    impactCategory: (keyof typeof categories)[];
     includedInBallots: number;
     profile: {
       name: string;
@@ -32,7 +42,28 @@ export type Results = {
   };
 }[];
 
-export default function RetroPGFResults({ results }: { results: Results }) {
+export default function RetroPGFResults({
+  results,
+  hasNextPage,
+}: {
+  results: Results;
+  hasNextPage: boolean;
+}) {
+  const fetching = useRef(false);
+
+  // TODO: frh type
+  // const loadMore = async (page: number | string) => {
+  //   if (!fetching.current && meta.hasNextPage) {
+  //     fetching.current = true;
+  //     const data = await fetchDelegateVotes(page, sortOrder);
+  //     setMeta(data.meta);
+  //     setDelegateVotes((prev) =>
+  //       getUniqueDelegateVotes(prev.concat(data.votes))
+  //     );
+  //     fetching.current = false;
+  //   }
+  // };
+  console.log("results: ", results);
   return (
     <Table>
       <TableHeader className="border-none">
@@ -58,19 +89,37 @@ export default function RetroPGFResults({ results }: { results: Results }) {
         </TableRow>
       </TableHeader>
       <TableBody className="text-gray-4f font-medium text-base">
-        {results.map((result) => {
-          const {
-            displayName,
-            id,
-            impactCategory,
-            includedInBallots,
-            profile,
-          } = result.node;
-          const _profile = "solidity.lang.eth";
-          return (
-            <TableRow className="border-none" key={id}>
-              <TableCell>
-                {/* {profile?.profileImageUrl && (
+        {/* @ts-ignore */}
+        <InfiniteScroll
+          hasMore={true}
+          pageStart={0}
+          loadMore={() => console.log("heelo")}
+          loader={
+            <div key="loader">
+              Loading... <br />
+              <Image
+                src="/images/blink.gif"
+                alt="Blinking Agora Logo"
+                width={50}
+                height={20}
+              />
+            </div>
+          }
+        >
+          {results.map((result) => {
+            const {
+              awarded,
+              displayName,
+              id,
+              impactCategory,
+              includedInBallots,
+              profile,
+            } = result.node;
+            const _profile = "solidity.lang.eth";
+            return (
+              <TableRow className="border-none" key={id}>
+                <TableCell>
+                  {/* {profile?.profileImageUrl && (
                   <Image
                     src={profile.profileImageUrl}
                     alt={displayName}
@@ -78,31 +127,38 @@ export default function RetroPGFResults({ results }: { results: Results }) {
                     height="32"
                   />
                 )} */}
-                <span className="font-semibold text-black">{displayName}</span>
-              </TableCell>
-              <TableCell className="flex gap-2 items-center">
-                <Image src={profileIcon} alt={_profile} />
-                <span>{_profile}</span>
-              </TableCell>
-              <TableCell>
-                {impactCategory.map((category) => (
-                  <span
-                    className="mx-1 py-0.5 px-1 rounded-[4px] bg-gray-fa text-xs"
-                    key={category}
-                  >
-                    End UX & Adoption
+                  <span className="font-semibold text-black">
+                    {displayName}
                   </span>
-                ))}
-              </TableCell>
-              <TableCell className="text-right">{includedInBallots}</TableCell>
-              <TableCell className="text-right">14</TableCell>
-              <TableCell className="text-right flex justify-end gap-2 items-center">
-                <span className="font-semibold text-black">500,000 OP</span>
-                <span className="text-xs">(2,42%)</span>
-              </TableCell>
-            </TableRow>
-          );
-        })}
+                </TableCell>
+                <TableCell className="flex gap-2 items-center">
+                  <Image src={profileIcon} alt={_profile} />
+                  <span>{_profile}</span>
+                </TableCell>
+                <TableCell>
+                  {impactCategory.map((category) => (
+                    <span
+                      className="mx-1 py-0.5 px-1 rounded-[4px] bg-gray-fa text-xs"
+                      key={category}
+                    >
+                      {categories[category]}
+                    </span>
+                  ))}
+                </TableCell>
+                <TableCell className="text-right">
+                  {includedInBallots}
+                </TableCell>
+                {/* TODO: frh -> this field */}
+                <TableCell className="text-right">14</TableCell>
+                {/* TODO: frh -> see how to get total and formatting */}
+                <TableCell className="text-right flex justify-end gap-2 items-center">
+                  <span className="font-semibold text-black">500,000 OP</span>
+                  <span className="text-xs">(2,42%)</span>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </InfiniteScroll>
       </TableBody>
     </Table>
   );
