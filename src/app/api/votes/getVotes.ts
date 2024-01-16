79ffeb4c2076d96ed859e574e59a29a9f36326e4
Stepan Arsentjev <stepandel@gmail.com>
2024-01-16 16:05:15 -0500

    Optimisations (#65)
    * hardcode sort
    
    * default sort votes by weight
    
    * sort by block_number
    
    * Revert "sort by block_number"
    
    This reverts commit 6ac706efc6f72d29eee130d15bcbae95b2ad9e65.
    
    * use getItem instead of quuery when getting statement from dynamo
    
    * clean up delegate card

--- src/app/api/votes/getVotes.ts
+++ src/app/api/votes/getVotes.ts
@@ -65,7 +65,7 @@ async function getVotesForDelegateForAddress({
             WHERE
               proposals_mat.proposal_id = t.proposal_id) p ON TRUE
         ) q
-        ORDER BY ${sort} DESC
+        ORDER BY block_number DESC
         OFFSET ${skip}
         LIMIT ${take};
       `
@@ -137,7 +137,7 @@ export async function getVotesForProposal({
               proposals_mat.proposal_id = ${proposal_id} AND
               proposals_mat.proposal_id = t.proposal_id) p ON TRUE
         ) q
-        ORDER BY ${sort} DESC
+        ORDER BY weight::NUMERIC DESC
         OFFSET ${skip}
         LIMIT ${take};
       `
