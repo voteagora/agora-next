bed8dc6a50bd45a63abaf57bfd4937a0a2ac5108
ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-17 17:50:58 +0100

    fix is citizen param

--- src/app/api/citizens/isCitizen.ts
+++ src/app/api/citizens/isCitizen.ts
@@ -3,12 +3,7 @@ import "server-only";
 import { Prisma } from "@prisma/client";
 import prisma from "@/app/lib/prisma";
 
-export async function isCitizen({
-  address
-}: {
-  address: string;
-}) {
-
+export async function isCitizen(address: string) {
   return prisma.$queryRaw<{
     address: string;
   }[]>(
