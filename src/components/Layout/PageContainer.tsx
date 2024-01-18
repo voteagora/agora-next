858497ad53f7aab1340d542e8d00fc4e73d57b3b
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-19 00:57:02 +0100

    make tablet responsive (#88)

--- src/components/Layout/PageContainer.tsx
+++ src/components/Layout/PageContainer.tsx
@@ -4,7 +4,6 @@
 
 import React, { ReactNode } from "react";
 
-import { VStack } from "@/components/Layout/Stack";
 import { Analytics } from "@vercel/analytics/react";
 import { DialogProvider } from "../Dialogs/DialogProvider/DialogProvider";
 
@@ -15,7 +14,7 @@ type Props = {
 export function PageContainer({ children }: Props) {
   return (
     <DialogProvider>
-      <div className="container my-4 mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container my-4 mx-auto px-4 sm:px-6 lg:px-8 sm:overflow-x-scroll xl:overflow-x-visible sm:min-w-fit xl:min-w-auto">
         <div className="bg-dotted-pattern" />
         <div className="bg-radial-gradient" />
         {children}
