509481dd99082d6553d6e7ae99203386ef56dcd6
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-19 00:59:37 +0100

    Frodriguezhervias/agora 1457 typo and styling issues in proposal creation success modal (#86)
    * WIP
    
    * show correct dialogs after proposal creation
    
    * minor fixes
    
    * minor fix
    
    * minor fix

--- src/components/Proposals/ProposalCreation/CastProposalDialog.tsx
+++ src/components/Proposals/ProposalCreation/CastProposalDialog.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { HStack, VStack } from "@/components/Layout/Stack";
-import { icons } from "@/assets/icons/icons";
-import Image from "next/image";
 import styles from "./styles.module.scss";
 
 type Props = {
@@ -18,11 +16,12 @@ export function CastProposalDialog({
   isLoading,
   isSuccess,
   txHash,
+  closeDialog,
 }: Props) {
   return (
-    <VStack alignItems="items-center" className={styles.create_dialog}>
-      <VStack className={styles.create_dialog__content}>
-        <VStack gap={6} className={styles.create_dialog__text}>
+    <VStack alignItems="items-center">
+      <VStack className="w-full bg-white rounded-xl">
+        <VStack className="text-xs">
           {!isError && !isLoading && !isSuccess && (
             <div>Waiting for transaction execution...</div>
           )}
@@ -31,7 +30,7 @@ export function CastProposalDialog({
           )}
           {isError && !txHash && <div>error</div>}
           {isLoading && <Loading />}
-          {isSuccess && <SuccessMessage />}
+          {isSuccess && <SuccessMessage closeDialog={closeDialog} />}
         </VStack>
       </VStack>
     </VStack>
@@ -51,21 +50,40 @@ function Message({ text, image }: { text: string; image?: JSX.Element }) {
   );
 }
 
-export function SuccessMessage() {
+export function SuccessMessage({ closeDialog }: { closeDialog: () => void }) {
   return (
-    <Message
-      text="Success! Proposal has been created. It will appear once the transaction is
-    confirmed."
-      image={<Image src={icons.ballot} alt={icons.ballot} className="h-5" />}
-    />
+    <VStack className={styles.full_width}>
+      <img src={`/images/congrats.svg`} className="w-full mb-3" />
+      <div className="font-black text-2xl mb-2">
+        Proposal successfully created!
+      </div>
+      <div className="text-gray-700 text-sm mb-5 font-medium">
+        It might take up to a minute for the changes to be reflected.
+      </div>
+      <div>
+        <div onClick={closeDialog} className={`${styles.proposal_container}`}>
+          Got it
+        </div>
+      </div>
+    </VStack>
   );
 }
 
 export function Loading() {
   return (
-    <Message
-      text="Creating proposal"
-      image={<Image src={icons.spinner} alt={icons.spinner} />}
-    />
+    <VStack className={styles.full_width}>
+      <img src={`/images/action-pending.svg`} className="w-full mb-3" />
+      <div className="font-black text-2xl mb-2">Creating your proposal ...</div>
+      <div className="text-gray-4f mb-5 font-medium text-base">
+        It might take up to a minute for the changes to be reflected.
+      </div>
+      <div>
+        <div className="flex flex-row justify-center w-full py-3 bg-gray-eo rounded-lg">
+          <div className="text-gray-4f font-semibold text-base">
+            Writing your proposal to chain...
+          </div>
+        </div>
+      </div>
+    </VStack>
   );
 }
