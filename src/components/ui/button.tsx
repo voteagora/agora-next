f819a9670cba17e9564729d8fdecfe0a10063d74
ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-18 18:25:30 +0100

    minor fix

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,6 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        disabled: "bg-gray-eo text-gray-4f cursor-not-allowed",
       },
       size: {
         default: "h-10 px-4 py-2",
