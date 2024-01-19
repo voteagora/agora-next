a1fbaae52b102ff74858ad8512e86bbc9b5fa803
Yitong <zha.yitong@gmail.com>
2024-01-18 23:06:14 -0800

    Slight copy change on proposal list

--- src/components/Proposals/Proposal/OPApprovalProposalStatus.jsx
+++ src/components/Proposals/Proposal/OPApprovalProposalStatus.jsx
@@ -1,11 +1,15 @@
-import { HStack } from "@/components/Layout/Stack";
+import { HStack, VStack } from "@/components/Layout/Stack";
 import styles from "./proposal.module.scss";
 import { pluralize } from "@/lib/utils";
 
 export default function OPApprovalProposalStatus({ proposal }) {
+  const maxOptions = proposal.proposalData.proposalSettings.maxApprovals;
   return (
-    <HStack className={styles.proposal_status} gap={1}>
-      {pluralize("Choice", proposal.proposalData.options.length)}
-    </HStack>
+    <VStack alignItems="items-end">
+      <div className={styles.cell_content_title}>Select {maxOptions} of</div>
+      <HStack className={styles.proposal_status} gap={1}>
+        {pluralize("Option", proposal.proposalData.options.length)}
+      </HStack>
+    </VStack>
   );
 }
