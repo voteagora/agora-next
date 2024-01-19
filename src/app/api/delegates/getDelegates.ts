fe2060f13f593abd7195dc5d8175530c1a8d8b59
Stepan Arsentjev <stepandel@gmail.com>
2024-01-18 20:08:09 -0500

    Voting optimizations (#91)
    * load vp in parallel
    
    * vote delegate data in parallel

--- src/app/api/delegates/getDelegates.ts
+++ src/app/api/delegates/getDelegates.ts
@@ -1,7 +1,14 @@
 import "server-only";
 
 import { paginatePrismaResult } from "@/app/lib/pagination";
-import { Prisma } from "@prisma/client";
+import {
+  AdvancedVotingPower,
+  NumberOfDelegators,
+  Prisma,
+  VoterStats,
+  VoterStatsPayload,
+  VotingPower,
+} from "@prisma/client";
 import prisma from "@/app/lib/prisma";
 import { isAddress } from "viem";
 import { resolveENSName } from "@/app/lib/ENSUtils";
@@ -10,6 +17,7 @@ import { Delegate } from "./delegate";
 import { getCurrentQuorum } from "../quorum/getQuorum";
 import { isCitizen } from "../citizens/isCitizen";
 import { OptimismContracts } from "@/lib/contracts/contracts";
+import { Decimal, DefaultArgs } from "@prisma/client/runtime";
 
 export async function getDelegates({
   page = 1,
@@ -79,6 +87,20 @@ export async function getDelegates({
   };
 }
 
+type DelegateStats = {
+  voter: VoterStats["voter"];
+  proposals_created: VoterStats["proposals_created"];
+  proposals_voted: VoterStats["proposals_voted"];
+  for: VoterStats["for"];
+  against: VoterStats["against"];
+  abstain: VoterStats["abstain"];
+  participation_rate: VoterStats["participation_rate"];
+  last_10_props: VoterStats["last_10_props"];
+  voting_power: VotingPower["voting_power"];
+  advanced_vp: AdvancedVotingPower["advanced_vp"];
+  num_for_delegators: NumberOfDelegators["num_for_delegators"];
+};
+
 export async function getDelegate({
   addressOrENSName,
 }: {
@@ -88,35 +110,67 @@ export async function getDelegate({
     ? addressOrENSName.toLowerCase()
     : await resolveENSName(addressOrENSName);
 
-  const voterStats = await prisma.voterStats.findFirst({
-    where: { voter: address },
-  });
-  const votingPower = await prisma.votingPower.findFirst({
-    where: { delegate: address },
-  });
-  const advancedVotingPower = await prisma.advancedVotingPower.findFirst({
-    where: {
-      delegate: address,
-      contract: OptimismContracts.alligator.address.toLowerCase(),
-    },
-  });
+  const delegateQuery = prisma.$queryRaw<DelegateStats[]>(
+    Prisma.sql`
+    SELECT * FROM (
+      SELECT * FROM (
+      SELECT *
+      FROM (
+      SELECT
+        voter,
+        proposals_created,
+        proposals_voted,
+        "for",
+        "against",
+        "abstain",
+        participation_rate,
+        last_10_props
+      FROM center.voter_stats
+      WHERE voter=${address}
+      ) vs
+      
+      LEFT JOIN LATERAL (
+        SELECT
+          delegate,
+          voting_power
+        FROM
+          center.voting_power
+        WHERE delegate=vs.voter
+        LIMIT 1
+      ) vp ON TRUE
+      ) a
+      
+      LEFT JOIN LATERAL (
+        SELECT
+          advanced_vp
+        FROM center.advanced_voting_power
+        WHERE delegate=a.voter AND contract=${OptimismContracts.alligator.address.toLowerCase()}
+      ) ad ON TRUE
+      ) b
+      
+      LEFT JOIN LATERAL (
+        SELECT
+          num_for_delegators
+        FROM
+        center.num_of_delegators
+        WHERE delegate=b.voter
+        LIMIT 1
+      ) nd ON TRUE
+    `
+  );
+
+  const [delegate, votableSupply, delegateStatement, quorum, _isCitizen] =
+    await Promise.all([
+      (await delegateQuery)?.[0] || undefined,
+      prisma.votableSupply.findFirst({}),
+      getDelegateStatement({ addressOrENSName }),
+      getCurrentQuorum(),
+      isCitizen(address),
+    ]);
 
   const totalVotingPower =
-    BigInt(votingPower?.voting_power || 0) +
-    BigInt(advancedVotingPower?.advanced_vp.toFixed(0) || 0);
-
-  const votableSupply =
-    (await prisma.votableSupply.findFirst({}))?.votable_supply || 1n;
-
-  const numOfDelegators = await prisma.numberOfDelegators.findFirst({
-    where: { delegate: address },
-  });
-
-  const delegateStatement = await getDelegateStatement({ addressOrENSName });
-
-  const quorum = await getCurrentQuorum();
-
-  const _isCitizen = await isCitizen(address);
+    BigInt(delegate?.voting_power || 0) +
+    BigInt(delegate?.advanced_vp?.toFixed(0) || 0);
 
   // Build out delegate JSON response
   return {
@@ -125,20 +179,20 @@ export async function getDelegate({
     citizen: _isCitizen.length > 0,
     votingPower: totalVotingPower.toString(),
     votingPowerRelativeToVotableSupply: Number(
-      totalVotingPower / BigInt(votableSupply)
+      totalVotingPower / BigInt(votableSupply?.votable_supply || 0)
     ),
     votingPowerRelativeToQuorum:
       quorum && quorum > 0n
         ? Number((totalVotingPower * 10000n) / quorum) / 10000
         : 0,
-    proposalsCreated: voterStats?.proposals_created || 0n,
-    proposalsVotedOn: voterStats?.proposals_voted || 0n,
-    votedFor: voterStats?.for?.toFixed() || "0",
-    votedAgainst: voterStats?.against?.toFixed() || "0",
-    votedAbstain: voterStats?.abstain?.toFixed() || "0",
-    votingParticipation: voterStats?.participation_rate || 0,
-    lastTenProps: voterStats?.last_10_props?.toFixed() || "0",
-    numOfDelegators: numOfDelegators?.num_for_delegators || 0n,
+    proposalsCreated: delegate?.proposals_created || 0n,
+    proposalsVotedOn: delegate?.proposals_voted || 0n,
+    votedFor: delegate?.for?.toFixed() || "0",
+    votedAgainst: delegate?.against?.toFixed() || "0",
+    votedAbstain: delegate?.abstain?.toFixed() || "0",
+    votingParticipation: delegate?.participation_rate || 0,
+    lastTenProps: delegate?.last_10_props?.toFixed() || "0",
+    numOfDelegators: delegate?.num_for_delegators || 0n,
     statement: delegateStatement,
   };
 }
