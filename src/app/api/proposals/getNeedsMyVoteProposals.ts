0cf1468c7f008849861803f70ff573883a9a5525
wojtekwtf <wojciechkulikowski@icloud.com>
2024-01-22 16:57:53 +0100

    Add the raw query

--- src/app/api/proposals/getNeedsMyVoteProposals.ts
+++ src/app/api/proposals/getNeedsMyVoteProposals.ts
@@ -3,6 +3,7 @@ import { paginatePrismaResult } from "@/app/lib/pagination";
 import { parseProposal } from "@/lib/proposalUtils";
 import prisma from "@/app/lib/prisma";
 import provider from "@/app/lib/provider";
+import { Proposals } from "@prisma/client";
 
 import "server-only";
 import { OptimismContracts } from "@/lib/contracts/contracts";
@@ -14,26 +15,35 @@ export async function getNeedsMyVoteProposals({
 }: {
   address: string;
 }) {
-  const prodDataOnly = process.env.NEXT_PUBLIC_AGORA_ENV === "prod" && {
-    contract: OptimismContracts.governor.address.toLowerCase(),
-  };
-
   const latestBlock = await provider.getBlock("latest");
   const votableSupply = await getVotableSupply();
 
-  const proposals = await prisma.proposals.findMany({
-    skip: 0,
-    take: 10,
-    orderBy: {
-      ordinal: "desc",
-    },
-    where: {
-      ...(prodDataOnly || {}),
-      cancelled_block: null,
-    },
-  });
+  let proposals: Proposals[];
 
-  console.log(proposals);
+  if (process.env.NEXT_PUBLIC_AGORA_ENV === "prod") {
+    // get only proposals from the prod contract
+    proposals = await prisma.$queryRaw`
+      SELECT p.*
+      FROM "center"."proposals" p
+      LEFT JOIN "center"."votes" v ON p.proposal_id = v.proposal_id AND v.voter = ${address}
+      WHERE CAST(p.start_block AS INTEGER) < ${latestBlock?.number}
+        AND CAST(p.end_block AS INTEGER) > ${latestBlock?.number}
+        AND p.cancelled_block IS NULL
+        AND p.contract = ${OptimismContracts.governor.address.toLowerCase()}
+        AND v.proposal_id IS NULL;
+      `;
+  } else {
+    // get proposals from anywhere
+    proposals = await prisma.$queryRaw`
+      SELECT p.*
+      FROM "center"."proposals" p
+      LEFT JOIN "center"."votes" v ON p.proposal_id = v.proposal_id AND v.voter = ${address}
+      WHERE CAST(p.start_block AS INTEGER) < ${latestBlock?.number}
+        AND CAST(p.end_block AS INTEGER) > ${latestBlock?.number}
+        AND p.cancelled_block IS NULL
+        AND v.proposal_id IS NULL;
+      `;
+  }
 
   const resolvedProposals = Promise.all(
     proposals.map(async (proposal) => {
