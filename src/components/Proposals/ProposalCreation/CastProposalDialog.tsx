78e1529578abbf1cde299683dc29b181849efe5d
Dom-Mac <domenicochad@gmail.com>
2024-01-23 17:38:08 +0100

    create proposal tx link

--- src/components/Proposals/ProposalCreation/CastProposalDialog.tsx
+++ src/components/Proposals/ProposalCreation/CastProposalDialog.tsx
@@ -2,6 +2,8 @@
 
 import { HStack, VStack } from "@/components/Layout/Stack";
 import styles from "./styles.module.scss";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import { getBlockScanUrl } from "@/lib/utils";
 
 type Props = {
   isError: boolean;
@@ -30,7 +32,9 @@ export function CastProposalDialog({
           )}
           {isError && !txHash && <div>error</div>}
           {isLoading && <Loading />}
-          {isSuccess && <SuccessMessage closeDialog={closeDialog} />}
+          {isSuccess && txHash && (
+            <SuccessMessage txHash={txHash} closeDialog={closeDialog} />
+          )}
         </VStack>
       </VStack>
     </VStack>
@@ -50,20 +54,37 @@ function Message({ text, image }: { text: string; image?: JSX.Element }) {
   );
 }
 
-export function SuccessMessage({ closeDialog }: { closeDialog: () => void }) {
+export function SuccessMessage({
+  closeDialog,
+  txHash,
+}: {
+  closeDialog: () => void;
+  txHash: string;
+}) {
   return (
     <VStack className={styles.full_width}>
       <img src={`/images/congrats.svg`} className="w-full mb-3" />
-      <div className="font-black text-2xl mb-2">
+      <div className="mb-2 text-2xl font-black">
         Proposal successfully created!
       </div>
-      <div className="text-gray-700 text-sm mb-5 font-medium">
+      <div className="mb-5 text-sm font-medium text-gray-700">
         It might take up to a minute for the changes to be reflected.
       </div>
       <div>
         <div onClick={closeDialog} className={`${styles.proposal_container}`}>
           Got it
         </div>
+      </div>
+      <div className="pt-4 text-xs text-gray-4f">
+        <a
+          href={getBlockScanUrl(txHash)}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="flex flex-row items-center justify-between w-full hover:underline"
+        >
+          <p>View transaction on block explorer</p>
+          <ArrowTopRightOnSquareIcon className="w-4 h-4 ml-2" />
+        </a>
       </div>
     </VStack>
   );
@@ -73,13 +94,13 @@ export function Loading() {
   return (
     <VStack className={styles.full_width}>
       <img src={`/images/action-pending.svg`} className="w-full mb-3" />
-      <div className="font-black text-2xl mb-2">Creating your proposal ...</div>
-      <div className="text-gray-4f mb-5 font-medium text-base">
+      <div className="mb-2 text-2xl font-black">Creating your proposal ...</div>
+      <div className="mb-5 text-base font-medium text-gray-4f">
         It might take up to a minute for the changes to be reflected.
       </div>
       <div>
-        <div className="flex flex-row justify-center w-full py-3 bg-gray-eo rounded-lg">
-          <div className="text-gray-4f font-semibold text-base">
+        <div className="flex flex-row justify-center w-full py-3 rounded-lg bg-gray-eo">
+          <div className="text-base font-semibold text-gray-4f">
             Writing your proposal to chain...
           </div>
         </div>
