20c631c98917d57ce6ccc934283a1b848dacbb8c
woj.eth <wojciechkulikowski@icloud.com>
2024-01-26 00:28:51 +0100

    wojeth/agora-1315 Needs my vote (#93)
    * Add a new compontent
    
    * Add the raw query
    
    * Add final query
    
    * Optimize the query

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import ProposalsList from "@/components/Proposals/ProposalsList/ProposalsList";
+import NeedsMyVoteProposalsList from "@/components/Proposals/NeedsMyVoteProposalsList/NeedsMyVoteProposalsList";
 import DAOMetricsHeader from "@/components/Metrics/DAOMetricsHeader";
 import styles from "@/styles/homepage.module.scss";
 import Hero from "@/components/Hero/Hero";
@@ -7,6 +8,7 @@ import { VStack } from "@/components/Layout/Stack";
 import { getProposals } from "./api/proposals/getProposals";
 import { getMetrics } from "./api/metrics/getMetrics";
 import { getVotableSupply } from "src/app/api/votableSupply/getVotableSupply";
+import { getNeedsMyVoteProposals } from "./api/proposals/getNeedsMyVoteProposals";
 
 // Revalidate cache every 60 seconds
 export const revalidate = 60;
@@ -15,6 +17,12 @@ async function fetchProposals(page = 1) {
   "use server";
 
   return getProposals({ page });
+}
+
+async function fetchNeedsMyVoteProposals(address) {
+  "use server";
+
+  return getNeedsMyVoteProposals({ address });
 }
 
 async function fetchDaoMetrics() {
@@ -39,6 +47,10 @@ export default async function Home() {
       <Hero />
       <DAOMetricsHeader metrics={metrics} />
       <PageDivider />
+      <NeedsMyVoteProposalsList
+        fetchNeedsMyVoteProposals={fetchNeedsMyVoteProposals}
+        votableSupply={votableSupply}
+      />
       <ProposalsList
         initialProposals={proposals}
         fetchProposals={fetchProposals}
---
+++ src/components/Proposals/NeedsMyVoteProposalsList/NeedsMyVoteProposalsList.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import styles from "./needsMyVoteProposalLists.module.scss";
+import { HStack, VStack } from "@/components/Layout/Stack";
+import PageHeader from "@/components/Layout/PageHeader/PageHeader";
+import Proposal from "../Proposal/Proposal";
+
+import { useAccount } from "wagmi";
+
+export default function ProposalsList({
+  fetchNeedsMyVoteProposals,
+  votableSupply,
+}) {
+  const [proposals, setProposals] = useState([]);
+  const { address, isConnected } = useAccount();
+
+  const getProposals = async () => {
+    if (address) {
+      const proposals = await fetchNeedsMyVoteProposals(address);
+
+      setProposals(proposals.proposals);
+    }
+  };
+
+  useEffect(() => {
+    // reset when changing wallets
+    setProposals([]);
+
+    getProposals();
+  }, [address]);
+
+  return (
+    <>
+      {isConnected && proposals.length > 0 && (
+        <VStack className={styles.proposals_list_container}>
+          <PageHeader headerText="Needs my vote" />
+
+          <VStack className={styles.proposals_table_container}>
+            <div className={styles.proposals_table}>
+              {proposals.map((proposal) => (
+                <Proposal
+                  key={`${proposal.id}_${proposal.status}`}
+                  proposal={proposal}
+                  votableSupply={votableSupply}
+                />
+              ))}
+            </div>
+          </VStack>
+        </VStack>
+      )}
+    </>
+  );
+}
