43b5feac4e4b4d54b1c6d2bbf47531e44b5afbdb
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-26 00:31:59 +0100

    fetch proposal types from frontend (#104)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,5 @@
 import AdminForm from "@/components/Admin/AdminForm";
-import { getProposalTypes } from "../api/proposals/getProposals";
 import { getVotableSupply } from "../api/votableSupply/getVotableSupply";
-
-async function fetchProposalTypes() {
-  "use server";
-
-  return getProposalTypes();
-}
 
 async function fetchVotableSupply() {
   "use server";
@@ -15,10 +8,7 @@ async function fetchVotableSupply() {
 }
 
 export default async function Page() {
-  const proposalTypes = await fetchProposalTypes();
   const votableSupply = await fetchVotableSupply();
 
-  return (
-    <AdminForm votableSupply={votableSupply} proposalTypes={proposalTypes} />
-  );
+  return <AdminForm votableSupply={votableSupply} />;
 }
