4272db6dafb1b6816263073f272e803f2130de71
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-01-26 00:31:59 +0100

    fetch proposal types from frontend (#104)

--- src/components/Admin/AdminForm.tsx
+++ src/components/Admin/AdminForm.tsx
@@ -1,24 +1,18 @@
 import ProposalTypeSettings from "./ProposalTypeSettings";
 import GovernorSettings from "./GovernorSettings";
 import FAQs from "./FAQs";
-import { ProposalTypes } from "@prisma/client";
 
 // TODO: Take init values from the chain
 export default function AdminForm({
-  proposalTypes,
   votableSupply,
 }: {
-  proposalTypes: ProposalTypes[];
   votableSupply: string;
 }) {
   return (
     <div className="space-y-8 lg:space-y-0 lg:flex lg:gap-12 mt-12">
       <div className="space-y-8 lg:flex-grow">
         <GovernorSettings />
-        <ProposalTypeSettings
-          votableSupply={votableSupply}
-          initProposalTypes={proposalTypes}
-        />
+        <ProposalTypeSettings votableSupply={votableSupply} />
       </div>
       <FAQs />
     </div>
