eacb0f934136f203b2aea0c1eb45292797676124
wojtekwtf <wojciechkulikowski@icloud.com>
2024-01-29 18:41:45 +0100

    Add boilerplate proposal checklist

--- src/app/proposals/create-draft/page.tsx
+++ src/app/proposals/create-draft/page.tsx
@@ -1,9 +1,11 @@
+import DraftProposalChecklist from "@/components/ProposalLifecycle/DraftProposalChecklist/DraftProposalChecklist";
 import React from "react";
 
 const DraftProposalPage: React.FC = () => {
   return (
-    <div>
-      <h1>Create Draft Proposal</h1>
+    <div className="flex flex-row w-full gap-x-6 pt-9">
+      <div className="flex-grow">Create a temp check on discourse</div>
+      <DraftProposalChecklist />
     </div>
   );
 };
