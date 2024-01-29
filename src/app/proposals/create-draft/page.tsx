9ec851bd11728f31ae2d88c73c3a91a0adb92323
wojtekwtf <wojciechkulikowski@icloud.com>
2024-01-29 15:29:48 +0100

    Add new page

---
+++ src/app/proposals/create-draft/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DraftProposalPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Create Draft Proposal</h1>
+    </div>
+  );
+};
+
+export default DraftProposalPage;
