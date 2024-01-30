24e900c991ae1641b8f8584d312f343f7f66c558
stepandel <stepandel@gmail.com>
2024-01-29 19:26:24 -0500

    cleanup

--- src/components/Delegates/DelegateCard/DelegateButton.tsx
+++ src/components/Delegates/DelegateCard/DelegateButton.tsx
@@ -24,7 +24,7 @@ export function DelegateButton({
           params: {
             delegate,
             fetchBalanceForDirectDelegation,
-            fetchDirectDelegatee: fetchDirectDelegatee,
+            fetchDirectDelegatee,
           },
         });
       }}
