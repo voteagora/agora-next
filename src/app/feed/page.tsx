aa04e82febfacd339a1374c466055b15467e6195
ferrodri <f.rodriguez.hervias@gmail.com>
2024-02-01 21:29:28 +0100

    all events except subdelegation error being fetch to feed

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,12 +1,15 @@
-import prisma from "@/app/lib/prisma";
+import getFeedLogs from "@/lib/serverGetFeedLogs";
+
+async function getLogs() {
+  "use server";
+
+  return getFeedLogs();
+}
 
 /**
  * TODO: frh ->
  *
- * 1.  Experiment with a query where max 20 or 37 events are scanned, since right now there are 37 tables on center db
- *     with _events. Try union all and check speeds.
- * 2.  Check delegated events, partial delegation vs not partial delegation. Check voted for all kinds of proposals,
- *     check with params in vote. Check proposal creation events. Check other types of events.
+ * 1.  Paint data and then check subdelegation error
  * 3.  Think about cool filters for this page, some ideas right now are filter by address, filter by action (delegate,
  *     vote, proposal creation), by volume, by date, think about current ux. See how all this events can be aggregated
  *     together for filtering.
@@ -18,14 +21,24 @@ import prisma from "@/app/lib/prisma";
  * 9.  Think about current tabs and in mobile.
  * 10. Lastly research some other projects and feed for ideas.
  *
+ * 11. If time make a loading that it is not the whole page, maybe different PR but it is much more efficient for LCP.
+ *
  * On linear issue this are the events:
  * Delegations
  * Partial delegations
  * Proposal Creation
  * Does not say anything about votes but i am going to include everything for the best UX following the
  * "all of the events of the DAO together."
+ *
  */
 
 export default async function Page() {
+  const logs = await getLogs();
+  console.log("logs: ", logs.length);
+  let uniqueEvents = logs
+    .filter((v, i, a) => a.findIndex((t) => t.eventName === v.eventName) === i)
+    .map((item) => item.eventName);
+
+  console.log("uniqueEvents: ", uniqueEvents);
   return <span>events page</span>;
 }
---
+++ src/lib/serverGetFeedLogs.ts
@@ -0,0 +1,63 @@
+import "server-only";
+
+import { createPublicClient, http, parseAbi, parseAbiItem } from 'viem'
+import { optimism } from "viem/chains";
+import { OptimismContracts } from "@/lib/contracts/contracts";
+
+const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
+const getContractAddresses = () => {
+    const contractAdresses = [];
+    for (const [key, value] of Object.entries(OptimismContracts)) {
+        contractAdresses.push(value.address);
+    }
+    return contractAdresses as `0x${string}`[];
+}
+
+export default async function getFeedLogs() {
+    const publicClient = createPublicClient({
+        chain: optimism,
+        transport: http(`https://opt-mainnet.g.alchemy.com/v2/${alchemyId}`)
+    });
+
+    const { number } = await publicClient.getBlock();
+
+    // TODO: frh ->         
+    // 'event SubDelegation(address indexed from, address indexed to, SubdelegationRules subdelegationRules)'
+    // event SubDelegations(address indexed from, address[] to, SubdelegationRules subdelegationRules);
+    // event SubDelegations(address indexed from, address[] to, SubdelegationRules[] subdelegationRules);
+    // TODO: frh -> once display is correctly check that for example initialized show for both alligator and governor contract
+    return await publicClient.getLogs({
+        address: getContractAddresses(),
+        events: parseAbi([
+            'event AdminChanged(address previousAdmin, address newAdmin)',
+            'event Approval(address indexed owner, address indexed spender, uint256 value)',
+            'event BeaconUpgraded(address indexed beacon)', // Beacon has never been upgraded yet
+            'event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate)',
+            'event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance)',
+            'event Initialized(uint8 version)',
+            'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
+            'event Paused(address account)',
+            'event ProposalCanceled(uint256 proposalId)',
+            'event ProposalCreated(uint256 indexed proposalId, address indexed proposer, address indexed votingModule, bytes proposalData, uint256 startBlock, uint256 endBlock, string description, uint8 proposalType)',
+            'event ProposalCreated(uint256 indexed proposalId, address indexed proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description, uint8 proposalType)',
+            'event ProposalCreated(uint256 proposalId, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)',
+            'event ProposalCreated(uint256 proposalId, address proposer, address votingModule, bytes proposalData, uint256 startBlock, uint256 endBlock, string description)',
+            'event ProposalDeadlineUpdated(uint256 proposalId, uint64 deadline)',
+            'event ProposalExecuted(uint256 proposalId)', //TODO: frh no proposals have been executed
+            'event ProposalThresholdSet(uint256 oldProposalThreshold, uint256 newProposalThreshold)',
+            'event ProposalTypeSet(uint256 indexed proposalTypeId, uint16 quorum, uint16 approvalThreshold, string name)',
+            'event ProposalTypeUpdated(uint256 indexed proposalId, uint8 proposalType)', // Not on center db since there have been 0 events
+            'event QuorumNumeratorUpdated(uint256 oldQuorumNumerator, uint256 newQuorumNumerator)',
+            'event Transfer(address indexed from, address indexed to, uint256 value)',
+            'event Unpaused(address account)',
+            'event Upgraded(address indexed implementation)',
+            'event VoteCast(address indexed proxy, address indexed voter, address[] authority, uint256 proposalId, uint8 support)',
+            'event VoteCast(address indexed voter, uint256 proposalId, uint8 support, uint256 weight, string reason)',
+            'event VotesCast(address[] proxies, address indexed voter, address[][] authorities, uint256 proposalId, uint8 support)',
+            'event VoteCastWithParams(address indexed voter, uint256 proposalId, uint8 support, uint256 weight, string reason, bytes params)',
+            'event VotingDelaySet(uint256 oldVotingDelay, uint256 newVotingDelay)',
+            'event VotingPeriodSet(uint256 oldVotingPeriod, uint256 newVotingPeriod)'
+        ]),
+        fromBlock: number,
+    });
+}
