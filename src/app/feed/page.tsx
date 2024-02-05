47188eb7dba7f99a44231affa1ff82c920ebcfa5
ferrodri <f.rodriguez.hervias@gmail.com>
2024-02-05 10:12:04 +0100

    get logs from server

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,15 +1,15 @@
-import getFeedLogs from "@/lib/serverGetFeedLogs";
+import { getFeedLogs } from "@/app/api/feed/getFeedLogs";
 
 async function getLogs() {
   "use server";
 
-  return getFeedLogs();
+  return getFeedLogs({});
 }
 
 /**
  * TODO: frh ->
  *
- * 1.  Then check subdelegation error
+ * 1.  See how to get date from events
  * 2.  Think about "more tweets" style for updating feed
  * 3.  Think about cool filters for this page, some ideas right now are filter by address, filter by action (delegate,
  *     vote, proposal creation), by volume, by date, think about current ux. See how all this events can be aggregated
@@ -24,50 +24,48 @@ async function getLogs() {
  *
  * 11. If time make a loading that it is not the whole page, maybe different PR but it is much more efficient for LCP.
  *
- * On linear issue this are the events:
- * Delegations
- * Partial delegations
- * Proposal Creation
- * Does not say anything about votes but i am going to include everything for the best UX following the
- * "all of the events of the DAO together."
- *
  */
 
 export default async function Page() {
   const logs = await getLogs();
-  let uniqueEvents = logs
-    .filter((v, i, a) => a.findIndex((t) => t.eventName === v.eventName) === i)
-    .map((item) => item.eventName);
+  console.log("logs: ", logs);
+  // let uniqueEvents = logs
+  //   .filter((v, i, a) => a.findIndex((t) => t.eventName === v.eventName) === i)
+  //   .map((item) => item.eventName);
 
-  console.log("uniqueEvents: ", uniqueEvents);
+  // console.log("uniqueEvents: ", uniqueEvents);
   return (
-    <div className="flex flex-col gap-4">
-      {/* TODO: frh -> optimize this reverse */}
-      {logs.reverse().map((log, i) => {
-        // Block number, block hash, transaction index and transaction hash can be the same in many elements
-        // args provides a decoded, human-readable representation of the event data, topics includes the hashed event signature and indexed parameters, and data contains the raw, encoded event data, which often requires decoding based on the contract's ABI
-        const {
-          address,
-          blockNumber,
-          blockHash,
-          args,
-          logIndex,
-          eventName,
-          data,
-        } = log;
-        return (
-          <div
-            // logIndex represents the position or index of a specific log entry within the block in which it is included.
-            key={data + blockHash + logIndex}
-            className="flex flex-col gap-1 bg-gray-300 rounded-xl p-4"
-          >
-            <span>Contract: {address}</span>
-            <span>Block Number: {blockNumber.toString()}</span>
-            <span>Args: {JSON.stringify(args)}</span>
-            <span>Event name: {eventName}</span>
-          </div>
-        );
-      })}
-    </div>
+    <span>hello</span>
+    // <div className="flex flex-col gap-4">
+    //   {/* TODO: frh -> optimize this reverse */}
+    //   {logs.reverse().map((log) => {
+    //     // Block number, block hash, transaction index and transaction hash can be the same in many elements
+    //     // args provides a decoded, human-readable representation of the event data, topics includes the hashed event signature and indexed parameters, and data contains the raw, encoded event data, which often requires decoding based on the contract's ABI
+    //     const {
+    //       address,
+    //       blockNumber,
+    //       blockHash,
+    //       args,
+    //       logIndex,
+    //       eventName,
+    //       data,
+    //     } = log;
+    //     return (
+    //       <div
+    //         // logIndex represents the position or index of a specific log entry within the block in which it is included.
+    //         key={data + blockHash + logIndex}
+    //         className="flex flex-col gap-1 bg-gray-300 rounded-xl p-4"
+    //       >
+    //         <span>
+    //           Block number: {blockNumber.toString()}{" "}
+    //           {/* {new Date(Number(timestamp) * 1000).toString()} */}
+    //         </span>
+    //         <span>Contract: {address}</span>
+    //         <span>Args: {JSON.stringify(args)}</span>
+    //         <span>Event name: {eventName}</span>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
   );
 }
--- src/lib/serverGetFeedLogs.ts
+++ src/lib/serverGetFeedLogs.ts
@@ -1,6 +1,7 @@
+// TODO: delete this file
 import "server-only";
 
-import { createPublicClient, http, parseAbi } from 'viem'
+import { createPublicClient, http, parseAbi, type PublicClient, type Transport, type Chain } from 'viem'
 import { optimism } from "viem/chains";
 import { OptimismContracts } from "@/lib/contracts/contracts";
 
@@ -13,46 +14,23 @@ const getContractAddresses = () => {
     return contractAdresses as `0x${string}`[];
 }
 
-export default async function getFeedLogs() {
-    const publicClient = createPublicClient({
-        chain: optimism,
-        transport: http(`https://opt-mainnet.g.alchemy.com/v2/${alchemyId}`)
-    });
 
-    // TODO: frh -> delete this if not needed
-    const { number } = await publicClient.getBlock();
-
-    return await publicClient.getLogs({
+// TODO: frh -> go back to db style with 5 events and see if there are built queries already that could work for us
+const fetchLogs = async (publicClient: PublicClient<Transport, Chain>, fromBlock: bigint, toBlock: bigint) => {
+    const logs = await publicClient.getLogs({
         address: getContractAddresses(),
         events: parseAbi([
-            'event AdminChanged(address previousAdmin, address newAdmin)',
-            'event Approval(address indexed owner, address indexed spender, uint256 value)',
-            'event BeaconUpgraded(address indexed beacon)', // Beacon has never been upgraded yet
-            'event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate)',
-            'event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance)',
-            'event Initialized(uint8 version)',
-            'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
-            'event Paused(address account)',
-            'event ProposalCanceled(uint256 proposalId)',
-            'event ProposalCreated(uint256 indexed proposalId, address indexed proposer, address indexed votingModule, bytes proposalData, uint256 startBlock, uint256 endBlock, string description, uint8 proposalType)',
-            'event ProposalCreated(uint256 indexed proposalId, address indexed proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description, uint8 proposalType)',
-            'event ProposalCreated(uint256 proposalId, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)',
-            'event ProposalCreated(uint256 proposalId, address proposer, address votingModule, bytes proposalData, uint256 startBlock, uint256 endBlock, string description)',
-            'event ProposalDeadlineUpdated(uint256 proposalId, uint64 deadline)',
-            'event ProposalExecuted(uint256 proposalId)', //TODO: frh no proposals have been executed
-            'event ProposalThresholdSet(uint256 oldProposalThreshold, uint256 newProposalThreshold)',
-            'event ProposalTypeSet(uint256 indexed proposalTypeId, uint16 quorum, uint16 approvalThreshold, string name)',
-            'event ProposalTypeUpdated(uint256 indexed proposalId, uint8 proposalType)', // Not on center db since there have been 0 events
-            'event QuorumNumeratorUpdated(uint256 oldQuorumNumerator, uint256 newQuorumNumerator)',
-            'event Transfer(address indexed from, address indexed to, uint256 value)',
-            'event Unpaused(address account)',
-            'event Upgraded(address indexed implementation)',
+            // Should be done together DelegateChanged, DelegateVotesChanged
+            // 'event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate)',
+            // 'event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance)'
+            // 'event ProposalCreated(uint256 indexed proposalId, address indexed proposer, address indexed votingModule, bytes proposalData, uint256 startBlock, uint256 endBlock, string description, uint8 proposalType)',
+            // 'event ProposalCreated(uint256 indexed proposalId, address indexed proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description, uint8 proposalType)',
+            // 'event ProposalCreated(uint256 proposalId, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)',
+            // 'event ProposalCreated(uint256 proposalId, address proposer, address votingModule, bytes proposalData, uint256 startBlock, uint256 endBlock, string description)',
             'event VoteCast(address indexed proxy, address indexed voter, address[] authority, uint256 proposalId, uint8 support)',
             'event VoteCast(address indexed voter, uint256 proposalId, uint8 support, uint256 weight, string reason)',
             'event VotesCast(address[] proxies, address indexed voter, address[][] authorities, uint256 proposalId, uint8 support)',
-            'event VoteCastWithParams(address indexed voter, uint256 proposalId, uint8 support, uint256 weight, string reason, bytes params)',
-            'event VotingDelaySet(uint256 oldVotingDelay, uint256 newVotingDelay)',
-            'event VotingPeriodSet(uint256 oldVotingPeriod, uint256 newVotingPeriod)'
+            'event VoteCastWithParams(address indexed voter, uint256 proposalId, uint8 support, uint256 weight, string reason, bytes params)'
             // @ts-ignore
         ]).concat([
             {
@@ -242,8 +220,67 @@ export default async function getFeedLogs() {
                 "type": "event"
             },
         ]),
-        // 'latest' | 'earliest' | 'pending' | 'safe' | 'finalized'
-        // TODO: frh if too big kb error change to latest
-        fromBlock: 'safe'
+        fromBlock: fromBlock,
+        toBlock: toBlock
     });
+
+    if (logs.length < 20) {
+        // TODO: frh -> configure this depending on the event, for delegation and subdelegation is different
+        let _fromBlock = fromBlock > 2000n ? fromBlock - 2000n : 0n;
+        return await fetchLogs(publicClient, _fromBlock, toBlock - 2000n)
+    } else {
+        return logs;
+    }
 }
+
+export default async function getFeedLogs() {
+    console.time("answer time");
+    console.timeLog("answer time");
+    console.log("hola 1");
+    const publicClient = createPublicClient({
+        chain: optimism,
+        transport: http(`https://opt-mainnet.g.alchemy.com/v2/${alchemyId}`)
+    }) as PublicClient<Transport, Chain>;
+
+    // TODO: frh -> delete this if not needed
+    const block = await publicClient.getBlock();
+    let { number: toBlock } = block;
+    //  You can make eth_getLogs requests with up to a 2K block range and no limit on the response size
+    let fromBlock = toBlock - 2000n;
+    console.timeLog("answer time");
+    console.log("hola 2");
+    const logs = await fetchLogs(publicClient, fromBlock, toBlock);
+    console.timeEnd("answer time");
+    console.log("hola 3");
+    return logs;
+    // const _logs = logs.map(async (log) => {
+    //     const { timestamp } = await publicClient.getBlock({ blockNumber: log.blockNumber });
+    //     return {
+    //         ...log,
+    //         timestamp
+    //     }
+    // });
+
+    // return await Promise.all(_logs);
+}
+
+// Other OP events we are not tracking
+// 'event AdminChanged(address previousAdmin, address newAdmin)',
+// 'event Approval(address indexed owner, address indexed spender, uint256 value)',
+// 'event BeaconUpgraded(address indexed beacon)', // Beacon has never been upgraded yet
+// 'event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance)'
+// 'event Initialized(uint8 version)',
+// 'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
+// 'event Paused(address account)',
+// 'event ProposalCanceled(uint256 proposalId)',
+// 'event ProposalDeadlineUpdated(uint256 proposalId, uint64 deadline)',
+// 'event ProposalExecuted(uint256 proposalId)',
+// 'event ProposalThresholdSet(uint256 oldProposalThreshold, uint256 newProposalThreshold)',
+// 'event ProposalTypeSet(uint256 indexed proposalTypeId, uint16 quorum, uint16 approvalThreshold, string name)',
+// 'event ProposalTypeUpdated(uint256 indexed proposalId, uint8 proposalType)', // Not on center db since there have been 0 events
+// 'event QuorumNumeratorUpdated(uint256 oldQuorumNumerator, uint256 newQuorumNumerator)',
+// 'event Transfer(address indexed from, address indexed to, uint256 value)',
+// 'event Unpaused(address account)',
+// 'event Upgraded(address indexed implementation)',
+// 'event VotingDelaySet(uint256 oldVotingDelay, uint256 newVotingDelay)',
+// 'event VotingPeriodSet(uint256 oldVotingPeriod, uint256 newVotingPeriod)'
