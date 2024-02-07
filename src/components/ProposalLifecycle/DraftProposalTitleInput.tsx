09e864659e5a780b6c72bc120ee73b1225e2be1d
woj.eth <wojciechkulikowski@icloud.com>
2024-02-07 13:36:01 +0100

    wojeth/agora 1566 submit proposal component (#113)
    * Frodriguezhervias/agora 1435 build results page for rpgf (#99)
    
    * WIP
    
    * style results table
    
    * WIP
    
    * WIP
    
    * pagination of retropgf results
    
    * list correct categories
    
    * WIP
    
    * WIP
    
    * filters without styling
    
    * thinking if table is the best fit for styles
    
    * add todo
    
    * remove flex css from table, refactor css for table
    
    * shuffle done
    
    * styles of filters done
    
    * retroPGFShareCardDialog
    
    * remove todo
    
    * minor fixes
    
    * minor fix loading
    
    * change font size
    
    * Frodriguezhervias/agora 1542 build rpgf project details page (#103)
    
    * WIP
    
    * WIP human address and list not showing in query
    
    * minor fix
    
    * minor fix dialog
    
    * minor fixes
    
    * remove list query and list components
    
    * last ui fixes applicant page
    
    * margin top 32px
    
    * wojeth/agora-1520 Add your vote at the top of the proposal votes list (#100)
    
    * Add your vote at the top of the proposal votes list
    
    * Remove user votes when they disconnect their wallet
    
    * Remove console log
    
    * Support optimistic proposals
    
    * Add approval proposal votes
    
    Co-authored-by: Stepan <stepan-cubic6@users.noreply.github.com>
    
    * Add proposals votes list
    
    * Clean up imports
    
    ---------
    
    Co-authored-by: Stepan <stepan-cubic6@users.noreply.github.com>
    
    * Fix an awkward layout shift on first render (#107)
    
    * Add proposal review and submit
    
    * Add state to the proposal
    
    * Add mock submit button
    
    * Add data context to the form
    
    * Add display data in the end
    
    * fix ts
    
    ---------
    
    Co-authored-by: Ferrodri <f.rodriguez.hervias@gmail.com>
    Co-authored-by: Stepan <stepan-cubic6@users.noreply.github.com>

--- src/components/ProposalLifecycle/DraftProposalTitleInput.tsx
+++ src/components/ProposalLifecycle/DraftProposalTitleInput.tsx
@@ -1,12 +1,19 @@
-interface DraftProposalTextInputProps {
+"use client";
+
+import React, { useContext } from "react";
+import { ProposalLifecycleDraftContext } from "@/contexts/ProposalLifecycleDraftContext";
+
+interface DraftProposalTitleInputProps {
   label: string;
   placeholder: string;
 }
 
-const DraftProposalTextInput: React.FC<DraftProposalTextInputProps> = (
+const DraftProposalTitleInput: React.FC<DraftProposalTitleInputProps> = (
   props
 ) => {
   const { label, placeholder } = props;
+
+  const { state, updateTitle } = useContext(ProposalLifecycleDraftContext);
 
   return (
     <div className="flex flex-col px-6 mb-5">
@@ -14,9 +21,11 @@ const DraftProposalTextInput: React.FC<DraftProposalTextInputProps> = (
       <input
         className="py-3 px-4 w-full border border-gray-eo placeholder-gray-af bg-gray-fa rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-af focus:border-transparent"
         placeholder={placeholder}
+        value={state.title}
+        onChange={(e) => updateTitle(e.target.value)}
       ></input>
     </div>
   );
 };
 
-export default DraftProposalTextInput;
+export default DraftProposalTitleInput;
