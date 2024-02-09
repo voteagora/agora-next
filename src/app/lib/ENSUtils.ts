b3de90a0a323cdc91311fd4250e045158a8da671
Kent Fenwick <kent.fenwick@gmail.com>
2024-02-08 23:21:40 -0500

    Added helper ENS methods

--- src/app/lib/ENSUtils.ts
+++ src/app/lib/ENSUtils.ts
@@ -1,4 +1,6 @@
 import { ethProvider } from "@/app/lib/provider";
+import { isAddress } from "viem";
+import { truncateAddress } from "@/app/lib/utils/text";
 
 export async function resolveENSName(name: string) {
   const address = await ethProvider.resolveName(name);
@@ -7,4 +9,40 @@ export async function resolveENSName(name: string) {
   }
 
   return address.toLowerCase();
+}
+
+export async function reverseResolveENSName(
+  address: string
+): Promise<string | null> {
+  try {
+    const ensName = await ethProvider.lookupAddress(address);
+
+    return ensName || null;
+  } catch (error) {
+    console.error("ENS Resolution Error", error);
+    return null;
+  }
+}
+
+export async function processAddressOrEnsName(addressOrENSName: string) {
+  if (isAddress(addressOrENSName)) {
+    const ensName = await reverseResolveENSName(addressOrENSName.toLowerCase());
+    if (ensName) {
+      return ensName;
+    } else {
+      return truncateAddress(addressOrENSName);
+    }
+  } else {
+    try {
+      const address = await resolveENSName(addressOrENSName);
+      if (address) {
+        return addressOrENSName;
+      } else {
+        return "";
+      }
+    } catch (error) {
+      console.error("Error resolving ENS name:", error);
+      return null;
+    }
+  }
 }
