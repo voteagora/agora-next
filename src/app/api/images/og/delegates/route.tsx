85a3584b5ee89cbef12d888158c13793f33813f8
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2024-02-15 10:51:39 -0700

    Andrei/agora 1527 design dynamic og image graphics for optimism (#123)
    * webstrom ied ignore
    
    * basic proposal template
    
    * Proposal meta and minor utils refactor
    
    * Proposal image meta
    
    * Proposal previews
    
    * Header scaling
    
    * Work in progress on the delegate card
    
    * Delegate layout
    
    * WIP on delegate card preview
    
    * WIP displaying delegate card
    
    * Using images as backgrounds
    
    * API routes for images and additional backgrounds
    
    * Updated meta
    
    * Backgrounds
    
    * Refactored background images
    
    * Finished removing bgs
    
    * OG titles update
    
    * Import cleanup
    
    * pages router to app router
    
    * Delegate route
    
    * Build issues and minor bugs
    
    * Added helper ENS methods
    
    * Added ENS processing logic
    
    * wip icluding avatar
    
    * ens avatar
    
    * Misc cleanup
    
    * Minor refactor of shared assets
    
    * Simplified asset paths
    
    * Code review
    
    * Prettier
    
    * Prettier
    
    * misc
    
    ---------
    
    Co-authored-by: Kent Fenwick <kent.fenwick@gmail.com>

---
+++ src/app/api/images/og/delegates/route.tsx
@@ -0,0 +1,77 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+import { LogoPill } from "@/app/api/images/og/assets/shared";
+
+export const runtime = "edge";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  const title = searchParams.get("title") || "Optimism Agora";
+  const description =
+    searchParams.get("description") ||
+    "Home of Token House Governance and RPGF";
+
+  const interBoldFont = await fetch(
+    new URL("../assets/Inter-Black.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  const interRegularFont = await fetch(
+    new URL("../assets/Inter-Medium.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  const bg = await fetch(
+    new URL("../assets/og-delegates-bg.png", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundColor: "white",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          fontFamily: '"Inter"',
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+        }}
+      >
+        {/* @ts-ignore */}
+        <img src={bg} style={{ position: "absolute" }} />
+        <div tw="flex h-full w-full px-[76px] pt-[70px] pb-[110px]">
+          <div tw="flex flex-col justify-between h-full w-[470px]">
+            <LogoPill />
+
+            <div tw="flex flex-col">
+              <div tw="font-bold text-5xl w-full">{title}</div>
+              <div tw="font-regular mt-[30px] text-4xl text-gray-600">
+                {description}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          data: interBoldFont,
+          name: "Inter",
+          style: "normal",
+          weight: 900,
+        },
+        {
+          data: interRegularFont,
+          name: "Inter",
+          style: "normal",
+          weight: 500,
+        },
+      ],
+    }
+  );
+}
