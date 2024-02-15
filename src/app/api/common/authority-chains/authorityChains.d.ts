4544efb644e1ff567bc352feffdbe96d593d613d
Stepan Arsentjev <stepandel@gmail.com>
2024-02-15 09:51:05 -0500

    Harden partial delegation (#127)
    * import prisma
    
    * validate chains before voting
    
    * update advanced vp query based on new architecture
    
    * exculde subdelegations from vp calculation
    
    * fix voting power calc
    
    * use delegatable allowance

---
+++ src/app/api/common/authority-chains/authorityChains.d.ts
@@ -0,0 +1,22 @@
+import { OptimismAuthorityChainsSnaps } from "@prisma/client";
+
+export type AuthorityChainsSnaps = OptimismAuthorityChainsSnaps;
+
+export type AuthorityChainRules = {
+  allowance: number;
+  allowance_type: number;
+  custom_rule: string;
+  not_valid_after: number;
+  not_valid_before: number;
+  max_redelegations: number;
+  blocks_before_vote_closes: number;
+};
+
+export type AuhtorityChainsAggregate = {
+  chains: String[][];
+  rules: Prisma.JsonValue[][];
+  balances: Decimal[];
+  proxies: String[];
+  subdelegated_share: Decimal;
+  subdelegated_amount: Decimal;
+};
