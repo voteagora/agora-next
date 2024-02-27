35501cd6288ae3ad1b645b013b3932f8350d29a9
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-02-27 19:31:45 +0100

    useSwitch network functionality done (#136)
    * useSwitch network functionality done
    
    * changes in switch network dialog
    
    * add cursor pointer
    
    * cannot close switch optimism dialog
    
    * switch to optimism modal for every chain, disallow connectkit modal
    
    * remove comments

---
+++ src/components/Dialogs/SwitchNetworkDialog/SwitchNetworkDialog.tsx
@@ -0,0 +1,38 @@
+import { useNetwork, useSwitchNetwork } from "wagmi";
+import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+
+export function SwitchNetwork({
+  chainId,
+  closeDialog,
+}: {
+  chainId: number;
+  closeDialog: () => void;
+}) {
+  const { chain } = useNetwork();
+  const { switchNetwork } = useSwitchNetwork();
+
+  useEffect(() => {
+    if (chain?.id === chainId) {
+      closeDialog();
+    }
+  }, [chain?.id, chainId, closeDialog]);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <img src="/images/action-pending.svg" className="w-full" />
+      <h1 className="text-2xl font-extrabold">Switch Networks</h1>
+      <p>Wrong network detected, switch to Optimism to continue.</p>
+      <Button
+        variant="outline"
+        className="font-bold"
+        onClick={() => {
+          switchNetwork?.(chainId);
+          closeDialog();
+        }}
+      >
+        Switch to Optimism
+      </Button>
+    </div>
+  );
+}
