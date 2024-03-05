15b5f7030c32607df3e296621c37bd43e94ee212
andreitr <andrei.taraschuk@gmail.com>
2024-03-05 11:15:38 -0700

    Tenant contracts

--- src/lib/tenantContract.ts
+++ src/lib/tenantContract.ts
@@ -1,44 +1,44 @@
 import { BaseContract } from "ethers";
 import { Address } from "viem";
 
-interface ITenantContract {
+export enum TenantContractType {"GOVERNOR", "TOKEN", "ALLIGATOR", "TYPES_CONFIGURATOR" }
+
+type TenantContractParams = {
   abi: any;
   chainId: number;
   contract: BaseContract;
-  mainnetAddress: Address;
-  testnetAddress?: Address;
+  address: Address;
+  type: TenantContractType;
   v6UpgradeBlock?: number;
-}
+};
 
 export class TenantContract {
-  private _isProd: boolean;
-  readonly abi: any;
-  readonly chainId: number;
-  readonly contract: BaseContract;
-  readonly mainnetAddress: Address;
-  readonly testnetAddress?: Address;
-  readonly v6UpgradeBlock?: number;
+  public abi: any;
+  public chainId: number;
+  public contract: BaseContract;
+  private _address: Address;
+  public type: TenantContractType;
+  public v6UpgradeBlock?: number;
 
-  constructor(props: ITenantContract) {
-    this.abi = props.abi;
-    this.chainId = props.chainId;
-    this.contract = props.contract;
-    this.mainnetAddress = props.mainnetAddress;
-    this.testnetAddress = props.testnetAddress;
-    this.v6UpgradeBlock = props.v6UpgradeBlock;
-
-    this._isProd = process.env.NEXT_PUBLIC_AGORA_ENV === "prod";
+  constructor({
+                abi,
+                chainId,
+                contract,
+                address,
+                type,
+                v6UpgradeBlock,
+              }: TenantContractParams,
+  ) {
+    this.abi = abi;
+    this.chainId = chainId;
+    this.contract = contract;
+    this._address = address;
+    this.type = type;
+    this.v6UpgradeBlock = v6UpgradeBlock;
   }
 
-  /**
-   * Returns a contract based on the current environment and applies the
-   * lover case transformation.
-   */
+
   get address(): string {
-    return (
-      this._isProd || !this.testnetAddress
-        ? this.mainnetAddress
-        : this.testnetAddress
-    ).toLowerCase();
+    return this._address.toLowerCase();
   }
 }
