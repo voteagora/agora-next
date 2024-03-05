984b0f371fad4d37f8b33d1aad2cc65027c8497c
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-06 00:47:39 +0100

    Ferrodri/agora 1688 display exact date in proposals (#154)
    * format active proposal end time
    
    * fix with utc time
    
    * try timezone one more time
    
    * local timezone
    
    * add log
    
    * proposal time status should be a client component
    
    * remove log
    
    * comment

--- src/components/Proposals/Proposal/ProposalTimeStatus.jsx
+++ src/components/Proposals/Proposal/ProposalTimeStatus.jsx
@@ -1,3 +1,6 @@
+// Use client for local timezone instead of server timezone
+"use client";
+
 import { HStack } from "@/components/Layout/Stack";
 import { formatDistanceToNowStrict } from "date-fns";
 
@@ -5,16 +8,25 @@ export default function ProposalTimeStatus({
   proposalStatus,
   proposalEndTime,
 }) {
+  const options = {
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+  };
+
+  const activeProposalEndTime = new Intl.DateTimeFormat(
+    "en-US",
+    options
+  ).format(proposalEndTime);
+
   switch (proposalStatus) {
     case "PENDING":
       return <HStack gap={1}>Voting</HStack>;
 
     case "ACTIVE":
-      return (
-        <HStack gap={1}>
-          Ends in {formatDistanceToNowStrict(proposalEndTime)}
-        </HStack>
-      );
+      return <HStack gap={1}>Ends {activeProposalEndTime}</HStack>;
 
     case "CANCELLED":
       return <HStack gap={1}></HStack>;
