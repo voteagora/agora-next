c1e447786a95458bba10133dafe92dd67f642f2e
andreitr <andrei.taraschuk@gmail.com>
2024-03-06 14:34:04 -0700

    leftover namespace

--- src/app/api/common/metrics/getMetrics.ts
+++ src/app/api/common/metrics/getMetrics.ts
@@ -4,7 +4,7 @@ import Tenant from "@/lib/tenant";
 
 export async function getMetrics() {
   const { namespace } = Tenant.getInstance();
-  const totalSupply = await getTokenSupply(namespace);
+  const totalSupply = await getTokenSupply();
   const votableSupply = await prisma[`${namespace}VotableSupply`].findFirst({});
   const quorum = (BigInt(Number(votableSupply?.votable_supply)) * 30n) / 100n;
 
