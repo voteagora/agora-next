c2dbdd68a644ee5a973f6c988c695591ee39be24
ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-06 20:07:45 +0100

    minor fix

--- src/app/api/common/governanceCalendar/getGovernanceCalendar.ts
+++ src/app/api/common/governanceCalendar/getGovernanceCalendar.ts
@@ -8,17 +8,17 @@ const parseICSDates = (_startDate: string, _endDate: string) => {
     const startDateTrim = _startDate.trim();
     const endDateTrim = _endDate.trim();
 
-    const startDate = new Date(
+    const startDate = new Date(Date.UTC(
         parseInt(startDateTrim.substring(0, 4), 10), // Year
         parseInt(startDateTrim.substring(4, 6), 10) - 1, // Month
         parseInt(startDateTrim.substring(6, 8), 10) // Day
-    );
+    ));
 
-    const endDate = new Date(
+    const endDate = new Date(Date.UTC(
         parseInt(endDateTrim.substring(0, 4), 10), // Year
         parseInt(endDateTrim.substring(4, 6), 10) - 1, // Month
         parseInt(endDateTrim.substring(6, 8), 10) // Day
-    );
+    ));
 
     return {
         startDate,
@@ -50,6 +50,8 @@ function parseICS(icsData: string) {
                     (currentEvent as Event)["DTEND;VALUE=DATE"]
                 );
                 if (isCurrentEvent(startDate, endDate)) {
+                    // Calendar ends one day later that it is supposed to
+                    endDate.setDate(endDate.getDate() - 1);
                     const day = endDate.getDate();
                     const suffix = (day >= 11 && day <= 13) ? 'th' : ['st', 'nd', 'rd'][day % 10 - 1] || 'th';
 
@@ -58,11 +60,11 @@ function parseICS(icsData: string) {
                         day: "numeric"
                     };
 
-                    const _endDate = new Intl.DateTimeFormat("en-US", options).format(endDate);
+                    const formattedEndDate = new Intl.DateTimeFormat("en-US", options).format(endDate);
 
                     event = {
                         title: (currentEvent as Event).SUMMARY.trim(),
-                        endDate: _endDate.replace(/\d+/, (day + suffix)),
+                        endDate: formattedEndDate.replace(/\d+/, (day + suffix)),
                         reviewPeriod: (currentEvent as Event).SUMMARY.includes("Review Period")
                     };
                     currentEvent = {};
