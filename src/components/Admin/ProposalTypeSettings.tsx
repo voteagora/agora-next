fc6eb4a3d0f5f0f6d8dadef03456b86ace3a8d6a
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2024-03-10 16:07:25 -0600

    Loading prop types form the top level page (#160)

--- src/components/Admin/ProposalTypeSettings.tsx
+++ src/components/Admin/ProposalTypeSettings.tsx
@@ -1,66 +1,51 @@
 "use client";
 
 import { Separator } from "@/components/ui/separator";
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState } from "react";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { OptimismProposalTypes } from "@prisma/client";
 import ProposalType from "./ProposalType";
-import { fetchProposalTypes } from "@/app/admin/actions";
-import AgoraLoader from "@/components/shared/AgoraLoader/AgoraLoader";
 
 // TODO: Take init values from the chain
 export default function ProposalTypeSettings({
   votableSupply,
+  proposalTypes,
 }: {
   votableSupply: string;
+  proposalTypes: OptimismProposalTypes[];
 }) {
-  const [loading, setLoading] = useState(true);
-  const [proposalTypes, setProposalTypes] = useState<
-    { name: string; quorum: number; approval_threshold: number }[]
-  >([]);
+  const fmtPropTypes = proposalTypes.map(
+    ({ quorum, approval_threshold, name }) => ({
+      name,
+      quorum: Number(quorum) / 100,
+      approval_threshold: Number(approval_threshold) / 100,
+    })
+  );
 
-  useEffect(() => {
-    const getProposalTypes = async () => {
-      const response = (await fetchProposalTypes().catch((error) =>
-        console.error(error)
-      )) as OptimismProposalTypes[];
-      const _proposalTypes = response.map(
-        ({ quorum, approval_threshold, name }) => ({
-          name,
-          quorum: Number(quorum) / 100,
-          approval_threshold: Number(approval_threshold) / 100,
-        })
-      );
-      setLoading(false);
-      setProposalTypes(_proposalTypes);
-    };
-    getProposalTypes();
-  }, []);
+  const [propTypes, setPropTypes] = useState(fmtPropTypes);
 
   return (
     <section className="gl_box">
       <h1 className="font-extrabold text-2xl">Proposal type settings</h1>
       <p>Create and manage different types of proposals</p>
-      {loading ? (
-        <AgoraLoader />
-      ) : (
-        proposalTypes.map((proposalType, key) => (
-          <Fragment key={key}>
-            <ProposalType
-              votableSupply={votableSupply}
-              proposalType={proposalType}
-              index={key}
-            />
-            <Separator className="my-8" />
-          </Fragment>
-        ))
-      )}
+
+      {propTypes.map((proposalType, key) => (
+        <Fragment key={key}>
+          <ProposalType
+            votableSupply={votableSupply}
+            proposalType={proposalType}
+            index={key}
+          />
+          <Separator className="my-8" />
+        </Fragment>
+      ))}
+
       <div
         className="inline-flex items-center gap-2 cursor-pointer"
         onClick={() => {
-          setProposalTypes((prev) => [
-            ...prev,
+          setPropTypes((prevPropTypes) => [
+            ...prevPropTypes,
             { quorum: 50, approval_threshold: 50, name: "" },
           ]);
         }}
