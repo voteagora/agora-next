8731f9ea8f43ff1b1b48f9d15edd846c5cfdd1a0
ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-15 19:03:26 +0100

    fix negative amount to allocate

--- src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
+++ src/components/Dialogs/AdvancedDelegateDialog/SubdelegationRow.tsx
@@ -35,7 +35,8 @@ function SubdelegationToRow({
     return idx === index ? sum : sum + current;
   }, 0);
 
-  const amountToAllocate = availableBalanceNumber - sumOtherAllowances;
+  const amountToAllocateRaw = availableBalanceNumber - sumOtherAllowances;
+  const amountToAllocate = amountToAllocateRaw > 0 ? amountToAllocateRaw : 0;
 
   const percent =
     Number.isNaN(allowance) || allowance === 0
@@ -102,35 +103,40 @@ function SubdelegationToRow({
   }, [allowance]);
 
   return (
-    <div className={styles.sub_row}>
-      <HStack gap={3}>
-        <div className={styles.avatar}>
-          <ENSAvatar ensName={data} />
-        </div>
-        <VStack>
-          <p className={styles.subtitle}>Delegated to</p>
-          <div className={styles.address_to}>
-            <HumanAddress address={to} />
+    <div className="flex flex-col border-b border-dashed border-gray-300 py-4">
+      <div className={styles.sub_row}>
+        <HStack gap={3}>
+          <div className={styles.avatar}>
+            <ENSAvatar ensName={data} />
           </div>
-        </VStack>
-      </HStack>
-      <div className="relative flex rounded-md border border-input bg-gray-fa">
-        {/* TODO: improve UX of this Input, what if value is 10,000,000 */}
-        <Input
-          value={newAllowanceInput}
-          placeholder="0"
-          className="max-w-[4rem] pl-2 pr-1 text-right focus:outline-none focus:ring-0"
-          variant="none"
-          onChange={(e) => handleAllowanceInput(e)}
-          type="text"
-          inputMode="numeric"
-        />
-        <div className="flex items-center pr-2 pl-1 w-[100px]">
-          <p>OP</p>
-          <div className="bg-input w-[1px] h-6 mx-1"></div>
-          <p>{percent}%</p>
+          <VStack>
+            <p className={styles.subtitle}>Delegated to</p>
+            <div className={styles.address_to}>
+              <HumanAddress address={to} />
+            </div>
+          </VStack>
+        </HStack>
+        <div className="relative flex rounded-md border border-input bg-gray-fa">
+          {/* TODO: improve UX of this Input, what if value is 10,000,000 */}
+          <Input
+            value={newAllowanceInput}
+            placeholder="0"
+            className="max-w-[4rem] pl-2 pr-1 text-right focus:outline-none focus:ring-0"
+            variant="none"
+            onChange={(e) => handleAllowanceInput(e)}
+            type="text"
+            inputMode="numeric"
+          />
+          <div className="flex items-center pr-2 pl-1 w-[100px]">
+            <p>OP</p>
+            <div className="bg-input w-[1px] h-6 mx-1"></div>
+            <p>{percent}%</p>
+          </div>
         </div>
       </div>
+      {allowances[index] === 0 && amountToAllocateRaw < 0 && (
+        <span className="pt-2">Total delegatable votes reached</span>
+      )}
     </div>
   );
 }
