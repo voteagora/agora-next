5ec563210fd7a53eb7ac0172e991ea996b3e1545
Flip-Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-03-18 16:33:36 -0600

    [1729]👁️ Integrate OpenTelemetry in Agora App (#164)
    * 👁️ add OTel
    * 📝 add env.sample to document env vars
    * 📝 add readme section on OTEL
    * 📦 add packages for custom OTel config
    * 📦 add auto instrumentation package for node in otel
    * 🛠️ add console exporters for traces metrics logs aand spans
    * 🤏 disable log coloring in production
    * 🤏 play nice w vercel + NEXT_PUBLIC_AGORA_ENV
    * 🤏 fix production environment name

--- src/app/lib/logging.ts
+++ src/app/lib/logging.ts
@@ -1,6 +1,10 @@
 import { performance } from "perf_hooks";
 import * as util from "util";
 
+// 'dev' is used in vercel dev and preview, both of which need to have coloring disabled
+// for emission and ingestion of logs into datadog
+const log_emission = process.env.NEXT_PUBLIC_AGORA_ENV === "prod" || process.env.NEXT_PUBLIC_AGORA_ENV === "dev";
+
 const time_this = async <T>(
   fn: () => Promise<T>,
   log_fields: Record<string, any>
@@ -15,7 +19,7 @@ const time_this = async <T>(
     console.log(
       util.inspect(
         { ...log_fields, time: end - start },
-        { showHidden: false, depth: null, colors: true }
+        { showHidden: false, depth: null, colors: !log_emission }
       )
     );
   }
@@ -35,7 +39,7 @@ const time_this_sync = <T>(
     console.log(
       util.inspect(
         { ...log_fields, time: end - start },
-        { showHidden: false, depth: null, colors: true }
+        { showHidden: false, depth: null, colors: !log_emission }
       )
     );
   }
