73669b784ccfa5424f854eeb12a329e19177978f
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-03-20 17:55:59 -0400

    🔎 plug in zod validators for query parameters

--- src/app/api/v1/delegates/route.ts
+++ src/app/api/v1/delegates/route.ts
@@ -8,23 +8,29 @@ import {
   type DelegatesGetPayload,
 } from "@/app/api/common/delegates/delegate";
 
+import { createOptionalNumberValidator, createOptionalStringValidator } from "@/app/api/common/utils/validators";
+
 const DEFAULT_SORT = "most_delegators";
 const DEFAULT_MAX_LIMIT = 100;
 const DEFAULT_LIMIT = 20;
 const DEFAULT_OFFSET = 0;
 
+const sortValidator = createOptionalStringValidator(["most_delegators", "weighted_random"], DEFAULT_SORT);
+const limitValidator = createOptionalNumberValidator(1, DEFAULT_MAX_LIMIT, DEFAULT_LIMIT);
+const offsetValidator = createOptionalNumberValidator(0, Number.MAX_SAFE_INTEGER, DEFAULT_OFFSET);
+
 export async function GET(request: NextRequest) {
   const params = request.nextUrl.searchParams;
   try {
-    const sort = z.literal("most_delegators").or(z.literal("weighted_random")).parse(params.get("sort")) || DEFAULT_SORT;
-    const limit = z.number().min(1).max(DEFAULT_MAX_LIMIT).parse(params.get("limit")) || DEFAULT_LIMIT;
-    const offest = z.number().min(0).parse(params.get("offset")) || DEFAULT_OFFSET;
+    const sort = sortValidator.parse(params.get("sort"));
+    const limit = limitValidator.parse(params.get("limit"));
+    const offest = offsetValidator.parse(params.get("offset"));
     const delegatesResult = await fetchDelegates({ sort: sort, limit: limit, offset: offest});
     return Response.json(delegatesResult.delegates);
   }
   catch (e: any) {
     if (e instanceof ZodError) {
-      return new Response("Invalid query parameters", { status: 400 });
+      return new Response("Invalid query parameters: " + e.toString(), { status: 400 });
     }
 
     return new Response("Internal server error", { status: 500 });
