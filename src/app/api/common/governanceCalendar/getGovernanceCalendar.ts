ec35fd5702c83ad31e0f26d25203d536c669acc6
andreitr <andrei.taraschuk@gmail.com>
2024-03-21 15:58:33 -0600

    Prettier

--- src/app/api/common/governanceCalendar/getGovernanceCalendar.ts
+++ src/app/api/common/governanceCalendar/getGovernanceCalendar.ts
@@ -11,17 +11,21 @@ const parseICSDates = (_startDate: string, _endDate: string) => {
   const startDateTrim = _startDate.trim();
   const endDateTrim = _endDate.trim();
 
-  const startDate = new Date(Date.UTC(
-    parseInt(startDateTrim.substring(0, 4), 10), // Year
-    parseInt(startDateTrim.substring(4, 6), 10) - 1, // Month
-    parseInt(startDateTrim.substring(6, 8), 10), // Day
-  ));
+  const startDate = new Date(
+    Date.UTC(
+      parseInt(startDateTrim.substring(0, 4), 10), // Year
+      parseInt(startDateTrim.substring(4, 6), 10) - 1, // Month
+      parseInt(startDateTrim.substring(6, 8), 10) // Day
+    )
+  );
 
-  const endDate = new Date(Date.UTC(
-    parseInt(endDateTrim.substring(0, 4), 10), // Year
-    parseInt(endDateTrim.substring(4, 6), 10) - 1, // Month
-    parseInt(endDateTrim.substring(6, 8), 10), // Day
-  ));
+  const endDate = new Date(
+    Date.UTC(
+      parseInt(endDateTrim.substring(0, 4), 10), // Year
+      parseInt(endDateTrim.substring(4, 6), 10) - 1, // Month
+      parseInt(endDateTrim.substring(6, 8), 10) // Day
+    )
+  );
 
   return {
     startDate,
@@ -50,10 +54,12 @@ function parseICS(icsData: string) {
       if ((currentEvent as Event).SUMMARY.includes("Voting Cycle")) {
         const { startDate, endDate } = parseICSDates(
           (currentEvent as Event)["DTSTART;VALUE=DATE"],
-          (currentEvent as Event)["DTEND;VALUE=DATE"],
+          (currentEvent as Event)["DTEND;VALUE=DATE"]
         );
         if (isCurrentEvent(startDate, endDate)) {
-          const reviewPeriod = (currentEvent as Event).SUMMARY.includes("Review Period");
+          const reviewPeriod = (currentEvent as Event).SUMMARY.includes(
+            "Review Period"
+          );
           if (!reviewPeriod) {
             /**
              * On voting period we should display the last day (the day voting ends). On review period we
@@ -64,19 +70,27 @@ function parseICS(icsData: string) {
             endDate.setDate(endDate.getDate() - 1);
           }
           const day = endDate.getDate();
-          const suffix = (day >= 11 && day <= 13) ? "th" : ["st", "nd", "rd"][day % 10 - 1] || "th";
+          const suffix =
+            day >= 11 && day <= 13
+              ? "th"
+              : ["st", "nd", "rd"][(day % 10) - 1] || "th";
 
           const options: Intl.DateTimeFormatOptions = {
             month: "long",
             day: "numeric",
           };
 
-          const formattedEndDate = new Intl.DateTimeFormat("en-US", options).format(endDate);
+          const formattedEndDate = new Intl.DateTimeFormat(
+            "en-US",
+            options
+          ).format(endDate);
 
           event = {
             title: (currentEvent as Event).SUMMARY.trim(),
-            endDate: formattedEndDate.replace(/\d+/, (day + suffix)),
-            reviewPeriod: (currentEvent as Event).SUMMARY.includes("Review Period"),
+            endDate: formattedEndDate.replace(/\d+/, day + suffix),
+            reviewPeriod: (currentEvent as Event).SUMMARY.includes(
+              "Review Period"
+            ),
           };
           currentEvent = {};
           break;
@@ -96,7 +110,6 @@ function parseICS(icsData: string) {
 }
 
 export async function fetchGovernanceCalendar() {
-
   const { ui } = Tenant.current();
   const link = ui.link("calendar");
 
@@ -104,12 +117,9 @@ export async function fetchGovernanceCalendar() {
     return null;
   }
 
-  const response = await fetch(
-    link.url,
-    {
-      method: "GET",
-    },
-  );
+  const response = await fetch(link.url, {
+    method: "GET",
+  });
   const calendarICS = await response.text();
   return parseICS(calendarICS);
 }
