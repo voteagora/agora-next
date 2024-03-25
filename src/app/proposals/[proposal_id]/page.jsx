302786fd591882bbdc48f35d8d9056e2516842f7
andreitr <andrei.taraschuk@gmail.com>
2024-03-25 16:31:19 -0600

    OG preview images

--- src/app/proposals/[proposal_id]/page.jsx
+++ src/app/proposals/[proposal_id]/page.jsx
@@ -26,13 +26,18 @@ export async function generateMetadata({ params }, parent) {
     title: title,
     description: description,
     openGraph: {
-      images: preview,
+      images: [
+        {
+          url: preview,
+          width: 1200,
+          height: 630,
+        },
+      ],
     },
     other: {
       ["twitter:card"]: "summary_large_image",
       ["twitter:title"]: title,
       ["twitter:description"]: description,
-      ["twitter:image"]: preview,
     },
   };
 }
