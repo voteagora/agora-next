1575bb76b16ad1885af850e81d5da42446e7e467
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2024-03-26 13:48:29 -0600

    WIP: ENS Tenant cleanup  (#189)
    * Implementing tenant links
    
    * wip
    
    * tenant links
    
    * tenant links
    
    * Links etc
    
    * Prettier
    
    * Logo cleanup
    
    * Duplicate og metadata
    
    * Misc
    
    * OG preview images
    
    * Metadata
    
    * Prettier

--- src/app/retropgf/3/summary/page.tsx
+++ src/app/retropgf/3/summary/page.tsx
@@ -22,19 +22,23 @@ export async function generateMetadata() {
     title: title,
     description: description,
     openGraph: {
-      images: preview,
+      images: [
+        {
+          url: preview,
+          width: 1200,
+          height: 630,
+        },
+      ],
     },
     other: {
       ["twitter:card"]: "summary_large_image",
       ["twitter:title"]: title,
       ["twitter:description"]: description,
-      ["twitter:image"]: preview,
     },
   };
 }
 
 export default function Page() {
-
   const { ui } = Tenant.current();
 
   if (!ui.toggle("retropgf")) {
