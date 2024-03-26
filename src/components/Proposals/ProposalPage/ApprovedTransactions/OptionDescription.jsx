ab372faae028a2b2031e79d2e088e0692bc6d3b1
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-26 19:58:50 +0100

    fix bug BigInt(0) (#203)

--- src/components/Proposals/ProposalPage/ApprovedTransactions/OptionDescription.jsx
+++ src/components/Proposals/ProposalPage/ApprovedTransactions/OptionDescription.jsx
@@ -1,10 +1,7 @@
 import TokenAmountDisplay from "@/components/shared/TokenAmountDisplay";
 import { shortAddress } from "@/lib/utils";
-import Tenant from "@/lib/tenant/tenant";
 
 export default function OptionDescription({ description, value, target }) {
-  const { token } = Tenant.current();
-
   return (
     <span>
       {"//"} {description} requesting <TokenAmountDisplay amount={value} />{" "}
