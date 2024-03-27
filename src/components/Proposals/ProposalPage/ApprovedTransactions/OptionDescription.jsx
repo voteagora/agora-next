b42f88ee0e2c6eadb57311cf7c6522cd3c0f8ce4
ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-27 20:47:00 +0100

    add note

--- src/components/Proposals/ProposalPage/ApprovedTransactions/OptionDescription.jsx
+++ src/components/Proposals/ProposalPage/ApprovedTransactions/OptionDescription.jsx
@@ -4,6 +4,7 @@ import { shortAddress } from "@/lib/utils";
 export default function OptionDescription({ description, value, target }) {
   return (
     <span>
+      {/* TODO: only leave option description for approval proposals, remove the rest. Also group this to only be shown once per option */}
       {"//"} {description} requesting <TokenAmountDisplay amount={value} />{" "}
       transfer to {shortAddress(target)}
     </span>
