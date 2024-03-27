870e3b87b9ff9d739a620bb9fb3225ca29e9895f
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-03-27 16:06:15 -0400

    🛠️ attempted fix for nonemission, use vercel::registerOTel

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,27 @@
-import { registerOTel } from '@vercel/otel'
+import { registerOTel } from "@vercel/otel";
+import {
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import {
+  PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
 
 export const SERVICE_NAME = 'agora-app';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./instrumentation.node');
-  } else {
-    registerOTel({ serviceName: SERVICE_NAME });
-  }
+  registerOTel({
+    serviceName: SERVICE_NAME,
+    traceExporter: new OTLPTraceExporter(),
+    metricReader: new PeriodicExportingMetricReader({
+      exporter: new OTLPMetricExporter(),
+    }),
+    spanProcessors: [
+      new SimpleSpanProcessor(
+        new OTLPTraceExporter()
+      )
+    ], 
+  });
 }
 
