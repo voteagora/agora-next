97b703ac25263c57519606f14654c98ae672a3da
ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-28 18:08:39 +0100

    file mismatch

--- src/app/api/v1/delegates/[addressorENSName]/route.ts
+++ src/app/api/v1/delegates/[addressorENSName]/route.ts
@@ -1,5 +1,13 @@
-export async function GET(request: Request) {
-    return new Response(JSON.stringify(`{result: "success"}`), {
-        status: 200,
-    });
+import { NextResponse, type NextRequest } from "next/server";
+import { fetchDelegate } from "@/app/api/common/delegates/getDelegates";
+
+export async function GET(request: NextRequest) {
+    try {
+        const addressOrENSName = request.nextUrl.pathname.split('/')[4];
+        const delegate = await fetchDelegate(addressOrENSName);
+        return NextResponse.json(delegate);
+    }
+    catch (e: any) {
+        return new Response("Internal server error: " + e.toString(), { status: 500 });
+    }
 }
