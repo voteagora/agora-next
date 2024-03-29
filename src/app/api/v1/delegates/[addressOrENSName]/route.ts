67ed59dea6e55640aef44bc5d80a78cfff0700e8
Ferrodri <f.rodriguez.hervias@gmail.com>
2024-03-29 17:14:16 +0100

    [1777] Fix voteHoverCard -> fetchDelegate server action not working (#200)
    * refactor votehovercard, introduce fetchdelegate apiroute
    
    * file mismatch
    
    * Delete src/app/api/v1/delegates/[addressorENSName] directory
    
    * authenticate API
    
    * credentials same origin
    
    * add corsW
    
    * config cors for prod or preview
    
    * deploy test
    
    * refactor agora api and revert auth middleware
    
    * revert to original middleware
    
    * authenticate user, change to delegates route and remove redirect to 404

---
+++ src/app/api/v1/delegates/[addressOrENSName]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { fetchDelegate } from "@/app/api/common/delegates/getDelegates";
+import { authenticateApiUser } from "@/app/lib/middleware/auth";
+import { setCurrentSpanAttributes } from "@/app/lib/logging";
+
+export async function GET(request: NextRequest) {
+    const authResponse = await authenticateApiUser(request);
+    setCurrentSpanAttributes({ "user_id": authResponse.userId })
+
+    if (!authResponse.authenticated) {
+        return new Response(authResponse.reason, { status: 401 });
+    }
+
+    try {
+        const addressOrENSName = request.nextUrl.pathname.split('/')[4];
+        const delegate = await fetchDelegate(addressOrENSName);
+        return NextResponse.json(delegate);
+    }
+    catch (e: any) {
+        return new Response("Internal server error: " + e.toString(), { status: 500 });
+    }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,7 @@ export function middleware(request: NextRequest) {
     }
   }
 }
- 
+
 export const config = {
   matcher: '/api/v1/:path*',
 }
