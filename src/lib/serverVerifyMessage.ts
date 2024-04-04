8f454ff17cf8039e529431390995dc8f44443c76
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2024-04-04 10:27:55 -0600

    Ether.fi delegate statement + Code of Conduct toggle  (#216)
    * Code of conduct flag
    
    * Updating delegate statement
    
    * typo

--- src/lib/serverVerifyMessage.ts
+++ src/lib/serverVerifyMessage.ts
@@ -1,27 +1,53 @@
 import "server-only";
 
-import { createPublicClient, http } from 'viem'
+import { createPublicClient, http } from "viem";
 import { mainnet, optimism } from "viem/chains";
-import { type DaoSlug } from "@prisma/client";
+import Tenant from "@/lib/tenant/tenant";
 
-
-export default async function verifyMessage({ address, message, signature, daoSlug }: {
-    address: `0x${string}`,
-    signature: `0x${string}`,
-    message: string,
-    daoSlug: DaoSlug
+export default async function verifyMessage({
+  address,
+  message,
+  signature,
+}: {
+  address: `0x${string}`;
+  signature: `0x${string}`;
+  message: string;
 }) {
-    // Alchemy key
-    const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
+  const { contracts } = Tenant.current();
 
-    const publicClient = createPublicClient({
-        chain: daoSlug === 'OP' ? optimism : mainnet,
-        transport: http(daoSlug === 'OP' ? `https://opt-mainnet.g.alchemy.com/v2/${alchemyId}` : `https://eth-goerli.g.alchemy.com/v2/${alchemyId}`)
-    });
+  const getChain = () => {
+    switch (contracts.token.chainId) {
+      case 1:
+        return mainnet;
+      case 10:
+        return optimism;
+      default:
+        throw new Error(`Invalid chainId: ${contracts.token.chainId}`);
+    }
+  };
 
-    return await publicClient.verifyMessage({
-        address,
-        message,
-        signature
-    });
+  const getRpcUrl = () => {
+    switch (contracts.token.chainId) {
+      case 1:
+        return `https://eth-mainnet.g.alchemy.com/v2/${alchemyId}`;
+      case 10:
+        return `https://opt-mainnet.g.alchemy.com/v2/${alchemyId}`;
+      default:
+        throw new Error(`Invalid chainId: ${contracts.token.chainId}`);
+    }
+  };
+
+  // Alchemy key
+  const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
+
+  const publicClient = createPublicClient({
+    chain: getChain(),
+    transport: http(getRpcUrl()),
+  });
+
+  return await publicClient.verifyMessage({
+    address,
+    message,
+    signature,
+  });
 }
