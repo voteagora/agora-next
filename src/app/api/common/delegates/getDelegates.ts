eecbb4e351e13059b987b76267446cd843d8fa3f
Flip-Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-08 10:12:04 -0700

    [AGORA-1851] Return delegate according to spec (#217)
    * 💻 add explicit typing for delegate statement; return extra fields for formal spec(incl proposals, issues)
    
    * 🤏🏷️ rename pagination metadata fields to camelCase

--- src/app/api/common/delegates/getDelegates.ts
+++ src/app/api/common/delegates/getDelegates.ts
@@ -107,8 +107,13 @@ async function getDelegatesApi(
         direct: delegate.direct_vp?.toFixed(0),
         advanced: delegate.advanced_vp?.toFixed(0) || "0",
       },
-      citizen: delegate.citizen,
-      statement: _delegates[index].statement,
+      isCitizen: delegate.citizen,
+      twitter: _delegates[index].statement?.twitter,
+      statement: _delegates[index].statement?.payload.delegateStatement,
+      leastValuableProposals: _delegates[index].statement?.payload.leastValuableProposals,
+      mostValuableProposals: _delegates[index].statement?.payload.mostValuableProposals,
+      openToSponsoringProposals: _delegates[index].statement?.payload.openToSponsoringProposals,
+      topIssues: _delegates[index].statement?.payload.topIssues,
     })),
   };
 }
