7408e221f61493af47d2e90a1084a9c7837e32f2
andreitr <andrei.taraschuk@gmail.com>
2024-04-09 15:46:30 -0600

    Uniswap tenant db placehoder

--- src/app/api/common/votableSupply/getVotableSupply.ts
+++ src/app/api/common/votableSupply/getVotableSupply.ts
@@ -5,8 +5,10 @@ import { cache } from "react";
 async function getVotableSupply() {
   const { namespace } = Tenant.current();
   const votableSupply = await prisma[`${namespace}VotableSupply`].findFirst({});
+
   if (!votableSupply) {
-    throw new Error("No votable supply found");
+    return 0;
+    // throw new Error("No votable supply found");
   }
   return votableSupply.votable_supply;
 }
