4b8eb960c00946991ec3b9f8d3a4025c0c21d1cc
Flip-Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-10 17:47:35 -0700

    [AGORA-1899][AGORA-1898][AGORA-1920] Use OTLP and not console for span emissions (#224)
    * 📦 add otlp metrics exporter
    * 👁️ drop console exporters, switch to otlp
    * 📦 next to 14.1.4
    * 📦 add otel/api and otel/apilogs
    * 👜 rename span attr setter; add doInSpan and addBaggage
    * 👁️ instrument fetchDelegates and fetchDelegateStatement
    * 👁️ propagate userID to child spans; add apiUtils library

--- src/app/api/v1/delegates/route.ts
+++ src/app/api/v1/delegates/route.ts
@@ -13,7 +13,7 @@ import {
   createOptionalNumberValidator,
   createOptionalStringValidator,
 } from "@/app/api/common/utils/validators";
-import { setCurrentSpanAttributes } from "@/app/lib/logging";
+import { withUserId } from "../apiUtils";
 
 const DEFAULT_SORT = "most_delegators";
 const DEFAULT_MAX_LIMIT = 100;
@@ -37,31 +37,32 @@ const offsetValidator = createOptionalNumberValidator(
 
 export async function GET(request: NextRequest) {
   const authResponse = await authenticateApiUser(request);
-  setCurrentSpanAttributes({ user_id: authResponse.userId });
 
   if (!authResponse.authenticated) {
     return new Response(authResponse.reason, { status: 401 });
   }
 
-  const params = request.nextUrl.searchParams;
-  try {
-    const sort = sortValidator.parse(params.get("sort"));
-    const limit = limitValidator.parse(params.get("limit"));
-    const offest = offsetValidator.parse(params.get("offset"));
-    const delegatesResult = await fetchDelegatesApi(sort, {
-      limit: limit,
-      offset: offest,
-    });
-    return NextResponse.json(delegatesResult);
-  } catch (e: any) {
-    if (e instanceof ZodError) {
-      return new Response("Invalid query parameters: " + e.toString(), {
-        status: 400,
+  return await withUserId(authResponse.userId as string, async () => {
+    const params = request.nextUrl.searchParams;
+    try {
+      const sort = sortValidator.parse(params.get("sort"));
+      const limit = limitValidator.parse(params.get("limit"));
+      const offest = offsetValidator.parse(params.get("offset"));
+      const delegatesResult = await fetchDelegatesApi(sort, {
+        limit: limit,
+        offset: offest,
+      });
+      return NextResponse.json(delegatesResult);
+    } catch (e: any) {
+      if (e instanceof ZodError) {
+        return new Response("Invalid query parameters: " + e.toString(), {
+          status: 400,
+        });
+      }
+
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
       });
     }
-
-    return new Response("Internal server error: " + e.toString(), {
-      status: 500,
-    });
-  }
+  });
 }
