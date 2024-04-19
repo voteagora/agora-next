881742ee047ba32c14b3aaf6d79d54f96bb4a61d
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-19 19:18:55 -0400

    drop _app _document

--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import "swagger-ui-react/swagger-ui.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
