bd18f3b878c944c29783e6d5b0039085c36aabcd
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-22 11:25:56 -0400

    💻 add delete route for /ballots/impact metrics

---
+++ src/app/api/v1/retrofunding/rounds/[roundId]/ballots/[ballotAddressOrEns]/impactMetrics/[impactMetricId]/route.ts
@@ -0,0 +1,9 @@
+export async function DELETE(
+  request: Request,
+  route: { params: { roundId: string; ballotAddressOrEns: string; impactMetricId: string } }
+) {
+  const { roundId, ballotAddressOrEns, impactMetricId } = route.params;
+  return new Response(null, {
+    status: 200,
+  });
+}
