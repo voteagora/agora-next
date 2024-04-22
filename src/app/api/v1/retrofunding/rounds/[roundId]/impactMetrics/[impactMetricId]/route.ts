aa24c5396c717a721329233af654fde9df352652
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-22 02:53:45 -0400

    💻 add route for getting impact metric by id

---
+++ src/app/api/v1/retrofunding/rounds/[roundId]/impactMetrics/[impactMetricId]/route.ts
@@ -0,0 +1,12 @@
+import { fetchImpactMetricsApi } from "@/app/api/common/impactMetrics/getImpactMetrics";
+
+export async function GET(
+  request: Request,
+  route: { params: { roundId: string; ballotAddressOrEns: string } }
+) {
+  const { roundId, ballotAddressOrEns } = route.params;
+  const impactMetrics = await fetchImpactMetricsApi(roundId, ballotAddressOrEns);
+  return new Response(JSON.stringify(impactMetrics), {
+    status: 200,
+  });
+}
