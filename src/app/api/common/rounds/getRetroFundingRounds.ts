ef0992de4f3c3dc6f4463b99b1cbe3d3b02cbd74
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-22 02:57:18 -0400

    ✨ prettier

--- src/app/api/common/rounds/getRetroFundingRounds.ts
+++ src/app/api/common/rounds/getRetroFundingRounds.ts
@@ -17,11 +17,11 @@ async function getRetroFundingRound(roundId: string) {
   ];
   const defaultround = {
     roundId: 3,
-    "name": "RetroPGF Round Three",
+    name: "RetroPGF Round Three",
     description: "The third retroactive funding round",
     externalLink: "https://vote.optimism.io/retropgf/3/summary",
-    events: defaultEvents
-  }
+    events: defaultEvents,
+  };
 
   return defaultround;
 }
