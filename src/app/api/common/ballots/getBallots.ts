dfab2aff0ca67631e6e9793f4f367a835b5142ca
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-22 02:23:24 -0400

    💻 add mocked route for ballots

---
+++ src/app/api/common/ballots/getBallots.ts
@@ -0,0 +1,34 @@
+import { cache } from "react";
+
+async function getBallotsApi(roundId: string) {
+  const defaultPaginationMetadata = {
+    hasNext: false,
+    totalReturned: 1,
+    nextOffset: 1,
+  };
+
+  const defaultBallots = {
+    ballots: [
+      {
+        address: "0x", 
+        ballotId: 0,
+        roundId: roundId,
+        status: "PENDING",
+        allocations: [
+          {
+            "metricId": 0,
+            "allocation": "0"
+          }
+        ],
+        submitterAddress: "0xDa6d1F091B672C0f9e215eB9fa6B5a84bF2c5e11",
+      }
+    ],
+  };
+
+  return {
+    metadata: defaultPaginationMetadata,
+    ballots: [defaultBallots],
+  };
+}
+
+export const fetchBallots = cache(getBallotsApi);
