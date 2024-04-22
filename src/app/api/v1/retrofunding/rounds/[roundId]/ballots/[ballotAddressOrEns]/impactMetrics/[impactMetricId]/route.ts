976eaeb574b726817d72e268feed6c6b80ad11de
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-22 11:26:21 -0400

    ✨ prettier

--- src/app/api/v1/retrofunding/rounds/[roundId]/ballots/[ballotAddressOrEns]/impactMetrics/[impactMetricId]/route.ts
+++ src/app/api/v1/retrofunding/rounds/[roundId]/ballots/[ballotAddressOrEns]/impactMetrics/[impactMetricId]/route.ts
@@ -1,6 +1,12 @@
 export async function DELETE(
   request: Request,
-  route: { params: { roundId: string; ballotAddressOrEns: string; impactMetricId: string } }
+  route: {
+    params: {
+      roundId: string;
+      ballotAddressOrEns: string;
+      impactMetricId: string;
+    };
+  }
 ) {
   const { roundId, ballotAddressOrEns, impactMetricId } = route.params;
   return new Response(null, {
