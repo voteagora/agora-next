c00522e76847a1b664ac59c54c90c67ba9254c4d
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-22 19:16:21 -0400

    💻🔐 impl /nonce

--- src/app/api/v1/auth/nonce/route.tsx
+++ src/app/api/v1/auth/nonce/route.tsx
@@ -1,5 +1,11 @@
+import { generateNonce } from "siwe";
+
 export async function GET(request: Request) {
-  return new Response(JSON.stringify(`{result: "success"}`), {
+  const nonce = generateNonce();
+  const headers = new Headers();
+  headers.set("Content-Type", "text/plain");
+  return new Response(nonce, {
+    headers,
     status: 200,
   });
 }
