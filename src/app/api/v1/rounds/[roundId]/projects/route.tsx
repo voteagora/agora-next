bd222e9d18acfa5843b2e2caf55b37e730da9c21
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-23 16:13:05 -0400

    💻🔐🪵 add auth + logging for rounds/projects

--- src/app/api/v1/rounds/[roundId]/projects/route.tsx
+++ src/app/api/v1/rounds/[roundId]/projects/route.tsx
@@ -1,8 +1,24 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { authenticateApiUser } from "@/app/lib/middleware/auth";
 import { fetchProjectsApi } from "@/app/api/common/projects/getProjects";
+import { traceWithUserId } from "../../../apiUtils";
 
-export async function GET(request: Request) {
-  const projects = await fetchProjectsApi();
-  return new Response(JSON.stringify(projects), {
-    status: 200,
+export async function GET(request: NextRequest) {
+  const authResponse = await authenticateApiUser(request);
+
+  if (!authResponse.authenticated) {
+    return new Response(authResponse.reason, { status: 401 });
+  }
+
+  return await traceWithUserId(authResponse.userId as string, async () => {
+    const params = request.nextUrl.searchParams;
+    try {
+      const projects = await fetchProjectsApi();
+      return NextResponse.json(projects);
+    } catch (e: any) {
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
+      });
+    }
   });
 }
