efb3df95c1bcbfe63db72bbf0eaf3a075bcde846
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-23 21:03:39 -0400

    🔐🤏 remove auth from /spec route

--- src/app/api/v1/spec/route.ts
+++ src/app/api/v1/spec/route.ts
@@ -6,31 +6,23 @@ import fs from "fs";
 import path from "path";
 
 export async function GET(request: NextRequest) {
-  const authResponse = await authenticateApiUser(request);
+  try {
+    const fp = path.join(process.cwd(), "spec/oas_v1.yaml");
+    const data = fs.readFileSync(fp, "utf8");
+    // set headers
+    const headers = new Headers();
+    headers.set("Content-Type", "application/x-yaml");
 
-  if (!authResponse.authenticated) {
-    return new Response(authResponse.reason, { status: 401 });
-  }
-
-  return await traceWithUserId(authResponse.userId as string, async () => {
-    try {
-      const fp = path.join(process.cwd(), "spec/oas_v1.yaml");
-      const data = fs.readFileSync(fp, "utf8");
-      // set headers
-      const headers = new Headers();
-      headers.set("Content-Type", "application/x-yaml");
-
-      return new Response(data, {
-        headers,
-        status: 200,
+    return new Response(data, {
+      headers,
+      status: 200,
+    });
+  } catch (err) {
+    if (err) {
+      console.error(err);
+      return new Response(JSON.stringify(`{result: "error"}`), {
+        status: 500,
       });
-    } catch (err) {
-      if (err) {
-        console.error(err);
-        return new Response(JSON.stringify(`{result: "error"}`), {
-          status: 500,
-        });
-      }
     }
-  });
+  }
 }
