eb5ffa38645e3c7dd1f6b21fff13c039f14a2111
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-26 10:56:54 -0400

    ✨ prettier

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -3,14 +3,22 @@ import jwt from "jsonwebtoken";
 const DEFAULT_JWT_TTL = "1d";
 const DEFAULT_USER_SCOPE = "";
 
-// Note: this is not included in lib/middleware/auth.ts since that file will be 
+// Note: this is not included in lib/middleware/auth.ts since that file will be
 // used in a non-node environment. This file is only intended to be used in/on node.
-export async function generateJwt(userId: string, scope?: string | null , ttl?: string | null) {
-  const resolvedScope = scope || await getScopeForUser(userId);
-  const resolvedTtl = ttl || await getExpiryForUser(userId);
-  return jwt.sign({ sub: userId, scope: resolvedScope }, process.env.JWT_SECRET as string, {
-    expiresIn: resolvedTtl,
-  });
+export async function generateJwt(
+  userId: string,
+  scope?: string | null,
+  ttl?: string | null
+) {
+  const resolvedScope = scope || (await getScopeForUser(userId));
+  const resolvedTtl = ttl || (await getExpiryForUser(userId));
+  return jwt.sign(
+    { sub: userId, scope: resolvedScope },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: resolvedTtl,
+    }
+  );
 }
 
 export async function getExpiryForUser(userId: string) {
