32d4141a696aa4e28e97f3db22080742ce6974d2
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-04-30 03:21:08 -0400

    🤏🛠️ drop delegatee txhash

--- src/app/api/common/delegations/getDelegations.ts
+++ src/app/api/common/delegations/getDelegations.ts
@@ -113,7 +113,6 @@ async function getCurrentDelegateesForAddress({
         Number(advancedDelegatee.delegated_share.toFixed(3)) >= 1
           ? "FULL"
           : "PARTIAL",
-      transaction_hash: advancedDelegatee.transaction_hash,
     })),
   ] as Delegation[];
 }
@@ -210,7 +209,6 @@ async function getCurrentDelegatorsForAddress({
         Number(advancedDelegator.delegated_share.toFixed(3)) === 1
           ? "FULL"
           : "PARTIAL",
-      transaction_hash: advancedDelegator.transaction_hash,
     })),
   ] as Delegation[];
 }
