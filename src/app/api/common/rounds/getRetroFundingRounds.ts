deaf02a6637f3c9f8a136e6c984c3662fe6edc78
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-05-02 23:41:38 -0400

    💻🛠️ add /rounds route

--- src/app/api/common/rounds/getRetroFundingRounds.ts
+++ src/app/api/common/rounds/getRetroFundingRounds.ts
@@ -26,4 +26,40 @@ async function getRetroFundingRound(roundId: string) {
   return defaultround;
 }
 
-export const fetchRetroFundingRounds = cache(getRetroFundingRound);
+async function getRetroFundingRounds() {
+  const pageMetadata = {
+    hasNext: false,
+    totalReturned: 1,
+    nextOffset: 0,
+  };
+
+  const defaultEvents = [
+    {
+      status: "PLANNED",
+      timestamp: "2024-01-01T00:00:00Z",
+    },
+    {
+      status: "SCHEDULED",
+      timestamp: "2024-02-01T00:00:00Z",
+    },
+    {
+      status: "DONE",
+      timestamp: "2024-03-01T00:00:00Z",
+    },
+  ];
+
+  const defaultRounds = [
+    {
+      roundId: 3,
+      name: "RetroPGF Round Three",
+      description: "The third retroactive funding round",
+      externalLink: "https://vote.optimism.io/retropgf/3/summary",
+      events: defaultEvents,
+    },
+  ];
+
+  return defaultRounds;
+}
+
+export const fetchRetroFundingRound = cache(getRetroFundingRound);
+export const fetchRetroFundingRounds = cache(getRetroFundingRounds);
