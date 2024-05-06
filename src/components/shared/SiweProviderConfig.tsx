7d4cc698daf1675cd7572f1a7b24f4248e8ca214
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-05-06 13:37:39 -0400

    🤏🔐 add SIWE enable/disable flag for connectkit

--- src/components/shared/SiweProviderConfig.tsx
+++ src/components/shared/SiweProviderConfig.tsx
@@ -7,6 +7,21 @@ const API_AUTH_PREFIX = "/api/v1/auth";
 const LOCAL_STORAGE_JWT_KEY = "agora-siwe-jwt";
 const AGORA_SIGN_IN = "Sign in to Agora with Ethereum";
 
+/* There's currently nothing stored on the backend to maintain session state.
+// All session state is stateless and stored in the JWT issued by the server.
+// Address, nonce, and chainId are all stored in the JWT, along with a particular
+// time to live/expiry.
+//
+// For signOut, the client should remove JWT from storage as applicable, and is otherwise
+// a no-op (pending AGORA-2015, or potential JWT-token tracking on our backend DB).
+//
+// JWT tokens for SIWE should therefore be issued with a short expiry time.
+*/
+
+const isSiweEnabled = () => {
+  return process.env.NEXT_PUBLIC_SIWE_ENABLED === "true";
+};
+
 export const siweProviderConfig: SIWEConfig = {
   getNonce: async () =>
     fetch(`${API_AUTH_PREFIX}/nonce`).then((res) => res.text()),
@@ -56,14 +71,5 @@ export const siweProviderConfig: SIWEConfig = {
     localStorage.removeItem(LOCAL_STORAGE_JWT_KEY);
     return Promise.resolve(true);
   },
+  enabled: isSiweEnabled(),
 };
-
-// There's currently nothing stored on the backend to maintain session state.
-// All session state is stateless and stored in the JWT issued by the server.
-// Address, nonce, and chainId are all stored in the JWT, along with a particular
-// time to live/expiry.
-//
-// For signOut, the client should remove JWT from storage as applicable, and is otherwise
-// a no-op (pending AGORA-2015, or potential JWT-token tracking on our backend DB).
-//
-// JWT tokens for SIWE should therefore be issued with a short expiry time.
