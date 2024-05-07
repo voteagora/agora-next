26f84c9cfc08d45d3f5e29ae74f55e4502162b32
stepandel <stepandel@gmail.com>
2024-05-07 12:06:38 -0700

    remove suppoert for voting with 2 txs

--- src/components/Proposals/ProposalPage/CastVoteDialog/CastVoteDialog.tsx
+++ src/components/Proposals/ProposalPage/CastVoteDialog/CastVoteDialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import { HStack, VStack } from "@/components/Layout/Stack";
 import TokenAmountDisplay from "@/components/shared/TokenAmountDisplay";
 import HumanAddress from "@/components/shared/HumanAddress";
@@ -10,7 +10,7 @@ import styles from "./castVoteDialog.module.scss";
 import useAdvancedVoting from "../../../../hooks/useAdvancedVoting";
 import { CastVoteDialogProps } from "@/components/Dialogs/DialogProvider/dialogs";
 import { Button } from "@/components/ui/button";
-import { MissingVote, getVpToDisplay } from "@/lib/voteUtils";
+import { getVpToDisplay } from "@/lib/voteUtils";
 import pendingImage from "../../../../../public/images/action-pending.svg";
 import congrats from "../../../../../public/images/congrats.svg";
 import BlockScanUrls from "@/components/shared/BlockScanUrl";
@@ -34,29 +34,16 @@ function CastVoteDialogContents({
   authorityChains,
   missingVote,
 }: CastVoteDialogProps) {
-  const [localMissingVote, setLocalMissingVote] =
-    useState<MissingVote>(missingVote);
-
   const { write, isLoading, isSuccess, data } = useAdvancedVoting({
     proposalId,
     support: ["AGAINST", "FOR", "ABSTAIN"].indexOf(supportType),
     advancedVP: BigInt(votingPower.advancedVP),
     authorityChains,
     reason,
-    missingVote: localMissingVote,
+    missingVote,
   });
 
   const vpToDisplay = getVpToDisplay(votingPower, missingVote);
-
-  useEffect(() => {
-    if (
-      missingVote == "BOTH" &&
-      data?.standardTxHash &&
-      !data?.advancedTxHash
-    ) {
-      setLocalMissingVote("ADVANCED");
-    }
-  }, [data, missingVote, setLocalMissingVote]);
 
   if (!delegate) {
     // todo: log
@@ -65,41 +52,6 @@ function CastVoteDialogContents({
 
   if (isLoading) {
     return <LoadingVote />;
-  }
-
-  if (missingVote === "BOTH" && !data.advancedTxHash && data.standardTxHash) {
-    return (
-      <VStack gap={4} className={styles.dialog_container}>
-        <HStack justifyContent="justify-between">
-          <VStack>
-            {delegate.address ? (
-              <div className={styles.subtitle}>
-                <HumanAddress address={delegate.address} />
-              </div>
-            ) : (
-              <div className={styles.subtitle}>Anonymous</div>
-            )}
-            <div className={styles.title}>
-              Casting vote&nbsp;{supportType.toLowerCase()}
-            </div>
-          </VStack>
-          <VStack alignItems="items-end">
-            <div className={styles.subtitle}>with</div>
-            <TokenAmountDisplay amount={vpToDisplay} />
-          </VStack>
-        </HStack>
-        <div className={styles.reason_box}>
-          {reason ? (
-            <div className={styles.has_reason}>{reason}</div>
-          ) : (
-            <div className={styles.no_reason}>No voting reason provided</div>
-          )}
-        </div>
-        <div>
-          <VoteButton onClick={write}>Sign transaction 2/2</VoteButton>
-        </div>
-      </VStack>
-    );
   }
 
   return (
@@ -141,8 +93,6 @@ function CastVoteDialogContents({
               <NoStatementView closeDialog={closeDialog} />
             )}
           </div>
-          {/* @ts-ignore */}
-          {missingVote === "BOTH" && <AdvancedVoteAlert />}
         </VStack>
       )}
       {isSuccess && <SuccessMessage closeDialog={closeDialog} data={data} />}
@@ -164,15 +114,6 @@ const VoteButton = ({
     </Button>
   );
 };
-
-export function AdvancedVoteAlert() {
-  return (
-    <div className={styles.alert}>
-      To cast your vote, you will be requested to sign two transactions, both of
-      which are needed to vote with your full balance.
-    </div>
-  );
-}
 
 export function SuccessMessage({
   closeDialog,
@@ -216,7 +157,7 @@ export function SuccessMessage({
 export function LoadingVote() {
   return (
     <VStack className={styles.full_width}>
-      <img
+      <Image
         src={`/images/action-pending.svg`}
         className="w-full mb-3"
         alt="Vote pending"
