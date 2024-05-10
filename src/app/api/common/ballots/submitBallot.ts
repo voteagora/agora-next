d14f4c2f19fb041f33b3a7491996dc5d88a55ca6
stepandel <stepandel@gmail.com>
2024-05-10 14:39:22 -0700

    implement ballotSubmission

---
+++ src/app/api/common/ballots/submitBallot.ts
@@ -0,0 +1,58 @@
+import { cache } from "react";
+import { addressOrEnsNameWrap } from "../utils/ensName";
+import verifyMessage from "@/lib/serverVerifyMessage";
+
+type BallotSubmission = {
+  ballotContnet: {
+    metric_id: string;
+    allocation: number;
+  }[];
+  signature: `0x${string}`;
+};
+
+const submitBallotApi = async (
+  data: BallotSubmission,
+  roundId: number,
+  ballotCasterAddressOrEns: string
+) =>
+  addressOrEnsNameWrap(submitBallotForAddress, ballotCasterAddressOrEns, {
+    data,
+    roundId,
+  });
+
+async function submitBallotForAddress({
+  data,
+  roundId,
+  address,
+}: {
+  data: BallotSubmission;
+  roundId: number;
+  address: string;
+}) {
+  const payload = JSON.stringify(data.ballotContnet);
+
+  const isSignatureValid = await verifyMessage({
+    address: address as `0x${string}`,
+    message: payload,
+    signature: data.signature,
+  });
+
+  // TODO: Validate ballot content
+
+  if (!isSignatureValid) {
+    throw new Error("Invalid signature");
+  }
+
+  const submission = await prisma.ballotSubmittions.create({
+    data: {
+      round: roundId,
+      address,
+      signature: data.signature,
+      payload,
+    },
+  });
+
+  return submission;
+}
+
+export const submitBallot = cache(submitBallotApi);
