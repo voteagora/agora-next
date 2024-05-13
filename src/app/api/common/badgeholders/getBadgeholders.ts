ed2880116a582ebab8e9ce25723a1f89674e2189
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2024-05-13 03:39:45 -0400

    🔒 add role/scope basic implementation for badgeholders

---
+++ src/app/api/common/badgeholders/getBadgeholders.ts
@@ -0,0 +1,7 @@
+import { cache } from "react";
+
+export async function isBadgeholder(address: string) {
+  // TODO: implement query for checking if a given address is a badgeholder
+  // for viewing and voting on ballots
+  return true;
+}
