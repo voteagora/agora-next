c0ab890ac14800b0e88fdef9e0cf7a7293a63168
Stepan Arsentjev <stepandel@gmail.com>
2024-05-19 23:36:55 -0400

    [RetroFunding API] Impact Metrics (#290)
    * retro funding data upload scripts
    
    * add scv loader
    
    * import test schemas for projects & metrics
    
    * update impacte metrics routes
    
    * update metrics swagger schema
    
    * use retro_funding schema instead of test3
    
    * update ballot endpoint to return per project allocation
    
    * add values column to metrics_projects table
    
    * update ballots query: apply os_multiplier
    
    * update ballots query
    
    * add view metric route
    
    * update ballots query
    
    * fix adjusted values in ballot query
    
    * try fix swagger schema

---
+++ src/app/api/common/impactMetrics/viewImactMetric.ts
@@ -0,0 +1,36 @@
+import { addressOrEnsNameWrap } from "../utils/ensName";
+
+const viewImpactMetric = async ({
+  addressOrENSName,
+  metricId,
+}: {
+  addressOrENSName: string;
+  metricId: string;
+}) =>
+  addressOrEnsNameWrap(viewImpactMetricForAddress, addressOrENSName, {
+    metricId,
+  });
+
+async function viewImpactMetricForAddress({
+  address,
+  metricId,
+}: {
+  address: string;
+  metricId: string;
+}) {
+  return prisma.metrics_views.upsert({
+    where: {
+      metric_id_address: {
+        metric_id: metricId,
+        address,
+      },
+    },
+    update: {},
+    create: {
+      metric_id: metricId,
+      address,
+    },
+  });
+}
+
+export const viewImpactMetricApi = viewImpactMetric;
