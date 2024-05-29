b70ce4643a02cb97ea095dc0f651e36c8ac79318
stepandel <stepandel@gmail.com>
2024-05-28 18:38:30 -0700

    order by indexes instead of ordinal

--- src/app/api/common/voting-power/votingPower.d.ts
+++ src/app/api/common/voting-power/votingPower.d.ts
@@ -1,10 +1,13 @@
 import {
   OptimismAdvancedVotingPower,
   OptimismVotingPower,
+  OptimismVotingPowerSnaps,
 } from "@prisma/client";
 
 export type AdvancedVotingPowerPayload = OptimismAdvancedVotingPower;
 export type VotingPowerPayload = OptimismVotingPower;
+
+export type VotingPowerSnapsPayload = OptimismVotingPowerSnaps;
 
 export type VotingPowerData = {
   directVP: string;
