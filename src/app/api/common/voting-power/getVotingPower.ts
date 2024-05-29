350e0ebb9a0b9b2866a7f8262177076ba3b33982
Stepan Arsentjev <stepandel@gmail.com>
2024-05-28 21:43:45 -0400

    Patch: speed up voting power snapshots query (#310)
    * add transaction index & log index to voting power snaps views
    
    * order by indexes instead of ordinal

--- src/app/api/common/voting-power/getVotingPower.ts
+++ src/app/api/common/voting-power/getVotingPower.ts
@@ -5,7 +5,7 @@ import {
   getTotalVotableAllowance,
 } from "@/lib/alligatorUtils";
 import { addressOrEnsNameWrap } from "../utils/ensName";
-import { VotingPowerData } from "./votingPower";
+import { VotingPowerData, VotingPowerSnapsPayload } from "./votingPower";
 import { AuhtorityChainsAggregate } from "../authority-chains/authorityChains";
 import Tenant from "@/lib/tenant/tenant";
 
@@ -40,17 +40,19 @@ async function getVotingPowerForProposalByAddress({
 }): Promise<VotingPowerData> {
   const { namespace, contracts } = Tenant.current();
 
-  const votingPowerQuery = prisma[`${namespace}VotingPowerSnaps`].findFirst({
-    where: {
-      delegate: address,
-      block_number: {
-        lte: blockNumber,
-      },
-    },
-    orderBy: {
-      ordinal: "desc",
-    },
-  });
+  const votingPowerQuery = prisma.$queryRawUnsafe<VotingPowerSnapsPayload>(
+    `
+    SELECT
+      *
+    FROM ${namespace + ".voting_power_snaps"}
+    WHERE delegate = $1
+      AND block_number <= $2
+    ORDER BY block_number DESC, transaction_index DESC, log_index DESC
+    LIMIT 1;
+    `,
+    address,
+    blockNumber
+  );
 
   // This query pulls only partially delegated voting power
   const advancedVotingPowerQuery = prisma.$queryRawUnsafe<
