20351add0ff8922d878bf43bea478225e5d1f8b3
Michael Gingras <mcg79@cornell.edu>
2024-06-03 09:28:14 -0600

    Adds skip to github PR step

--- src/app/proposals/draft/components/stages/GithubPRForm.tsx
+++ src/app/proposals/draft/components/stages/GithubPRForm.tsx
@@ -11,11 +11,13 @@ import {
   ProposalDraft,
   ProposalSocialOption,
   ProposalDraftTransaction,
+  ProposalChecklist,
 } from "@prisma/client";
 import { createGithubProposal } from "@/app/proposals/draft/utils/github";
 import { onSubmitAction as createGithubChecklistItem } from "../../actions/createGithubChecklistItem";
 import { useOpenDialog } from "@/components/Dialogs/DialogProvider/DialogProvider";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const GithubPRForm = ({
   draftProposal,
@@ -23,11 +25,17 @@ const GithubPRForm = ({
   draftProposal: ProposalDraft & {
     transactions: ProposalDraftTransaction[];
     social_options: ProposalSocialOption[];
+    checklist_items: ProposalChecklist[];
   };
 }) => {
+  const router = useRouter();
   const openDialog = useOpenDialog();
   const { address } = useAccount();
   const [isPending, setIsPending] = useState(false);
+
+  const github_pr_checklist_item = draftProposal.checklist_items.find(
+    (item) => item.title === "Docs updated"
+  );
 
   const handleClick = async () => {
     setIsPending(true);
@@ -70,24 +78,53 @@ const GithubPRForm = ({
           />
         </div>
         <p className="mt-4 text-stone-700">
-          You must create a PR with your proposal in the ENS docs repo. Click
-          below to allow Agora to create the PR for you.
+          {!!github_pr_checklist_item ? (
+            <span>
+              You've already opened a PR for this draft proposal. If you've
+              since updated your proposal, please{" "}
+              <a
+                href={github_pr_checklist_item.link || ""}
+                className="underline"
+                target="_blank"
+                rel="noreferrer"
+              >
+                edit the github PR
+              </a>{" "}
+              to account for new details.
+            </span>
+          ) : (
+            "You must create a PR with your proposal in the ENS docs repo. Click below to allow Agora to create the PR for you."
+          )}
         </p>
       </FormCard.Section>
       <FormCard.Section>
-        <div className="space-x-2 self-start">
-          <UpdatedButton
-            isSubmit={false}
-            type="primary"
-            fullWidth={true}
-            isLoading={isPending}
-            onClick={() => {
-              handleClick();
-            }}
-          >
-            Create PR
-          </UpdatedButton>
-        </div>
+        {!!github_pr_checklist_item ? (
+          <div className="space-x-2 self-start flex items-center">
+            <UpdatedButton
+              fullWidth={true}
+              type="primary"
+              onClick={() => {
+                router.push(`/proposals/draft/${draftProposal.id}?stage=3`);
+              }}
+            >
+              Continue
+            </UpdatedButton>
+          </div>
+        ) : (
+          <div className="space-x-2 self-start">
+            <UpdatedButton
+              isSubmit={false}
+              type="primary"
+              fullWidth={true}
+              isLoading={isPending}
+              onClick={() => {
+                handleClick();
+              }}
+            >
+              Create PR
+            </UpdatedButton>
+          </div>
+        )}
       </FormCard.Section>
     </FormCard>
   );
