7b9169ad0f8d3ac543483e7e223d7588ac3883ff
Kent Fenwick <kent.fenwick@gmail.com>
2024-06-09 09:31:20 -0400

    fixed title

--- src/components/Changelog/ChangelogEntryForm.tsx
+++ src/components/Changelog/ChangelogEntryForm.tsx
@@ -5,7 +5,6 @@ export default function ChangelogEntryForm() {
   return (
     <div className="space-y-8 sm:space-y-0 sm:flex sm:gap-12 mt-12">
       <div className="space-y-8 sm:flex-grow">
-        <h1>Add Changelog Entry</h1>
         <NewEntry />
       </div>
       <ChangelogFAQs />
