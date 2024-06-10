64f2207473f1c3a70145b69a8e597faa7e8062a8
jeff <jeff@voteagora.com>
2024-06-10 18:09:09 -0400

    Revert "Check if any votes exist, then delete"
    This reverts commit 41c0d64c28d49d32a9766fc1c1eec4822d2bb147.

--- src/app/api/common/comments/deleteImpactMetricComment.ts
+++ src/app/api/common/comments/deleteImpactMetricComment.ts
@@ -7,47 +7,22 @@ async function deleteImpactMetricCommentApi({
   commentId: number;
 }) {
 
-  const votesExist = await prisma.metrics_comments_votes.findMany({
+
+  const deletedVotes = await prisma.metrics_comments_votes.deleteMany({
     where: {
-      comment_id: commentId,
-    },
-  });
-
-  // If related votes exist, delete them
-  if (votesExist.length > 0) {
-    await prisma.metrics_comments_votes.deleteMany({
-      where: {
-        comment_id: commentId,
-      },
-    });
-  }
-
-  // Check if the comment exists before attempting to delete it
-  const commentExists = await prisma.metrics_comments.findUnique({
-    where: {
-      comment_id: commentId,
-    },
-  });
-
-  if (commentExists) {
-    // Delete the record in the main table
-    const deletedComment = await prisma.metrics_comments.delete({
-      where: {
-        comment_id: commentId,
-      },
-    });
-
-    return {
-      comment_id: deletedComment.comment_id,
-    };
-  }
-
-  else {
-    return {
-      comment_id: -1,
+      comment_id: commentId
     }
-  }
+  });
 
+  const deletedComment = await prisma.metrics_comments.delete({
+    where: {
+      comment_id: commentId,
+    }
+  });
+
+  return {
+    comment_id: deletedComment.comment_id,
+  };
 }
 
 export const deleteImpactMetricComment = cache(deleteImpactMetricCommentApi);
