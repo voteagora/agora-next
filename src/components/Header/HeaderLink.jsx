86f87d081fdc9ede515050c72a8ce4173014b2a9
andrei <andrei.taraschuk@gmail.com>
2024-06-11 09:43:13 -0600

    Andrei/agora 1921 turn on the list of delegates for etherfi (#330)
    * Merge
    
    * wip
    
    * Implemented external links in the navbar
    
    * Etherfi fixes
    
    * Quick implementation of the allow list
    
    ---------
    
    Co-authored-by: Andrei Taraschuk <andrei@Andreis-MacBook-Pro.local>

--- src/components/Header/HeaderLink.jsx
+++ src/components/Header/HeaderLink.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Link from "next/link";
 import styles from "./header.module.scss";
 
-export const HeaderLink = ({ className, children, href, isActive }) => {
+export const HeaderLink = ({ className, children, href, target, isActive }) => {
   const activeClass = isActive ? styles.nav_link_active : "";
 
   return (
     <Link
       href={href}
+      target={target}
       className={`${styles.nav_link} ${activeClass} ${className || ""}`}
     >
       {children}
