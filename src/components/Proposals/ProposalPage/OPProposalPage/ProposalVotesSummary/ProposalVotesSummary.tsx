ea312eec17c85501446fa1b25c550094141db867
andrei <andrei.taraschuk@gmail.com>
2024-06-12 14:54:00 -0600

    Misc fixes

--- src/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesSummary/ProposalVotesSummary.tsx
+++ src/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesSummary/ProposalVotesSummary.tsx
@@ -1,22 +1,17 @@
 "use client";
 import { useState } from "react";
-import { VStack, HStack } from "@/components/Layout/Stack";
 import styles from "./proposalVotesSummary.module.scss";
 import ProposalVotesBar from "../ProposalVotesBar/ProposalVotesBar";
 import { Proposal } from "@/app/api/common/proposals/proposal";
 import TokenAmountDisplay from "@/components/shared/TokenAmountDisplay";
 import { ParsedProposalResults } from "@/lib/proposalUtils";
 import ProposalStatusDetail from "@/components/Proposals/ProposalStatus/ProposalStatusDetail";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 import ProposalVotesSummaryDetails from "../ProposalVotesSummaryDetails/ProposalVotesSummaryDetails";
 
 export default function ProposalVotesSummary({
-  proposal,
-}: {
+                                               proposal,
+                                             }: {
   proposal: Proposal;
 }) {
   const [showDetails, setShowDetails] = useState(false);
@@ -27,26 +22,23 @@ export default function ProposalVotesSummary({
     <HoverCard
       open={showDetails}
       onOpenChange={setShowDetails}
-      openDelay={400}
-      closeDelay={600}
+      openDelay={10}
+      closeDelay={10}
     >
       <HoverCardTrigger className="w-full cursor-pointer block">
         {!showDetails && (
-          <VStack gap={2} className={styles.proposal_votes_summary_container}>
-            <HStack justifyContent="justify-between" className="mt-2">
+          <div className={`flex flex-col gap-2 ${styles.proposal_votes_summary_container}`}>
+            <div className="flex flex-row justify-between mt-2">
               <div className="gl_votes_for">
                 FOR <TokenAmountDisplay amount={results.for} />
               </div>
               <div className="gl_votes_against">
                 AGAINST <TokenAmountDisplay amount={results.against} />
               </div>
-            </HStack>
+            </div>
             <ProposalVotesBar proposal={proposal} />
-            <VStack className="font-medium">
-              <HStack
-                justifyContent="justify-between"
-                className="text-gray-4f pb-2"
-              >
+            <div className="flex flex-col font-medium">
+              <div className="flex flex-row text-gray-4f pb-2 justify-between">
                 <>
                   {proposal.quorum && (
                     <div>
@@ -63,7 +55,7 @@ export default function ProposalVotesSummary({
                     </div>
                   )}
                 </>
-              </HStack>
+              </div>
               <ProposalStatusDetail
                 proposalStartTime={proposal.start_time}
                 proposalEndTime={proposal.end_time}
@@ -71,8 +63,8 @@ export default function ProposalVotesSummary({
                 proposalCancelledTime={proposal.cancelled_time}
                 cancelledTransactionHash={proposal.cancelled_transaction_hash}
               />
-            </VStack>
-          </VStack>
+            </div>
+          </div>
         )}
       </HoverCardTrigger>
       <HoverCardContent className="w-full pb-0" side="top">
