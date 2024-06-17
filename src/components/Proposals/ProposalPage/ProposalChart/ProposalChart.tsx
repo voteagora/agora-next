232c4ca25baff28f8b4015d4f2e72e513db7ca82
Yitong <zha.yitong@gmail.com>
2024-06-17 23:46:36 +0200

    fixed some more jank

--- src/components/Proposals/ProposalPage/ProposalChart/ProposalChart.tsx
+++ src/components/Proposals/ProposalPage/ProposalChart/ProposalChart.tsx
@@ -19,11 +19,11 @@ export default function ProposalChart({ proposal, proposalVotes }) {
   return (
     <div className="border border-gray-300 rounded-lg p-4 pb-2 w-full font-inter">
       <div className="flex justify-between w-full items-center text-xs font-semibold mb-2 cursor-pointer">
-        <div onClick={handleExpandChart}>
+        <div onClick={handleExpandChart} className="flex gap-1">
           Proposal Visualization{" "}
-          <Image src={icons.chevronSelectorVertical} alt="chevronIcon" />{" "}
+          <Image src={icons.chevronSelectorVertical} alt="chevronIcon" />
         </div>
-        <div className="flex gap-x-1.5 items-center">
+        <div className="flex gap-x-2 items-center">
           <button
             className={`tab ${tabIndex === 0 ? "active" : "text-stone-500"}`}
             onClick={() => handleTabsChange(0)}
@@ -38,21 +38,23 @@ export default function ProposalChart({ proposal, proposalVotes }) {
           </button>
         </div>
       </div>
-      <div className="tab-panels">
-        {tabIndex === 0 && (
-          <div className="tab-panel">
-            <VotingTimelineChart
-              proposal={proposal}
-              proposalVotes={proposalVotes}
-            />
-          </div>
-        )}
-        {tabIndex === 1 && (
-          <div className="tab-panel">
-            <TreeMapChart proposal={proposal} proposalVotes={proposalVotes} />
-          </div>
-        )}
-      </div>
+      {showChart && (
+        <div className="tab-panels">
+          {tabIndex === 0 && (
+            <div className="tab-panel">
+              <VotingTimelineChart
+                proposal={proposal}
+                proposalVotes={proposalVotes}
+              />
+            </div>
+          )}
+          {tabIndex === 1 && (
+            <div className="tab-panel">
+              <TreeMapChart proposal={proposal} proposalVotes={proposalVotes} />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
