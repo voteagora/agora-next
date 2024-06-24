7ddcc70dc1b7fc2ababb3b53174c17ed131237ac
stepandel <stepandel@gmail.com>
2024-06-24 16:40:14 +0200

    make sure tests pass

--- src/app/api/common/ballots/ballotAllocations.ts
+++ src/app/api/common/ballots/ballotAllocations.ts
@@ -3,14 +3,25 @@ import { Ballot, BallotResponse } from "./ballot";
 const CAP = 0.05;
 const TOTAL_FUNDING = 10_000_000;
 
-export function calculateAllocations(ballot: Ballot[]): BallotResponse {
+export function calculateAllocations(
+  ballot: Ballot[],
+  cap: number = CAP
+): BallotResponse {
   const projectTotals = new Map<string, number>();
   const projectData = new Map<
     string,
-    { name: string; image: string; is_os: boolean }
+    {
+      name: string;
+      image: string;
+      is_os: boolean;
+      allocations_per_metric: {
+        metric_id: string;
+        allocation: number;
+      }[];
+    }
   >();
 
-  const adjustedAllocations = ballot.map((b) => {
+  ballot.map((b) => {
     const metricId = b.metric_id;
     const adjustedValues = b.allocations.map((a) => {
       const value = a.is_os
@@ -24,91 +35,96 @@ export function calculateAllocations(ballot: Ballot[]): BallotResponse {
       };
     });
 
-    console.log(adjustedValues);
-
     const total = adjustedValues.reduce((acc, a) => acc + a.value, 0);
-
-    let excess = 0;
-
-    const projectAllocations = new Map<string, number>();
-    const cappedAllocations = adjustedValues.map((a) => {
-      console.log(excess);
-
-      const allocation = a.value / total;
-      const cappedAllocation = Math.min(allocation + excess * allocation, CAP); // in %
-      console.log(allocation + excess * allocation, cappedAllocation);
-      excess += allocation + excess * allocation - cappedAllocation;
-
-      console.log(a.project_id, a.value, excess, total);
-      console.log(a.project_id, cappedAllocation);
-
-      // Add to project totals
-      projectTotals.set(
-        a.project_id,
-        (projectTotals.get(a.project_id) || 0) +
-          (cappedAllocation * b.allocation) / 100
-      );
-
-      // Add to project allocations
-      projectAllocations.set(
-        a.project_id,
-        (cappedAllocation * b.allocation) / 100
-      );
-
-      // Add to project data
-      projectData.set(a.project_id, {
-        name: a.name,
-        image: a.image,
-        is_os: a.is_os,
-      });
-
-      return {
-        ...a,
-        allocation: (cappedAllocation * b.allocation) / 100,
-      };
-    });
-
-    console.log(projectAllocations);
-    console.log(projectTotals);
 
     return {
       address: b.address,
       round_id: b.round,
       status: b.status,
       metric_id: metricId,
-      allocation: (b.allocation / 100) * TOTAL_FUNDING, // in OP adjusded for ballot allocation
-      projectAllocations: projectAllocations,
+      projectAllocations: adjustedValues.map((a) => {
+        const data = projectData.get(a.project_id);
+        projectData.set(a.project_id, {
+          name: a.name,
+          image: a.image,
+          is_os: a.is_os,
+          allocations_per_metric: [
+            ...(data?.allocations_per_metric || []),
+            {
+              metric_id: b.metric_id,
+              allocation: (a.value / total) * (b.allocation / 100),
+            },
+          ],
+        });
+
+        projectTotals.set(
+          a.project_id,
+          (projectTotals.get(a.project_id) || 0) +
+            (a.value / total) * (b.allocation / 100)
+        );
+
+        return {
+          project_id: a.project_id,
+          name: a.name,
+          image: a.image,
+          is_os: a.is_os,
+          allocation: (a.value / total) * (b.allocation / 100),
+        };
+      }),
       locked: b.locked,
     };
   });
 
-  console.log("projectTotals", projectTotals);
-
-  const projectAllocations = Array.from(projectTotals.entries())
-    .map(([projectId, allocation]) => ({
+  // transpose to get the allocations per project
+  const allocations = Array.from(projectData.entries())
+    .map(([projectId, data]) => ({
       project_id: projectId,
-      name: projectData.get(projectId)!.name,
-      image: projectData.get(projectId)!.image,
-      is_os: projectData.get(projectId)!.is_os,
-      allocation: allocation * TOTAL_FUNDING,
-      allocation_per_metric: adjustedAllocations.map((a) => ({
-        metric_id: a.metric_id,
-        allocation: a.projectAllocations.get(projectId)! * TOTAL_FUNDING,
-      })),
+      name: data.name,
+      image: data.image,
+      is_os: data.is_os,
+      allocation_per_metric: data.allocations_per_metric,
+      allocation: data.allocations_per_metric.reduce(
+        (acc, a) => acc + a.allocation,
+        0
+      ),
     }))
-    .sort((a, b) => b.allocation - a.allocation); // Sort by allocation desc
+    .sort((a, b) => b.allocation - a.allocation);
 
-  console.log(projectAllocations);
+  // cap allocations
+  let total = 1;
+  let adjustedTotal = 1;
+  const cappedAllocations = allocations.map((a) => {
+    const cappedAllocation = Math.min(
+      (a.allocation * adjustedTotal) / total,
+      cap
+    );
+    total -= a.allocation; // remove the allocation from the total
+    adjustedTotal -= cappedAllocation; // add the exess to the total
+    return {
+      ...a,
+      allocation: cappedAllocation * TOTAL_FUNDING,
+      allocation_per_metric: a.allocation_per_metric.map((apm) => {
+        return {
+          ...apm,
+          allocation:
+            Math.round(
+              (100 * (TOTAL_FUNDING * (apm.allocation * cappedAllocation))) /
+                a.allocation
+            ) / 100, // prevent floating point errors
+        };
+      }),
+    };
+  });
 
   return {
     address: ballot[0].address,
     round_id: ballot[0].round,
     status: ballot[0].status,
-    allocations: adjustedAllocations.map((a) => ({
-      metric_id: a.metric_id,
-      allocation: a.allocation,
-      locked: a.locked,
+    allocations: ballot.map((b) => ({
+      metric_id: b.metric_id,
+      allocation: (b.allocation * TOTAL_FUNDING) / 100,
+      locked: b.locked,
     })),
-    project_allocations: projectAllocations,
+    project_allocations: cappedAllocations,
   };
 }
