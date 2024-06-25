028cb0a9422be95cccd1c29c5008cdccdaa2234f
jefag <jeff@voteagora.com>
2024-06-25 13:43:54 -0400

    Add top delegate weights endpoint (`/analytics/top/delegates`) (#358)
    * Add top delegate weights endpoint
    
    * prettier
    
    * Auth
    
    * prettier

---
+++ src/app/api/analytics/top/delegates/route.ts
@@ -0,0 +1,59 @@
+import prisma from "@/app/lib/prisma";
+import Tenant from "@/lib/tenant/tenant";
+import { NextResponse, type NextRequest } from "next/server";
+import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
+import { cache } from "react";
+
+type AddressWeight = {
+  address: string;
+  weight: string;
+};
+
+async function getTopDelegateWeights() {
+  const { contracts } = Tenant.current();
+
+  const QRY = `WITH 
+                
+                total_voting_power
+                AS (SELECT Sum(voting_power) tot
+                    FROM   uniswap.delegates
+                    WHERE  contract = '${contracts.token.address}'),
+                
+                weightings
+                AS (SELECT delegate,
+                            voting_power / (SELECT tot
+                                            FROM   total_voting_power) AS
+                            fraction_of_voting_power
+                    FROM   uniswap.delegates
+                    WHERE  direct_vp > 0
+                    ORDER  BY voting_power)
+
+                SELECT delegate AS address,
+                       round(fraction_of_voting_power * 100.0,3) AS weight
+                FROM   weightings
+                WHERE  fraction_of_voting_power > 0.01
+                ORDER  BY fraction_of_voting_power DESC; `;
+
+  const result = await prisma.$queryRawUnsafe<AddressWeight[]>(QRY);
+
+  return { result };
+}
+
+const fetchDelegateWeights = cache(getTopDelegateWeights);
+
+export async function GET(request: NextRequest) {
+  const authResponse = await authenticateApiUser(request);
+
+  if (!authResponse.authenticated) {
+    return new Response(authResponse.failReason, { status: 401 });
+  }
+
+  try {
+    const weights = await fetchDelegateWeights();
+    return NextResponse.json(weights);
+  } catch (e: any) {
+    return new Response("Internal server error: " + e.toString(), {
+      status: 500,
+    });
+  }
+}
