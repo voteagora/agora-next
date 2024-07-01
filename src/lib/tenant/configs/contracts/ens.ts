c24174f62d91161bd423fea98f44bb651f8be815
Michael Gingras <mcg79@cornell.edu>
2024-07-01 08:12:09 -0600

    Replaces PLM config to new tenant design

--- src/lib/tenant/configs/contracts/ens.ts
+++ src/lib/tenant/configs/contracts/ens.ts
@@ -10,8 +10,13 @@ import { mainnet } from "viem/chains";
 import { IGovernorContract } from "@/lib/contracts/common/interfaces/IGovernorContract";
 
 export const ensTenantContractConfig = (isProd: boolean): TenantContracts => {
-  const TOKEN = "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72";
-  const GOVERNOR = "0x0";
+  const TOKEN = isProd
+    ? "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72"
+    : "0xca83e6932cf4F03cDd6238be0fFcF2fe97854f67";
+
+  const GOVERNOR = isProd
+    ? "0x0"
+    : "0x630a6a268191c654ce084aad2d7910ff651e0797";
 
   const provider = ethProvider;
 
