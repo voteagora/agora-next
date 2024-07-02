ee39342e205ab6edde5c59fdcac7e3f0c56a206c
andrei <andrei.taraschuk@gmail.com>
2024-07-02 14:59:11 -0600

    Code review

--- src/app/info/components/GovernanceCharts.tsx
+++ src/app/info/components/GovernanceCharts.tsx
@@ -9,17 +9,15 @@ import ChartGovernanceActiveDelegates from "@/app/info/components/ChartGovernanc
 interface TabProps {
   value: string;
   title: string;
-  description: string;
 }
 
-const TabTrigger: React.FC<TabProps> = ({ value, title, description }) => {
+const TabTrigger: React.FC<TabProps> = ({ value, title }) => {
   return (
     <TabsTrigger
       value={value}
       className="flex flex-col last:col-span-2  last:sm:col-span-1  opacity-100 border !border-gray-300 border-t-0 border-l-0 first:rounded-tl-lg last:rounded-tr-lg  bg-gray-fa data-[state=active]:bg-white p-4 sm:p-8 w-full"
     >
       <h3 className="text-sm sm:text-base font-semibold text-black">{title}</h3>
-      <p className="text-xs font-medium text-gray-4f">{description}</p>
     </TabsTrigger>
   );
 };
@@ -39,27 +37,22 @@ const GovernanceCharts = ({
     {
       value: "top-delegates",
       title: "Top Delegates",
-      description: "",
     },
     {
       value: "active-delegates",
       title: "Active Delegates",
-      description: "",
     },
     {
       value: "avg-voters",
       title: "Proposal Voters",
-      description: "",
     },
     {
       value: "delegates-needed",
       title: "Delegates needed",
-      description: "",
     },
     {
       value: "total-votable-supply",
       title: "Total votable supply",
-      description: "",
     },
   ];
 
@@ -69,12 +62,7 @@ const GovernanceCharts = ({
       <Tabs className="mt-4 border rounded-lg" defaultValue="top-delegates">
         <TabsList className="grid grid-cols-2 sm:grid-cols-5 !gap-0 w-full h-fit">
           {tabs.map((tab) => (
-            <TabTrigger
-              key={tab.value}
-              value={tab.value}
-              title={tab.title}
-              description={tab.description}
-            />
+            <TabTrigger key={tab.value} value={tab.value} title={tab.title} />
           ))}
         </TabsList>
 
