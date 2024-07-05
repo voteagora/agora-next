290bf7eb3d09992fb7092b93c08123f9e2ec6c58
andrei <andrei.taraschuk@gmail.com>
2024-07-05 15:21:07 -0600

    Andrei/agora 2209 add support for stakeholders i represent in delegate (#385)
    * Merge
    
    * Adding stakeholders to the tenant
    
    * Misc name changes
    
    * Misc
    
    * Code review
    
    ---------
    
    Co-authored-by: Andrei Taraschuk <andrei@Andreis-MacBook-Pro.local>

--- src/components/shared/AgoraLoader/AgoraLoader.tsx
+++ src/components/shared/AgoraLoader/AgoraLoader.tsx
@@ -1,28 +1,23 @@
 import logo from "@/assets/agora_logo.svg";
 import Image from "next/image";
-import { VStack } from "@/components/Layout/Stack";
 import styles from "./agoraLoader.module.scss";
 
 export default function AgoraLoader() {
   return (
-    <VStack
-      justifyContent="justify-center"
-      alignItems="items-center"
-      className={styles.loading_container}
+    <div
+      className={`flex flex-col justify-center items-center ${styles.loading_container}`}
     >
       <Image alt="loading" width={24} height={24} src={logo} />
-    </VStack>
+    </div>
   );
 }
 
 export function AgoraLoaderSmall() {
   return (
-    <VStack
-      justifyContent="justify-center"
-      alignItems="items-center"
-      className={styles.loading_container__small}
+    <div
+      className={`flex flex-col justify-center items-center ${styles.loading_container__small}`}
     >
       <Image alt="loading" width={24} height={24} src={logo} />
-    </VStack>
+    </div>
   );
 }
