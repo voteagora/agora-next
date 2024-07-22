f884569f9bfea6d38e560fc21ff282ed8ec6bc7b
andrei <andrei.taraschuk@gmail.com>
2024-07-22 14:14:56 -0600

    Cyber assets

--- src/lib/tenant/configs/ui/cyber.ts
+++ src/lib/tenant/configs/ui/cyber.ts
@@ -1,25 +1,24 @@
 import { TenantUI } from "@/lib/tenant/tenantUI";
-import etherfiHero from "@/assets/tenant/etherfi_hero.svg";
-import etherfiLogo from "@/assets/tenant/etherfi_logo.svg";
-import delegateImage from "@/assets/tenant/etherfi_delegate.svg";
-import successImage from "@/assets/tenant/etherfi_success.svg";
-import pendingImage from "@/assets/tenant/etherfi_pending.svg";
+import cyberHero from "@/assets/tenant/cyber_hero.svg";
+import cyberLogo from "@/assets/tenant/cyber_logo.svg";
+import delegateImage from "@/assets/tenant/cyber_delegate.svg";
+import successImage from "@/assets/tenant/cyber_success.svg";
+import pendingImage from "@/assets/tenant/cyber_pending.svg";
 
 export const cyberTenantUIConfig = new TenantUI({
   title: "Cyber Agora",
   color: "#2F38FF",
-  hero: etherfiHero,
-  logo: etherfiLogo,
+  hero: cyberHero,
+  logo: cyberLogo,
 
   assets: {
-    // TODO: Replace success and pending images
     success: successImage,
     pending: pendingImage,
     delegate: delegateImage,
   },
 
   organization: {
-    title: "Cyber",
+    title: "Cyber DAO",
   },
 
   delegates: {
@@ -28,48 +27,20 @@ export const cyberTenantUIConfig = new TenantUI({
     retired: [],
   },
 
-  links: [
-    {
-      name: "discord",
-      title: "Discord",
-      url: "https://discord.gg/vBJkUYBuwX",
-    },
-    {
-      name: "bugs",
-      title: "Report bugs & feedback",
-      url: "https://app.deform.cc/form/7180b273-7662-4f96-9e66-1eae240a52bc",
-    },
-    {
-      name: "governanceForum",
-      title: "Governance Forum",
-      url: "https://governance.ether.fi/",
-    },
-  ],
-
   pages: [
     {
       route: "/",
-      title: "Agora is the home of ether.fi governance",
+      title: "Welcome to Cyber governance",
       description:
-        "ether.fi governance is launching now. Start by claiming your token and joining Discourse to engage in the discussion. Delegation and voting are coming soon.",
+        "Cyber delegates are the stewards of the Cyber DAO, appointed by token holders to make governance decisions on their behalf.",
       meta: {
-        title: "ether.fi Agora",
-        description: "Home of token governance",
-      },
-    },
-    {
-      route: "info",
-      title: "Agora is the home of ether.fi governance",
-      description:
-        "ether.fi governance is launching now. Start by claiming your token and joining Discourse to engage in the discussion. Delegation and voting are coming soon.",
-      meta: {
-        title: "ether.fi Agora",
+        title: "Welcome to Cyber governance",
         description: "Home of token governance",
       },
     },
     {
       route: "delegates",
-      title: "Agora is the home of ether.fi delegates",
+      title: "Welcome to Cyber governance",
       description:
         "ether.fi voters are the stewards for the DAO. You can see them all below, delegate your votes to them, or contact them about your ideas.",
       meta: {
@@ -77,26 +48,11 @@ export const cyberTenantUIConfig = new TenantUI({
         description: "Delegate your voting power to a trusted representative",
       },
     },
-    {
-      route: "proposals",
-      href: "https://snapshot.org/#/etherfi-dao.eth",
-      title: "Agora is the home of ether.fi delegates",
-      description:
-        "ETHER.FI voters are the stewards for the DAO. You can see them all below, delegate your votes to them, or contact them about your ideas.",
-      meta: {
-        title: "ether.fi Agora",
-        description: "Home of token governance",
-      },
-    },
   ],
 
   toggles: [
     {
       name: "delegates",
-      enabled: true,
-    },
-    {
-      name: "info",
       enabled: true,
     },
   ],
