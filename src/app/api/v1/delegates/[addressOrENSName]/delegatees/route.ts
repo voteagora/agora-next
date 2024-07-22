7f3344007711f4eddc9390e2d8c34165b366ae17
stepandel <stepandel@gmail.com>
2024-07-22 17:47:19 -0400

    add delegatees route

---
+++ src/app/api/v1/delegates/[addressOrENSName]/delegatees/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
+import { traceWithUserId } from "../../../apiUtils";
+import { fetchCurrentDelegatees } from "@/app/delegates/actions";
+
+export async function GET(
+  request: NextRequest,
+  route: { params: { addressOrENSName: string } }
+) {
+  const authResponse = await authenticateApiUser(request);
+
+  if (!authResponse.authenticated) {
+    return new Response(authResponse.failReason, { status: 401 });
+  }
+
+  return await traceWithUserId(authResponse.userId as string, async () => {
+    try {
+      const { addressOrENSName } = route.params;
+      const delegate = await fetchCurrentDelegatees(addressOrENSName);
+      return NextResponse.json(delegate);
+    } catch (e: any) {
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
+      });
+    }
+  });
+}
