5e79a32f69f7a6024265c09caa3186b6eae7c655
Stepan Arsentjev <stepandel@gmail.com>
2024-07-23 17:34:34 +0200

    [API 0.1.2] Patch: correct routes for delegates/address & delegators (#413)
    * correct routes for delegates/address & delegators
    
    * get addressOrEns from route

--- src/app/api/v1/delegates/[addressOrENSName]/route.ts
+++ src/app/api/v1/delegates/[addressOrENSName]/route.ts
@@ -1,23 +1,7 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { fetchDelegate } from "@/app/api/common/delegates/getDelegates";
 import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
 import { traceWithUserId } from "../../apiUtils";
-import { fetchCurrentDelegators } from "@/app/api/common/delegations/getDelegations";
-import { createOptionalNumberValidator } from "@/app/api/common/utils/validators";
-
-const DEFAULT_MAX_LIMIT = 100;
-const DEFAULT_LIMIT = 20;
-const DEFAULT_OFFSET = 0;
-
-const limitValidator = createOptionalNumberValidator(
-  1,
-  DEFAULT_MAX_LIMIT,
-  DEFAULT_LIMIT
-);
-const offsetValidator = createOptionalNumberValidator(
-  0,
-  Number.MAX_SAFE_INTEGER,
-  DEFAULT_OFFSET
-);
 
 export async function GET(
   request: NextRequest,
@@ -30,15 +14,9 @@ export async function GET(
   }
 
   return await traceWithUserId(authResponse.userId as string, async () => {
-    const params = request.nextUrl.searchParams;
     try {
       const { addressOrENSName } = route.params;
-      const limit = limitValidator.parse(params.get("limit"));
-      const offset = offsetValidator.parse(params.get("offset"));
-      const delegate = await fetchCurrentDelegators(addressOrENSName, {
-        limit,
-        offset,
-      });
+      const delegate = await fetchDelegate(addressOrENSName);
       return NextResponse.json(delegate);
     } catch (e: any) {
       return new Response("Internal server error: " + e.toString(), {
