001b640c63d65a1715e804bb2b21b5ee50533e51
Michael Gingras <mcg79@cornell.edu>
2024-07-24 13:00:37 -0600

    Adds cyber UI updates
    * Resolves modal sheet + table issues
    
    * Ensures all filters have same style
    
    * Stragglers

--- src/app/delegates/components/IssuesFilter.tsx
+++ src/app/delegates/components/IssuesFilter.tsx
@@ -59,12 +59,12 @@ export default function IssuesFilter() {
     >
       {() => (
         <>
-          <Listbox.Button className="w-full sm:w-fit bg-[#F7F7F7] text-base font-medium border-none rounded-full py-2 px-4 flex items-center">
+          <Listbox.Button className="w-full sm:w-fit bg-wash text-base font-medium border-none rounded-full py-2 px-4 flex items-center">
             {issuesFilterOptions[filterParam]?.value ||
               issuesFilterOptions.all.value}
             <ChevronDown className="h-4 w-4 ml-[2px] opacity-30 hover:opacity-100" />
           </Listbox.Button>
-          <Listbox.Options className="mt-3 absolute bg-[#F7F7F7] border border-[#ebebeb] p-2 rounded-2xl flex flex-col gap-1 z-20 w-max">
+          <Listbox.Options className="mt-3 absolute bg-wash border border-line p-2 rounded-2xl flex flex-col gap-1 z-20 w-max">
             {Object.keys(issuesFilterOptions).map((key) => (
               <Listbox.Option key={key} value={key} as={Fragment}>
                 {({ selected }) => {
@@ -72,8 +72,8 @@ export default function IssuesFilter() {
                     <li
                       className={`cursor-pointer text-base py-2 px-3 border rounded-xl font-medium ${
                         selected
-                          ? "text-black bg-white border-[#ebebeb]"
-                          : "text-[#66676b] border-transparent"
+                          ? "text-black bg-white border-line"
+                          : "text-tertiary border-transparent"
                       }`}
                     >
                       {issuesFilterOptions[key].value}
