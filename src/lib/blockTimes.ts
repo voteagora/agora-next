ed1cfe42badc6c109bb2357af52d8d5cc6644cae
andrei <andrei.taraschuk@gmail.com>
2024-07-25 14:25:05 -0600

    Fxied block calculation bug for mainnet (#422)

--- src/lib/blockTimes.ts
+++ src/lib/blockTimes.ts
@@ -9,7 +9,7 @@ const chainId = Tenant.current().contracts.token.chain.id;
  * @param {number} latestBlockNumber
  * @param {number} latestBlockTimestamp
  * @returns {Date}
- * @description Converts a block number to a human readable date
+ * @description Converts a block number to a human-readable date
  */
 export function getHumanBlockTime(
   blockNumber: number | string | bigint,
@@ -17,39 +17,46 @@ export function getHumanBlockTime(
 ) {
   switch (chainId) {
     // Optimism
-    case 10:
+    case 10: {
       const secondsPerBlock = 2;
       const secondsPerBlockBeforeBedrock = 0.5;
       const bedrockBlockNumber = 105235062;
 
+      const blocksBeforeBedrock = Math.max(
+        bedrockBlockNumber - Number(blockNumber),
+        0
+      );
+      const blocksAfterBedrock = Math.min(
+        Number(latestBlock.number) - bedrockBlockNumber,
+        Number(latestBlock.number) - Number(blockNumber)
+      );
+
       const timeBeforeBedrock =
-        Math.max(bedrockBlockNumber - Number(blockNumber), 0) *
-        secondsPerBlockBeforeBedrock;
-      const timeAfterBedrock =
-        Math.min(
-          Number(latestBlock.number) - Number(blockNumber),
-          Number(latestBlock.number) - bedrockBlockNumber
-        ) * secondsPerBlock;
+        blocksBeforeBedrock * secondsPerBlockBeforeBedrock;
+      const timeAfterBedrock = blocksAfterBedrock * secondsPerBlock;
 
       return new Date(
-        (Date.now() / 1000 - timeBeforeBedrock - timeAfterBedrock) * 1000
+        (latestBlock.timestamp - timeBeforeBedrock - timeAfterBedrock) * 1000
       );
+    }
 
     //   Cyber Mainnet
     //   Cyber Testnet
     case 7560:
-    case 111557560:
+    case 111557560: {
       const estCyberSecondsDiff =
-        Number(latestBlock.number) - Number(blockNumber) * 2; // 2 seconds per block
+        (Number(latestBlock.number) - Number(blockNumber)) * 2; // 2 seconds per block
       return new Date((latestBlock.timestamp - estCyberSecondsDiff) * 1000);
+    }
 
     //   Ethereum Mainnet
     //   Ethereum Sepolia
     case 1:
-    case 11155111:
+    case 11155111: {
       const estEthSecondsDiff =
-        Number(latestBlock.number) - Number(blockNumber) * 12; // 12 seconds per block
+        (Number(latestBlock.number) - Number(blockNumber)) * 12.05; // 12.05 seconds per block
       return new Date((latestBlock.timestamp - estEthSecondsDiff) * 1000);
+    }
 
     default:
       return new Date();
