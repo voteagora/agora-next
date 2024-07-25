28cc5f61b0ef8c8d18f6fa7010920d1768301659
andrei <andrei.taraschuk@gmail.com>
2024-07-25 11:55:22 -0600

    Add default input value for delegate statement

--- src/components/DelegateStatement/DelegateStatementFormSection.tsx
+++ src/components/DelegateStatement/DelegateStatementFormSection.tsx
@@ -1,12 +1,12 @@
 import Markdown from "@/components/shared/Markdown/Markdown";
-import { HStack, VStack } from "@/components/Layout/Stack";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { FormField } from "@/components/ui/form";
 import { Textarea } from "@/components/ui/textarea";
-import { useWatch, type UseFormReturn } from "react-hook-form";
+import { type UseFormReturn, useWatch } from "react-hook-form";
 import { type DelegateStatementFormValues } from "./CurrentDelegateStatement";
 import Tenant from "@/lib/tenant/tenant";
 import { TENANT_NAMESPACES } from "@/lib/constants";
+import { FormEvent } from "react";
 
 export default function DelegateStatementFormSection({
   form,
@@ -18,11 +18,24 @@ export default function DelegateStatementFormSection({
 
   const showTemplate = namespace === TENANT_NAMESPACES.OPTIMISM;
 
+  const addDefaultValueOnFocus = (e: FormEvent) => {
+    if ((e.target as HTMLInputElement).value === "") {
+      (e.target as HTMLInputElement).value = defaultValue;
+    }
+  };
+
+  // Keep this value multiline
+  const defaultValue = `A brief intro to yourself:
+
+A message to the community and ecosystem:
+
+Discourse username:`;
+
   return (
-    <VStack className="py-8 px-6 border-b border-line">
+    <div className="flex flex-col py-8 px-6 border-b border-line">
       <Tabs defaultValue="write">
-        <HStack className="gap-4 justify-between items-baseline">
-          <HStack className="items-baseline gap-2">
+        <div className="flex flex-row gap-4 justify-between items-baseline">
+          <div className="flex flex-row items-baseline gap-2">
             <h3 className="font-bold">Delegate statement</h3>
             {showTemplate && (
               <a
@@ -33,7 +46,7 @@ export default function DelegateStatementFormSection({
                 <p className="text-sm opacity-50">View template</p>
               </a>
             )}
-          </HStack>
+          </div>
           <TabsList className="gap-0">
             <TabsTrigger variant="gray" className="text-sm" value="write">
               Write
@@ -42,7 +55,7 @@ export default function DelegateStatementFormSection({
               Preview
             </TabsTrigger>
           </TabsList>
-        </HStack>
+        </div>
         <TabsContent value="write">
           <FormField
             control={form.control}
@@ -50,11 +63,8 @@ export default function DelegateStatementFormSection({
             render={({ field }) => (
               <Textarea
                 className="mt-2 min-h-[16rem]"
-                placeholder={`A brief intro to yourself:
-
-A message to the community and ecosystem:
-
-Discourse username:`}
+                onFocus={addDefaultValueOnFocus}
+                placeholder={defaultValue}
                 {...field}
               />
             )}
@@ -64,6 +74,6 @@ Discourse username:`}
           <Markdown content={delegateStatement} />
         </TabsContent>
       </Tabs>
-    </VStack>
+    </div>
   );
 }
