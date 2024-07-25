e7ac3c41380a2844d6362d03069469e57651519b
andrei <andrei.taraschuk@gmail.com>
2024-07-25 15:17:14 -0600

    OG test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
-import { fetchNeedsMyVoteProposals as apiFetchNeedsMyVoteProposals } from "@/app/api/common/proposals/getNeedsMyVoteProposals";
+import {
+  fetchNeedsMyVoteProposals as apiFetchNeedsMyVoteProposals,
+} from "@/app/api/common/proposals/getNeedsMyVoteProposals";
 import {
   fetchDraftProposalForSponsor as apiFetchDraftProposalsForSponsorship,
   fetchDraftProposals as apiFetchDraftProposals,
   fetchProposals as apiFetchProposals,
 } from "@/app/api/common/proposals/getProposals";
 import { fetchVotableSupply as apiFetchVotableSupply } from "@/app/api/common/votableSupply/getVotableSupply";
-import { fetchGovernanceCalendar as apiFetchGovernanceCalendar } from "@/app/api/common/governanceCalendar/getGovernanceCalendar";
+import {
+  fetchGovernanceCalendar as apiFetchGovernanceCalendar,
+} from "@/app/api/common/governanceCalendar/getGovernanceCalendar";
 import Hero from "@/components/Hero/Hero";
 import NeedsMyVoteProposalsList from "@/components/Proposals/NeedsMyVoteProposalsList/NeedsMyVoteProposalsList";
 import ProposalsList from "@/components/Proposals/ProposalsList/ProposalsList";
@@ -49,13 +53,14 @@ export async function generateMetadata({}, parent) {
   const { title, description } = page.meta;
 
   const preview = `/api/images/og/proposals?title=${encodeURIComponent(
-    title
+    title,
   )}&description=${encodeURIComponent(description)}`;
 
   return {
     title: title,
     description: description,
     openGraph: {
+      url: parent.url,
       images: [
         {
           url: preview,
@@ -81,10 +86,10 @@ export default async function Home() {
 
   const governanceCalendar = await fetchGovernanceCalendar();
   const relevalntProposals = await fetchProposals(
-    proposalsFilterOptions.relevant.filter
+    proposalsFilterOptions.relevant.filter,
   );
   const allProposals = await fetchProposals(
-    proposalsFilterOptions.everything.filter
+    proposalsFilterOptions.everything.filter,
   );
 
   const votableSupply = await fetchVotableSupply();
