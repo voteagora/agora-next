546fdb7175b9535e712e66743bdf53bd861b0b05
stepandel <stepandel@gmail.com>
2024-07-25 17:35:30 -0400

    remove redirects

--- next.config.js
+++ next.config.js
@@ -9,23 +9,7 @@ module.exports = {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      {
-        source: "/delegate/:addressOrENSName",
-        destination: "/delegates/:addressOrENSName",
-        permanent: true,
-      },
-      {
-        source: "/api/v1/proposals/:proposalId",
-        destination: "/404",
-        permanent: true,
-      },
-      {
-        source: "/api/v1/proposals",
-        destination: "/404",
-        permanent: true,
-      },
-    ];
+    return [];
   },
   async headers() {
     return [
