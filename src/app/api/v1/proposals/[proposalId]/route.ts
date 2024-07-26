8fc88941fe522d7fb977818a1795ca2ac173ce62
stepandel <stepandel@gmail.com>
2024-07-26 14:14:06 -0400

    open proposal route

--- src/app/api/v1/proposals/[proposalId]/route.ts
+++ src/app/api/v1/proposals/[proposalId]/route.ts
@@ -1,5 +1,27 @@
-export async function GET(request: Request) {
-  return new Response(JSON.stringify(`{result: "success"}`), {
-    status: 200,
+import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
+import { NextRequest, NextResponse } from "next/server";
+import { traceWithUserId } from "../../apiUtils";
+import { fetchProposal } from "../../../common/proposals/getProposals";
+
+export async function GET(
+  request: NextRequest,
+  route: { params: { proposalId: string } }
+) {
+  const authResponse = await authenticateApiUser(request);
+
+  if (!authResponse.authenticated) {
+    return new Response(authResponse.failReason, { status: 401 });
+  }
+
+  return await traceWithUserId(authResponse.userId as string, async () => {
+    try {
+      const { proposalId } = route.params;
+      const proposal = await fetchProposal(proposalId);
+      return NextResponse.json(proposal);
+    } catch (e: any) {
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
+      });
+    }
   });
 }
