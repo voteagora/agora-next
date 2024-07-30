ed944d6a1341d28235d6a3f0d6569862f199b3eb
jeff <jeff@voteagora.com>
2024-07-30 19:39:13 -0400

    Finish query

--- src/app/api/analytics/vote/getProposalVoteCounts.ts
+++ src/app/api/analytics/vote/getProposalVoteCounts.ts
@@ -14,7 +14,8 @@ async function getProposalVoteCounts() {
 
   const govContract = contracts.governor;
 
-  const QRY = `WITH proposal_counts AS
+  const QRY = `WITH 
+                proposal_counts AS
                 (
                         SELECT   proposal_id,
                                   Sum(voter_count) voter_count
@@ -22,16 +23,21 @@ async function getProposalVoteCounts() {
                         WHERE    dao_slug = '${slug}'
                         AND      contract = '${govContract}'
                         GROUP BY 1
-                        ORDER BY 1), relevant_proposals AS
+                        ORDER BY 1), 
+
+                relevant_proposals AS
                 (
                       SELECT *
                       FROM   uniswap.proposals
                       WHERE  contract = '${govContract}')
+
                 SELECT    ROW_NUMBER() OVER (ORDER BY p.start_block) AS position,
                           pc.proposal_id, 
                           pc.voter_count,
                           split_part(p.description, e'\n', 1)
-                FROM      proposal_counts pc`;
+                FROM      proposal_counts pc left join relevant_proposals p 
+                ON        pc.proposal_id::text = p.proposal_id 
+                ORDER BY  p.start_block`;
 
   const result = await prisma.$queryRawUnsafe<ProposalCount[]>(QRY);
 
