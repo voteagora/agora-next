41fe31217a26d87c2a01e5995c569a2c78a5d0a0
stepandel <stepandel@gmail.com>
2024-07-31 12:58:57 -0400

    add votable & total supply to token response

--- src/app/api/v1/contracts/token/route.ts
+++ src/app/api/v1/contracts/token/route.ts
@@ -2,6 +2,7 @@ import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
 import { NextRequest, NextResponse } from "next/server";
 import { traceWithUserId } from "../../apiUtils";
 import Tenant from "@/lib/tenant/tenant";
+import { fetchMetrics } from "@/app/api/common/metrics/getMetrics";
 
 export async function GET(request: NextRequest) {
   const authResponse = await authenticateApiUser(request);
@@ -15,7 +16,15 @@ export async function GET(request: NextRequest) {
       const { contracts } = Tenant.current();
       const address = contracts.token.address;
       const chainId = contracts.token.chain.id;
-      return NextResponse.json({ address, chainId });
+
+      const { votableSupply, totalSupply } = await fetchMetrics();
+
+      return NextResponse.json({
+        address,
+        chainId,
+        votableSupply,
+        totalSupply,
+      });
     } catch (e: any) {
       return new Response("Internal server error: " + e.toString(), {
         status: 500,
