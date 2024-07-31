3ea3587b4ecda5438df824f6b5f777d11c88364f
jefag <jeff@voteagora.com>
2024-07-31 16:03:07 -0400

    Several Community Info Related Tweaks (#434)
    * Several Community Info Related Tweaks
    
    * Finish query
    
    * Fix bug

--- src/app/api/analytics/vote/getProposalVoteCounts.ts
+++ src/app/api/analytics/vote/getProposalVoteCounts.ts
@@ -3,19 +3,41 @@ import prisma from "@/app/lib/prisma";
 import { cache } from "react";
 
 type ProposalCount = {
+  position: number;
   proposalId: number;
   voter_count: number;
+  proposal_title: string;
 };
 
 async function getProposalVoteCounts() {
-  const { namespace } = Tenant.current();
+  const { slug, contracts } = Tenant.current();
 
-  const QRY = `SELECT proposal_id,
-                    SUM(voter_count) voter_count
-                FROM   alltenant.dao_engagement_votes
-                WHERE  tenant = '${namespace}'
-                    GROUP  BY 1
-                    ORDER  BY 1`;
+  const govContract = contracts.governor.address;
+
+  const QRY = `WITH 
+                proposal_counts AS
+                (
+                        SELECT   proposal_id,
+                                  Sum(voter_count) voter_count
+                        FROM     alltenant.dao_engagement_votes_cs
+                        WHERE    dao_slug = '${slug}'
+                        AND      contract = '${govContract}'
+                        GROUP BY 1
+                        ORDER BY 1), 
+
+                relevant_proposals AS
+                (
+                      SELECT *
+                      FROM   uniswap.proposals
+                      WHERE  contract = '${govContract}')
+
+                SELECT    ROW_NUMBER() OVER (ORDER BY p.start_block) AS position,
+                          pc.proposal_id, 
+                          pc.voter_count,
+                          split_part(p.description, e'\n', 1)
+                FROM      proposal_counts pc left join relevant_proposals p 
+                ON        pc.proposal_id::text = p.proposal_id 
+                ORDER BY  p.start_block`;
 
   const result = await prisma.$queryRawUnsafe<ProposalCount[]>(QRY);
 
