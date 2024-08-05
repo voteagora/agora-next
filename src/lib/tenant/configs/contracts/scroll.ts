38c618b4c9e8dc1db1a43fa18c5b2903b86ab446
andrei <andrei.taraschuk@gmail.com>
2024-08-05 14:49:43 -0600

    Scroll tenant config (#430)

---
+++ src/lib/tenant/configs/contracts/scroll.ts
@@ -0,0 +1,48 @@
+import {
+  EtherfiToken__factory,
+  OptimismGovernor__factory,
+} from "@/lib/contracts/generated";
+import { ITokenContract } from "@/lib/contracts/common/interfaces/ITokenContract";
+import { TenantContract } from "@/lib/tenant/tenantContract";
+import { TenantContracts } from "@/lib/types";
+import { scroll } from "viem/chains";
+
+import { IGovernorContract } from "@/lib/contracts/common/interfaces/IGovernorContract";
+import { JsonRpcProvider } from "ethers";
+
+interface Props {
+  isProd: boolean;
+  alchemyId: string;
+}
+
+export const scrollTenantContractConfig = ({
+  isProd,
+  alchemyId,
+}: Props): TenantContracts => {
+  const TOKEN = "0x5300000000000000000000000000000000000004";
+  const GOVERNOR = "0x0";
+
+  const provider = new JsonRpcProvider(
+    `https://scroll-mainnet.g.alchemy.com/v2/${alchemyId}`
+  );
+  const chain = scroll;
+
+  return {
+    token: new TenantContract<ITokenContract>({
+      abi: EtherfiToken__factory.abi,
+      address: TOKEN as `0x${string}`,
+      chain,
+      contract: EtherfiToken__factory.connect(TOKEN, provider),
+      provider,
+    }),
+
+    // PLACEHOLDER CONTRACT
+    governor: new TenantContract<IGovernorContract>({
+      abi: [],
+      address: GOVERNOR,
+      chain,
+      contract: OptimismGovernor__factory.connect(GOVERNOR, provider),
+      provider,
+    }),
+  };
+};
