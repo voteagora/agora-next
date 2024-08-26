2dc8dc22e3736f47a287f07765c27a79a810573d
Stepan Arsentjev <stepandel@gmail.com>
2024-08-27 01:19:49 +0200

    [RetroFunding R5 API] 0.2.1 Ballots (#469)
    * create ProjectAllocations table
    
    * enforce not null on projectAllocatons.impact
    
    * fix otel lock
    
    * get ballot for R5
    
    * implement update ballot allocation
    
    * update project impact: re-rank projects
    
    * update positions for project in ballot
    
    * shuffle projects
    
    * add categories and categry_allocaitons table
    
    * update categories schema
    
    * include category allocations to ballot payload
    
    * update and rebalance category allocations
    
    * handle 0 allocation or no categories case
    
    * implement ballot submission
    
    * parse project data
    
    * add category_allocations to ballot return payload
    
    * remove unused
    
    * :memo: remove R5 tag in /projects
    
    * :memo: update ballots payload
    
    * :memo: remove round 5 tag from /ballots
    
    * :memo: update ballot submission response
    
    * :memo: add ballot state manipulation endpoints
    
    * :memo: update categories endpoint
    
    * :memo: fix swagger error
    
    * :memo: unique operationIds
    
    * :memo: define global params
    
    * update release schedule

--- src/app/api/common/ballots/getBallots.ts
+++ src/app/api/common/ballots/getBallots.ts
@@ -1,7 +1,7 @@
 import { paginateResult } from "@/app/lib/pagination";
 import { cache } from "react";
 import { addressOrEnsNameWrap } from "../utils/ensName";
-import { Ballots } from "@prisma/client";
+import { Ballots, Prisma, projects_data } from "@prisma/client";
 import { Ballot } from "./ballot";
 import prisma from "@/app/lib/prisma";
 import { calculateAllocations } from "./ballotAllocations";
@@ -71,51 +71,159 @@ async function getBallotForAddress({
   roundId: number;
   address: string;
 }) {
-  console.log("roundId", roundId);
-  console.log("address", address);
+  if (roundId === 4) {
+    return getR4Ballot({ roundId, address });
+  }
 
+  if (roundId === 5) {
+    return getR5Ballot({ roundId, address });
+  }
+}
+
+async function getR5Ballot({
+  roundId,
+  address,
+}: {
+  roundId: number;
+  address: string;
+}) {
+  const [ballot, projects] = await Promise.all([
+    prisma.ballots.findFirst({
+      where: {
+        round: roundId,
+        address,
+      },
+      include: {
+        project_allocations: {
+          include: {
+            projects_data: true,
+          },
+          orderBy: {
+            rank: "desc",
+          },
+        },
+        category_allocations: {
+          orderBy: {
+            allocation: "desc",
+          },
+        },
+      },
+    }),
+    prisma.$queryRaw<projects_data[]>`
+      SELECT 
+        *
+      FROM 
+        retro_funding.projects_data
+      ORDER BY RANDOM();
+    `,
+  ]);
+
+  if (!ballot) {
+    return {
+      address,
+      round_id: roundId,
+      status: "NOT STARTED",
+      project_allocations: [],
+      category_allocations: [],
+      projects_to_be_evaluated: projects.map((project) => project.project_id),
+      total_projects: projects.length,
+    };
+  }
+
+  if (ballot.project_allocations.length < projects.length) {
+    return {
+      address: ballot.address,
+      round_id: ballot.round,
+      status: "PROJECT RANKING",
+      project_allocations: parseProjectAllocations(ballot.project_allocations),
+      category_allocations: ballot.category_allocations,
+      projects_to_be_evaluated: projects
+        .filter(
+          (project) =>
+            !ballot.project_allocations.some(
+              (allocation) => allocation.project_id === project.project_id
+            )
+        )
+        .map((project) => project.project_id),
+      total_projects: projects.length,
+    };
+  }
+
+  return {
+    address: ballot.address,
+    round_id: ballot.round,
+    status: "PENDING SUBMISSION",
+    project_allocations: parseProjectAllocations(ballot.project_allocations),
+    category_allocations: ballot.category_allocations,
+    projects_to_be_evaluated: [],
+    total_projects: projects.length,
+  };
+}
+
+function parseProjectAllocations(
+  allocations: Prisma.ProjectAllocationsGetPayload<{
+    include: { projects_data: true };
+  }>[]
+) {
+  return allocations.map((allocation, i) => ({
+    project_id: allocation.project_id,
+    name: allocation.projects_data.project_name,
+    image: allocation.projects_data.project_image,
+    position: i,
+    allocation: allocation.allocation,
+    impact: allocation.impact,
+  }));
+}
+
+async function getR4Ballot({
+  roundId,
+  address,
+}: {
+  roundId: number;
+  address: string;
+}) {
   const ballot = await prisma.$queryRawUnsafe<Ballot[]>(
     `
-    SELECT 
-      b.address,
-      b.round,
-      CASE WHEN EXISTS (
-                  SELECT 1 
-                  FROM retro_funding.ballot_submittions bs 
-                  WHERE bs.address = b.address AND bs.round = b.round
-              ) THEN 'SUBMITTED'
-              ELSE 'PENDING'
-          END AS status,
-      b.created_at,
-      b.updated_at,
-      (
-        SELECT updated_at
-        FROM retro_funding.ballot_submittions bs 
-        WHERE bs.address = b.address AND bs.round = b.round
-      ) as published_at,
-      os_only, 
-      os_multiplier, 
-      a.metric_id, 
-      a.allocation,
-      locked,
-      COALESCE(
-        (SELECT json_agg(json_build_object(
-                'project_id', pd.project_id, 
-                'name', pd.project_name,
-                'image', pd.project_image,
-                'is_os', mp.is_os,
-                'value', mp.values
-        ))
-        FROM retro_funding.metrics_projects mp
-        JOIN retro_funding.projects_data pd ON mp.project_id = pd.project_id
-        WHERE mp.metric_id = a.metric_id),
-        '[]'::json
-      ) AS allocations
-    FROM retro_funding.ballots b
-    JOIN retro_funding.allocations a ON b.address = a.address AND b.round = a.round
-    WHERE 
-      b.round = $1 AND b.address = $2;
-  `,
+  SELECT 
+    b.address,
+    b.round,
+    CASE WHEN EXISTS (
+                SELECT 1 
+                FROM retro_funding.ballot_submittions bs 
+                WHERE bs.address = b.address AND bs.round = b.round
+            ) THEN 'SUBMITTED'
+            ELSE 'PENDING'
+        END AS status,
+    b.created_at,
+    b.updated_at,
+    (
+      SELECT updated_at
+      FROM retro_funding.ballot_submittions bs 
+      WHERE bs.address = b.address AND bs.round = b.round
+    ) as published_at,
+    os_only, 
+    os_multiplier, 
+    a.metric_id, 
+    a.allocation,
+    locked,
+    COALESCE(
+      (SELECT json_agg(json_build_object(
+              'project_id', pd.project_id, 
+              'name', pd.project_name,
+              'image', pd.project_image,
+              'is_os', mp.is_os,
+              'value', mp.values
+      ))
+      FROM retro_funding.metrics_projects mp
+      JOIN retro_funding.projects_data pd ON mp.project_id = pd.project_id
+      WHERE mp.metric_id = a.metric_id),
+      '[]'::json
+    ) AS allocations
+  FROM retro_funding.ballots b
+  JOIN retro_funding.allocations a ON b.address = a.address AND b.round = a.round
+  WHERE 
+    b.round = $1 AND b.address = $2;
+`,
     roundId,
     address
   );
---
+++ src/app/api/common/ballots/updateBallotCategories.ts
@@ -0,0 +1,141 @@
+import { cache } from "react";
+import { addressOrEnsNameWrap } from "../utils/ensName";
+import prisma from "@/app/lib/prisma";
+import { fetchBallot } from "./getBallots";
+import { autobalanceAllocations } from "./autobalance";
+import { Decimal } from "@prisma/client/runtime";
+
+type BallotContent = {
+  category_slug: string;
+  allocation: number;
+  locked: boolean;
+};
+
+const updateBallotCategoryApi = async (
+  data: BallotContent,
+  roundId: number,
+  ballotCasterAddressOrEns: string
+) =>
+  addressOrEnsNameWrap(
+    updateBallotCategoryForAddress,
+    ballotCasterAddressOrEns,
+    {
+      data,
+      roundId,
+    }
+  );
+
+async function updateBallotCategoryForAddress({
+  data,
+  roundId,
+  address,
+}: {
+  data: BallotContent;
+  roundId: number;
+  address: string;
+}) {
+  // Create ballot if it doesn't exist
+  await prisma.ballots.upsert({
+    where: {
+      address_round: {
+        address,
+        round: roundId,
+      },
+    },
+    update: {
+      updated_at: new Date(),
+    },
+    create: {
+      round: roundId,
+      address,
+    },
+  });
+
+  // Add or update allocation
+  await prisma.categoryAllocations.upsert({
+    where: {
+      category_slug_address_round: {
+        category_slug: data.category_slug,
+        round: roundId,
+        address,
+      },
+    },
+    update: {
+      allocation: data.allocation,
+      locked: data.locked,
+      updated_at: new Date(),
+    },
+    create: {
+      category_slug: data.category_slug,
+      round: roundId,
+      address,
+      allocation: data.allocation,
+      locked: data.locked,
+    },
+  });
+
+  await autobalanceCategories(address, roundId, data.category_slug);
+
+  // Return full ballot
+  return fetchBallot(roundId, address);
+}
+
+async function autobalanceCategories(
+  address: string,
+  roundId: number,
+  categoryToSkip: string
+) {
+  const [curAllocations, categories] = await Promise.all([
+    prisma.categoryAllocations.findMany({
+      where: {
+        address,
+        round: roundId,
+      },
+    }),
+    prisma.categories.findMany({}),
+  ]);
+
+  const allocations = categories.map((category) => {
+    const curAllocation = curAllocations.find(
+      (a) => a.category_slug === category.slug
+    );
+    return {
+      id: category.slug,
+      allocation: curAllocation?.allocation || new Decimal(0),
+      locked: curAllocation?.locked || false,
+    };
+  });
+
+  const autobalancedAllocations = autobalanceAllocations({
+    allocations,
+    idToSkip: categoryToSkip,
+  });
+
+  console.log(autobalancedAllocations);
+
+  await Promise.all(
+    autobalancedAllocations.map(async (allocation) => {
+      await prisma.categoryAllocations.upsert({
+        where: {
+          category_slug_address_round: {
+            category_slug: allocation.id,
+            address,
+            round: roundId,
+          },
+        },
+        update: {
+          allocation: allocation.allocation,
+        },
+        create: {
+          category_slug: allocation.id,
+          round: roundId,
+          address,
+          allocation: allocation.allocation,
+          locked: allocation.locked,
+        },
+      });
+    })
+  );
+}
+
+export const updateBallotCategory = cache(updateBallotCategoryApi);
