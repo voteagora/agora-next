7eb51529926b8f9e4c92e6eddcd27bda8ba4d7e4
Stepan Arsentjev <stepandel@gmail.com>
2024-09-06 01:32:32 +0200

    [RetroFunding R5 API] Batch ballot update endpoint (#486)
    * add a post route to update all projects in ballot
    
    * update swagger docs

---
+++ src/app/api/v1/retrofunding/rounds/[roundId]/ballots/[ballotCasterAddressOrEns]/projects/route.ts
@@ -0,0 +1,67 @@
+import { updateAllProjectsInBallot } from "@/app/api/common/ballots/updateBallotProject";
+import { traceWithUserId } from "@/app/api/v1/apiUtils";
+import {
+  authenticateApiUser,
+  getCategoryScope,
+  validateAddressScope,
+} from "@/app/lib/auth/serverAuth";
+import { NextResponse, type NextRequest } from "next/server";
+import { z } from "zod";
+
+const ballotPayloadSchema = z.object({
+  projects: z.array(
+    z.object({
+      projectId: z.string(),
+      allocation: z.string(z.number().min(0).max(100)),
+      impact: z.number(),
+    })
+  ),
+});
+
+export async function POST(
+  request: NextRequest,
+  route: { params: { roundId: string; ballotCasterAddressOrEns: string } }
+) {
+  const authResponse = await authenticateApiUser(request);
+
+  if (!authResponse.authenticated) {
+    return new Response(authResponse.failReason, { status: 401 });
+  }
+
+  const { roundId, ballotCasterAddressOrEns } = route.params;
+  const scopeError = await validateAddressScope(
+    ballotCasterAddressOrEns,
+    authResponse
+  );
+  if (scopeError) return scopeError;
+
+  return await traceWithUserId(authResponse.userId as string, async () => {
+    try {
+      const categoryScope = getCategoryScope(authResponse);
+
+      if (!categoryScope) {
+        return new Response(
+          "This user does not have a category scope. Regenerate the JWT token",
+          {
+            status: 401,
+          }
+        );
+      }
+
+      const payload = await request.json();
+      const parsedPayload = ballotPayloadSchema.parse(payload);
+
+      const ballot = await updateAllProjectsInBallot(
+        parsedPayload.projects,
+        categoryScope,
+        Number(roundId),
+        ballotCasterAddressOrEns
+      );
+      return NextResponse.json(ballot);
+    } catch (e: any) {
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
+      });
+    }
+  });
+}
