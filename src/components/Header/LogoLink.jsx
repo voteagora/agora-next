223ce72412a4d90bbaef00c35530f807ea4d6752
andrei <andrei.taraschuk@gmail.com>
2024-09-13 09:30:57 -0600

    Removed agora logo from the header (#502)
    * Updated . top issues and added hovers
    
    * Minor tenant fixes
    
    * Prettier
    
    * Agora link in the footer and info cards

--- src/components/Header/LogoLink.jsx
+++ src/components/Header/LogoLink.jsx
@@ -3,6 +3,7 @@ import Tenant from "@/lib/tenant/tenant";
 import Image from "next/image";
 import Link from "next/link";
 import { HStack } from "../Layout/Stack";
+import { TENANT_NAMESPACES } from "@/lib/constants";
 
 export default function LogoLink() {
   const { namespace, ui } = Tenant.current();
@@ -11,14 +12,18 @@ export default function LogoLink() {
     <HStack justifyContent="justify-between">
       <Link href="/">
         <HStack className="gap-2 h-full" alignItems="items-center">
-          <Image
-            src={logo}
-            alt="logo"
-            width="16"
-            height="16"
-            className="hidden sm:block"
-          />
-          <div className="h-3 w-[2px] bg-line rounded-full hidden sm:block"></div>
+          {namespace !== TENANT_NAMESPACES.NEW_DAO && (
+            <>
+              <Image
+                src={logo}
+                alt="logo"
+                width="16"
+                height="16"
+                className="hidden sm:block"
+              />
+              <div className="h-3 w-[2px] bg-line rounded-full hidden sm:block"></div>
+            </>
+          )}
           <Image src={ui.logo} alt="logo" width="18" height="18" />
           <span className="hidden sm:block font-medium text-primary">{`${ui.title}`}</span>
         </HStack>
