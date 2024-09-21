9ec55f2dca61beb5b6f499cc9f9e658a46ab2b78
Stepan Arsentjev <stepandel@gmail.com>
2024-09-21 03:01:36 +0200

    [RetroFunding R5 API] Badgeholders (#516)
    * add badgeholder schema
    
    * update badgeholder server logic
    
    * create allowlist

--- src/app/api/common/badgeholders/getBadgeholders.ts
+++ src/app/api/common/badgeholders/getBadgeholders.ts
@@ -1,16 +1,53 @@
 import { CATEGORY_ROLES } from "@/app/lib/auth/constants";
 import { cache } from "react";
+import { addressOrEnsNameWrap } from "../utils/ensName";
+import Tenant from "@/lib/tenant/tenant";
+import prisma from "@/app/lib/prisma";
 
-export async function isBadgeholder(address: string) {
-  // TODO: implement. This method makes a distinction between badgeholders and citizens
-  // for viewing and voting on ballots
+const allowlist = [
+  "0xa18d0226043a76683950f3baabf0a87cfb32e1cb",
+  "0x92084bed8635d82db600e100539a61a6d4209403",
+  "0x2410d50ba4993c1fe13b3db0bcdae51b1c617d0a",
+  "0x6aaa8733cfee3931d826c2d2ad4679db9accf6bb",
+  "0xe3b4b326d34686ea3a44dfa1e19e5ffd0dff04f3",
+];
 
-  // TODO: return badgeholder date including round & category
+const getBadgeholder = async (addressOrEnsName: string) =>
+  addressOrEnsNameWrap(getBadgeholderForAddress, addressOrEnsName);
 
-  return true;
+async function getBadgeholderForAddress({ address }: { address: string }) {
+  const { slug } = Tenant.current();
+
+  const badgehodler = await prisma.badgeholders.findFirst({
+    where: {
+      dao_slug: slug,
+      address: address,
+      retro_funding_round: "5",
+    },
+  });
+
+  return {
+    isBadgeholder: !!badgehodler || allowlist.includes(address),
+    votingCategory: badgehodler
+      ? parseVotingCategory(JSON.parse(badgehodler.metadata).votingGroup)
+      : randomVotingCategory(address),
+  };
 }
 
-export function votingCategory(address: string) {
+export const fetchBadgeholder = cache(getBadgeholder);
+
+function parseVotingCategory(category: "A" | "B" | "C") {
+  switch (category) {
+    case "A":
+      return Object.entries(CATEGORY_ROLES)[0][1];
+    case "B":
+      return Object.entries(CATEGORY_ROLES)[1][1];
+    case "C":
+      return Object.entries(CATEGORY_ROLES)[2][1];
+  }
+}
+
+function randomVotingCategory(address: string) {
   // get a number seeded by the address
   const seed = parseInt(address.slice(2, 10), 16);
   const categoryIndex = seed % Object.keys(CATEGORY_ROLES).length;
