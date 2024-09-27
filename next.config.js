1f15b86dd6e95a1c931ab405a06dea340ea2738f
stepandel <stepandel@gmail.com>
2024-09-27 16:06:10 -0700

    :bug: add Access-Control-Allow-Credentials header to all api routes

--- next.config.js
+++ next.config.js
@@ -28,6 +28,10 @@ const nextConfig = {
             key: "Access-Control-Allow-Headers",
             value: "Content-Type, Authorization",
           },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
         ],
       },
     ];
