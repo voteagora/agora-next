1dce69dc43e2af7077391eca96a518113122e637
Stepan Arsentjev <stepandel@gmail.com>
2024-10-01 03:43:43 +0200

    :bug: [RetroFunding R5 API] Patch project positioning in ballot (#532)
    * fix positioning query: add condition for moving project up or down
    
    * fix allocation / impact boundaries

--- src/app/api/common/ballots/updateBallotProject.ts
+++ src/app/api/common/ballots/updateBallotProject.ts
@@ -245,45 +245,77 @@ async function updateBallotProjectPositionForAddress({
               project_id,
               rank,
               impact,
-              ROW_NUMBER() OVER (ORDER BY rank DESC) AS current_position
-          FROM
-              retro_funding.project_allocations
-          WHERE address = $1 AND round = $2
-      ),
-      position_bounds AS (
-          SELECT
-              (SELECT rank FROM ranked_projects WHERE current_position = ${position}) AS lower_bound,
-              (SELECT rank FROM ranked_projects WHERE current_position = ${position} + 1) AS upper_bound
-      ),
-      project_impact AS (
-          SELECT
-              impact
+              ROW_NUMBER() OVER (ORDER BY rank DESC) - 1 AS current_position
           FROM
               retro_funding.project_allocations
           WHERE
-              address = $1 AND round = $2 AND project_id = $3
+              address = $1 AND round = $2 AND impact > 0
+      ),
+      current_project_position AS (
+          SELECT current_position
+          FROM ranked_projects
+          WHERE project_id = $3
+      ),
+      project_count AS (
+          SELECT MAX(current_position) AS max_position
+          FROM ranked_projects
+      ),
+      position_bounds AS (
+          SELECT
+              CASE
+                  WHEN ${position} < (SELECT current_position FROM current_project_position)
+                  THEN (SELECT rank FROM ranked_projects WHERE current_position = ${position} - 1)
+                  WHEN ${position} > (SELECT max_position FROM project_count)
+                  THEN (SELECT rank FROM ranked_projects WHERE current_position = (SELECT max_position FROM project_count))
+                  ELSE (SELECT rank FROM ranked_projects WHERE current_position = ${position})
+              END AS lower_bound,
+              CASE
+                  WHEN ${position} < (SELECT current_position FROM current_project_position)
+                  THEN (SELECT rank FROM ranked_projects WHERE current_position = ${position})
+                  WHEN ${position} > (SELECT max_position FROM project_count)
+                  THEN NULL
+                  ELSE (SELECT rank FROM ranked_projects WHERE current_position = ${position} + 1)
+              END AS upper_bound
       ),
       impact_bounds AS (
           SELECT
-              (SELECT impact FROM ranked_projects WHERE current_position = ${position}) AS lower_impact,
-              (SELECT impact FROM ranked_projects WHERE current_position = ${position} + 1) AS upper_impact,
-              (SELECT impact FROM project_impact) AS project_impact
+              CASE
+                  WHEN ${position} < (SELECT current_position FROM current_project_position)
+                  THEN (SELECT impact FROM ranked_projects WHERE current_position = ${position} - 1)
+                  WHEN ${position} > (SELECT max_position FROM project_count)
+                  THEN (SELECT impact FROM ranked_projects WHERE current_position = (SELECT max_position FROM project_count))
+                  ELSE (SELECT impact FROM ranked_projects WHERE current_position = ${position})
+              END AS lower_impact,
+              CASE
+                  WHEN ${position} < (SELECT current_position FROM current_project_position)
+                  THEN (SELECT impact FROM ranked_projects WHERE current_position = ${position})
+                  WHEN ${position} > (SELECT max_position FROM project_count)
+                  THEN NULL
+                  ELSE (SELECT impact FROM ranked_projects WHERE current_position = ${position} + 1)
+              END AS upper_impact,
+              (SELECT impact FROM retro_funding.project_allocations WHERE address = $1 AND round = $2 AND project_id = $3) AS project_impact
       ),
       new_rank_value AS (
           SELECT
-              COALESCE(ROUND((lower_bound + upper_bound) / 2), 
+              GREATEST(1, COALESCE(ROUND((lower_bound + upper_bound) / 2),
                   CASE
-                      WHEN lower_bound IS NULL THEN ROUND(upper_bound + 1000)
-                      WHEN upper_bound IS NULL THEN lower_bound + 1000
+                      WHEN lower_bound IS NULL AND upper_bound IS NULL THEN 1000
+                      WHEN lower_bound IS NULL THEN upper_bound + 1000
+                      WHEN upper_bound IS NULL THEN lower_bound - 1000
                       ELSE NULL
                   END
-              ) AS new_rank
+              )) AS new_rank
           FROM
               position_bounds
       ),
       new_impact_value AS (
           SELECT
-              LEAST(COALESCE(upper_impact, 5), GREATEST(project_impact, COALESCE(lower_impact, 1))) as new_impact
+              CASE
+                  WHEN upper_impact IS NULL AND lower_impact IS NULL THEN project_impact
+                  WHEN upper_impact IS NULL THEN GREATEST(1, lower_impact - 1)
+                  WHEN lower_impact IS NULL THEN LEAST(5, upper_impact + 1)
+                  ELSE ROUND((lower_impact + upper_impact) / 2.0)
+              END AS new_impact
           FROM
               impact_bounds
       )
