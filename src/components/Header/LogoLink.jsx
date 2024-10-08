51b5640a9491d6efd79a237709414c7ff39a0553
Michael Gingras <mcg79@cornell.edu>
2024-10-08 15:58:15 -0600

    Adds agora logo back to header

--- src/components/Header/LogoLink.jsx
+++ src/components/Header/LogoLink.jsx
@@ -2,6 +2,7 @@ import Tenant from "@/lib/tenant/tenant";
 import Image from "next/image";
 import Link from "next/link";
 import { HStack } from "../Layout/Stack";
+import logo from "@/assets/agora_logo.svg";
 
 export default function LogoLink() {
   const { namespace, ui } = Tenant.current();
@@ -11,18 +12,18 @@ export default function LogoLink() {
       <Link href="/">
         <HStack className="gap-2 h-full" alignItems="items-center">
           {/*{namespace !== TENANT_NAMESPACES.NEW_DAO && (*/}
-          {/*  <>*/}
-          {/*    <Image*/}
-          {/*      src={logo}*/}
-          {/*      alt="logo"*/}
-          {/*      width="24"*/}
-          {/*      height="24"*/}
-          {/*      className="hidden sm:block"*/}
-          {/*    />*/}
-          {/*    <div className="h-3 w-[2px] bg-line rounded-full hidden sm:block"></div>*/}
-          {/*  </>*/}
+          <>
+            <Image
+              src={logo}
+              alt="logo"
+              width="20"
+              height="20"
+              className="hidden sm:block"
+            />
+            <div className="h-3 w-[2px] bg-line rounded-full hidden sm:block"></div>
+          </>
           {/*)}*/}
-          <Image src={ui.logo} alt="logo" width="24" height="24" />
+          <Image src={ui.logo} alt="logo" width="20" height="20" />
           <span className="hidden sm:block font-medium text-primary">{`${ui.title}`}</span>
         </HStack>
       </Link>
