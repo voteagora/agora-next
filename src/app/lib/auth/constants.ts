0581da92684d9e10f611ff7cea3176a9a111b39f
Stepan Arsentjev <stepandel@gmail.com>
2024-10-17 17:07:18 -0700

    [RetroFunding R6 API] Update categories (#563)

--- src/app/lib/auth/constants.ts
+++ src/app/lib/auth/constants.ts
@@ -14,13 +14,16 @@ export const ROLE_PUBLIC_READER = "reader:public";
 export const ROLE_BADGEHOLDER = "badgeholder";
 export const ROLE_CITIZEN = "citizen";
 export const ROLE_RF_DEMO_USER = "rf-demo-user";
-export const ROLE_CATEGORY_ETH_CORE = "category:ethereum_core_contributions";
-export const ROLE_CATEGORY_OP_STACK =
-  "category:op_stack_research_and_development";
-export const ROLE_CATEGORY_OP_STACK_TOOLING = "category:op_stack_tooling";
+// export const ROLE_CATEGORY_ETH_CORE = "category:ethereum_core_contributions";
+// export const ROLE_CATEGORY_OP_STACK =
+//   "category:op_stack_research_and_development";
+// export const ROLE_CATEGORY_OP_STACK_TOOLING = "category:op_stack_tooling";
+export const ROLE_CATEGORY_GOV_INFRA = "category:governance_infra_and_tooling";
+export const ROLE_CATEGORY_GOV_ANALYTICS = "category:governance_analytics";
+export const ROLE_CATEGORY_GOV_LEADERSHIP = "category:governance_leadership";
 
 export const CATEGORY_ROLES = {
-  ETHEREUM_CORE_CONTRIBUTIONS: ROLE_CATEGORY_ETH_CORE,
-  OP_STACK_RESEARCH_AND_DEVELOPMENT: ROLE_CATEGORY_OP_STACK,
-  OP_STACK_TOOLING: ROLE_CATEGORY_OP_STACK_TOOLING,
+  GOVERNANCE_ANALYTICS: ROLE_CATEGORY_GOV_ANALYTICS,
+  GOVERNANCE_INFRA_AND_TOOLING: ROLE_CATEGORY_GOV_INFRA,
+  GOVERNANCE_LEADERSHIP: ROLE_CATEGORY_GOV_LEADERSHIP,
 };
