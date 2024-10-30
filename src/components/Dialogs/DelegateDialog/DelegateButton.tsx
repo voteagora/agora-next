9ef4596d1bec583fe0f62f3b2d292e8962e49564
andrei <andrei.taraschuk@gmail.com>
2024-10-30 16:23:38 -0600

    Misc

--- src/components/Dialogs/DelegateDialog/DelegateButton.tsx
+++ src/components/Dialogs/DelegateDialog/DelegateButton.tsx
@@ -33,29 +33,28 @@ export const DelegateButton = ({ delegate, delegator, write }: Props) => {
   );
 };
 
-
-const delegateWallet = () => {
-
-  const { contracts } = Tenant.current();
-
-
-  const tokenAbi =
-    ["function castVote(uint256 proposalId,uint8 support) external returns (uint256)"];
-  const proposalId =
-    "103846607594260222319322602408177061482869129590998759087401482308348702652659";
-
-  const contract = new Contract(contracts.token.address, contracts.token.abi);
-
-  const data = contract.interface.encodeFunctionData("castVote", [proposalId, 1]);
-
-
-  client.sendUserOperation({
-    uo: {
-      target: "0xb65C031Ac61128AE791D42Ae43780f012E2F7f89",
-      data: data,
-    },
-  }).then((tx) => {
-    console.log("Transaction sent", tx);
-  });
-};
-}
+// const delegateWallet = () => {
+//
+//   const { contracts } = Tenant.current();
+//
+//
+//   const tokenAbi =
+//     ["function castVote(uint256 proposalId,uint8 support) external returns (uint256)"];
+//   const proposalId =
+//     "103846607594260222319322602408177061482869129590998759087401482308348702652659";
+//
+//   const contract = new Contract(contracts.token.address, contracts.token.abi);
+//
+//   const data = contract.interface.encodeFunctionData("castVote", [proposalId, 1]);
+//
+//
+//   client.sendUserOperation({
+//     uo: {
+//       target: "0xb65C031Ac61128AE791D42Ae43780f012E2F7f89",
+//       data: data,
+//     },
+//   }).then((tx) => {
+//     console.log("Transaction sent", tx);
+//   });
+// };
+// }
