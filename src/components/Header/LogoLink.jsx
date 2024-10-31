130de1540a8c34e39b7f6489392372fdc4e216f2
andrei <andrei.taraschuk@gmail.com>
2024-10-31 12:12:18 -0600

    Scroll tenant feature flags

--- src/components/Header/LogoLink.jsx
+++ src/components/Header/LogoLink.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { HStack } from "../Layout/Stack";
 import logo from "@/assets/agora_logo.svg";
+import { TENANT_NAMESPACES } from "@/lib/constants";
 
 export default function LogoLink() {
   const { namespace, ui } = Tenant.current();
@@ -11,18 +12,18 @@ export default function LogoLink() {
     <HStack justifyContent="justify-between">
       <Link href="/">
         <HStack className="gap-2 h-full" alignItems="items-center">
-          {/*{namespace !== TENANT_NAMESPACES.NEW_DAO && (*/}
-          <>
-            <Image
-              src={logo}
-              alt="logo"
-              width="20"
-              height="20"
-              className="hidden sm:block"
-            />
-            <div className="h-3 w-[2px] bg-line rounded-full hidden sm:block"></div>
-          </>
-          {/*)}*/}
+          {namespace !== TENANT_NAMESPACES.SCROLL && (
+            <>
+              <Image
+                src={logo}
+                alt="logo"
+                width="20"
+                height="20"
+                className="hidden sm:block"
+              />
+              <div className="h-3 w-[2px] bg-line rounded-full hidden sm:block"></div>
+            </>
+          )}
           <Image src={ui.logo} alt="logo" width="20" height="20" />
           <span className="hidden sm:block font-medium text-primary">{`${ui.title}`}</span>
         </HStack>
