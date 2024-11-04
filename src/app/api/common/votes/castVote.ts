c190ab4ea575dc22acab8da3215fb9a776cb5f9e
stepandel <stepandel@gmail.com>
2024-11-04 11:53:56 -0800

    refactor

--- src/app/api/common/votes/castVote.ts
+++ src/app/api/common/votes/castVote.ts
@@ -1,6 +1,7 @@
 import Tenant from "@/lib/tenant/tenant";
+import { getWalletClient } from "@/lib/viem";
 import { cache } from "react";
-import { createWalletClient, hexToSignature, http } from "viem";
+import { parseSignature } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 
 const SPONSOR_PRIVATE_KEY = process.env.NEXT_PUBLIC_SPONSOR_PRIVATE_KEY;
@@ -15,27 +16,19 @@ async function voteBySignature({
   support: number;
 }): Promise<`0x${string}`> {
   if (!SPONSOR_PRIVATE_KEY) {
-    throw new Error("SPONSOR_PRIVATE_KEY not defined");
+    throw new Error("SPONSOR_PRIVATE_KEY is not defined");
   }
-
-  const { governor } = Tenant.current().contracts;
-
-  const { contracts } = Tenant.current();
-  const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID!;
-  const hasAlchemy = contracts.token.chain.rpcUrls?.alchemy;
-
-  const transport = hasAlchemy
-    ? `${contracts.token.chain.rpcUrls.alchemy.http[0]}/${alchemyId}`
-    : `${contracts.token.chain.rpcUrls.default.http[0]}`;
-
-  const walletClient = createWalletClient({
-    chain: contracts.token.chain,
-    transport: http(transport),
-  });
 
   const account = privateKeyToAccount(SPONSOR_PRIVATE_KEY as `0x${string}`);
 
-  const { r, s, v } = hexToSignature(signature);
+  const { governor } = Tenant.current().contracts;
+  const walletClient = getWalletClient(governor.chain.id);
+
+  const { r, s, v } = parseSignature(signature);
+
+  if (!v) {
+    throw new Error("Unsupported signature type");
+  }
 
   const voteTx = await walletClient.writeContract({
     address: governor.address as `0x${string}`,
