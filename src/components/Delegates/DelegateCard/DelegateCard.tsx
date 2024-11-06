8bc05d2cf3496acb74dbc9b13ea419cb78c889bc
andrei <andrei.taraschuk@gmail.com>
2024-11-06 13:42:03 -0700

    Removed scw display

--- src/components/Delegates/DelegateCard/DelegateCard.tsx
+++ src/components/Delegates/DelegateCard/DelegateCard.tsx
@@ -2,11 +2,8 @@ import { bpsToString, pluralizeAddresses } from "@/lib/utils";
 import { DelegateProfileImage } from "./DelegateProfileImage";
 import DelegateCardClient from "./DelegateCardClient";
 import { Delegate } from "@/app/api/common/delegates/delegate";
-import { SCWProfileImage } from "@/components/Delegates/DelegateCard/SCWProfileImage";
 
 export default function DelegateCard({ delegate }: { delegate: Delegate }) {
-  const hasSCWAddress = Boolean(delegate.statement?.scw_address);
-
   return (
     <div className="flex flex-col sticky top-16 flex-shrink-0 width-[20rem]">
       <div className="flex flex-col bg-white border border-line shadow-newDefault rounded-xl">
@@ -19,14 +16,6 @@ export default function DelegateCard({ delegate }: { delegate: Delegate }) {
             copyable={true}
           />
         </div>
-        {hasSCWAddress && (
-          <div className="flex flex-col items-stretch p-6 border-b border-line">
-            <SCWProfileImage
-              address={delegate.statement?.scw_address}
-              copyable={true}
-            />
-          </div>
-        )}
 
         <div className="flex flex-col p-6">
           <div className="flex flex-col gap-4">
