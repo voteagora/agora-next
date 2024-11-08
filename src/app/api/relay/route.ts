d4cfebf958bcd535f586a29d87fcd3eb51c907c9
stepandel <stepandel@gmail.com>
2024-11-08 05:57:23 -0800

    add route to check relay wallet balanc

---
+++ src/app/api/relay/route.ts
@@ -0,0 +1,20 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
+import { apiFetchRelayStatus } from "./getRelayStatus";
+
+export async function GET(request: NextRequest) {
+  const authResponse = await authenticateApiUser(request);
+
+  if (!authResponse.authenticated) {
+    return new Response(authResponse.failReason, { status: 401 });
+  }
+
+  try {
+    const status = await apiFetchRelayStatus();
+    return NextResponse.json(status);
+  } catch (e: any) {
+    return new Response("Internal server error: " + e.toString(), {
+      status: 500,
+    });
+  }
+}
