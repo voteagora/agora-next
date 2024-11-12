2d40339b53ac404db0015e4c6b8832ea4ec9fa70
Michael Gingras <mcg79@cornell.edu>
2024-11-12 11:49:56 -0700

    Stashes updates

---
+++ src/app/proposals/draft/components/ProposalRequirements.tsx
@@ -0,0 +1,77 @@
+import { formatUnits } from "viem";
+import { PLMConfig, ProposalGatingType } from "@/app/proposals/draft/types";
+import { ProposalType } from "@/app/proposals/draft/types";
+import Tenant from "@/lib/tenant/tenant";
+import { useManager } from "@/hooks/useManager";
+import { useProposalThreshold } from "@/hooks/useProposalThreshold";
+import { DraftProposal } from "@/app/proposals/draft/types";
+
+const ProposalRequirements = ({
+  proposalDraft,
+}: {
+  proposalDraft: DraftProposal;
+}) => {
+  const tenant = Tenant.current();
+  const plmToggle = tenant.ui.toggle("proposal-lifecycle");
+  const gatingType = (plmToggle?.config as PLMConfig)?.gatingType;
+  const votingModuleType = proposalDraft.voting_module_type;
+
+  const { data: threshold } = useProposalThreshold();
+  const { data: manager } = useManager();
+
+  const renderProposalRequirements = () => {
+    if (votingModuleType === ProposalType.SOCIAL) {
+      return (
+        <div className="first-of-type:rounded-t-xl first-of-type:border-t border-x border-b last-of-type:rounded-b-xl p-4 flex flex-row items-center space-x-4">
+          <p className="flex-grow">Token balance</p>
+          <span className="text-secondary font-mono text-xs">
+            {"> "}
+            {(plmToggle?.config as PLMConfig)?.snapshotConfig?.requiredTokens}
+            {" tokens"}
+          </span>
+        </div>
+      );
+    }
+
+    if (
+      gatingType === ProposalGatingType.MANAGER ||
+      gatingType === ProposalGatingType.GOVERNOR_V1
+    ) {
+      return (
+        <div className="first-of-type:rounded-t-xl first-of-type:border-t border-x border-b last-of-type:rounded-b-xl p-4 flex flex-row items-center space-x-4">
+          <p className="flex-grow">Manager address</p>
+          <span className="text-secondary font-mono text-xs">
+            {manager?.toString()}
+          </span>
+        </div>
+      );
+    }
+
+    if (
+      gatingType === ProposalGatingType.TOKEN_THRESHOLD ||
+      gatingType === ProposalGatingType.GOVERNOR_V1
+    ) {
+      return (
+        <div className="first-of-type:rounded-t-xl first-of-type:border-t border-x border-b last-of-type:rounded-b-xl p-4 flex flex-row items-center space-x-4">
+          <p className="flex-grow">Token balance</p>
+          <span className="text-secondary font-mono text-xs">
+            {"> "}
+            {threshold
+              ? Math.round(
+                  parseFloat(
+                    formatUnits(BigInt(threshold), tenant.token.decimals)
+                  )
+                )
+              : "0"}{" "}
+            tokens
+          </span>
+        </div>
+      );
+    }
+
+    return null;
+  };
+  return <>{renderProposalRequirements()}</>;
+};
+
+export default ProposalRequirements;
