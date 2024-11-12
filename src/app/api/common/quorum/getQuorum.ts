2142db63f58affd0231d8ee4053603f14f600b5e
jefag <jeff@voteagora.com>
2024-11-12 18:10:52 -0500

    Use Votable Supply for Quorum until post-snapshot (#603)

--- src/app/api/common/quorum/getQuorum.ts
+++ src/app/api/common/quorum/getQuorum.ts
@@ -3,6 +3,7 @@ import prisma from "@/app/lib/prisma";
 import { ProposalPayload } from "../proposals/proposal";
 import Tenant from "@/lib/tenant/tenant";
 import { TENANT_NAMESPACES } from "@/lib/constants";
+import { findVotableSupply } from "@/lib/prismaUtils";
 
 async function getQuorumForProposal(proposal: ProposalPayload) {
   const { namespace, contracts } = Tenant.current();
@@ -28,7 +29,7 @@ async function getQuorumForProposal(proposal: ProposalPayload) {
 
       // If no quorum is set, calculate it based on votable supply
       if (!quorum) {
-        votableSupply = await prisma[`${namespace}VotableSupply`].findFirst({});
+        votableSupply = await findVotableSupply({ namespace });
         return (BigInt(Number(votableSupply?.votable_supply)) * 30n) / 100n;
       }
       return quorum;
@@ -40,11 +41,18 @@ async function getQuorumForProposal(proposal: ProposalPayload) {
       // https://voteagora.slack.com/archives/C07ATDL9P8F/p1723657375357649?thread_ts=1723579392.179389&cid=C07ATDL9P8F
       // https://voteagora.slack.com/archives/C07ATDL9P8F/p1723657834565499
 
-      votableSupply = await prisma[`${namespace}VotableSupply`].findFirst({});
+      votableSupply = await findVotableSupply({ namespace });
       return (BigInt(Number(votableSupply?.votable_supply)) * 30n) / 100n;
 
     default: // TENANT_NAMESPACES.PGUILD - yes, TENANT_NAMESPACES.SCROLL?
-      quorum = await contracts.governor.contract.quorum!(proposal.proposal_id);
+      try {
+        quorum = await contracts.governor.contract.quorum!(
+          proposal.proposal_id
+        );
+      } catch {
+        // this is a hack, because... // https://linear.app/agora-app/issue/AGORA-3246/quorum-isnt-known-for-proposal-before-its-snapshot
+        quorum = await findVotableSupply({ namespace });
+      }
 
       return BigInt(Number(quorum));
   }
