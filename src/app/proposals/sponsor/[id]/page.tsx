2775e211127e2799e928bae37028af2a5361818e
Michael Gingras <mcg79@cornell.edu>
2024-11-13 16:49:34 -0700

    Adds ability to reject proposal

--- src/app/proposals/sponsor/[id]/page.tsx
+++ src/app/proposals/sponsor/[id]/page.tsx
@@ -1,9 +1,5 @@
 import prisma from "@/app/lib/prisma";
-import SponsorForm from "../components/SponsorForm";
-import Image from "next/image";
-import { icons } from "@/icons/icons";
 import SponsorAuthCheck from "../components/SponsorAuthCheck";
-import ENSName from "@/components/shared/ENSName";
 import ArchivedDraftProposal from "../../draft/components/ArchivedDraftProposal";
 import { DraftProposal } from "../../../proposals/draft/types";
 import SponsorActionPanel from "../components/SponsorActionPanel";
@@ -89,48 +85,6 @@ const ProposalSponsorPage = async ({ params }: { params: { id: string } }) => {
       </main>
     </SponsorAuthCheck>
   );
-
-  //   return (
-  //     <SponsorAuthCheck sponsorAddress={draftProposal.sponsor_address!}>
-  //       <main className="max-w-screen-xl mx-auto mt-10">
-  //         <div className="grid grid-cols-3 gap-12">
-  //           <div className="col-span-2">
-  //             <SponsorForm draftProposal={draftProposal} />
-  //           </div>
-  //           <div className="self-start">
-  //             <div className="border bg-[#FAFAF2] border-[#ECE3CA] text-[#B16B19] p-6 rounded-lg">
-  //               <div className="flex flex-row items-center space-x-4">
-  //                 <Image
-  //                   className="border bg-[#FAFAF2] border-[#ECE3CA] rounded-md p-2 shadow-newDefault"
-  //                   src={icons.sponsor}
-  //                   alt="Sponsor"
-  //                   width={42}
-  //                   height={42}
-  //                 />
-
-  //                 <p className="font-semibold">
-  //                   <ENSName address={draftProposal.author_address} /> would like
-  //                   your help to submit this proposal
-  //                 </p>
-  //               </div>
-  //               <p className="text-[#B16B19]/70 mt-2">
-  //                 The proposer has created a draft proposal, but might not meet
-  //                 the proposal threshold to submit it themselves. They&apos;d like
-  //                 you to help them submit it.
-  //               </p>
-  //               <p className="text-[#B16B19]/70 mt-2">
-  //                 If you choose to do so, this proposal will be marked as
-  //                 &apos;submitted by{" "}
-  //                 <ENSName address={draftProposal.sponsor_address || ""} />,
-  //                 authored by <ENSName address={draftProposal.author_address} />
-  //                 &apos;
-  //               </p>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </main>
-  //     </SponsorAuthCheck>
-  //   );
 };
 
 export default ProposalSponsorPage;
