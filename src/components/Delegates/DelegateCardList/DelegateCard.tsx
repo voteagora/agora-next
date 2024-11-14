a44dba9f1a1f0a70e5d8561b78e75742ffcf1a52
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2024-11-14 14:28:02 -0800

    Format

--- src/components/Delegates/DelegateCardList/DelegateCard.tsx
+++ src/components/Delegates/DelegateCardList/DelegateCard.tsx
@@ -23,6 +23,7 @@ const DelegateCard = ({
 }) => {
   const { token } = Tenant.current();
   const { advancedDelegators } = useConnectedDelegate();
+
   const { data: votingStats, isPending: isVotingStatsPending } = useVotingStats(
     {
       address: delegate.address as `0x${string}`,
