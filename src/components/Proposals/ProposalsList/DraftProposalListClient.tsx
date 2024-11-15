e08e693eadee2ffebf1f43d51597df8a5b984b4a
Michael Gingras <mcg79@cornell.edu>
2024-11-15 11:54:47 -0700

    Updates draftProposals table to use infinite query

--- src/components/Proposals/ProposalsList/DraftProposalListClient.tsx
+++ src/components/Proposals/ProposalsList/DraftProposalListClient.tsx
@@ -1,123 +1,22 @@
 "use client";
 
-import { useOptimistic } from "react";
 import { useAccount } from "wagmi";
-import useDraftProposals from "@/hooks/useDraftProposals";
+import {
+  useDraftProposalsInfinite,
+  createQueryKey,
+} from "@/hooks/useDraftProposals";
 import { useSearchParams } from "next/navigation";
-import HumanAddress from "@/components/shared/HumanAddress";
-import { ProposalDraft, ProposalDraftVote } from "@prisma/client";
-import Link from "next/link";
 import {
   draftProposalsFilterOptions,
   draftProposalsSortOptions,
 } from "@/lib/constants";
-import { animate, AnimatePresence, motion } from "framer-motion";
-import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
-import voteForProposalDraft from "./actions/voteForProposalDraft";
-
-const DraftProposalCard = ({
-  proposal,
-  refetch,
-}: {
-  proposal: ProposalDraft & { votes: ProposalDraftVote[] };
-  refetch: () => void;
-}) => {
-  const { address } = useAccount();
-
-  const voterInVotes = proposal.votes.find((v) => v.voter === address);
-
-  const [optimisticState, addOptimistic] = useOptimistic(
-    proposal,
-    (
-      currentState: ProposalDraft & { votes: ProposalDraftVote[] },
-      vote: any
-    ) => {
-      const newVotes = voterInVotes
-        ? currentState.votes.map((v) => (v.voter === vote.voter ? vote : v))
-        : [...currentState.votes, vote];
-      return { ...currentState, votes: newVotes };
-    }
-  );
-
-  const handleVote = async (direction: 1 | -1) => {
-    if (!address) return;
-    addOptimistic({
-      voter: address,
-      weight: 1,
-      direction,
-    });
-    await voteForProposalDraft({
-      address,
-      proposalId: proposal.id.toString(),
-      direction,
-    });
-    refetch();
-  };
-
-  // TODO: how inefficient is doing this client side vs db side?
-  const score = optimisticState.votes.reduce(
-    (acc, vote) => acc + Number(vote.weight) * vote.direction,
-    0
-  );
-
-  return (
-    <Link
-      href={`/proposals/sponsor/${proposal.id}`}
-      className="block cursor-pointer border-b border-line last:border-b-0 hover:bg-tertiary/5 transition-colors"
-    >
-      <div className="py-4 px-6 flex flex-row gap-4 items-center">
-        <div className="flex flex-col gap-2 items-center">
-          <div
-            className={`w-5 h-5 bg-neutral border border-line rounded flex items-center justify-center hover:bg-tertiary/5 transition-colors ${
-              voterInVotes && voterInVotes.direction === 1
-                ? "bg-tertiary/5"
-                : ""
-            }`}
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              if (!address) return;
-              handleVote(1);
-            }}
-          >
-            <ChevronUpIcon className="w-4 h-4 text-secondary" />
-          </div>
-          <div className="text-xs text-secondary font-bold">{score}</div>
-          <div
-            className={`w-5 h-5 bg-neutral border border-line rounded flex items-center justify-center hover:bg-tertiary/5 transition-colors ${
-              voterInVotes && voterInVotes.direction === -1
-                ? "bg-tertiary/5"
-                : ""
-            }`}
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              if (!address) return;
-              handleVote(-1);
-            }}
-          >
-            <ChevronDownIcon className="w-4 h-4 text-secondary" />
-          </div>
-        </div>
-        <div className="flex flex-col justify-between">
-          <div className="flex flex-row gap-1 text-xs text-secondary">
-            <div>
-              Draft Proposal{" "}
-              <span className="hidden sm:inline">
-                by <HumanAddress address={proposal.author_address} />
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row gap-1">
-            <span className="text-primary">{proposal.title || "Untitled"}</span>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
-};
+import { useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import DraftProposalCard from "./DraftProposalCard";
 
 const DraftProposalListClient = () => {
+  const queryClient = useQueryClient();
   const { address } = useAccount();
   const filter =
     useSearchParams()?.get("filter") ||
@@ -128,44 +27,111 @@ const DraftProposalListClient = () => {
 
   const {
     data: draftProposals,
-    isLoading,
-    error,
-    refetch,
-  } = useDraftProposals({ address, filter, sort });
+    isLoading: infiniteIsLoading,
+    fetchNextPage: infiniteFetchNextPage,
+    hasNextPage: infiniteHasNextPage,
+    isFetchingNextPage: infiniteIsFetchingNextPage,
+  } = useDraftProposalsInfinite({
+    address,
+    filter,
+    sort,
+    pagination: {
+      limit: 2,
+      offset: 0,
+    },
+  });
+
+  const updateProposalVote = (proposalId: number, vote: any) => {
+    queryClient.setQueryData(
+      createQueryKey({ address, filter, sort }),
+      (oldData: any) => {
+        if (!oldData) return oldData;
+
+        const updates = oldData.pages.map((page: any) => {
+          const { data, meta } = page;
+          const proposals = data;
+          const proposal = proposals.find((p: any) => p.id === proposalId);
+
+          // Did not find the proposal in this page, return page
+          if (!proposal) return page;
+
+          const existingVotes = proposal.votes;
+          const voterInVotes = existingVotes.find(
+            (v: any) => v.voter === vote.voter
+          );
+          const newVotes = voterInVotes
+            ? proposal.votes.map((v: any) =>
+                v.voter === vote.voter ? vote : v
+              )
+            : [...proposal.votes, vote];
+
+          const newVoteWeight = newVotes.reduce(
+            (acc: number, vote: any) =>
+              acc + Number(vote.weight) * vote.direction,
+            0
+          );
+
+          const updatedProposals = proposals.map((p: any) => {
+            if (p.id !== proposalId) return p;
+
+            return {
+              ...proposal,
+              votes: newVotes,
+              vote_weight: newVoteWeight,
+            };
+          });
+
+          return {
+            ...page,
+            data: updatedProposals,
+          };
+        });
+
+        return {
+          ...oldData,
+          pages: updates,
+        };
+      }
+    );
+  };
 
   return (
-    <>
-      {isLoading ? (
-        <motion.div className="flex flex-col bg-neutral border border-line rounded-lg shadow-newDefault overflow-hidden">
-          <div className="flex flex-row justify-center py-8 text-tertiary">
-            Loading...
-          </div>
-        </motion.div>
+    <div className="flex flex-col bg-neutral border border-line rounded-lg shadow-newDefault overflow-hidden">
+      {draftProposals.length === 0 ? (
+        <div className="flex flex-row justify-center py-8 text-secondary">
+          No draft proposals found
+        </div>
       ) : (
-        <motion.div
-          key={filter}
-          className="flex flex-col bg-neutral border border-line rounded-lg shadow-newDefault overflow-hidden"
+        <InfiniteScroll
+          hasMore={infiniteHasNextPage}
+          pageStart={0}
+          loadMore={() => {
+            if (infiniteHasNextPage) {
+              infiniteFetchNextPage();
+            }
+          }}
+          loader={
+            <div key={0}>
+              <div
+                key="loader"
+                className="flex flex-row gl_loader justify-center py-6 text-sm text-secondary"
+              >
+                Loading...
+              </div>
+            </div>
+          }
+          element="main"
         >
-          <div>
-            {!draftProposals || draftProposals?.length === 0 ? (
-              <div className="flex flex-row justify-center py-8 text-tertiary">
-                No draft proposals found
-              </div>
-            ) : (
-              <div>
-                {draftProposals?.map((proposal) => (
-                  <DraftProposalCard
-                    key={proposal.id}
-                    proposal={proposal}
-                    refetch={refetch}
-                  />
-                ))}
-              </div>
-            )}
-          </div>
-        </motion.div>
+          {draftProposals.map((proposal) => (
+            <DraftProposalCard
+              key={proposal.id}
+              proposal={proposal}
+              updateProposalVote={updateProposalVote}
+            />
+          ))}
+        </InfiniteScroll>
       )}
-    </>
+    </div>
   );
 };
 
--- src/hooks/useDraftProposals/action.tsx
+++ src/hooks/useDraftProposals/action.tsx
@@ -6,18 +6,25 @@ import {
   ProposalDraftVote,
 } from "@prisma/client";
 import prisma from "@/app/lib/prisma";
-import { draftProposalsSortOptions } from "@/lib/constants";
+import {
+  draftProposalsFilterOptions,
+  draftProposalsSortOptions,
+} from "@/lib/constants";
 import Tenant from "@/lib/tenant/tenant";
 import {
   ProposalStage as PrismaProposalStage,
   ProposalDraft,
 } from "@prisma/client";
+import { paginateResult, PaginationParams } from "@/app/lib/pagination";
+import { doInSpan } from "@/app/lib/logging";
 
 const action = async (
   address: `0x${string}` | undefined,
-  ownerOnly = false,
-  sort: string
+  filter: string,
+  sort: string,
+  pagination: PaginationParams
 ) => {
+  const ownerOnly = filter === draftProposalsFilterOptions.myDrafts.filter;
   const isNewestSort = sort === draftProposalsSortOptions.newest.sort;
   const isOldestSort = sort === draftProposalsSortOptions.oldest.sort;
   const isMostVotesSort = sort === draftProposalsSortOptions.mostVotes.sort;
@@ -43,35 +50,102 @@ const action = async (
     : isOldestSort
       ? "p.created_at ASC"
       : isMostVotesSort
-        ? "COALESCE(SUM(v.weight * v.direction), 0) DESC"
-        : "COALESCE(SUM(v.weight * v.direction), 0) ASC";
+        ? "vote_weight DESC"
+        : "vote_weight ASC";
 
+  // TODO: make sure the only owner stuff is correct
   const query = `
-    SELECT p.*,
-      COALESCE(SUM(v.weight * v.direction), 0) as vote_weight,
-      COALESCE(json_agg(t.*) FILTER (WHERE t.id IS NOT NULL), '[]') as transactions,
-      COALESCE(json_agg(s.*) FILTER (WHERE s.id IS NOT NULL), '[]') as approved_sponsors,
-      COALESCE(json_agg(v.*) FILTER (WHERE v.id IS NOT NULL), '[]') as votes
-    FROM alltenant.proposals p
-    LEFT JOIN alltenant.proposal_votes v ON p.id = v.proposal_id
-    LEFT JOIN alltenant.proposal_transactions t ON p.id = t.proposal_id
-    LEFT JOIN alltenant.proposal_approved_sponsors s ON p.id = s.proposal_id
-    WHERE p.stage = $1::alltenant.proposal_stage
+WITH vote_weights AS (
+    SELECT
+        proposal_id,
+        COALESCE(SUM(weight * direction), 0) as vote_weight,
+        json_agg(json_build_object(
+            'id', id,
+            'proposal_id', proposal_id,
+            'voter', voter,
+            'weight', weight,
+            'direction', direction,
+            'created_at', created_at,
+            'updated_at', updated_at
+        )) as votes
+    FROM alltenant.proposal_votes
+    GROUP BY proposal_id
+),
+sponsors AS (
+    SELECT
+        proposal_id,
+        json_agg(json_build_object(
+            'id', id,
+            'proposal_id', proposal_id,
+            'sponsor_address', sponsor_address,
+            'status', status,
+            'created_at', created_at,
+            'updated_at', updated_at
+        )) as approved_sponsors
+    FROM alltenant.proposal_approved_sponsors
+    GROUP BY proposal_id
+),
+transactions AS (
+    SELECT
+        proposal_id,
+        json_agg(json_build_object(
+            'id', id,
+            'proposal_id', proposal_id,
+            'proposal_approval_option_id', proposal_approval_option_id,
+            'order', "order",
+            'target', target,
+            'value', value,
+            'calldata', calldata,
+            'description', description,
+            'simulation_state', simulation_state,
+            'simulation_id', simulation_id,
+            'created_at', created_at,
+            'updated_at', updated_at
+        ) ORDER BY "order") as transactions
+    FROM alltenant.proposal_transactions
+    GROUP BY proposal_id
+)
+SELECT
+    p.*,
+    COALESCE(v.vote_weight, 0) as vote_weight,
+    COALESCE(v.votes, '[]') as votes,
+    COALESCE(s.approved_sponsors, '[]') as approved_sponsors,
+    COALESCE(t.transactions, '[]') as transactions
+FROM alltenant.proposals p
+LEFT JOIN vote_weights v ON p.id = v.proposal_id
+LEFT JOIN sponsors s ON p.id = s.proposal_id
+LEFT JOIN transactions t ON p.id = t.proposal_id
+WHERE p.stage = $1::alltenant.proposal_stage
       AND p.chain_id = $2
       AND p.contract = $3
       ${ownerOnly ? onlyOwnerQuery : ""}
-      AND p.author_address = $4
-    GROUP BY p.id
     ORDER BY ${sortFilter}
+    OFFSET $5
+    LIMIT $6
 `;
 
-  return prisma.$queryRawUnsafe<
-    (ProposalDraft & {
-      transactions: ProposalDraftTransaction[];
-      approved_sponsors: ProposalDraftApprovedSponsors[];
-      votes: ProposalDraftVote[];
-    })[]
-  >(query, String(stage), chainId, contract, address);
+  const rawQuery = async (skip: number, take: number) =>
+    prisma.$queryRawUnsafe<
+      (ProposalDraft & {
+        transactions: ProposalDraftTransaction[];
+        approved_sponsors: ProposalDraftApprovedSponsors[];
+        votes: ProposalDraftVote[];
+        vote_weight: number;
+      })[]
+    >(query, String(stage), chainId, contract, address, skip, take);
+
+  const getDraftProposalsExecution = doInSpan(
+    { name: "getDraftProposals" },
+    async () => paginateResult(rawQuery, pagination)
+  );
+
+  const draftProposals = await getDraftProposalsExecution;
+  console.log("draftProposals", draftProposals);
+
+  return {
+    meta: draftProposals.meta,
+    data: draftProposals.data,
+  };
 };
 
 export default action;
