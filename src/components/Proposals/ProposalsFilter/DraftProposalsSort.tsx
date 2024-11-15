d6f107b94c3dd584330102e16cab4f92bb55798f
Michael Gingras <mcg79@cornell.edu>
2024-11-15 14:26:54 -0700

    Resolves sort filter issue

--- src/components/Proposals/ProposalsFilter/DraftProposalsSort.tsx
+++ src/components/Proposals/ProposalsFilter/DraftProposalsSort.tsx
@@ -7,6 +7,14 @@ import { useEffect, useState } from "react";
 import { ChevronDown } from "lucide-react";
 import { useAddSearchParam } from "@/hooks/useAddSearchParam";
 
+const getSortOptionValue = (sort: string) => {
+  const sortOption = Object.values(draftProposalsSortOptions).find(
+    (option) => option.sort === sort
+  );
+
+  return sortOption?.value || draftProposalsSortOptions.newest.value;
+};
+
 export default function DraftProposalsSort() {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -15,7 +23,6 @@ export default function DraftProposalsSort() {
   const [selected, setSelected] = useState(
     sortParam || draftProposalsSortOptions.newest.sort
   );
-  const isNewestSort = selected === draftProposalsSortOptions.newest.sort;
 
   useEffect(() => {
     const handleChanges = (value: string) => {
@@ -23,17 +30,13 @@ export default function DraftProposalsSort() {
     };
 
     handleChanges(selected);
-  }, [router, selected, isNewestSort]);
+  }, [router, selected]);
 
   return (
     <div className="relative w-full sm:w-fit">
       <Listbox value={selected} onChange={setSelected}>
         <Listbox.Button className="w-full sm:w-fit bg-wash text-base font-medium border-none rounded-full py-2 px-4 flex items-center justify-between">
-          <span>
-            {selected === draftProposalsSortOptions.newest.sort
-              ? draftProposalsSortOptions.newest.value
-              : draftProposalsSortOptions.newest.value}
-          </span>
+          <span>{getSortOptionValue(selected)}</span>
           <ChevronDown className="h-4 w-4 ml-[2px] opacity-30 hover:opacity-100" />
         </Listbox.Button>
         <Listbox.Options className="mt-3 absolute bg-wash border border-line p-2 rounded-2xl flex flex-col gap-1 z-10 w-max">
