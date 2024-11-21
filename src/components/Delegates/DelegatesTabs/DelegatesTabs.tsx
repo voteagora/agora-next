94eb8ce67e2e6e3be4e04cfa02ea086d1e66f683
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2024-11-20 22:36:29 -0800

    Add layout picker

--- src/components/Delegates/DelegatesTabs/DelegatesTabs.tsx
+++ src/components/Delegates/DelegatesTabs/DelegatesTabs.tsx
@@ -5,15 +5,24 @@ import { useRouter, useSearchParams } from "next/navigation";
 import DelegatesFilter from "@/components/Delegates/DelegatesFilter/DelegatesFilter";
 import CitizensFilter from "@/components/Delegates/DelegatesFilter/CitizensFilter";
 import DelegatesSearch from "@/components/Delegates/DelegatesSearch/DelegatesSearch";
-import { type ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import Tenant from "@/lib/tenant/tenant";
 import { useAddSearchParam, useDeleteSearchParam } from "@/hooks";
 import StakeholdersFilter from "@/app/delegates/components/StakeholdersFilter";
 import IssuesFilter from "@/app/delegates/components/IssuesFilter";
 import EndorsedFilter from "@/app/delegates/components/EndorsedFilter";
 import DelegateeFilter from "@/app/delegates/components/DelegatorFilter";
+import { LayoutGrid, AlignJustify } from "lucide-react";
+import { useLayout } from "@/contexts/LayoutContext";
 
 export default function DelegateTabs({ children }: { children: ReactNode }) {
+  const searchParams = useSearchParams();
+  const tabParam = searchParams?.get("tab");
+
+  const [value, setValue] = useState(tabParam || "delegates");
+
+  const { layout, setLayout } = useLayout();
+
   const { ui } = Tenant.current();
 
   const hasIssuesFilter = Boolean(
@@ -31,14 +40,13 @@ export default function DelegateTabs({ children }: { children: ReactNode }) {
   );
 
   const router = useRouter();
-  const searchParams = useSearchParams();
   const addSearchParam = useAddSearchParam();
   const deleteSearchParam = useDeleteSearchParam();
-  const tabParam = searchParams?.get("tab");
 
   const hasCitizens = ui.toggle("citizens")?.enabled;
 
   const handleTabChange = (value: string) => {
+    setValue(value);
     router.push(
       value === "citizens"
         ? addSearchParam({ name: "tab", value, clean: true })
@@ -50,7 +58,7 @@ export default function DelegateTabs({ children }: { children: ReactNode }) {
   return (
     <Tabs
       className="max-w-full"
-      defaultValue={tabParam || "delegates"}
+      value={value}
       onValueChange={(value) => handleTabChange(value)}
     >
       <div className="flex flex-col sm:flex-row justify-between items-baseline gap-2">
@@ -73,6 +81,20 @@ export default function DelegateTabs({ children }: { children: ReactNode }) {
           {hasIssuesFilter && tabParam !== "citizens" && <IssuesFilter />}
           {hasEndorsedFilter && tabParam !== "citizens" && <EndorsedFilter />}
           {tabParam === "citizens" ? <CitizensFilter /> : <DelegatesFilter />}
+          {value === "delegates" && (
+            <div className="flex items-center gap-2 bg-wash rounded-full px-4 py-2">
+              <button onClick={() => setLayout("grid")}>
+                <LayoutGrid
+                  className={`h-6 w-6 ${layout === "grid" ? "text-secondary fill-secondary" : "text-secondary/30 fill-secondary/30"}`}
+                />
+              </button>
+              <button onClick={() => setLayout("list")}>
+                <AlignJustify
+                  className={`h-6 w-6 ${layout === "list" ? "text-primary" : "text-secondary/30"}`}
+                />
+              </button>
+            </div>
+          )}
         </div>
       </div>
       {children}
