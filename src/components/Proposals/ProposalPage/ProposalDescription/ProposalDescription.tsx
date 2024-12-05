d38dc4243fec1b6fae4c958885662eef41a37697
Michael Gingras <mcg79@cornell.edu>
2024-12-05 09:30:03 -0600

    Creates a shared markdown component

--- src/components/Proposals/ProposalPage/ProposalDescription/ProposalDescription.tsx
+++ src/components/Proposals/ProposalPage/ProposalDescription/ProposalDescription.tsx
@@ -8,29 +8,7 @@ import ProposalChart from "../ProposalChart/ProposalChart";
 import { Proposal } from "@/app/api/common/proposals/proposal";
 import { Vote } from "@/app/api/common/votes/vote";
 import { PaginatedResult } from "@/app/lib/pagination";
-import MarkdownPreview from "@uiw/react-markdown-preview";
-import Tenant from "@/lib/tenant/tenant";
-
-const defaults = {
-  primary: "23 23 23",
-  secondary: "64 64 64",
-  tertiary: "115 115 115",
-  neutral: "255 255 255",
-  wash: "250 250 250",
-  line: "229 229 229",
-  positive: "0 153 43",
-  negative: "197 47 0",
-  brandPrimary: "23 23 23",
-  brandSecondary: "255 255 255",
-  font: "var(--font-inter)",
-};
-
-const toRGBA = (hex: string, alpha: number) => {
-  return `rgba(${hex
-    .split(" ")
-    .map((n) => parseInt(n, 10))
-    .join(",")}, ${alpha})`;
-};
+import Markdown from "@/components/shared/Markdown/Markdown";
 
 export default function ProposalDescription({
   proposal,
@@ -39,13 +17,6 @@ export default function ProposalDescription({
   proposal: Proposal;
   proposalVotes: PaginatedResult<Vote[]>;
 }) {
-  const { ui } = Tenant.current();
-  const primary = ui?.customization?.primary ?? defaults.primary;
-  const secondary = ui?.customization?.secondary ?? defaults.secondary;
-  const tertiary = ui?.customization?.tertiary ?? defaults.tertiary;
-  const line = ui?.customization?.line ?? defaults.line;
-  const positive = ui?.customization?.positive ?? defaults.positive;
-
   const proposalsWithBadDescription = [
     "94365805422398770067924881378455503928423439630602149628781926844759467250082",
     "64930538748268257621925093712454552173772860987977453334165023026835711650357",
@@ -106,26 +77,11 @@ export default function ProposalDescription({
           />
         )}
         <div className={styles.proposal_description_md}>
-          <MarkdownPreview
-            source={stripTitleFromDescription(
+          <Markdown
+            content={stripTitleFromDescription(
               shortTitle,
               patchedDescription ?? ""
             )}
-            style={
-              {
-                "--color-fg-default": toRGBA(secondary, 1),
-                "--color-canvas-default": toRGBA(primary, 0),
-                "--color-border-default": toRGBA(line, 1),
-                "--color-border-muted": toRGBA(line, 1),
-                "--color-canvas-subtle": toRGBA(tertiary, 0.05),
-                "--color-prettylights-syntax-entity-tag": toRGBA(positive, 1),
-                fontFamily: ui?.customization?.font ?? defaults.font,
-              } as React.CSSProperties
-            }
-            className={`h-full py-3 rounded-t-lg max-w-full bg-transparent prose prose-code:bg-wash`}
-            wrapperElement={{
-              "data-color-mode": "light",
-            }}
           />
         </div>
       </div>
