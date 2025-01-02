0dbb63e68740f763672ba4744464a396466ae0dd
Michael Gingras <mcg79@cornell.edu>
2025-01-02 16:12:12 -0700

    Adds charts, adds event for delegation

---
+++ src/app/admin/analytics/components/VotesDataView.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import VotesBarChart from "./VotesBarChart";
+import VotesTable from "./VotesTable";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/shared/Switch";
+
+const VotesDataView = ({ votes }: { votes: any }) => {
+  const [view, setView] = useState<"table" | "chart">("table");
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between items-center pb-2 mt-4 mb-4 border-b border-gray-200">
+        <h2 className="text-lg font-bold text-primary flex-1">
+          Voter participation
+        </h2>
+        <div className="w-[200px]">
+          <Switch
+            onSelectionChanged={() => {
+              setView(view === "table" ? "chart" : "table");
+            }}
+            selection={view}
+            options={["table", "chart"]}
+          />
+        </div>
+      </div>
+      {view === "table" ? (
+        <VotesTable votes={votes} />
+      ) : (
+        <div className="mt-6 w-full h-[400px]">
+          <VotesBarChart data={votes} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VotesDataView;
--- src/app/admin/analytics/page.tsx
+++ src/app/admin/analytics/page.tsx
@@ -1,72 +1,12 @@
 import { getVotes } from "./actions/getVotes";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import VotesDataView from "./components/VotesDataView";
 
 const AnalyticsPage = async () => {
   const votes = await getVotes();
   return (
     <div>
       <h1 className="text-2xl font-bold text-primary mt-6">Analytics</h1>
-      <h2 className="text font-semibold text-primary mt-4">
-        Vote participation
-      </h2>
-      <p className="text-sm text-tertiary mb-6 mt-2">
-        This table shows the total number of votes cast on each proposal and the
-        number of votes that were cast on Agora.
-      </p>
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-base font-semibold text-secondary bg-wash">
-              Proposal ID
-            </TableHead>
-            <TableHead className="text-base font-semibold text-secondary bg-wash mx-0 px-0">
-              Total Votes
-            </TableHead>
-            <TableHead className="text-base font-semibold text-secondary bg-wash mx-0 px-0">
-              Votes on Agora
-            </TableHead>
-            <TableHead className="text-base font-semibold text-secondary bg-wash mx-0 px-0">
-              Percent on Agora
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {votes.map((vote, index) => (
-            <TableRow key={index}>
-              <TableCell
-                className={`pr-1 text-base text-secondary ${
-                  index === votes.length - 1 && "rounded-bl-xl"
-                }`}
-              >
-                {vote.proposal_id}
-              </TableCell>
-              <TableCell className="px-1 text-base text-primary">
-                {vote.vote_count}
-              </TableCell>
-              <TableCell
-                className={`px-1 ${index === votes.length - 1 && "rounded-br-xl"}`}
-              >
-                {vote.event_count}
-              </TableCell>
-              <TableCell className="px-1 text-base text-primary">
-                {(
-                  (Number(vote.event_count) / Number(vote.vote_count)) *
-                  100
-                ).toFixed(2)}
-                %
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <VotesDataView votes={votes} />
     </div>
   );
 };
