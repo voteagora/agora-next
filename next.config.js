e6ef3e5e83d57c682aaddcf311c2e69b57b7c13c
Kent Fenwick <kent.fenwick@gmail.com>
2025-01-15 10:24:37 -0500

    Kent/remove sentry (#729)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const path = require("path");
-module.exports = {
+
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-};
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   async redirects() {
     return [];
   },
@@ -31,39 +28,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "agora-ph",
-    project: "agora-next",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-);
