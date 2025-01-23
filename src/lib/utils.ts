2154829a3843a36b0b87267e5f5c14202334e628
Michael Gingras <mcg79@cornell.edu>
2025-01-23 14:24:29 -0700

    Fixes PTC on admin

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,7 @@ import {
   DERIVE_MAINNET_RPC,
   DERIVE_TESTNET_RPC,
 } from "@/lib/tenant/configs/contracts/derive";
+import { ProposalType } from "../app/proposals/draft/types";
 
 const { token } = Tenant.current();
 
@@ -403,5 +404,19 @@ export const getTransportForChain = (chainId: number) => {
     // for each new dao with a new chainId add them here
     default:
       return null;
+  }
+};
+
+export const getVotingModuleTypeForProposalType = (proposalType: {
+  quorum: number;
+  approval_threshold: number;
+  name: string;
+}) => {
+  if (proposalType.name.toLowerCase().includes("approval")) {
+    return ProposalType.APPROVAL;
+  } else if (proposalType.name.toLowerCase().includes("optimistic")) {
+    return ProposalType.OPTIMISTIC;
+  } else {
+    return ProposalType.BASIC;
   }
 };
