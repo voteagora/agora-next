d719d8593d886d1182c2daa22901f3e771e9af40
andrei <andrei.taraschuk@gmail.com>
2025-01-23 10:49:08 -0700

    Fixed Optimistic proposal page

--- src/components/Proposals/ProposalPage/OPProposalPage/OPProposalOptimisticPage.jsx
+++ src/components/Proposals/ProposalPage/OPProposalPage/OPProposalOptimisticPage.jsx
@@ -1,63 +1,17 @@
-import { fetchDelegate as apiFetchDelegate } from "@/app/api/common/delegates/getDelegates";
-import { fetchDelegateStatement as apiFetchDelegateStatement } from "@/app/api/common/delegateStatement/getDelegateStatement";
-import { fetchCurrentDelegators as apiFetchCurrentDelegators } from "@/app/api/common/delegations/getDelegations";
-import { fetchVotableSupply as apiFetchVoteableSupply } from "@/app/api/common/votableSupply/getVotableSupply";
-import {
-  fetchUserVotesForProposal as apiFetchUserVotesForProposal,
-  fetchVotesForProposal,
-} from "@/app/api/common/votes/getVotes";
+import { fetchVotableSupply } from "@/app/api/common/votableSupply/getVotableSupply";
 import { disapprovalThreshold } from "@/lib/constants";
 import { formatNumber } from "@/lib/utils";
 import { formatUnits } from "ethers";
 import ProposalDescription from "../ProposalDescription/ProposalDescription";
-import OptimisticProposalVotesCard from "../../ProposalPage/OPProposalPage/ProposalVotesCard/OptimisticProposalVotesCard";
 import { ProposalStateAdmin } from "@/app/proposals/components/ProposalStateAdmin";
-
-async function fetchProposalVotes(proposalId, pagintaion) {
-  "use server";
-
-  return fetchVotesForProposal({ proposalId, pagintaion });
-}
-
-async function fetchDelegate(addressOrENSName) {
-  "use server";
-
-  return await apiFetchDelegate(addressOrENSName);
-}
-
-async function fetchUserVotesForProposal(proposalId, address) {
-  "use server";
-
-  return await apiFetchUserVotesForProposal({
-    proposalId,
-    address,
-  });
-}
-
-async function fetchVotableSupply() {
-  "use server";
-
-  return apiFetchVoteableSupply();
-}
-
-async function fetchDelegateStatement(addressOrENSName) {
-  "use server";
-
-  return await apiFetchDelegateStatement(addressOrENSName);
-}
-
-async function fetchCurrentDelegators(addressOrENSName) {
-  "use server";
-
-  return apiFetchCurrentDelegators(addressOrENSName);
-}
+import OptimisticProposalVotesCard from "@/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesCard/OptimisticProposalVotesCard";
 
 export default async function OPProposalPage({ proposal }) {
   const votableSupply = await fetchVotableSupply();
-
   const formattedVotableSupply = Number(
     BigInt(votableSupply) / BigInt(10 ** 18)
   );
+
   const againstLengthString = formatNumber(
     proposal.proposalResults.against,
     18,
@@ -80,16 +34,9 @@ export default async function OPProposalPage({ proposal }) {
         <ProposalDescription proposal={proposal} />
         <OptimisticProposalVotesCard
           proposal={proposal}
-          proposalVotes={proposalVotes}
-          nonVoters={nonVoters}
           againstRelativeAmount={againstRelativeAmount}
           againstLengthString={againstLengthString}
           disapprovalThreshold={disapprovalThreshold}
-          fetchProposalVotes={fetchProposalVotes}
-          fetchDelegate={fetchDelegate}
-          fetchDelegateStatement={fetchDelegateStatement}
-          fetchUserVotesForProposal={fetchUserVotesForProposal}
-          fetchCurrentDelegators={fetchCurrentDelegators}
           status={status}
         />
       </div>
