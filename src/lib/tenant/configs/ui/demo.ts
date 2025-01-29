cbca1e7d7d58dafa2bafd5f5e585eeb398ca2318
Pedro Fernandes <pedrohasftb@gmail.com>
2025-01-29 15:36:28 -0300

    force new deployment for agora-next-demo

--- src/lib/tenant/configs/ui/demo.ts
+++ src/lib/tenant/configs/ui/demo.ts
@@ -162,7 +162,7 @@ export const demoTenantUIConfig = new TenantUI({
       title: "About {Brand name}",
       hero: demoHero,
       description:
-        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.",
+        "At vero eos et accusamus et iust odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.",
       meta: {
         title: "Info of Agora",
         description: "Welcome to the {Brand name} Agora",
