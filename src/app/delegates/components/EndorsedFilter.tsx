483aa253e2cadfa3ebd67a3ee463d630c136a710
Pedro Fernandes <43489810+pedrohasf@users.noreply.github.com>
2025-02-05 14:55:50 -0300

    Fix voters page filters overflow + change view refreshing query (#786)

--- src/app/delegates/components/EndorsedFilter.tsx
+++ src/app/delegates/components/EndorsedFilter.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { Listbox } from "@headlessui/react";
-import { Fragment } from "react";
-import { ChevronDown } from "lucide-react";
 import { useAddSearchParam, useDeleteSearchParam } from "@/hooks";
 import { useAgoraContext } from "@/contexts/AgoraContext";
 import Tenant from "@/lib/tenant/tenant";
 import { UIEndorsedConfig } from "@/lib/tenant/tenantUI";
+import FilterListbox from "@/components/common/FilterListbox";
 
 const FILTER_PARAM = "endorsedFilter";
 
@@ -28,26 +26,25 @@ export default function EndorsedFilter() {
     return null;
   }
 
+  const config = toggle.config as UIEndorsedConfig;
   const filterParam =
-    searchParams?.get(FILTER_PARAM) ||
-    (toggle.config as UIEndorsedConfig).defaultFilter.toString();
+    searchParams?.get(FILTER_PARAM) || config.defaultFilter.toString();
 
-  const endorsedFilterOptions: Record<string, { value: string; sort: string }> =
-    {
-      true: {
-        value: (toggle.config as UIEndorsedConfig).showFilterLabel,
-        sort: "true",
-      },
-      false: {
-        value: (toggle.config as UIEndorsedConfig).hideFilterLabel,
-        sort: "false",
-      },
-    };
+  const endorsedFilterOptions = {
+    true: {
+      value: config.showFilterLabel,
+      sort: "true",
+    },
+    false: {
+      value: config.hideFilterLabel,
+      sort: "false",
+    },
+  };
 
   const handleChange = (value: string) => {
     setIsDelegatesFiltering(true);
     router.push(
-      value === (toggle.config as UIEndorsedConfig).defaultFilter.toString()
+      value === config.defaultFilter.toString()
         ? deleteSearchParam({ name: FILTER_PARAM })
         : addSearchParam({ name: FILTER_PARAM, value }),
       { scroll: false }
@@ -55,39 +52,10 @@ export default function EndorsedFilter() {
   };
 
   return (
-    <Listbox
-      as="div"
-      value={filterParam || endorsedFilterOptions.false.value}
-      onChange={(value) => handleChange(value)}
-    >
-      {() => (
-        <>
-          <Listbox.Button className="w-full sm:w-[200px] bg-wash text-base font-medium border border-line rounded-full py-2 px-4 flex items-center justify-between">
-            <span>
-              {endorsedFilterOptions[filterParam]?.value ||
-                endorsedFilterOptions.true.value}
-            </span>
-            <ChevronDown className="h-4 w-4 ml-[6px] text-secondary/30" />
-          </Listbox.Button>
-          <Listbox.Options className="mt-3 absolute bg-wash border border-[#ebebeb] p-2 rounded-2xl flex flex-col gap-1 z-20 w-max">
-            {Object.keys(endorsedFilterOptions).map((key) => (
-              <Listbox.Option key={key} value={key} as={Fragment}>
-                {({ selected }) => (
-                  <li
-                    className={`cursor-pointer text-base py-2 px-3 rounded-xl font-medium hover:text-primary hover:bg-tertiary/20 ${
-                      selected
-                        ? "text-primary bg-tertiary/20"
-                        : "text-secondary border-transparent"
-                    }`}
-                  >
-                    {endorsedFilterOptions[key].value}
-                  </li>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </>
-      )}
-    </Listbox>
+    <FilterListbox
+      value={filterParam}
+      onChange={handleChange}
+      options={endorsedFilterOptions}
+    />
   );
 }
--- src/app/delegates/components/IssuesFilter.tsx
+++ src/app/delegates/components/IssuesFilter.tsx
@@ -1,24 +1,21 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { Listbox } from "@headlessui/react";
-import { Fragment } from "react";
-import { ChevronDown } from "lucide-react";
 import { useAddSearchParam, useDeleteSearchParam } from "@/hooks";
 import { useAgoraContext } from "@/contexts/AgoraContext";
 import Tenant from "@/lib/tenant/tenant";
+import FilterListbox from "@/components/common/FilterListbox";
 
 const FILTER_PARAM = "issueFilter";
 const DEFAULT_FILTER = "all";
 
 export default function IssuesFilter() {
   const { ui } = Tenant.current();
-
   const router = useRouter();
   const searchParams = useSearchParams();
   const addSearchParam = useAddSearchParam();
   const deleteSearchParam = useDeleteSearchParam();
-  const filterParam = searchParams?.get(FILTER_PARAM) || "all";
+  const filterParam = searchParams?.get(FILTER_PARAM) || DEFAULT_FILTER;
   const { setIsDelegatesFiltering } = useAgoraContext();
 
   const hasIssues = Boolean(
@@ -26,7 +23,7 @@ export default function IssuesFilter() {
   );
   if (!hasIssues) return null;
 
-  let issuesFilterOptions: any = {
+  const issuesFilterOptions: any = {
     [DEFAULT_FILTER]: {
       value: "All Issues",
       sort: DEFAULT_FILTER,
@@ -52,41 +49,10 @@ export default function IssuesFilter() {
   };
 
   return (
-    <Listbox
-      as="div"
-      value={filterParam || issuesFilterOptions.all.value}
-      onChange={(value) => handleChange(value)}
-    >
-      {() => (
-        <>
-          <Listbox.Button className="w-full sm:w-[200px] font-medium border border-line rounded-full py-2 px-4 flex items-center justify-between bg-neutral text-primary">
-            <span>
-              {issuesFilterOptions[filterParam]?.value ||
-                issuesFilterOptions.all.value}
-            </span>
-            <ChevronDown className="h-4 w-4 ml-[6px] text-secondary/30" />
-          </Listbox.Button>
-          <Listbox.Options className="mt-3 absolute bg-wash border border-line p-2 rounded-2xl flex flex-col gap-1 z-20 w-max">
-            {Object.keys(issuesFilterOptions).map((key) => (
-              <Listbox.Option key={key} value={key} as={Fragment}>
-                {({ selected }) => {
-                  return (
-                    <li
-                      className={`cursor-pointer text-base py-2 px-3 rounded-xl font-medium hover:text-primary hover:bg-tertiary/20 ${
-                        selected
-                          ? "text-primary bg-tertiary/20"
-                          : "text-secondary border-transparent"
-                      }`}
-                    >
-                      {issuesFilterOptions[key].value}
-                    </li>
-                  );
-                }}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </>
-      )}
-    </Listbox>
+    <FilterListbox
+      value={filterParam}
+      onChange={handleChange}
+      options={issuesFilterOptions}
+    />
   );
 }
