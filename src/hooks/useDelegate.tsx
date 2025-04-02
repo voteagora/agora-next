fcf5f3f69ebfa27a9ea678b3371b0bdb31c90a95
Sudheer <sudheer.105@gmail.com>
2025-04-02 16:31:58 +0530

    Remove unused import

--- src/hooks/useDelegate.tsx
+++ src/hooks/useDelegate.tsx
@@ -1,7 +1,6 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { fetchDelegate } from "@/app/delegates/actions";
 import { Delegate } from "@/app/api/common/delegates/delegate";
-import { useCallback } from "react";
 
 const CACHE_TIME = 180000; // 3 minute cache
 
