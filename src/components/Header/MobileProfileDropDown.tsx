d993656313e34419824082747a3e9bab80a23bed
Sudheer <sudheer.105@gmail.com>
2025-04-09 14:43:57 +0530

    Fix: Mobile logout issue on mobile (#931)

--- src/components/Header/MobileProfileDropDown.tsx
+++ src/components/Header/MobileProfileDropDown.tsx
@@ -17,6 +17,7 @@ import { PowerIcon } from "@/icons/PowerIcon";
 import { useDelegate } from "@/hooks/useDelegate";
 import { useTokenBalance } from "@/hooks/useTokenBalance";
 import { Logout } from "@/icons/logout";
+import { createPortal } from "react-dom";
 
 type Props = {
   ensName: string | undefined;
@@ -94,135 +95,141 @@ export const MobileProfileDropDown = ({ ensName }: Props) => {
 
             <Transition className="absolute z-[70]">
               <Popover.Panel>
-                {({ close }) => (
-                  <motion.div
-                    className="bg-neutral py-8 px-6 rounded-t-lg w-full fixed bottom-0 left-0"
-                    initial="hidden"
-                    animate="show"
-                    exit="exit"
-                    variants={variants}
-                    transition={{ duration: 0.2 }}
-                  >
-                    <div className="flex flex-col gap-3 min-h-[280px] justify-center">
-                      <div className="flex flex-col">
-                        <div className="flex flex-row items-center gap-4 mb-1">
-                          <div
-                            className={`relative aspect-square ${
-                              isFetching && "animate-pulse"
-                            }`}
-                          >
-                            <ENSAvatar ensName={ensName} />
-                          </div>
-                          <div className="flex flex-col flex-1">
-                            {ensName ? (
-                              <>
-                                <span className="text-primary">{ensName}</span>
-                                <span className="text-xs text-secondary">
-                                  {shortAddress(address!)}
-                                </span>
-                              </>
-                            ) : (
-                              <>
-                                <span className="text-primary">
-                                  {shortAddress(address!)}
-                                </span>
-                              </>
-                            )}
-                          </div>
-                        </div>
-                        {scwAddress && (
-                          <div>
-                            <div className="w-[44px] flex justify-center items-center">
-                              <div className="border-l border-dashed border-line h-2"></div>
+                {({ close }) =>
+                  createPortal(
+                    <motion.div
+                      className="bg-neutral py-8 px-6 rounded-t-lg w-full fixed z-[70] bottom-0 left-0"
+                      initial="hidden"
+                      animate="show"
+                      exit="exit"
+                      variants={variants}
+                      transition={{ duration: 0.2 }}
+                    >
+                      <div className="flex flex-col gap-3 min-h-[280px] justify-center">
+                        <div className="flex flex-col">
+                          <div className="flex flex-row items-center gap-4 mb-1">
+                            <div
+                              className={`relative aspect-square ${
+                                isFetching && "animate-pulse"
+                              }`}
+                            >
+                              <ENSAvatar ensName={ensName} />
                             </div>
-                            <div className="flex flex-row items-center gap-3">
+                            <div className="flex flex-col flex-1">
+                              {ensName ? (
+                                <>
+                                  <span className="text-primary">
+                                    {ensName}
+                                  </span>
+                                  <span className="text-xs text-secondary">
+                                    {shortAddress(address!)}
+                                  </span>
+                                </>
+                              ) : (
+                                <>
+                                  <span className="text-primary">
+                                    {shortAddress(address!)}
+                                  </span>
+                                </>
+                              )}
+                            </div>
+                          </div>
+                          {scwAddress && (
+                            <div>
                               <div className="w-[44px] flex justify-center items-center">
-                                <div className="flex items-center justify-center rounded-full border border-line w-[30px] h-[30px]">
-                                  <CubeIcon
-                                    className="w-5 h-5"
-                                    fill={rgbStringToHex(
-                                      ui?.customization?.primary
-                                    )}
-                                  />
+                                <div className="border-l border-dashed border-line h-2"></div>
+                              </div>
+                              <div className="flex flex-row items-center gap-3">
+                                <div className="w-[44px] flex justify-center items-center">
+                                  <div className="flex items-center justify-center rounded-full border border-line w-[30px] h-[30px]">
+                                    <CubeIcon
+                                      className="w-5 h-5"
+                                      fill={rgbStringToHex(
+                                        ui?.customization?.primary
+                                      )}
+                                    />
+                                  </div>
+                                </div>
+                                <div className="text-primary">
+                                  {shortAddress(scwAddress)}
                                 </div>
                               </div>
-                              <div className="text-primary">
-                                {shortAddress(scwAddress)}
-                              </div>
                             </div>
-                          </div>
-                        )}
-                      </div>
+                          )}
+                        </div>
 
-                      <div className="self-stretch py-8 flex flex-col gap-6">
-                        <PanelRow
-                          title={
-                            ui.tacticalStrings?.myBalance || "My token balance"
-                          }
-                          detail={
-                            <RowSkeletonWrapper isLoading={isFetching}>
-                              <TokenAmountDecorated
-                                amount={tokenBalance || BigInt(0)}
-                              />
-                            </RowSkeletonWrapper>
-                          }
-                        />
-                        <PanelRow
-                          title="My voting power"
-                          detail={
-                            <RowSkeletonWrapper isLoading={isFetching}>
-                              <TokenAmountDecorated
-                                amount={
-                                  delegate?.votingPower.total || BigInt(0)
-                                }
-                              />
-                            </RowSkeletonWrapper>
-                          }
-                        />
-
-                        {isFetching ? (
-                          <div className="animate-pulse bg-tertiary/10 h-[50px] mt-1 w-full rounded-2xl"></div>
-                        ) : (
-                          <>
-                            {canCreateDelegateStatement && !hasStatement && (
-                              <Link
-                                href={`/delegates/create`}
-                                className="rounded-lg py-3 px-2 bg-brandPrimary hover:bg-none text-neutral flex justify-center mt-1"
-                                onClick={() => close()}
-                              >
-                                Create delegate statement
-                              </Link>
-                            )}
-
-                            {hasStatement && (
-                              <Link
-                                href={`/delegates/${ensName ?? address}`}
-                                onClick={() => close()}
-                                className="px-5 py-3 rounded-lg shadow-[0px_2px_2px_0px_rgba(0,0,0,0.03)] border border-neutral-200 flex justify-center"
-                              >
-                                <span className="text-primary text-base font-semibold">
-                                  View my profile
-                                </span>
-                              </Link>
-                            )}
-                          </>
-                        )}
-                      </div>
-                      <div className="py-4 border-t border-line bg-neutral rounded-[0px_0px_12px_12px]">
-                        <div
-                          onClick={() => disconnect()}
-                          className="cursor-pointer flex"
-                        >
-                          <Logout
-                            fill={rgbStringToHex(ui?.customization?.primary)}
-                            className={"mr-[10px] self-center"}
+                        <div className="self-stretch py-8 flex flex-col gap-6">
+                          <PanelRow
+                            title={
+                              ui.tacticalStrings?.myBalance ||
+                              "My token balance"
+                            }
+                            detail={
+                              <RowSkeletonWrapper isLoading={isFetching}>
+                                <TokenAmountDecorated
+                                  amount={tokenBalance || BigInt(0)}
+                                />
+                              </RowSkeletonWrapper>
+                            }
                           />
-                          <span className="text-primary">Logout</span>
+                          <PanelRow
+                            title="My voting power"
+                            detail={
+                              <RowSkeletonWrapper isLoading={isFetching}>
+                                <TokenAmountDecorated
+                                  amount={
+                                    delegate?.votingPower.total || BigInt(0)
+                                  }
+                                />
+                              </RowSkeletonWrapper>
+                            }
+                          />
+
+                          {isFetching ? (
+                            <div className="animate-pulse bg-tertiary/10 h-[50px] mt-1 w-full rounded-2xl"></div>
+                          ) : (
+                            <>
+                              {canCreateDelegateStatement && !hasStatement && (
+                                <Link
+                                  href={`/delegates/create`}
+                                  className="rounded-lg py-3 px-2 bg-brandPrimary hover:bg-none text-neutral flex justify-center mt-1"
+                                  onClick={() => close()}
+                                >
+                                  Create delegate statement
+                                </Link>
+                              )}
+
+                              {hasStatement && (
+                                <Link
+                                  href={`/delegates/${ensName ?? address}`}
+                                  onClick={() => close()}
+                                  className="px-5 py-3 rounded-lg shadow-[0px_2px_2px_0px_rgba(0,0,0,0.03)] border border-neutral-200 flex justify-center"
+                                >
+                                  <span className="text-primary text-base font-semibold">
+                                    View my profile
+                                  </span>
+                                </Link>
+                              )}
+                            </>
+                          )}
+                        </div>
+                        <div className="py-4 border-t border-line bg-neutral rounded-[0px_0px_12px_12px]">
+                          <div
+                            onClick={() => disconnect()}
+                            className="cursor-pointer flex"
+                          >
+                            <Logout
+                              fill={rgbStringToHex(ui?.customization?.primary)}
+                              className={"mr-[10px] self-center"}
+                            />
+                            <span className="text-primary">Logout</span>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </motion.div>
-                )}
+                    </motion.div>,
+                    document.body
+                  )
+                }
               </Popover.Panel>
             </Transition>
           </>
