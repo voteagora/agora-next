bc0e06e666be03d842e2cea01cf3fda00a4c60c8
Jeff <jeff@voteagora.com>
2025-04-25 23:18:31 -0400

    Working proposal state

--- src/app/api/common/proposals/getProposals.ts
+++ src/app/api/common/proposals/getProposals.ts
@@ -28,7 +28,7 @@ import { Block } from "ethers";
 import { withMetrics } from "@/lib/metricWrapper";
 import { unstable_cache } from "next/cache";
 import { getPublicClient } from "@/lib/viem";
-import { getProposalTypesFromDaoNode } from "@/app/lib/dao-node/client";
+import { getProposalTypesFromDaoNode, getProposalsFromDaoNode } from "@/app/lib/dao-node/client";
 import Decimal from "decimal.js";
 
 interface DAONodeAPIResponse {
@@ -57,18 +57,65 @@ interface DAONodeAPIResponse {
     log_index: number;
     id: string;
   };
-  proposal_results: Record<string, string>;
+  totals: Record<string, string>;
+  voting_module_name: string;
 }
 
 function adaptDAONodeResponse(
   apiResponse: DAONodeAPIResponse
 ): ProposalPayload {
+
+  const votingModuleName = apiResponse.voting_module_name
+
+  let proposalResults;
+  
+  if (votingModuleName == 'standard') {
+
+    proposalResults = {
+      standard: {
+        "0": BigInt(apiResponse.totals['no-param']?.['0'] ?? '0'),
+        "1": BigInt(apiResponse.totals['no-param']?.['1'] ?? '0'),
+        "2": BigInt(apiResponse.totals['no-param']?.['2'] ?? '0')
+      },
+      approval: null
+    }
+
+  } else if (votingModuleName == 'approval') {
+
+    const approvalVotes = Object.entries(apiResponse.totals)
+      .filter(([key]) => key !== 'no-param')
+      .map(([param, votes]) => ({
+        param,
+        votes: BigInt(votes['1'])
+      }));
+    
+    proposalResults = {
+      approval: approvalVotes,
+      standard: {
+        "0": BigInt(apiResponse.totals['no-param']?.['0'] ?? '0'),
+        "1": approvalVotes.reduce((sum, vote) => sum + vote.votes, BigInt(0)),
+        "2": BigInt(apiResponse.totals['no-param']?.['2'] ?? '0')
+      }
+    };
+  } else if (votingModuleName == 'optimistic') {
+    
+    proposalResults = {
+      standard: {
+        "0": BigInt(apiResponse.totals['no-param']?.['0'] ?? '0'),
+      },
+      approval: null
+    }
+
+  } else {
+    throw new Error(`Unknown voting module name: ${votingModuleName}`);
+  }
+
   return {
     proposal_id: apiResponse.id,
-    contract: process.env.DAONODE_CONTRACT_ADDRESS ?? "",
+    contract: null,
     proposer: apiResponse.proposer.toLowerCase(),
     description: apiResponse.description,
-    ordinal: new Decimal(apiResponse.block_number),
+    ordinal: null,
     created_block: BigInt(apiResponse.block_number),
     start_block: apiResponse.start_block.toString(),
     end_block: apiResponse.end_block.toString(),
@@ -91,128 +138,14 @@ function adaptDAONodeResponse(
       calldatas: apiResponse.calldatas,
       signatures: apiResponse.signatures,
     }),
-    proposal_type: "STANDARD",
+    proposal_type: apiResponse.voting_module_name.toUpperCase(),
     proposal_type_data: null,
-    proposal_results: {
-      standard: Object.entries(apiResponse.proposal_results).reduce(
-        (acc, [key, value]) => {
-          acc[key] = Number(value);
-          return acc;
-        },
-        {} as Record<string, number>
-      ),
-    },
+    proposal_results: proposalResults,
     created_transaction_hash: null,
     cancelled_transaction_hash: null,
     queued_transaction_hash: null,
     executed_transaction_hash: null,
   };
-}
-
-async function fetchProposalsFromDAONodeApi({
-  namespace,
-  skip,
-  take,
-  filter,
-}: {
-  namespace: string;
-  skip: number;
-  take: number;
-  filter: string;
-}): Promise<{ data: ProposalPayload[] }> {
-  try {
-    const response = await fetch(
-      `${process.env.DAONODE_API_URL}/v1/proposals?set=relevant`
-    );
-
-    if (!response.ok) {
-      throw new Error(`API responded with status: ${response.status}`);
-    }
-
-    const result = await response.json();
-    const adaptedData = result.proposals.map(adaptDAONodeResponse);
-    return {
-      data: adaptedData,
-    };
-  } catch (error) {
-    console.error("Failed to fetch from REST API:", error);
-    throw error;
-  }
-
-  /* 
-  CURRENT DAO NODE REPONSE:
-
-      {
-      "block_number": 13538153,
-      "transaction_index": 376,
-      "log_index": 606,
-      "id": "9",
-      "proposer": "0x9b68c14e936104e9a7a24c712beecdc220002984",
-      "targets": [
-        "0x1f98431c8ad98523631ae4a59f267346ea31f984"
-      ],
-      "values": [
-        0
-      ],
-      "signatures": [
-        "enableFeeAmount(uint24,int24)"
-      ],
-      "calldatas": [
-        "00000000000000000000000000000000000000000000000000000000000000640000000000000000000000000000000000000000000000000000000000000001"
-      ],
-      "start_block": 13551293,
-      "end_block": 13591613,
-      "description": "# Add 1 Basis P...",
-      "queue_event": {
-        "block_number": 13591709,
-        "transaction_index": 311,
-        "log_index": 539,
-        "id": "9",
-        "eta": 1636763195
-      },
-      "execute_event": {
-        "block_number": 13604706,
-        "transaction_index": 244,
-        "log_index": 354,
-        "id": "9"
-      },
-      "proposal_results": {
-        "1": "71369769192668307336680735"
-      }
-    }
-
-   DB RECORD RESPONSE:
-
-  /*{
-    proposal_id: '62',
-    proposer: '0xecc2a9240268bc7a26386ecb49e1befca2706ac9',
-    description: '# Mobilizing the Uniswap Treasury\n' +
-      'The UTWG will also try to collaborate with those working on various legal developments including the recent [<u>Wyoming Decentralized Unincorporated Nonprofit Association (DUNA) Act</u>](https://a16zcrypto.com/posts/article/duna-for-daos/). This process may fall outside of the jurisdiction of the treasury committee and may require outsourcing entirely to another party. It’s vital that this treasury research is analyzed in the context of potential legal structures, and we won’t move forward with implementing our research unless there are proper legal frameworks in place. The Uniswap DAO has yet t'... 5635 more characters,
-    created_block: 19748314n,
-    start_block: '19761454',
-    end_block: '19801774',
-    cancelled_block: null,
-    executed_block: 19816075n,
-    queued_block: 19801776n,
-    proposal_data: {
-      values: [ 0 ],
-      targets: [ '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984' ],
-      calldatas: [
-        '0xa9059cbb0000000000000000000000003b59c6d0034490093460787566dc5d6ce17f2f9c00000000000000000000000000000000000000000000014542ba12a337c00000'
-      ],
-      signatures: [ '' ]
-    },
-    proposal_results: {
-      standard: {
-        '0': 4.6242347731945804e+22,
-        '1': 4.20659862829919e+25,
-        '2': 304963417219416640
-      },
-      approval: null
-    },
-    proposal_type: 'STANDARD'
-  }
-  */
 }
 
 async function getProposals({
@@ -231,21 +164,22 @@ async function getProposals({
         const getProposalsExecution = doInSpan(
           { name: "getProposals" },
           async () => {
+
             const useRestApi =
               ui.toggle("use-daonode-for-proposals")?.enabled ?? false;
 
             let proposalsResult;
+            
             if (useRestApi) {
               try {
                 proposalsResult = await paginateResult(
                   async (skip: number, take: number) => {
-                    const result = await fetchProposalsFromDAONodeApi({
-                      namespace,
+                    const result = await getProposalsFromDaoNode(
                       skip,
                       take,
-                      filter,
-                    });
-                    return result.data;
+                      filter
+                    );
+                    return result.map(adaptDAONodeResponse);
                   },
                   pagination
                 );
@@ -289,6 +223,8 @@ async function getProposals({
           latestBlockPromise,
           fetchVotableSupply(),
         ]);
+
+        console.log("proposals", proposals);
 
         const resolvedProposals = await Promise.all(
           proposals.data.map(async (proposal: ProposalPayload) => {
