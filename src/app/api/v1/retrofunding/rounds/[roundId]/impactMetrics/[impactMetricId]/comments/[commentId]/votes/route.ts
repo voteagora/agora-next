d18a4b82e8fd46251ed772cf1168b5a3297dae1b
Jeff <jeff@voteagora.com>
2025-05-06 11:39:49 -0400

    Revert src/app/api/v1/retrofunding/rounds/[roundId]/impactMetrics/[impactMetricId]/comments/[commentId]/votes/route.ts

--- src/app/api/v1/retrofunding/rounds/[roundId]/impactMetrics/[impactMetricId]/comments/[commentId]/votes/route.ts
+++ src/app/api/v1/retrofunding/rounds/[roundId]/impactMetrics/[impactMetricId]/comments/[commentId]/votes/route.ts
@@ -1,164 +1,84 @@
+import { fetchImpactMetricCommentVotes } from "@/app/api/common/comments/getImpactMetricCommentVotes";
+import { updateImpactMetricCommentVote } from "@/app/api/common/comments/updateImpactMetricCommentVote";
+import { traceWithUserId } from "@/app/api/v1/apiUtils";
+import { authenticateApiUser } from "@/app/lib/auth/serverAuth";
 import { NextRequest, NextResponse } from "next/server";
+import { createOptionalNumberValidator } from "@/app/api/common/utils/validators";
+
+const voteValidator = createOptionalNumberValidator(-1, 1, 0);
 
 export async function GET(
   request: NextRequest,
-  {
-    params,
-  }: { params: { roundId: string; impactMetricId: string; commentId: string } }
+  route: {
+    params: { roundId: string; impactMetricId: string; commentId: string };
+  }
 ) {
-  const { authenticateApiUser } = await import("@/app/lib/auth/serverAuth");
-  const { prismaWeb2Client } = await import("@/app/lib/prisma");
-  const { traceWithUserId } = await import("@/app/api/v1/apiUtils");
-
   const authResponse = await authenticateApiUser(request);
 
   if (!authResponse.authenticated) {
-    return NextResponse.json(
-      { error: authResponse.failReason },
-      { status: 401 }
-    );
+    return new Response(authResponse.failReason, { status: 401 });
   }
 
-  if (!authResponse.userId) {
-    return NextResponse.json(
-      { error: "User ID not found in auth response" },
-      { status: 401 }
-    );
-  }
-
-  const userId = authResponse.userId;
-
-  return await traceWithUserId(userId, async () => {
+  return await traceWithUserId(authResponse.userId as string, async () => {
+    const { roundId, impactMetricId, commentId } = route.params;
     try {
-      const { roundId, impactMetricId, commentId } = params;
-
-      const votes = await prismaWeb2Client.metrics_comments_votes.findMany({
-        where: {
-          comment_id: parseInt(commentId, 10),
-        },
-        orderBy: {
-          created_at: "desc",
-        },
+      const comments = await fetchImpactMetricCommentVotes({
+        commentId: Number(commentId),
       });
-
-      return NextResponse.json(votes);
+      return NextResponse.json(comments);
     } catch (e: any) {
-      return NextResponse.json(
-        { error: "Internal server error: " + e.toString() },
-        { status: 500 }
-      );
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
+      });
     }
   });
 }
 
-export async function POST(
+export async function PUT(
   request: NextRequest,
-  {
-    params,
-  }: { params: { roundId: string; impactMetricId: string; commentId: string } }
+  route: {
+    params: { roundId: string; impactMetricId: string; commentId: string };
+  }
 ) {
-  const { authenticateApiUser } = await import("@/app/lib/auth/serverAuth");
-  const { prismaWeb2Client } = await import("@/app/lib/prisma");
-  const { traceWithUserId } = await import("@/app/api/v1/apiUtils");
-  const { z } = await import("zod");
-
   const authResponse = await authenticateApiUser(request);
 
   if (!authResponse.authenticated) {
-    return NextResponse.json(
-      { error: authResponse.failReason },
-      { status: 401 }
-    );
+    return new Response(authResponse.failReason, { status: 401 });
   }
 
   if (!authResponse.userId) {
-    return NextResponse.json(
-      { error: "User ID not found in auth response" },
-      { status: 401 }
-    );
+    return new Response("Can't get user address from auth token", {
+      status: 401,
+    });
   }
 
-  const userId = authResponse.userId;
+  if (!authResponse.scope?.includes("badgeholder")) {
+    return new Response("Only badgeholder can vote on a comment", {
+      status: 401,
+    });
+  }
 
-  return await traceWithUserId(userId, async () => {
-    try {
-      const { roundId, impactMetricId, commentId } = params;
-      const { vote } = await request.json();
+  return await traceWithUserId(authResponse.userId, async () => {
+    const { commentId } = route.params;
 
-      const VoteSchema = z.object({
-        vote: z.number().int().min(-1).max(1),
-      });
-
-      const validatedData = VoteSchema.parse({ vote });
-
-      const newVote = await prismaWeb2Client.metrics_comments_votes.create({
-        data: {
-          comment_id: parseInt(commentId, 10),
-          voter: userId,
-          vote: validatedData.vote,
-        },
-      });
-
-      return NextResponse.json(newVote);
-    } catch (e: any) {
-      if (e instanceof z.ZodError) {
-        return NextResponse.json({ error: e.toString() }, { status: 400 });
-      }
-      return NextResponse.json(
-        { error: "Internal server error: " + e.toString() },
-        { status: 500 }
-      );
+    const body = await request.json();
+    if (body.vote == undefined) {
+      return new Response("Missing vote in request body", { status: 400 });
     }
-  });
-}
 
-export async function DELETE(
-  request: NextRequest,
-  {
-    params,
-  }: { params: { roundId: string; impactMetricId: string; commentId: string } }
-) {
-  const { authenticateApiUser } = await import("@/app/lib/auth/serverAuth");
-  const { prismaWeb2Client } = await import("@/app/lib/prisma");
-  const { traceWithUserId } = await import("@/app/api/v1/apiUtils");
-
-  const authResponse = await authenticateApiUser(request);
-
-  if (!authResponse.authenticated) {
-    return NextResponse.json(
-      { error: authResponse.failReason },
-      { status: 401 }
-    );
-  }
-
-  if (!authResponse.userId) {
-    return NextResponse.json(
-      { error: "User ID not found in auth response" },
-      { status: 401 }
-    );
-  }
-
-  const userId = authResponse.userId;
-
-  return await traceWithUserId(userId, async () => {
     try {
-      const { roundId, impactMetricId, commentId } = params;
+      const vote = voteValidator.parse(body.vote);
 
-      await prismaWeb2Client.metrics_comments_votes.delete({
-        where: {
-          comment_id_voter: {
-            comment_id: parseInt(commentId, 10),
-            voter: userId,
-          },
-        },
+      const comments = await updateImpactMetricCommentVote({
+        commentId: Number(commentId),
+        vote,
+        address: authResponse.userId as string,
       });
-
-      return NextResponse.json({ success: true });
+      return NextResponse.json(comments);
     } catch (e: any) {
-      return NextResponse.json(
-        { error: "Internal server error: " + e.toString() },
-        { status: 500 }
-      );
+      return new Response("Internal server error: " + e.toString(), {
+        status: 500,
+      });
     }
   });
 }
