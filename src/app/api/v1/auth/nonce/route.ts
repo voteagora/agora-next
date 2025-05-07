d061bd8645123d796fbaf020a1044a87cdc6d823
Jeff <jeff@voteagora.com>
2025-05-06 21:20:08 -0400

    Cleanup

--- src/app/api/v1/auth/nonce/route.ts
+++ src/app/api/v1/auth/nonce/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
-
 // export const dynamic = 'force-dynamic'; // this line is uncommented for e2e tests
+
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const { generateNonce } = await import("siwe");
