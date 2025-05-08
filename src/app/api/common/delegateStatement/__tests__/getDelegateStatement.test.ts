64c9880309ed2f3a6b6c67e9ae1f77f7f8c6cac1
daktic <garrett@voteagora.com>
2025-05-08 18:20:09 -0400

    Adding a getDelegateStatement.test.ts
    Currently, we are creating and deleting the values, but we need to verify them still

--- src/app/api/common/delegateStatement/__tests__/getDelegateStatement.test.ts
+++ src/app/api/common/delegateStatement/__tests__/getDelegateStatement.test.ts
@@ -1,3 +1,108 @@
+import { describe, it } from "vitest"; // Import from vitest
+import { stageStatus } from "@/app/lib/sharedEnums";
+import { setDefaultValues } from "@/app/api/common/delegateStatement/__tests__/sharedTestInfra";
+import { vi } from "vitest";
+import verifyMessage from "@/lib/serverVerifyMessage";
+import { createDelegateStatement } from "@/app/api/common/delegateStatement/createDelegateStatement";
+import { createHash } from "crypto";
+import { getDelegateStatementsForAddress } from "@/app/api/common/delegateStatement/getDelegateStatement";
+import { prismaWeb2Client } from "@/app/lib/prisma";
+import Tenant from "@/lib/tenant/tenant";
+const { slug } = Tenant.current();
+
+vi.mock("server-only", () => ({})); // Mock server-only module
+
+// Because we aren't signing a message onchain, we cannot verify, and most skip over this check.
+vi.mock("@/lib/serverVerifyMessage", () => ({
+  default: vi.fn(),
+}));
+
+// Add some default values that are refrenced multiple times
+const mockAddress =
+  "0xcC0B26236AFa80673b0859312a7eC16d2b72C1e4" as `0x${string}`;
+const mockStage = stageStatus.PUBLISHED as stageStatus;
+const mockMessage = "test";
+
+// Define default values for the schema
+const mockDelegateStatement = {
+  address: mockAddress,
+  stage: mockStage,
+  created_at: Date.now(),
+  discord: "discord",
+  endorsed: false,
+  payload: { delegateStatement: "A delegate, I am" },
+  signature: "0xstring" as `0x${string}`,
+  twitter: "twitter",
+  updated_at: Date.now(),
+  warpcast: "warpcast",
+  scw_address: null,
+  email: null,
+  notification_preferences: {
+    wants_proposal_created_email: "prompt",
+    wants_proposal_ending_soon_email: "prompt",
+    last_updated_at: Date.now(),
+  },
+};
+
+const mockDelegateStatementFV = setDefaultValues(mockDelegateStatement);
+
 describe("getDelegteStatement", () => {
-  it("should return a delegate statement", () => {});
+  it("should return all delegate statements ", async () => {
+    let args = {
+      address: mockAddress,
+      delegateStatement: mockDelegateStatementFV,
+      signature: "0xsomesignaturemock" as `0x${string}`,
+      message: mockMessage,
+      stage: mockStage,
+    };
+
+    const messageHash1 = createHash("sha256")
+      .update(args.message)
+      .digest("hex");
+
+    const mockVerifyMessage = vi.mocked(verifyMessage);
+    mockVerifyMessage.mockResolvedValueOnce(true);
+    mockVerifyMessage.mockResolvedValueOnce(true); // for second create
+
+    // Create a delegate statement
+    await createDelegateStatement(args);
+    // Create a second delegate statement
+    args.message = "second-message";
+    await createDelegateStatement(args);
+
+    const messageHash2 = createHash("sha256")
+      .update(args.message)
+      .digest("hex");
+
+    // Get the statements
+    const delegateStatements = await getDelegateStatementsForAddress({
+      address: mockAddress,
+      stage: mockStage,
+    });
+
+    // Verify they are as expected
+    // TODO
+
+    // Delete the delegate statements to clean up
+    await prismaWeb2Client.delegateStatements.delete({
+      where: {
+        address_dao_slug_stage_message_hash: {
+          address: args.address.toLowerCase(),
+          dao_slug: slug,
+          stage: args.stage,
+          message_hash: messageHash1,
+        },
+      },
+    });
+    await prismaWeb2Client.delegateStatements.delete({
+      where: {
+        address_dao_slug_stage_message_hash: {
+          address: args.address.toLowerCase(),
+          dao_slug: slug,
+          stage: args.stage,
+          message_hash: messageHash2,
+        },
+      },
+    });
+  });
 });
