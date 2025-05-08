41eb8674a605b02f5a31b00ff02aeda68eee817b
daktic <garrett@voteagora.com>
2025-05-08 17:39:07 -0400

    created a shared util so I could reuse it for configuration

--- src/app/api/common/delegateStatement/__tests__/createDelegateStatement.test.ts
+++ src/app/api/common/delegateStatement/__tests__/createDelegateStatement.test.ts
@@ -3,10 +3,10 @@ import { createDelegateStatement } from "@/app/api/common/delegateStatement/crea
 import verifyMessage from "@/lib/serverVerifyMessage";
 import Tenant from "@/lib/tenant/tenant";
 import { stageStatus } from "@/app/lib/sharedEnums";
-import { DelegateStatement } from "@/app/api/common/delegateStatement/delegateStatement";
-const { ui, slug } = Tenant.current();
+const { slug } = Tenant.current();
 import { prismaWeb2Client } from "@/app/lib/prisma";
 import { createHash } from "crypto";
+import { setDefaultValues } from "@/app/api/common/delegateStatement/__tests__/sharedTestInfra";
 
 import { getDraftMessageHash } from "@/app/api/common/delegateStatement/createDelegateStatement";
 
@@ -46,115 +46,6 @@ const mockDelegateStatement = {
     wants_proposal_ending_soon_email: "prompt",
     last_updated_at: Date.now(),
   },
-};
-const requireCodeOfConduct = !!ui.toggle("delegates/code-of-conduct")?.enabled;
-const requireDaoPrinciples = !!ui.toggle("delegates/dao-principles")?.enabled;
-const topIssues = ui.governanceIssues;
-const defaultIssues = !topIssues;
-
-const setDefaultValues = (delegateStatement: DelegateStatement | null) => {
-  return {
-    agreeCodeConduct: !requireCodeOfConduct,
-    agreeDaoPrinciples: !requireDaoPrinciples,
-    daoSlug: slug,
-    discord: delegateStatement?.discord || "",
-    delegateStatement:
-      (delegateStatement?.payload as { delegateStatement?: string })
-        ?.delegateStatement || "",
-    email: delegateStatement?.email || "",
-    twitter: delegateStatement?.twitter || "",
-    warpcast: delegateStatement?.warpcast || "",
-    scwAddress: delegateStatement?.scw_address || "",
-    topIssues: Array.isArray(
-      (
-        delegateStatement?.payload as {
-          topIssues: { value: string; type: string }[];
-        }
-      )?.topIssues
-    )
-      ? (
-          delegateStatement?.payload as {
-            topIssues: { value: string; type: string }[];
-          }
-        )?.topIssues
-      : defaultIssues
-        ? [] // Convert boolean `defaultIssues` to an empty array
-        : [],
-
-    topStakeholders:
-      (
-        delegateStatement?.payload as {
-          topStakeholders: {
-            value: string;
-            type: string;
-          }[];
-        }
-      )?.topStakeholders?.length > 0
-        ? (
-            delegateStatement?.payload as {
-              topStakeholders: {
-                value: string;
-                type: string;
-              }[];
-            }
-          )?.topStakeholders
-        : [],
-
-    openToSponsoringProposals:
-      (
-        delegateStatement?.payload as {
-          openToSponsoringProposals?: "yes" | "no" | null;
-        }
-      )?.openToSponsoringProposals || null,
-    mostValuableProposals: Array.isArray(
-      (
-        delegateStatement?.payload as {
-          mostValuableProposals?: { number: string }[];
-        }
-      )?.mostValuableProposals
-    )
-      ? (
-          (
-            delegateStatement?.payload as {
-              mostValuableProposals?: object[];
-            }
-          )?.mostValuableProposals as { number: string }[]
-        ).filter(
-          (proposal) =>
-            typeof proposal === "object" &&
-            "number" in proposal &&
-            typeof proposal.number === "string"
-        )
-      : [],
-    leastValuableProposals: Array.isArray(
-      (
-        delegateStatement?.payload as {
-          leastValuableProposals?: { number: string }[];
-        }
-      )?.leastValuableProposals
-    )
-      ? (
-          (
-            delegateStatement?.payload as {
-              leastValuableProposals?: object[];
-            }
-          )?.leastValuableProposals as { number: string }[]
-        ).filter(
-          (proposal) =>
-            typeof proposal === "object" &&
-            "number" in proposal &&
-            typeof proposal.number === "string"
-        )
-      : [],
-    notificationPreferences: (delegateStatement?.notification_preferences as {
-      wants_proposal_created_email: "prompt" | "prompted" | boolean;
-      wants_proposal_ending_soon_email: "prompt" | "prompted" | boolean;
-    }) || {
-      wants_proposal_created_email: "prompt",
-      wants_proposal_ending_soon_email: "prompt",
-    },
-    last_updated: new Date().toISOString(),
-  };
 };
 
 const mockDelegateStatementFV = setDefaultValues(mockDelegateStatement);
