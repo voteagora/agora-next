ebc57906f3a1b824535318cbc2b6fb0727622fa8
Jeff <jeff@voteagora.com>
2025-05-13 20:03:27 -0400

    prettier

--- src/app/api/proposals/[proposalId]/chart/route.ts
+++ src/app/api/proposals/[proposalId]/chart/route.ts
@@ -4,10 +4,11 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { proposalId: string } }
 ) {
-  
   const { getVotesChart } = await import("@/app/api/proposals/getVotesChart");
-  const { getSnapshotVotesChart } = await import("@/app/api/proposals/getVotesChart");  
-  
+  const { getSnapshotVotesChart } = await import(
+    "@/app/api/proposals/getVotesChart"
+  );
+
   const searchParams = request.nextUrl.searchParams;
   const proposalType = searchParams.get("proposalType");
   try {
