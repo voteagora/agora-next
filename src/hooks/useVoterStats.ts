64160a8ce5b7f6fbc69acf8fe1748ce795dc39a8
daktic <garrett@voteagora.com>
2025-05-28 16:23:35 -0400

    bridge to fetch participation stats

--- src/hooks/useVoterStats.ts
+++ src/hooks/useVoterStats.ts
@@ -1,10 +1,15 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import {
+  fetchParticipationStats,
+  fetchVoterStats,
+} from "@/app/delegates/actions";
 import { fetchDelegateStats, fetchVoterStats } from "@/app/delegates/actions";
-import { VoterStats, DelegateResponse } from "@/lib/types";
+import { VoterStats,ParticipationStats, DelegateResponse } from "@/lib/types";
 import { getPublicClient } from "@/lib/viem";
 import Tenant from "@/lib/tenant/tenant";
 
 export const VOTER_STATS_QK = "voterStats";
+export const PARTICIPATION_STATS_QK = "participationStats";
 export const DELEGATE_STATS_QK = "participationStats";
 
 const CACHE_TIME = 180000; // 3 minute cache
@@ -40,6 +45,19 @@ export const useVoterStats = ({
         blockNumberOrTimestamp = Number(blockNumber);
       }
       return await fetchVoterStats(address!, blockNumberOrTimestamp);
+    },
+    staleTime: CACHE_TIME,
+  });
+};
+
+export const useParticipationStats = ({
+  address,
+}: Props): UseQueryResult<ParticipationStats, Error> => {
+  return useQuery({
+    enabled: !!address,
+    queryKey: [PARTICIPATION_STATS_QK, address],
+    queryFn: async () => {
+      return await fetchParticipationStats(address!);
     },
     staleTime: CACHE_TIME,
   });
