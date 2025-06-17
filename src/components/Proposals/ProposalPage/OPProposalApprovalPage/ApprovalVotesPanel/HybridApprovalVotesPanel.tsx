6bfbf579ce7983379df08e40303d59874934f58a
Sudheer <sudheer.105@gmail.com>
2025-06-17 21:48:28 +0530

    Fix styles and change users from 1000 - 1100

--- src/components/Proposals/ProposalPage/OPProposalApprovalPage/ApprovalVotesPanel/HybridApprovalVotesPanel.tsx
+++ src/components/Proposals/ProposalPage/OPProposalApprovalPage/ApprovalVotesPanel/HybridApprovalVotesPanel.tsx
@@ -27,57 +27,59 @@ export default function HybridApprovalVotesPanel({ proposal }: Props) {
   return (
     <>
       <div
-        className={`fixed flex-col justify-between gap-4 md:sticky top-[auto] md:top-20 md:max-h-[calc(100vh-220px)] max-h-[calc(100%-160px)] items-stretch flex-shrink w-[calc(100vw-2rem)] sm:w-[calc(100vw-4rem)] md:w-[20rem] lg:w-[24rem] bg-neutral border border-line rounded-xl mb-2 transition-all bottom-[20px] h-[calc(100%-160px)] md:h-auto overflow-hidden`}
+        className={`fixed flex-col justify-between gap-4 md:sticky top-[auto] md:top-20 md:max-h-[calc(100vh-220px)] max-h-[calc(100%-160px)] items-stretch flex-shrink w-[calc(100vw-2rem)] sm:w-[calc(100vw-4rem)] md:w-[20rem] lg:w-[24rem] bg-neutral transition-all bottom-[20px] h-[calc(100%-160px)] md:h-auto overflow-hidden`}
         style={{
           transition: "bottom 600ms cubic-bezier(0, 0.975, 0.015, 0.995)",
         }}
       >
-        <ProposalVotesTab setTab={setActiveTab} activeTab={activeTab} />
+        <div className="border border-line rounded-xl mb-2">
+          <ProposalVotesTab setTab={setActiveTab} activeTab={activeTab} />
 
-        {activeTab === "results" ? (
-          <>
-            <div className="p-4 border-b border-line">
-              <ProposalStatusDetail
-                proposalStatus={proposal.status}
-                proposalEndTime={proposal.endTime}
-                proposalStartTime={proposal.startTime}
-                proposalCancelledTime={proposal.cancelledTime}
-                proposalExecutedTime={proposal.executedTime}
-                cancelledTransactionHash={proposal.cancelledTransactionHash}
-                className="border-none m-0 p-0 bg-neutral"
-              />
-            </div>
-            <HybridOptionsResultsPanel proposal={proposal} />
-            <div className="absolute bottom-0 w-full right-0 bg-neutral border-t">
-              <div className="p-4">
-                <HybridApprovalCriteria
-                  proposalSettings={hybridApprovalData?.proposalSettings}
+          {activeTab === "results" ? (
+            <>
+              <div className="p-4 border-b border-line">
+                <ProposalStatusDetail
+                  proposalStatus={proposal.status}
+                  proposalEndTime={proposal.endTime}
+                  proposalStartTime={proposal.startTime}
+                  proposalCancelledTime={proposal.cancelledTime}
+                  proposalExecutedTime={proposal.executedTime}
+                  cancelledTransactionHash={proposal.cancelledTransactionHash}
+                  className="border-none m-0 p-0 bg-neutral"
                 />
               </div>
-              <div className="p-4 border-t pt-1">
-                <ApprovalCastVoteButton proposal={proposal} />
+              <HybridOptionsResultsPanel proposal={proposal} />
+              <div className="sticky bottom-0 w-full right-0 border-t">
+                <div className="p-4 bg-neutral">
+                  <HybridApprovalCriteria
+                    proposalSettings={hybridApprovalData?.proposalSettings}
+                  />
+                </div>
+                <div className="p-4 border-t pt-1">
+                  <ApprovalCastVoteButton proposal={proposal} />
+                </div>
               </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="px-3 py-[10px]">
-              <ProposalVotesFilter
-                initialSelection={showVoters ? "Voters" : "Hasn't voted"}
-                onSelectionChange={(value) => {
-                  setShowVoters(value === "Voters");
-                }}
-              />
-            </div>
-            {showVoters ? (
-              <ProposalVotesList proposalId={proposal.id} />
-            ) : (
-              <ProposalNonVoterList proposal={proposal} />
-            )}
-          </>
-        )}
+            </>
+          ) : (
+            <>
+              <div className="px-3 py-[10px]">
+                <ProposalVotesFilter
+                  initialSelection={showVoters ? "Voters" : "Hasn't voted"}
+                  onSelectionChange={(value) => {
+                    setShowVoters(value === "Voters");
+                  }}
+                />
+              </div>
+              {showVoters ? (
+                <ProposalVotesList proposalId={proposal.id} />
+              ) : (
+                <ProposalNonVoterList proposal={proposal} />
+              )}
+            </>
+          )}
+        </div>
+        <VoteOnAtlas />
       </div>
-      <VoteOnAtlas />
     </>
   );
 }
--- src/components/Proposals/ProposalPage/OPProposalApprovalPage/OptionResultsPanel/HybridOptionsResultsPanel.tsx
+++ src/components/Proposals/ProposalPage/OPProposalApprovalPage/OptionResultsPanel/HybridOptionsResultsPanel.tsx
@@ -135,7 +135,7 @@ export default function HybridOptionsResultsPanel({
   return (
     <div
       className={cn(
-        "flex flex-col max-h-[calc(100vh-482px)] overflow-y-auto flex-shrink p-4 min-h-[36px] mb-[192px]",
+        "flex flex-col max-h-[calc(100vh-544px)] overflow-y-auto flex-shrink p-4 min-h-[36px]",
         className
       )}
     >
