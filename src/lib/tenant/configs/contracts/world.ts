932255a4bb45fd5fe858a1245d03e8989ef1b5e9
Pedro Fernandes <pedrohasftb@gmail.com>
2025-06-26 13:03:59 -0300

    new world module

--- src/lib/tenant/configs/contracts/world.ts
+++ src/lib/tenant/configs/contracts/world.ts
@@ -32,7 +32,7 @@ export const worldTenantConfig = ({ alchemyId }: Props): TenantContracts => {
 
   const TIMELOCK = "0x10374c5d846179ba9ac03b468497b58e13c5f74e";
 
-  const VOTING_MODULE = "0xf3A1D7e9DF5fE6780462F413fD06e08bC5302148";
+  const VOTING_MODULE = "0xc71CDdE562ADA529c68a5d6a07f263487B982148";
 
   const provider = new JsonRpcProvider(
     `https://worldchain-mainnet.g.alchemy.com/v2/${alchemyId}`
