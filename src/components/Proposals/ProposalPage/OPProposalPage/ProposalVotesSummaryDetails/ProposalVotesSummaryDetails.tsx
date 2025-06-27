0b065f56934b294fb8ea9cfc165bc9387a47d878
Sudheer <sudheer.105@gmail.com>
2025-06-27 20:08:57 +0530

    Enhance hybrid standard proposal votes summary with detailed tooltip (#1095)

--- src/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesSummaryDetails/ProposalVotesSummaryDetails.tsx
+++ src/components/Proposals/ProposalPage/OPProposalPage/ProposalVotesSummaryDetails/ProposalVotesSummaryDetails.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/lib/proposalUtils";
 import { format } from "date-fns";
 import Link from "next/link";
+import { StepperRow } from "@/components/common/StepperRow";
 
 import Tenant from "@/lib/tenant/tenant";
 import { TENANT_NAMESPACES } from "@/lib/constants";
@@ -211,42 +212,3 @@ export default function ProposalVotesSummaryDetails({
     </div>
   );
 }
-
-const StepperRow = ({
-  label,
-  value,
-  isActive,
-  isCompleted,
-  isLastStep,
-  href,
-}: {
-  label: string;
-  value: string;
-  isActive?: boolean;
-  isCompleted?: boolean;
-  isLastStep?: boolean;
-  href?: string;
-}) => {
-  return (
-    <li
-      className={`relative flex-1  ${!isLastStep && "after:content-[''] after:w-[1.5px] after:h-[35px]  after:bg-line after:inline-block after:absolute after:top-3 after:left-0.5"} `}
-    >
-      <Link href={href ?? "#"} className="flex items-center gap-x-3">
-        <div
-          className={`w-1.5 h-1.5 rounded-full ${isCompleted ? "bg-black" : isActive ? "bg-blue-600" : "bg-primary/30"}`}
-        />
-
-        <div className="w-full flex items-center justify-between text-xs font-semibold">
-          <div
-            className={`${isCompleted ? "text-primary" : isActive ? "text-blue-600" : "text-secondary"} flex items-center gap-x-1`}
-          >
-            {label}
-            {href && <Image src={linkIcon} alt="redirect" />}
-          </div>
-
-          <p className="text-xs font-medium text-secondary">{value}</p>
-        </div>
-      </Link>
-    </li>
-  );
-};
