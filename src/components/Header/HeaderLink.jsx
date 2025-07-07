734b0c71fd63f26f26a263df6bb3166e5c0f497a
Yitong <zha.yitong@gmail.com>
2025-07-07 12:48:06 -0700

    cleaned up navbar

--- src/components/Header/HeaderLink.jsx
+++ src/components/Header/HeaderLink.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export const HeaderLink = ({ className, children, href, target, isActive }) => {
   const activeClass = isActive
-    ? "bg-primary text-inherit text-neutral"
+    ? "bg-white text-inherit text-primary border border-line"
     : "text-tertiary";
 
   return (
     <Link
       href={href}
       target={target}
-      className={`px-4 rounded-full text-sm sm:text-base h-[40px] content-center ${activeClass} ${className || ""}`}
+      className={`px-4 rounded-full text-sm sm:text-base h-[38px] content-center ${activeClass} ${className || ""}`}
     >
       {children}
     </Link>
