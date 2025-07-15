6e04127683cffd2097259a6ec4f9e4b43baf4beb
Atomauro <mauro.henaog@gmail.com>
2025-07-15 07:14:04 -0500

    Add Shape tenant font and font configuration

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,6 +2,7 @@ import { NextFont, NextFontWithVariable } from "next/dist/compiled/@next/font";
 import { Inter } from "next/font/google";
 import { Rajdhani } from "next/font/google";
 import { Chivo_Mono } from "next/font/google";
+import localFont from "next/font/local";
 
 export const inter = Inter({
   variable: "--font-inter",
@@ -19,8 +20,14 @@ export const chivoMono = Chivo_Mono({
   subsets: ["latin"],
 });
 
+export const fontShape = localFont({
+  src: "../../public/fonts/Shape/font1shape.ttf",
+  variable: "--font-shape",
+});
+
 export const fontMapper = {
   "font-inter": inter,
   "font-rajdhani": rajdhani,
   "font-chivoMono": chivoMono,
+  "font-shape": fontShape,
 } as Record<string, NextFontWithVariable>;
