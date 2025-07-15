9ed2b1a981b8573b53ecd0b01844cdebdf0f935d
Atomauro <mauro.henaog@gmail.com>
2025-07-15 07:13:51 -0500

    Add Shape tenant contract and UI configuration files

---
+++ src/lib/tenant/configs/contracts/shape.ts
@@ -0,0 +1,102 @@
+import {
+  AgoraGovernor_11__factory,
+  AgoraTimelock__factory,
+  ProposalTypesConfiguratorScopes__factory,
+  Membership__factory,
+} from "@/lib/contracts/generated";
+import { TenantContract } from "@/lib/tenant/tenantContract";
+import { TenantContracts } from "@/lib/types";
+import { mainnet, sepolia } from "viem/chains";
+import { IGovernorContract } from "@/lib/contracts/common/interfaces/IGovernorContract";
+import { AlchemyProvider, BaseContract, JsonRpcProvider } from "ethers";
+import { createTokenContract } from "@/lib/tokenUtils";
+import { ITimelockContract } from "@/lib/contracts/common/interfaces/ITimelockContract";
+import {
+  DELEGATION_MODEL,
+  GOVERNOR_TYPE,
+  TIMELOCK_TYPE,
+} from "@/lib/constants";
+
+interface Props {
+  isProd: boolean;
+  alchemyId: string;
+}
+
+export const shapeTenantConfig = ({
+  isProd,
+  alchemyId,
+}: Props): TenantContracts => {
+  const TOKEN = isProd
+    ? "0xD6e705292f293Df65c9FB9C28f67C3794fC66D5F"
+    : "0x27b0031c64f4231f0aff28e668553d73f48125f3";
+
+  const GOVERNOR = isProd
+    ? "0x4CEF361ACd29eA0E3b39Fd33fc6Dfa7c3BB83820"
+    : "0xa6388314fe37484883266970967ab918996f3bf0";
+
+  const TIMELOCK = isProd
+    ? "0xb09A941C4843f79423c8f2C8562aeD1691cbe674"
+    : "0x184ad2fD9959b8F5C247Ff1188114Dffd12069a0";
+
+  const TYPES = isProd
+    ? "0x1986516d07ABEddF8107F98b443F21ECFEE1d164"
+    : "0xb7687e62d6b2cafb3ed3c3c81b0b6cf0a3884602";
+
+  const TREASURY = [TIMELOCK];
+
+  const usingForkedNode = process.env.NEXT_PUBLIC_FORK_NODE_URL !== undefined;
+
+  const provider = usingForkedNode
+    ? new JsonRpcProvider(process.env.NEXT_PUBLIC_FORK_NODE_URL)
+    : isProd
+      ? new AlchemyProvider("mainnet", alchemyId)
+      : new AlchemyProvider("sepolia", alchemyId);
+
+  const chain = isProd ? mainnet : sepolia;
+
+  return {
+    token: createTokenContract({
+      abi: Membership__factory.abi,
+      address: TOKEN as `0x${string}`,
+      chain: chain,
+      contract: Membership__factory.connect(TOKEN, provider),
+      provider,
+      type: "erc721",
+    }),
+
+    governor: new TenantContract<IGovernorContract>({
+      abi: AgoraGovernor_11__factory.abi,
+      address: GOVERNOR,
+      chain,
+      contract: AgoraGovernor_11__factory.connect(GOVERNOR, provider),
+      provider,
+    }),
+
+    timelock: new TenantContract<ITimelockContract>({
+      abi: AgoraTimelock__factory.abi,
+      address: TIMELOCK,
+      chain,
+      contract: AgoraTimelock__factory.connect(TIMELOCK, provider),
+      provider,
+    }),
+
+    proposalTypesConfigurator: new TenantContract<BaseContract>({
+      abi: ProposalTypesConfiguratorScopes__factory.abi,
+      address: TYPES,
+      chain,
+      contract: ProposalTypesConfiguratorScopes__factory.connect(
+        TYPES,
+        provider
+      ),
+      provider,
+    }),
+
+    treasury: TREASURY,
+
+    delegationModel: DELEGATION_MODEL.FULL,
+    governorType: GOVERNOR_TYPE.AGORA,
+    timelockType:
+      TIMELOCK_TYPE.TIMELOCKCONTROLLER_WITH_ACCESS_CONTROL_ERC721_ERC115,
+    supportScopes: true,
+  };
+};
---
+++ src/lib/tenant/configs/ui/shape.ts
@@ -0,0 +1,198 @@
+import { TenantUI } from "@/lib/tenant/tenantUI";
+import shapeLogo from "@/assets/tenant/shape_logo.svg";
+import shapeHero from "@/assets/tenant/shape_hero.svg";
+import shapeSuccess from "@/assets/tenant/shape_success.svg";
+import shapePending from "@/assets/tenant/shape_pending.svg";
+import shapeDelegate from "@/assets/tenant/shape_delegate.svg";
+import { ProposalGatingType, ProposalType } from "@/app/proposals/draft/types";
+import { ProposalStage as PrismaProposalStage } from "@prisma/client";
+import TenantTokenFactory from "@/lib/tenant/tenantTokenFactory";
+import { TENANT_NAMESPACES } from "@/lib/constants";
+
+export const shapeTenantUIConfig = new TenantUI({
+  title: "Shape",
+  logo: shapeLogo,
+  tokens: [TenantTokenFactory.create(TENANT_NAMESPACES.SHAPE)],
+
+  assets: {
+    success: shapeSuccess,
+    pending: shapePending,
+    delegate: shapeDelegate,
+  },
+
+  organization: {
+    title: "Shape",
+  },
+
+  delegates: {
+    allowed: [],
+    advanced: [],
+    retired: [],
+  },
+
+  customization: {
+    primary: "23 23 23",
+    secondary: "64 64 64",
+    tertiary: "115 115 115",
+    neutral: "252 251 247",
+    wash: "255 255 255",
+    line: "223 223 223",
+    positive: "66 122 43",
+    negative: "182 13 13",
+    brandPrimary: "144 193 41",
+    brandSecondary: "242 242 242",
+    font: "font-shape",
+    letterSpacing: "0",
+    tokenAmountFont: "font-chivoMono",
+  },
+
+  links: [
+    {
+      name: "changelog",
+      title: "Change log",
+      url: "/changelog",
+    },
+  ],
+
+  pages: [
+    {
+      route: "/",
+      title: "Agora is the home of Shape voters",
+      description:
+        "Shape Delegates are the stewards of Shape governance, appointed by token holders to make governance decisions on their behalf.",
+      hero: shapeHero,
+      meta: {
+        title: "Shape Agora",
+        description: "Home of Shape governance",
+        imageTitle: "Shape Agora",
+        imageDescription: "Home of Shape governance",
+      },
+    },
+    {
+      route: "proposals",
+      title: "Agora is the home of Shape voters",
+      description:
+        "Shape Delegates are the stewards of Shape governance, appointed by token holders to make governance decisions on their behalf.",
+      hero: shapeHero,
+      meta: {
+        title: "Shape Agora",
+        description: "Home of Shape governance",
+        imageTitle: "Shape Agora",
+        imageDescription: "Home of Shape governance",
+      },
+    },
+    {
+      route: "delegates",
+      title: "Agora is the home of Shape voters",
+      description:
+        "Shape Delegates are the stewards of Shape governance, appointed by token holders to make governance decisions on their behalf.",
+      hero: shapeHero,
+      meta: {
+        title: "Shape Delegates",
+        description: "Delegate your voting power to a trusted representative",
+        imageTitle: "Shape Delegates",
+        imageDescription:
+          "Delegate your voting power to a trusted representative",
+      },
+    },
+    {
+      route: "info",
+      title: "Welcome to Shape",
+      description:
+        "A collective of companies, communities, and citizens working together.",
+      hero: shapeHero,
+      meta: {
+        title: "Shape Info",
+        description: "Welcome to Shape",
+        imageTitle: "Shape Info",
+        imageDescription: "Welcome to Shape",
+      },
+    },
+  ],
+
+  toggles: [
+    {
+      name: "admin",
+      enabled: true,
+    },
+    {
+      name: "proposals",
+      enabled: true,
+    },
+    {
+      name: "info",
+      enabled: true,
+    },
+    {
+      name: "delegates",
+      enabled: true,
+    },
+    {
+      name: "delegates/edit",
+      enabled: true,
+    },
+    {
+      name: "snapshotVotes",
+      enabled: false,
+    },
+    {
+      name: "proposal-execute",
+      enabled: true,
+    },
+    {
+      name: "proposal-lifecycle",
+      enabled: true,
+      config: {
+        stages: [
+          {
+            stage: PrismaProposalStage.DRAFTING,
+            order: 0,
+            isPreSubmission: true,
+          },
+          {
+            stage: PrismaProposalStage.AWAITING_SUBMISSION,
+            order: 1,
+            isPreSubmission: true,
+          },
+          {
+            stage: PrismaProposalStage.PENDING,
+            order: 2,
+            isPreSubmission: false,
+          },
+          {
+            stage: PrismaProposalStage.QUEUED,
+            order: 3,
+            isPreSubmission: false,
+          },
+          {
+            stage: PrismaProposalStage.EXECUTED,
+            order: 4,
+            isPreSubmission: false,
+          },
+        ],
+        proposalTypes: [
+          {
+            type: ProposalType?.BASIC,
+            prodAddress: null,
+            testnetAddress: null,
+          },
+          {
+            type: "optimistic",
+            prodAddress: "0x2e360A2bb39B47749D5F34cf0E1A285C76c088c3",
+            testnetAddress: "0x4414D030cFfEC5Edc011a27c653Ce21704b12d85",
+          },
+        ],
+        copy: {
+          helperText: `
+## Proposal checklist
+1. Select the correct proposal type that matches the correct transfer size.
+2. Add a title, description
+3. Add transactions.
+If you need help creating transactions / calldata, please see este [video](https://www.loom.com/share/33b000ef682c4129995c8fa4bc35db57).
+`.trim(),
+        },
+        gatingType: ProposalGatingType.MANAGER,
+      },
+    },
+  ],
+});
