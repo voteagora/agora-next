d5bbe0b0abc7f20c170480347f1ba99e83d15e73
Atomauro <mauro.henaog@gmail.com>
2025-08-08 14:26:20 -0500

    feat(shape-contracts): configure Shape Governor v2.0 contracts and viem; update token factory and block times

--- src/lib/tenant/configs/contracts/shape.ts
+++ src/lib/tenant/configs/contracts/shape.ts
@@ -2,11 +2,13 @@ import {
   AgoraGovernor_11__factory,
   AgoraTimelock__factory,
   ProposalTypesConfiguratorScopes__factory,
-  Membership__factory,
+  ERC20__factory,
 } from "@/lib/contracts/generated";
 import { TenantContract } from "@/lib/tenant/tenantContract";
 import { TenantContracts } from "@/lib/types";
 import { mainnet, sepolia } from "viem/chains";
+import { defineChain } from "viem";
+import type { Chain } from "viem";
 import { IGovernorContract } from "@/lib/contracts/common/interfaces/IGovernorContract";
 import { AlchemyProvider, BaseContract, JsonRpcProvider } from "ethers";
 import { createTokenContract } from "@/lib/tokenUtils";
@@ -22,25 +24,69 @@ interface Props {
   alchemyId: string;
 }
 
+export const shapeSepolia: Chain = defineChain({
+  id: 11011,
+  name: "Shape Sepolia",
+  network: "shape-sepolia",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://shape-sepolia.g.alchemy.com/v2/"],
+    },
+    public: {
+      http: ["https://sepolia.shape.network"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Shape Sepolia Explorer",
+      url: "https://sepolia.shapescan.xyz",
+    },
+  },
+  testnet: true,
+});
+
+export const shapeMainnet: Chain = defineChain({
+  id: 360,
+  name: "Shape",
+  network: "shape",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://shape-mainnet.g.alchemy.com/v2/"],
+    },
+    public: {
+      http: ["https://mainnet.shape.network"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Shape Explorer",
+      url: "https://shapescan.xyz",
+    },
+  },
+  testnet: false,
+});
+
 export const shapeTenantConfig = ({
   isProd,
   alchemyId,
 }: Props): TenantContracts => {
-  const TOKEN = isProd
-    ? "0xD6e705292f293Df65c9FB9C28f67C3794fC66D5F"
-    : "0x27b0031c64f4231f0aff28e668553d73f48125f3";
+  const TOKEN = "0x10374c5D846179BA9aC03b468497B58E13C5f74e";
+  const GOVERNOR = "0x8E7B12df08278Ebe26fadc13913B57Fa2f3c4ba2";
+  const TIMELOCK = "0x28c8be698a115bc062333cd9b281abad971b0785";
+  const TYPES = "0x98607c6d56bd3ea5a1b516ce77e07ca54e5f3fff";
 
-  const GOVERNOR = isProd
-    ? "0x4CEF361ACd29eA0E3b39Fd33fc6Dfa7c3BB83820"
-    : "0xa6388314fe37484883266970967ab918996f3bf0";
-
-  const TIMELOCK = isProd
-    ? "0xb09A941C4843f79423c8f2C8562aeD1691cbe674"
-    : "0x184ad2fD9959b8F5C247Ff1188114Dffd12069a0";
-
-  const TYPES = isProd
-    ? "0x1986516d07ABEddF8107F98b443F21ECFEE1d164"
-    : "0xb7687e62d6b2cafb3ed3c3c81b0b6cf0a3884602";
+  const APPROVAL_MODULE = "0xba17b665d463771bf4b10138e7d651883f582148";
+  const OPTIMISTIC_MODULE = "0xaa5f39acb9ac2517e7e9753d20b185cd674a2048";
 
   const TREASURY = [TIMELOCK];
 
@@ -49,19 +95,23 @@ export const shapeTenantConfig = ({
   const provider = usingForkedNode
     ? new JsonRpcProvider(process.env.NEXT_PUBLIC_FORK_NODE_URL)
     : isProd
-      ? new AlchemyProvider("mainnet", alchemyId)
-      : new AlchemyProvider("sepolia", alchemyId);
+      ? new JsonRpcProvider(
+          `https://shape-mainnet.g.alchemy.com/v2/${alchemyId}`
+        )
+      : new JsonRpcProvider(
+          `https://shape-sepolia.g.alchemy.com/v2/${alchemyId}`
+        );
 
-  const chain = isProd ? mainnet : sepolia;
+  const chain = isProd ? shapeMainnet : shapeSepolia;
 
   return {
     token: createTokenContract({
-      abi: Membership__factory.abi,
+      abi: ERC20__factory.abi,
       address: TOKEN as `0x${string}`,
       chain: chain,
-      contract: Membership__factory.connect(TOKEN, provider),
+      contract: ERC20__factory.connect(TOKEN, provider),
       provider,
-      type: "erc721",
+      type: "erc20",
     }),
 
     governor: new TenantContract<IGovernorContract>({
@@ -94,7 +144,7 @@ export const shapeTenantConfig = ({
     treasury: TREASURY,
 
     delegationModel: DELEGATION_MODEL.FULL,
-    governorType: GOVERNOR_TYPE.AGORA,
+    governorType: GOVERNOR_TYPE.AGORA_20,
     timelockType:
       TIMELOCK_TYPE.TIMELOCKCONTROLLER_WITH_ACCESS_CONTROL_ERC721_ERC115,
     supportScopes: true,
