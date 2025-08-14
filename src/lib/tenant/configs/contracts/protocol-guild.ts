0211284a5c8ac831593589165e1ffa09833206de
jefag <jeff@voteagora.com>
2025-08-13 22:20:01 -0400

    Update protocol-guild.ts
    Signed-off-by: jefag <jeff@voteagora.com>

--- src/lib/tenant/configs/contracts/protocol-guild.ts
+++ src/lib/tenant/configs/contracts/protocol-guild.ts
@@ -28,19 +28,19 @@ export const protocolGuildTenantContractConfig = ({
 }: Props): TenantContracts => {
   const TOKEN = isProd
     ? "0xD6e705292f293Df65c9FB9C28f67C3794fC66D5F"
-    : "0x27b0031c64f4231f0aff28e668553d73f48125f3";
+    : "0xfffa91ea8800532f9d8f987829bdfcc11f385fa6";
 
   const GOVERNOR = isProd
     ? "0x4CEF361ACd29eA0E3b39Fd33fc6Dfa7c3BB83820"
-    : "0xa6388314fe37484883266970967ab918996f3bf0";
+    : "0x8C177a1735520A571F76e29D4259a8c42Bf4254f";
 
   const TIMELOCK = isProd
     ? "0xb09A941C4843f79423c8f2C8562aeD1691cbe674"
-    : "0x184ad2fD9959b8F5C247Ff1188114Dffd12069a0";
+    : "0x5c5a1a4671f1feba6b37911fc40bd6974b653fd5";
 
   const TYPES = isProd
     ? "0x1986516d07ABEddF8107F98b443F21ECFEE1d164"
-    : "0xb7687e62d6b2cafb3ed3c3c81b0b6cf0a3884602";
+    : "0x1f31d2f02875fdc69d092bce819a15a23bc1a3d1";
 
   const TREASURY = [TIMELOCK];
 
