926d8bd9c23abd6209b6e063d8f2fdec982eafef
Aarna Arora <aarna@voteagora.com>
2025-08-18 11:25:47 -0700

    static duna addition to info page

--- src/app/duna/components/index.ts
+++ src/app/duna/components/index.ts
@@ -1,5 +1,6 @@
 export { default as TownsDunaAdministration } from "./TownsDunaAdministration";
 export { default as DunaAdministration } from "./DunaAdministration";
+export { default as TownsDunaAdministration } from "./TownsDunaAdministration";
 export { default as QuarterlyReportsSection } from "./QuarterlyReportsSection";
 export { default as QuarterlyReportCard } from "./QuarterlyReportCard";
 export { default as DocumentsSection } from "./DocumentsSection";
