c36b5780297f7b41001bf395d13fa166d1201d62
jefag <jeff@voteagora.com>
2025-09-06 11:35:04 -0400

    Fix quote decoding in correct spot (#1178)

--- src/app/lib/utils/text.ts
+++ src/app/lib/utils/text.ts
@@ -1,10 +1,7 @@
 export const cleanString = (text: string) => {
   return text
     .replace(/#{1,6}\s/g, "") // Removes Markdown headings
-    .replace(/\n/g, " ") // Replaces newlines with space
-    .replace(/&quot;/g, "") // Decode HTML entity for double quotes
-    .replace(/&#39;/g, "") // Decode HTML entity for single quotes
-    .replace(/&amp;/g, "and"); // Decode HTML entity for ampersand
+    .replace(/\n/g, " "); // Replaces newlines with space
 };
 
 export const truncateString = (str: string, maxLength: number) => {
