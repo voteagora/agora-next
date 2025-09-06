4ad2d471b0ac825ceb0e3afc7ea3402829b5d21a
Jeff <jeff@voteagora.com>
2025-09-06 01:07:07 -0400

    skip quotes

--- src/app/lib/utils/text.ts
+++ src/app/lib/utils/text.ts
@@ -2,11 +2,9 @@ export const cleanString = (text: string) => {
   return text
     .replace(/#{1,6}\s/g, "") // Removes Markdown headings
     .replace(/\n/g, " ") // Replaces newlines with space
-    .replace(/&quot;/g, '"') // Decode HTML entity for double quotes
-    .replace(/&#39;/g, "'") // Decode HTML entity for single quotes
-    .replace(/&amp;/g, "&") // Decode HTML entity for ampersand
-    .replace(/&lt;/g, "<") // Decode HTML entity for less than
-    .replace(/&gt;/g, ">"); // Decode HTML entity for greater than
+    .replace(/&quot;/g, '') // Decode HTML entity for double quotes
+    .replace(/&#39;/g, "") // Decode HTML entity for single quotes
+    .replace(/&amp;/g, "and") // Decode HTML entity for ampersand
 };
 
 export const truncateString = (str: string, maxLength: number) => {
