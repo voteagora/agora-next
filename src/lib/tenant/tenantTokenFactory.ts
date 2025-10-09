853ab9c25967bc8ff757c81973989f982a4ce88f
Aarna Arora <aarna@voteagora.com>
2025-10-09 16:09:45 -0400

    0g setup

--- src/lib/tenant/tenantTokenFactory.ts
+++ src/lib/tenant/tenantTokenFactory.ts
@@ -136,6 +136,13 @@ export default class TenantTokenFactory {
           decimals: 18,
           address: "0x0000000000000000000000000000000000000000",
         };
+      case TENANT_NAMESPACES.OG:
+        return {
+          name: "0g Protocol",
+          symbol: "0G",
+          decimals: 18,
+          address: "0x0000000000000000000000000000000000000000",
+        };
       default:
         throw new Error(`Invalid namespace: ${namespace}`);
     }
