6b4ee781dfb5542cb359e8b895a1678335314cf5
Aarna Arora <aarna@voteagora.com>
2025-10-09 16:18:13 -0400

    fix: tests failing

--- src/__mocks__/fonts.ts
+++ src/__mocks__/fonts.ts
@@ -17,6 +17,12 @@ export const Chivo_Mono = () => ({
   variable: "--mock-chivo-mono",
 });
 
+export const FamiljenGrotesk = () => ({
+  style: { fontFamily: "FamiljenGrotesk" },
+  className: "mock-familjen-grotesk",
+  variable: "--mock-familjen-grotesk",
+});
+
 // Mock for next/font/local (default export)
 export default () => ({
   style: { fontFamily: "MockFont" },
