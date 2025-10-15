6d4ea9916cd5e571d5aa57a2345c393127e4b384
Jeff <jeff@voteagora.com>
2025-10-15 00:57:14 -0400

    Feedback 8 - remove goverance data

--- src/lib/tenant/configs/ui/uniswap.tsx
+++ src/lib/tenant/configs/ui/uniswap.tsx
@@ -245,7 +245,7 @@ export const uniswapTenantUIConfig = new TenantUI({
     },
     {
       name: "info/governance-charts",
-      enabled: true,
+      enabled: false,
     },
     {
       name: "sponsoredVote",
