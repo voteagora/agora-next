7f8d8eab74c5e1666a457cb22ea29f0bba4ecea8
corydickson <7246942+corydickson@users.noreply.github.com>
2025-12-01 11:00:55 -0500

    Update block time to 2s for Scroll mainnet (#1314)
    Co-authored-by: Sudheer <sudheer.105@gmail.com>

--- src/lib/blockTimes.ts
+++ src/lib/blockTimes.ts
@@ -1,6 +1,7 @@
 // Cast for more accurate arithmetic
 import Tenant from "@/lib/tenant/tenant";
 import { Block } from "ethers";
+import { scroll } from "viem/chains";
 
 const { contracts, ui } = Tenant.current();
 
@@ -32,7 +33,7 @@ export function getSecondsPerBlock(chainId: number | undefined): number {
       return 2;
 
     case 534352: // Scroll
-      return 1.5;
+      return 2;
 
     case 42161: // Arbitrum one
     case 421614: // Arbitrum sepolia
@@ -70,6 +71,7 @@ export function getHumanBlockTime(
       bedrockBlockNumber - Number(blockNumber),
       0
     );
+
     const blocksAfterBedrock = Math.min(
       Number(latestBlock.number) - bedrockBlockNumber,
       Number(latestBlock.number) - Number(blockNumber)
@@ -81,6 +83,30 @@ export function getHumanBlockTime(
 
     return new Date(
       (latestBlock.timestamp - timeBeforeBedrock - timeAfterBedrock) * 1000
+    );
+  }
+
+  // Special case for Scroll mainnet block time change
+  if (chainIdToUse === scroll.id) {
+    const blockSeconds = getSecondsPerBlock(chainIdToUse);
+    const secondsPerBlockBeforeUpdate = 1.5;
+    const blockTimeUpdateBlockNumber = 25688713;
+
+    const blocksBeforeUpdate = Math.max(
+      blockTimeUpdateBlockNumber - Number(blockNumber),
+      0
+    );
+
+    const blocksAfterUpdate = Math.min(
+      Number(latestBlock.number) - blockTimeUpdateBlockNumber,
+      Number(latestBlock.number) - Number(blockNumber)
+    );
+
+    const timeBeforeUpdate = blocksBeforeUpdate * secondsPerBlockBeforeUpdate;
+    const timeAfterUpdate = blocksAfterUpdate * blockSeconds;
+
+    return new Date(
+      (latestBlock.timestamp - timeBeforeUpdate - timeAfterUpdate) * 1000
     );
   }
 
