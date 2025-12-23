4538d99c193835b1d925ab69b45fbb12fe1397d6
Pedro Fernandes <pedrohasftb@gmail.com>
2025-12-23 18:17:46 -0300

    a few more colors

--- src/components/Delegates/Delegations/EncourageConnectWalletDialog.tsx
+++ src/components/Delegates/Delegations/EncourageConnectWalletDialog.tsx
@@ -17,10 +17,10 @@ export const EncourageConnectWalletDialog = ({
   return (
     <div className="flex flex-col gap-7 justify-center ">
       <div>
-        <div className="text-neutral-900 text-2xl font-bold leading-loose">
+        <div className="text-primary text-2xl font-bold leading-loose">
           Governance starts with you!
         </div>
-        <div className="justify-start text-neutral-700 text-base font-medium leading-normal">
+        <div className="justify-start text-primary text-base font-medium leading-normal">
           Your tokens matter— connect your wallet to delegate your voting power
           and shape the future of the collective.
         </div>
