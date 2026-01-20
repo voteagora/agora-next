4c03a9cc5bbc26b0d034fde5192475bc5b6dd785
Pedro Fernandes <pedrohasftb@gmail.com>
2026-01-20 11:55:00 -0300

    Include proposalConfigType in parseProposalToForm

--- src/app/proposals/draft/components/stages/DraftForm/DraftFormClient.tsx
+++ src/app/proposals/draft/components/stages/DraftForm/DraftFormClient.tsx
@@ -186,7 +186,8 @@ const DraftFormClient = ({
   const methods = useForm<z.output<typeof DraftProposalSchema>>({
     resolver: zodResolver(DraftProposalSchema),
     mode: "onBlur",
-    defaultValues: parseProposalToForm(draftProposal) || DEFAULT_FORM,
+    defaultValues: (parseProposalToForm(draftProposal) ||
+      DEFAULT_FORM) as z.output<typeof DraftProposalSchema>,
   });
 
   const { watch, handleSubmit, control } = methods;
@@ -207,14 +208,7 @@ const DraftFormClient = ({
     );
 
     setValidProposalTypes(newValidProposalTypes);
-
-    if (newValidProposalTypes.length > 0) {
-      methods.setValue(
-        "proposalConfigType",
-        newValidProposalTypes[0].proposal_type_id
-      );
-    }
-  }, [votingModuleType, proposalTypes, methods]);
+  }, [votingModuleType, proposalTypes]);
 
   // Ensure proposalConfigType is set when returning from other stages
   useEffect(() => {
