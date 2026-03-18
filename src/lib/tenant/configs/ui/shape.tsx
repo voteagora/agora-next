eafe9da473599711133512cc4bd32845f58b24fb
Sudheer <sudheer.105@gmail.com>
2026-03-18 21:13:25 +0530

    Update voting text

--- src/lib/tenant/configs/ui/shape.tsx
+++ src/lib/tenant/configs/ui/shape.tsx
@@ -68,7 +68,7 @@ export const shapeTenantUIConfig = new TenantUI({
     {
       name: "twitter",
       title: "Twitter",
-      url: "https://x.com/Shape",
+      url: "https://x.com/structura",
     },
     {
       name: "shape-website",
@@ -309,33 +309,30 @@ export const shapeTenantUIConfig = new TenantUI({
                     <li>
                       first be submitted as a <strong>Temp-Check</strong>, which
                       is a five-day period during which <strong>Members</strong>{" "}
-                      can utilize their SYND token to indicate support for a
+                      can utilize their SHAPE token to indicate support for a
                       proposal. In order for a proposal to transition from a{" "}
                       <strong>Temp-Check</strong> to a vote of the membership,
                       the <strong>Temp-Check</strong> must attain the support of
-                      5% of the SYND tokens minted (excludes future emissions),
-                      except as limited by Article 13 of the Association
-                      Agreement.
+                      5% of the SHAPE tokens in circulation, except as limited
+                      by Article 13.
                     </li>
                     <li>
                       upon a successful <strong>Temp-Check</strong>, the{" "}
                       <strong>Governance Proposal</strong> period is open for
                       seven days, during which time
-                      <strong> Members</strong> can utilize their SYND token to
+                      <strong> Members</strong> can utilize their SHAPE token to
                       affirm, deny, or participate without voting on the
                       proposal. A proposal:
                       <ul className="list-[square] list-outside space-y-1 ml-6 mt-2 text-sm leading-relaxed">
                         <li>
-                          <strong>passes</strong> if the majority of votes
-                          affirm the proposal and 10% of the minted SYND tokens
-                          (excludes future emissions) participate in the vote;
-                          and
+                          <strong>Passes</strong> if the majority of votes
+                          affirm the proposal and 10% of the SHAPE tokens in
+                          circulation participate in the vote; and
                         </li>
                         <li>
-                          <strong>fails</strong> if the majority of votes deny
-                          the proposal or 10% of the SYND tokens minted
-                          (excludes future emissions) did not participate in the
-                          vote.
+                          <strong>Fails</strong> if the majority of votes deny
+                          the proposal or 10% of the SHAPE tokens in circulation
+                          did not participate in the vote.
                         </li>
                       </ul>
                     </li>
