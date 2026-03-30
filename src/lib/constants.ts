3e99b5cd2ce4e673fdf89a44ea385470680b3315
Pedro Fernandes <pedrohasftb@gmail.com>
2026-03-30 13:18:23 -0300

    Test rig init

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -264,10 +264,14 @@ export type EASApprovalCriteria =
 // Default tiers for optimistic voting (percentages)
 export const EAS_DEFAULT_OPTIMISTIC_TIERS = [20, 20, 20];
 
-export const ARCHIVE_GCS_BUCKET =
+// Archive GCS bucket - can be overridden via env var for E2E testing
+const DEFAULT_ARCHIVE_GCS_BUCKET =
   process.env.NEXT_PUBLIC_AGORA_ENV === "prod"
     ? "https://storage.googleapis.com/cpls-usmr-prd-25q4"
     : "https://storage.googleapis.com/cpls-usmr-dev-test-26q1";
+
+export const ARCHIVE_GCS_BUCKET =
+  process.env.NEXT_PUBLIC_ARCHIVE_GCS_BUCKET || DEFAULT_ARCHIVE_GCS_BUCKET;
 
 export const getArchiveSlugGCSbucket = (namespace: string) => {
   return `${ARCHIVE_GCS_BUCKET}/data/${namespace}`;
